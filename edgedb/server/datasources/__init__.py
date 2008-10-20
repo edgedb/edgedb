6f78a11423847dd2458e60cd00d265ca2518c2d7
Yury Selivanov <yselivanov@sprymix.com>
2008-10-20 13:30:15 -0400

    config: Refactor configuration to use the new import machinery

---
+++ edgedb/server/datasources/__init__.py
@@ -0,0 +1,6 @@
+from semantix.lib.caos.datasources.concept_link_types import ConceptLinkTypes
+from semantix.lib.caos.datasources.concept_links import ConceptLinks
+from semantix.lib.caos.datasources.entity import Entity
+from semantix.lib.caos.datasources.entity_attributes import EntityAttributes
+
+__all__ = ['ConceptLinkTypes', 'ConceptLinks', 'Entity', 'EntityAttributes']
