1863721c5b0549af9358450baf67f4e4fba091ea
Elvis Pranskevichus <elprans@sprymix.com>
2009-02-24 22:22:37 -0500

    utils.datastructures: Implement GenericWrapper helper
    Implement GenericWrapper helper class that eases subclassing types by
    providing a mechanism to set hooks on a set of given methods without
    explicitly overloading them.
    
    Define such a wrapper for set.

--- edgedb/lang/common/datastructures.py
+++ edgedb/lang/common/datastructures.py
@@ -1,5 +1,50 @@
 import bisect
 
+class GenericWrapperMeta(type):
+
+    def __init__(cls, name, bases, dict):
+        super().__init__(name, bases, dict)
+        cls._sethook('read')
+        cls._sethook('write')
+
+
+class GenericWrapper(object, metaclass=GenericWrapperMeta):
+    write_methods = ()
+    read_methods = ()
+
+    @classmethod
+    def _sethook(cls, hookname):
+        hookmethod = '_' + hookname + '_hook'
+        if hasattr(cls, hookmethod):
+            hook = getattr(cls, hookmethod)
+            methods = getattr(cls, hookname + '_methods')
+
+            if hook:
+                for method in methods:
+                    setattr(cls, method, cls._gethook(hook, method))
+
+    @classmethod
+    def _gethook(cls, hook, method):
+        def hookbody(self, *args, **kwargs):
+            hook(self, method, *args, **kwargs)
+            original = getattr(cls.original_base, method)
+            return original(self, *args, **kwargs)
+
+        return hookbody
+
+
+class SetWrapper(set, GenericWrapper):
+
+    write_methods = ('update', 'intersection_update', 'difference_update', 'symmetric_difference_update',
+                     'add', 'remove', 'discard', 'pop', 'clear')
+
+    read_methods = ('__iter__', '__len__', '__contains__', 'isdisjoint', 'issubset', '__le__', '__lt__',
+                    'issuperset', '__ge__', '__gt__', 'union', '__or__', 'intersection', '__and__',
+                    'difference', '__sub__', 'symmetric_difference', '__xor__')
+
+    original_base = set
+
+
 class SortedList(list):
     """
     A list that maintains it's order by a given criteria
