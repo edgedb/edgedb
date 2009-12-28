d7dc50746b3073f40da71900baf2df8d89b6af17
Elvis Pranskevichus <elprans@sprymix.com>
2009-12-28 02:18:40 -0500

    utils.functional.memoized: Fix identation

--- edgedb/lang/common/functional.py
+++ edgedb/lang/common/functional.py
@@ -32,20 +32,20 @@ class memoized(object):
     not re-evaluated.
     """
     def __init__(self, func):
-       self.func = func
-       self.cache = {}
+        self.func = func
+        self.cache = {}
 
     def __call__(self, *args):
-       try:
-          return self.cache[args]
-       except KeyError:
-          self.cache[args] = value = self.func(*args)
-          return value
-       except TypeError:
-          # uncachable -- for instance, passing a list as an argument.
-          # Better to not cache than to blow up entirely.
-          return self.func(*args)
+        try:
+            return self.cache[args]
+        except KeyError:
+            self.cache[args] = value = self.func(*args)
+            return value
+        except TypeError:
+            # uncachable -- for instance, passing a list as an argument.
+            # Better to not cache than to blow up entirely.
+            return self.func(*args)
 
     def __repr__(self):
-       """Return the function's docstring."""
-       return self.func.__doc__
+        """Return the function's docstring."""
+        return self.func.__doc__
