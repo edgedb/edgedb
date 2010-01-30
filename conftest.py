50c101deb40d389ebb965220b774f9de8f18c7fe
Yury Selivanov <yselivanov@sprymix.com>
2010-01-29 20:04:29 -0500

    tests: add optional "--colorize" argument
     * Turns python code highlight on in error reports

--- conftest.py
+++ conftest.py
@@ -1,17 +1,45 @@
 import py
 import re
+from semantix.utils.debug import highlight
 
 
 test_patterns = []
+
+class PyTestColorizerPatcher:
+    target = None
+    old_method = None
+
+    @classmethod
+    def patch(cls):
+        cls.target = __import__('py._code.code', None, None, ['FormattedExcinfo']).FormattedExcinfo
+        cls.old_method = cls.target.get_source
+
+        def get_source(self, *args, **kwargs):
+            lst = cls.old_method(self, *args, **kwargs)
+            return highlight('\n'.join(lst), 'python').split('\n')
+
+        cls.target.get_source = get_source
+
+    @classmethod
+    def unpatch(cls):
+        if cls.old_method and cls.target:
+            cls.target.get_source = cls.old_method
+            cls.old_method = None
 
 
 def pytest_addoption(parser):
     parser.addoption("--semantix-debug", dest="semantix_debug", action="append")
     parser.addoption("--tests", dest="test_patterns", action="append")
 
+    group = parser.getgroup("terminal reporting")
+    group._addoption('--colorize', default=False, action='store_true', dest='colorize')
+
 
 def pytest_configure(config):
     global test_patterns, semantix_debug
+
+    if config.option.colorize:
+        PyTestColorizerPatcher.patch()
 
     patterns = []
     tp = config.getvalue('test_patterns')
@@ -31,6 +59,11 @@ def pytest_configure(config):
         import semantix.utils.debug
         semantix.utils.debug.enabled = True
         semantix.utils.debug.channels.update(debug)
+
+
+def pytest_unconfigure(config):
+    if config.option.colorize:
+        PyTestColorizerPatcher.unpatch()
 
 
 def pytest_pycollect_makeitem(__multicall__, collector, name, obj):
