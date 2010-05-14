6d270ecb16e44d47a74e6411065a572249a7ee27
Yury Selivanov <yselivanov@sprymix.com>
2010-05-14 18:19:22 -0400

    utils.lang.generic: Add 'generic' language.
    Generic Language is a simplified and generalized version of Python
    Language.  For instance, 'a, b = 1, 2' is one AST node in Python, and
    two separate Assignment nodes in Generic.  Hence, it is much easier to
    write code analyzers and reflectors using Generic language
    infrastructure.  It can easily be converted to python back, which is
    actually done in unittests to ensure translation correctness.
    
    Right now, a very limited set of python language features are
    implemented: functions (arguments and return statement), basic
    operations (binary, boolean, unary), and literals.

---
+++ edgedb/lang/common/lang/generic/tests/base.py
@@ -0,0 +1,76 @@
+##
+# Copyright (c) 2008-2010 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+import types
+import ast
+import inspect
+import functools
+
+from semantix.utils.lang.python.convert import AstToPyAstConverter, PyAstToGeneric
+from semantix.utils.lang.generic.codegen.python import GenericPythonSourceGenerator
+from semantix.utils.debug import _indent_code, debug, highlight
+from semantix.utils.ast.dump import pretty_dump
+
+
+class GenericLangTestSuiteMeta(type):
+    def __new__(mcls, name, bases, dct):
+        for method_name, method in dct.items():
+            if method_name.startswith('test_') and isinstance(method, types.FunctionType):
+
+                @debug
+                def wrapper(method, *args, **kwargs):
+                    source = _indent_code(inspect.getsource(method), absolute=0)
+                    tree = AstToPyAstConverter.convert(
+                               ast.parse(source, method.__code__.co_filename)
+                           )
+
+                    """LOG [lang.generic] Python Source
+                    print(highlight(source, 'python'))
+                    """
+
+                    """LOG [lang.generic] Python Ast Tree
+                    print(pretty_dump(tree, colorize=True, width=120))
+                    """
+
+                    ctree = PyAstToGeneric().convert(tree)
+
+                    """LOG [lang.generic] Generic Ast Tree
+                    print(pretty_dump(ctree, colorize=True, width=120))
+                    """
+
+                    csource = GenericPythonSourceGenerator.to_source(ctree)
+
+                    """LOG [lang.generic] Generic Source to Python
+                    print(highlight(csource, 'python'))
+                    """
+
+                    result = method(*args, **kwargs)
+
+                    code = compile(ast.parse(csource), '<string>', 'exec')
+
+                    _locals = {}
+                    exec(code, _locals, _locals)
+                    new_result = _locals[method.__name__](None)
+
+                    assert result == new_result
+
+                def makewrapper(method):
+                    def wrap(*args, **kwargs):
+                        return wrapper(method, *args, **kwargs)
+                    wrap.__name__ = method.__name__
+                    wrap.__doc__ = method.__doc__
+                    return wrap
+
+                dct[method_name] = makewrapper(method)
+
+        cls = super().__new__(mcls, name, bases, dct)
+        return cls
+
+
+class GenericLangTestSuite(metaclass=GenericLangTestSuiteMeta):
+    pass
