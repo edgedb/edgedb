df6e884e19f42ddea43a4f40c75fb05edd4f4594
Yury Selivanov <yselivanov@sprymix.com>
2010-05-25 18:04:30 -0400

    utils.datastructures: add immutables sub-package with 'frozendict' datatype
    Frozendict is an immutable version of regular python dict, just like
    frozenset to set.

---
+++ edgedb/lang/common/datastructures/immutables.py
@@ -0,0 +1,39 @@
+##
+# Copyright (c) 2008-2010 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+import abc
+import collections
+
+
+class ImmutableMeta(type):
+    def __new__(mcls, name, bases, dct):
+        if '_shadowed_methods_' in dct:
+            shadowed = dct['_shadowed_methods_']
+            del dct['_shadowed_methods_']
+
+            for method in shadowed:
+                def meth(self, *args, _allow_mutation_=False, **kwargs):
+                    if not _allow_mutation_:
+                        raise TypeError('%r is immutable' % self.__class__.__name__)
+                    return super()[method](*args, **kwargs)
+                meth.__name__ = method
+
+                dct[method] = meth
+
+        return super().__new__(mcls, name, bases, dct)
+
+
+class frozendict(dict, metaclass=ImmutableMeta):
+    _shadowed_methods_ = ('__setitem__', '__delitem__', 'update', 'clear',
+                          'pop', 'popitem', 'setdefault')
+
+    def __reduce_ex__(self, protocol):
+        return type(self), (dict(self),)
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, super().__repr__())
---
+++ edgedb/lang/common/datastructures/tests/test_immutables.py
@@ -0,0 +1,40 @@
+##
+# Copyright (c) 2010 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from semantix.utils.datastructures import immutables
+from semantix.utils.debug import assert_raises
+
+
+class TestDatastructImmutables:
+    def test_utils_ds_immutables_frozendict(self):
+        import pickle
+
+        mutable = {'a': 'b'}
+        immutable = immutables.frozendict(mutable)
+
+        assert immutable['a'] == mutable['a']
+
+        with assert_raises(TypeError):
+            immutable['a'] = 2
+
+        with assert_raises(TypeError):
+            del immutable['a']
+
+        with assert_raises(TypeError):
+            immutable.pop('a')
+
+        with assert_raises(TypeError):
+            immutable.setdefault('b', 'c')
+
+        assert immutable['a'] == mutable['a']
+
+        pickled = pickle.loads(pickle.dumps(immutable))
+        assert isinstance(pickled, immutables.frozendict)
+        assert pickled == immutable
+
+        assert len(immutables.frozendict()) == 0
