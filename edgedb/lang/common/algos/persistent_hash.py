0d3a0b364a7fdf028759ce45e697c2ef5792ab61
Elvis Pranskevichus <elprans@sprymix.com>
2010-09-02 16:37:21 -0400

    utils.algos: Turn persistent_hash into a class
    persistent_hash is now a subclass of int and all type-specific hashes
    are classmethods.  This turns persistent_hash into a useful
    infrastructure for custom hash creation.

--- edgedb/lang/common/algos/persistent_hash.py
+++ edgedb/lang/common/algos/persistent_hash.py
@@ -13,87 +13,96 @@ import decimal
 from hashlib import md5
 
 
-def persistent_hash(value):
-    """Compute a persistent hash for a given value.
+class persistent_hash(int):
+    magic_method = 'persistent_hash'
 
-    The hash is guaranteed to be universally stable.
-    """
+    def __new__(cls, value):
+        """Compute a persistent hash for a given value.
 
-    if value is None:
-        return str_hash('__semantix__NONE__')
-    elif isinstance(value, str):
-        return str_hash(value)
-    elif isinstance(value, bool):
-        return str_hash('__semantix__TRUE__' if value else '__semantix__FALSE__')
-    elif isinstance(value, int):
-        return int_hash(value)
-    elif isinstance(value, float):
-        return float_hash(value)
-    elif isinstance(value, decimal.Decimal):
-        return decimal_hash(value)
-    elif isinstance(value, tuple):
-        return tuple_hash(value)
-    elif isinstance(value, frozenset):
-        return frozenset_hash(value)
-    else:
-        phm = getattr(value, 'persistent_hash', None)
-        if phm:
-            return phm()
-    raise TypeError("un(persistently-)hashable type: '%s'" % type(value).__name__)
+        The hash is guaranteed to be universally stable.
+        """
 
+        hash = None
 
-def str_hash(value):
-    """Compute a persistent hash for a string value"""
-    return int(md5(value.encode()).hexdigest(), 16)
+        if value is None:
+            hash = cls.str_hash('__semantix__NONE__')
+        elif isinstance(value, str):
+            hash = cls.str_hash(value)
+        elif isinstance(value, bool):
+            hash = cls.str_hash('__semantix__TRUE__' if value else '__semantix__FALSE__')
+        elif isinstance(value, int):
+            hash = cls.int_hash(value)
+        elif isinstance(value, float):
+            hash = cls.float_hash(value)
+        elif isinstance(value, decimal.Decimal):
+            hash = cls.decimal_hash(value)
+        elif isinstance(value, tuple):
+            hash = cls.tuple_hash(value)
+        elif isinstance(value, frozenset):
+            hash = cls.frozenset_hash(value)
+        else:
+            phm = getattr(value, cls.magic_method, None)
+            if phm:
+                hash = phm()
 
+        if hash is None:
+            raise TypeError("un(persistently-)hashable type: '%s'" % type(value).__name__)
 
-def int_hash(value):
-    """Compute a persistent hash for an integer value"""
-    return value
+        return super().__new__(cls, hash)
 
+    @classmethod
+    def str_hash(cls, value):
+        """Compute a persistent hash for a string value"""
+        return int(md5(value.encode()).hexdigest(), 16)
 
-def float_hash(value):
-    """Compute a persistent hash for a float value"""
-    # XXX: Check if this is really persistent cross-arch
-    return hash(value)
+    @classmethod
+    def int_hash(cls, value):
+        """Compute a persistent hash for an integer value"""
+        return value
 
+    @classmethod
+    def float_hash(cls, value):
+        """Compute a persistent hash for a float value"""
+        # XXX: Check if this is really persistent cross-arch
+        return hash(value)
 
-def decimal_hash(value):
-    """Compute a persistent hash for a Decimal value"""
-    # XXX: Check if this is really persistent cross-arch
-    return hash(value)
+    @classmethod
+    def decimal_hash(cls, value):
+        """Compute a persistent hash for a Decimal value"""
+        # XXX: Check if this is really persistent cross-arch
+        return hash(value)
 
+    @classmethod
+    def tuple_hash(cls, value):
+        """Compute a persistent hash for a tuple"""
 
-def tuple_hash(value):
-    """Compute a persistent hash for a tuple"""
+        # This algorithm is borrowed from CPython implementation.
 
-    # This algorithm is borrowed from CPython implementation.
+        multiplier = 1000003
 
-    multiplier = 1000003
+        result = 0x345678
 
-    result = 0x345678
+        length = len(value)
 
-    length = len(value)
+        for i, item in enumerate(value):
+            hash = cls(item)
+            result = ((result ^ hash) * multiplier) & ((1 << 128) - 1)
+            multiplier += 82520 + (length - i) * 2
+        result += 97531
 
-    for i, item in enumerate(value):
-        hash = persistent_hash(item)
-        result = ((result ^ hash) * multiplier) & ((1 << 128) - 1)
-        multiplier += 82520 + (length - i) * 2
-    result += 97531
+        return result
 
-    return result
+    @classmethod
+    def frozenset_hash(cls, value):
+        """Compute a persistent hash for a frozenset"""
 
+        # This algorithm is borrowed from CPython implementation.
 
-def frozenset_hash(value):
-    """Compute a persistent hash for a frozenset"""
+        result = 1927868237
 
-    # This algorithm is borrowed from CPython implementation.
+        for hash in sorted(cls(item) for item in value):
+            result ^= (hash ^ (hash << 16) ^ 89869747)  * 3644798167
 
-    result = 1927868237
+        result = result * 69069 + 907133923
 
-    for hash in sorted(persistent_hash(item) for item in value):
-        result ^= (hash ^ (hash << 16) ^ 89869747)  * 3644798167
-
-    result = result * 69069 + 907133923
-
-    return result
+        return result
