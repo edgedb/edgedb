5c35465d7391fbb2cf32343427dc5c2c9fbe21b4
Elvis Pranskevichus <elprans@sprymix.com>
2010-10-06 21:56:41 -0400

    caos.backends.pgsql: Fix DateTime packing
    py-postgresql packing agorithm includes transformation of the DateTime
    value into an absolute time interval, which for Semantix DateTime is
    represented by TimeDelta, which is not compatible with
    datetime.timedelta.  The latter stores the delta as days, seconds,
    microseconds triple, which py-postgresql expects to see, whereas
    TimeDelta stores normalized values.  This resulted in hour/minute
    information being lost for datetime atoms on updates.

--- edgedb/server/pgsql/driver.py
+++ edgedb/server/pgsql/driver.py
@@ -6,6 +6,7 @@
 ##
 
 
+import datetime
 import uuid
 from functools import partial
 
@@ -13,6 +14,7 @@ import postgresql
 from postgresql.python import socket as pg_socket
 from postgresql.driver import pq3
 from postgresql import types as pg_types
+from postgresql.types.io import stdlib_datetime as pg_datetime_io
 from postgresql.string import quote_ident
 from postgresql.python.functools import Composition as compose
 
@@ -20,6 +22,7 @@ from semantix.caos.objects.datetime import TimeDelta, DateTime, Time
 from semantix.caos.objects import numeric, string
 from semantix.caos.backends import pool as caos_pool
 from semantix.caos import CaosError
+from semantix.utils import datetime as sx_datetime
 
 
 _GREEN_SOCKET = False
@@ -41,12 +44,35 @@ def interval_unpack(mds):
     months, days, (seconds, microseconds) = mds
     return TimeDelta(months=months, days=days, seconds=seconds, microseconds=microseconds)
 
+def timestamptz_pack(x,
+                     seconds_in_day=pg_datetime_io.seconds_in_day,
+                     pg_epoch_datetime_utc=pg_datetime_io.pg_epoch_datetime_utc,
+                     UTC=pg_datetime_io.UTC):
+    x = (x.astimezone(UTC) - pg_epoch_datetime_utc)
+    if isinstance(x, sx_datetime.TimeDelta):
+        months, days, seconds, microseconds = x.to_months_days_seconds_microseconds()
+        if months:
+            raise CaosError('cannot pack relative timedelta: %s' % x)
+    else:
+        days, seconds, microseconds = x.days, x.seconds, x.microseconds
+    return ((days * seconds_in_day) + seconds, microseconds)
+
 
 oid_to_io = {
     postgresql.types.INTERVALOID: (
         compose((interval_pack, postgresql.types.io.lib.interval64_pack)),
         compose((postgresql.types.io.lib.interval64_unpack, interval_unpack))
-    )
+    ),
+
+    postgresql.types.TIMESTAMPTZOID: (
+        pg_datetime_io.proc_when_not_in(compose((timestamptz_pack,
+                                                 postgresql.types.io.lib.time64_pack)),
+                                        pg_datetime_io.time64_pack_constants),
+        pg_datetime_io.proc_when_not_in(compose((postgresql.types.io.lib.time64_unpack,
+                                                 pg_datetime_io.timestamptz_unpack)),
+                                        pg_datetime_io.time64_unpack_constants),
+        datetime.datetime
+    ),
 }
 
 oid_to_type = {
