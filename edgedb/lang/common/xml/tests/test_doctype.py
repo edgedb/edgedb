9d5485b362610fb942b448e76b47537006d3449f
Yury Selivanov <yselivanov@sprymix.com>
2010-11-03 16:33:35 -0400

    utils.xml: new package; utils.xml.types.Doctype - xml DOCTYPE datatype

---
+++ edgedb/lang/common/xml/tests/test_doctype.py
@@ -0,0 +1,22 @@
+##
+# Copyright (c) 2008-2010 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from semantix.utils.xml.types import Doctype
+
+
+class TestXMLDoctype:
+    def test_utils_xml_types_doctype(self):
+        assert str(Doctype('html')) == '<!DOCTYPE html>'
+        assert str(Doctype('html', pubid="-//W3C//DTD HTML 4.01//EN"))\
+                         == '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">'
+
+        assert str(Doctype('html',
+                           pubid="-//W3C//DTD XHTML 1.0 Transitional//EN",
+                           sysid='http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'))\
+                == '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" ' \
+                   '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
