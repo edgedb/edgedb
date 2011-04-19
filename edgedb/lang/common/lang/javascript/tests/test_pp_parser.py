ab8201860c1778cdf6e2350af2e535e5b90613df
Vicor Petrovykh <victor.petrovykh@gmail.com>
2011-04-19 14:34:03 -0400

    utils.lang.preprocessor: added preprocessor to the JS parser
    Created additional AST nodes and code generator for preprocessor directives.
    Supported directives:
     #include "name"
     #warning optional message
     #error optional message
     #define NAME code
     #define NAME(a,b) code
     #if, #ifdef, #ifndef
     #elif, #else
     #endif
    
    At the moment the preprocessor itself is integrated with the JSParser.
    This feature is turned on by including ppsupport=True when initializing
    the parser.
    e.g.
    jsp = JSParser(ppsupport=True)
    jsp.parse('#define FOO 42')
    
    utils.lang.javascript.tests: added tests for js preprocessor
    
    Testing the parser on JS code with preprocessor directives with and
    without ppsupport.

---
+++ edgedb/lang/common/lang/javascript/tests/test_pp_parser.py
@@ -0,0 +1,199 @@
+##
+# Copyright (c) 2008-2011 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from .base import MetaJSParserTest_Base, jxfail, flags
+from semantix.utils.lang.javascript.parser.jsparser import UnknownToken, UnexpectedToken, \
+    PP_UnexpectedToken, PP_MalformedToken
+
+
+class TestJSParser_withPP(metaclass=MetaJSParserTest_Base):
+# behaves
+
+    @flags(ppsupport=True)
+    @jxfail(UnknownToken, attrs={'line' : 1, 'col' : 1})
+    def test_utils_lang_js_parser_pp_wrong1(self):
+        '''#bogus define MAX 1'''
+
+    @flags(ppsupport=True)
+    @jxfail(UnexpectedToken, attrs={'line' : 1, 'col' : 11})
+    def test_utils_lang_js_parser_pp_wrong2(self):
+        '''bogus = 1 #define MAX 1'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error1(self):
+        '''#error '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error2(self):
+        '''#error "Testing"'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error3(self):
+        '''#error this is a test message'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error4(self):
+        r'''#error this is a test message\
+        print('very long message....');'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error5(self):
+        r'''#error this is a test message
+        print('very long message....');'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error6(self):
+        '''
+        #error '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_error7(self):
+        '''print('test');
+        #error '''
+
+    @jxfail(UnknownToken, attrs={'line' : 1, 'col' : 1})
+    def test_utils_lang_js_parser_pp_error8(self):
+        '''#error '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_warning1(self):
+        '''#warning this is a test message'''
+
+    @jxfail(UnknownToken, attrs={'line' : 1, 'col' : 1})
+    def test_utils_lang_js_parser_pp_warning2(self):
+        '''#warning this is a test message'''
+
+    @flags(ppsupport=True)
+    @jxfail(PP_UnexpectedToken, attrs={'line' : 1, 'col' : 10})
+    def test_utils_lang_js_parser_pp_include1(self):
+        '''#include bad include'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_include2(self):
+        '''#include "good_include"'''
+
+    @flags(ppsupport=True)
+    @jxfail(PP_MalformedToken, attrs={'line' : 1, 'col' : 9})
+    def test_utils_lang_js_parser_pp_include3(self):
+        '''#include'''
+
+    @jxfail(UnknownToken)
+    def test_utils_lang_js_parser_pp_include4(self):
+        '''#include "good_include"'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_define1(self):
+        '''#define MAX 1'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_define2(self):
+        '''#define MAX (a, b) ((a)>(b)?(a):(b))'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_define3(self):
+        r'''#define MAX (a, b) \
+        ((a)>(b)?(a):(b))'''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_define4(self):
+        r'''#define FOO (a) (a(b, a, b##a()))'''
+
+    @jxfail(UnknownToken)
+    def test_utils_lang_js_parser_pp_define5(self):
+        '#define MAX 1'
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_if1(self):
+        '''
+        #if 1
+        print('hello');
+        #elif 2
+        print('Hallo');
+        #endif
+        print('always');
+        '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_if2(self):
+        '''
+        #if 1
+        print('hello');
+        #define FOO 1
+        #elif 2
+        print('Hallo');
+        #warning 'test'
+        #endif
+        print('always');
+        '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_if3(self):
+        '''
+        #if 1
+            print('hello');
+            #ifndef OS
+                #define OS 'bad'
+                #if 1
+
+                #endif
+            #endif
+        #elif 2
+            print('Hallo');
+            #if 1
+                print('nesting')
+            #endif
+        #endif
+
+        print('always');
+        '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_if4(self):
+        '''
+        #if FLAG > 2
+        print('hello');
+        #elif FLAG == 1
+        print('Hallo');
+        #endif
+        print('always');
+        '''
+
+    @jxfail(UnknownToken, attrs={'line' : 2, 'col' : 9})
+    def test_utils_lang_js_parser_pp_if5(self):
+        '''
+        #if 1
+        print('hello');
+        #elif 2
+        print('Hallo');
+        #endif
+        print('always');
+        '''
+
+    @flags(ppsupport=True)
+    def test_utils_lang_js_parser_pp_ifdef1(self):
+        '''
+        #ifdef WINDOWS
+        print('hello');
+        #warning 'test'
+        #endif
+        #ifndef MAC
+        print('Hallo');
+        #define MAC 10
+        #endif
+        print('always');
+        '''
+
+    @jxfail(UnknownToken, attrs={'line' : 2, 'col' : 9})
+    def test_utils_lang_js_parser_pp_ifdef2(self):
+        '''
+        #ifdef WINDOWS
+        print('hello');
+        #warning 'test'
+        #endif
+        '''
+
---
+++ edgedb/lang/common/lang/preprocessor/codegen.py
@@ -0,0 +1,111 @@
+##
+# Copyright (c) 2008-2011 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from semantix.utils.ast import SourceGenerator
+from semantix.utils.lang.preprocessor import ast as ppast
+
+
+class PP_SourceGenerator(SourceGenerator):
+    def visit_list_helper(self, list, separator=', '):
+        "goes through a list and visits each member printing ','"
+
+        for i, var in enumerate(list):
+            if var:
+                self.visit(var)
+            if i != (len(list) - 1):
+                self.write(separator)
+
+    def visit_PP_Error(self, node):
+        self.write('#error')
+        if node.message:
+            self.write(' %r' % node.message)
+        self.newline()
+
+    def visit_PP_Warning(self, node):
+        self.write('#warning')
+        if node.message:
+            self.write(' %r' % node.message)
+        self.newline()
+
+    def visit_PP_Include(self, node):
+        self.write('#include ')
+        self.write('%r' % node.package)
+        self.newline()
+
+    def visit_PP_DefineName(self, node):
+        self.write('#define %s ' % node.name)
+        self.visit_list_helper(node.chunks, separator=' ')
+        self.newline()
+
+    def visit_PP_DefineCallable(self, node):
+        self.write('#define %s (' % node.name)
+        self.visit_list_helper(node.param)
+        self.write(') ')
+        self.visit_list_helper(node.chunks, separator=' ')
+        self.newline()
+
+    def visit_PP_If(self, node):
+        self.write('#if ')
+        self.visit(node.condition)
+        self.newline()
+        self._visit_if_guts(node)
+
+    def visit_PP_Ifdef(self, node):
+        self.write('#ifdef %s' % node.name)
+        self.newline()
+        self._visit_if_guts(node)
+
+    def visit_PP_Ifndef(self, node):
+        self.write('#ifndef %s' % node.name)
+        self.newline()
+        self._visit_if_guts(node)
+
+    def _visit_if_guts(self, node):
+        self.indentation += 1
+        self.visit(node.firstblock)
+        self.indentation -= 1
+        for elifblock in node.elifblocks:
+            self.visit(elifblock)
+        if node.elseblock:
+            self.visit(node.elseblock)
+        self.write('#endif')
+        self.newline()
+
+
+    def visit_PP_Elif(self, node):
+        self.write('#elif ')
+        self.visit(node.condition)
+        self.newline()
+        self.indentation += 1
+        self.visit(node.block)
+        self.indentation -= 1
+
+    def visit_PP_Else(self, node):
+        self.write('#else')
+        self.newline()
+        self.indentation += 1
+        self.visit(node.block)
+        self.indentation -= 1
+
+    def visit_PP_Call(self, node):
+        self.write('(')
+        self.visit_list_helper(node.arguments, separator=' ')
+        self.write(')')
+
+    def visit_PP_CodeChunk(self, node):
+        self.write(node.string)
+
+    def visit_PP_Quote(self, node):
+        self.write('#')
+
+    def visit_PP_Concat(self, node):
+        self.write('##')
+
+    def visit_PP_Param(self, node):
+        self.write(node.name)
+
