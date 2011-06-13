2611c79e2364ee35e0c31c92ca28919e7e715e7e
Yury Selivanov <yselivanov@sprymix.com>
2011-06-13 13:40:50 -0400

    shell: add 'run' command.
    '$ sx run FILENAME' runs python files in a semantix context with the
    following setup:
    
     - Active application
     - Logger & error reporting

---
+++ edgedb/lang/common/shell/run.py
@@ -0,0 +1,39 @@
+##
+# Copyright (c) 2011 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+import os
+import imp
+from semantix.utils import shell
+
+
+class RunCommand(shell.Command, name='run', expose=True):
+    def get_parser(self, subparsers, **kwargs):
+        parser = super().get_parser(subparsers, description='Run python files in semantix context.')
+
+        parser.add_argument('--callable', dest='callable', default='main',
+                            help='name of function/callable to execute')
+        parser.add_argument('file', help=('path to a python file to be executed'))
+
+        return parser
+
+    def __call__(self, args):
+        path = os.path.abspath(args.file)
+
+        if not os.path.exists(path):
+            raise ValueError('path not found: %r' % path)
+
+        mod_name = os.path.splitext(os.path.split(path)[-1])[0]
+        mod = imp.load_source(mod_name, path)
+
+        try:
+            callable = getattr(mod, args.callable)
+        except AttributeError:
+            raise ValueError('unable to run file %r: has no callable %r defined' % \
+                             (path, args.callable))
+
+        callable()
