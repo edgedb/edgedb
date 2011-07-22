f6cc4dc013a2617a8e99891c4b065c053d896555
Yury Selivanov <yselivanov@sprymix.com>
2011-07-22 11:02:55 -0400

    spin: update tests for python3.2 compatibility; fix config module

--- edgedb/lang/common/config/__init__.py
+++ edgedb/lang/common/config/__init__.py
@@ -352,7 +352,7 @@ def configurable(obj, *, basename=None, bind_to=None):
         def _decorate(wrapped):
             return configurable(wrapped, basename=obj_name + '.' + wrapped.__name__, bind_to=obj)
 
-        for attr_name, attr_value in obj.__dict__.items():
+        for attr_name, attr_value in tuple(obj.__dict__.items()):
             if isinstance(attr_value, cvalue):
                 assert not attr_value.bound_to
 
