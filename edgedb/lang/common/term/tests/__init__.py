3f503f7db0d22f4afa5fa415930f445870197b89
Yury Selivanov <yselivanov@sprymix.com>
2011-12-22 08:45:14 -0500

    utils: Add 'term' package - a substitute for 'utils.io.terminal'
    New package provides more generic and well-defined routines and classes
    to work with styled terminal output.  It supports features detection
    (number of supported colors, with/height), provides better integration
    with the 'shell' package and contains classes to improve and simplify
    styled output.  It also supports 256-color terminals with automatic RGB
    colors translation.

---
+++ edgedb/lang/common/term/tests/__init__.py
@@ -0,0 +1,7 @@
+##
+# Copyright (c) 2011 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
---
+++ edgedb/lang/common/term/tests/test_style.py
@@ -0,0 +1,42 @@
+##
+# Copyright (c) 2011 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from semantix.utils.debug import assert_raises
+from semantix.utils.term import Style16, Style256
+
+
+class TestUtilsIOTermStyle:
+    def test_utils_term_style16(self):
+        s = Style16(color='red', bgcolor='green', bold=True)
+
+        assert s.color == 'red'
+        assert s.bgcolor == 'green'
+
+        assert s.bold
+        s.bold = False
+        assert not s.bold
+        s.underline = True
+        assert s.underline
+
+        s.color = 'yellow'
+        assert s.color == 'yellow'
+
+        with assert_raises(ValueError, error_re='unknown color'):
+            s.color = '#FFF'
+
+        assert not s.empty
+        assert Style16().empty
+
+    def test_utils_term_style256(self):
+        assert Style256(color='red')._color == 196
+        assert Style256(color='#FF0000')._color == 196
+        assert Style256(color='#FE0000')._color == 196
+        assert Style256(color='darkmagenta')._color == 90
+
+        with assert_raises(ValueError, error_re='Unknown color'):
+            Style256(color='foooocolor')
