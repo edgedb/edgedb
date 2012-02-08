2843370c1ae7ec14ddb4c814ce8a2c65df975eed
Yury Selivanov <yselivanov@sprymix.com>
2012-02-08 14:49:35 -0500

    utils.json: Remove debug script

--- edgedb/lang/common/json/test.py
+++
@@ -1,8 +0,0 @@
-from semantix.utils.json import _encoder
-from semantix.utils.json._encoder import Encoder
-import sys
-from decimal import Decimal
-from uuid import UUID
-
-exec(''.join(sys.argv[1:]), locals(), globals())
-
