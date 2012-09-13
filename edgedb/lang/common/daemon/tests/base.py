130fb95d816e19315d5bbf9e4ae72182b0722171
Yury Selivanov <yselivanov@sprymix.com>
2012-09-13 09:59:35 -0400

    utils.daemon.tests: Further improve stability of tests

--- edgedb/lang/common/daemon/tests/base.py
+++ edgedb/lang/common/daemon/tests/base.py
@@ -6,22 +6,48 @@
 ##
 
 
+import errno
 import os
 import os.path
+import functools
+import random
 import tempfile
+import time
+
+
+def _finalize_funcarg(request, fn):
+    i = 0
+    while True:
+        i += 1
+        try:
+            os.unlink(fn)
+        except (OSError, IOError) as ex:
+            if ex.errno == errno.ENOENT:
+                return
+            else:
+                if i < 10:
+                    time.sleep(0.5)
+                    continue
+            raise
+        else:
+            return
+
+
+def _gen_base_fn_funcarg(sn):
+    def funcarg(self, request, sn=sn):
+        path = self.gen_tmp_filename(sn, randomize=True)
+        if os.path.exists(path):
+            os.unlink(path)
+        request.addfinalizer(functools.partial(_finalize_funcarg, request, path))
+        return path
+    return funcarg
 
 
 class BaseDaemonTestCase:
-    def gen_tmp_filename(self, sn):
+    def gen_tmp_filename(self, sn, randomize=False):
+        if randomize:
+            sn = str(random.randint(0, 10**5)) + '.' + sn
         return os.path.join(tempfile.gettempdir(), sn)
-
-    def _gen_base_fn_funcarg(sn):
-        def funcarg(self, request, sn=sn):
-            path = self.gen_tmp_filename(sn)
-            if os.path.exists(path):
-                os.unlink(path)
-            return path
-        return funcarg
 
     pytest_funcarg__pid    = _gen_base_fn_funcarg('semantix.test.pid')
     pytest_funcarg__stdin  = _gen_base_fn_funcarg('semantix.test.stdin')
