3ec3aa538f38efe926c172a1b4debb83bfbe7dab
Elvis Pranskevichus <elprans@sprymix.com>
2012-11-24 15:12:43 -0500

    utils.lang.import_: Move reload() and modules_from_import_statements() into utils sub-package

--- edgedb/lang/common/lang/import_/__init__.py
+++ edgedb/lang/common/lang/import_/__init__.py
@@ -11,93 +11,9 @@ import importlib
 import os
 import sys
 
-from . import module as module_types
 from .context import ImportContext
 
 from .finder import install, update_finders
-
-
-def reload(module):
-    if isinstance(module, module_types.BaseProxyModule):
-        sys.modules[module.__name__] = module.__wrapped__
-
-        # XXX: imp.reload has a hardcoded check that fails on instances of module subclasses
-        try:
-            new_mod = module.__wrapped__.__loader__.load_module(module.__name__)
-
-            if isinstance(new_mod, module_types.BaseProxyModule):
-                module.__wrapped__ = new_mod.__wrapped__
-            else:
-                module.__wrapped__ = new_mod
-
-        finally:
-            sys.modules[module.__name__] = module
-
-        return module
-
-    else:
-        return imp.reload(module)
-
-
-def modules_from_import_statements(package, imports):
-    modules = []
-
-    for name, fromlist in imports:
-        level = 0
-
-        while level < len(name) and name[level] == '.':
-            level += 1
-
-        if level > 0:
-            steps = package.rsplit('.', level - 1)
-            if len(steps) < level:
-                raise ValueError('relative import reaches beyond top-level package')
-
-            suffix = name[level:]
-
-            if suffix:
-                fq_name = '{}.{}'.format(steps[0], name[level:])
-            else:
-                fq_name = steps[0]
-        else:
-            fq_name = name
-
-        path = None
-        steps = fq_name.split('.')
-
-        add_package = True
-
-        for i in range(len(steps)):
-            modname = '.'.join(steps[:i + 1])
-
-            loader = importlib.find_loader(modname, path=path)
-
-            if loader is None:
-                raise ValueError('could not find loader for module {}'.format(modname))
-
-            if not loader.is_package(modname):
-                break
-
-            modfile = loader.get_filename(modname)
-            # os.path.dirname(__file__) is a common importlib assumption for __path__
-            path = [os.path.dirname(modfile)]
-        else:
-            if fromlist:
-                add_package = False
-
-                for entry in fromlist:
-                    modname = '{}.{}'.format(fq_name, entry)
-                    entry_loader = importlib.find_loader(modname, path=path)
-
-                    if entry_loader is not None and entry_loader.path != loader.path:
-                        modules.append(modname)
-                    else:
-                        add_package = True
-
-        if add_package:
-            modules.append(fq_name)
-
-    return modules
 
 
 class ObjectImportError(Exception):
--- edgedb/lang/common/lang/import_/utils.py
+++ edgedb/lang/common/lang/import_/utils.py
@@ -1,5 +1,5 @@
 ##
-# Copyright (c) 2011 Sprymix Inc.
+# Copyright (c) 2011-2012 Sprymix Inc.
 # All rights reserved.
 #
 # See LICENSE for details.
@@ -9,8 +9,12 @@
 import imp
 import importlib
 import os.path
+import sys
+
 
 from .context import ImportContext
+from . import module as module_types
+
 
 def cache_from_source(path, debug_override=None, cache_ext=None):
     cachepath = imp.cache_from_source(path, debug_override)
@@ -50,3 +54,86 @@ def import_module(full_module_name, *, loader=None):
         full_module_name = context
 
     return importlib.import_module(full_module_name)
+
+
+def reload(module):
+    if isinstance(module, module_types.BaseProxyModule):
+        sys.modules[module.__name__] = module.__wrapped__
+
+        # XXX: imp.reload has a hardcoded check that fails on instances of module subclasses
+        try:
+            new_mod = module.__wrapped__.__loader__.load_module(module.__name__)
+
+            if isinstance(new_mod, module_types.BaseProxyModule):
+                module.__wrapped__ = new_mod.__wrapped__
+            else:
+                module.__wrapped__ = new_mod
+
+        finally:
+            sys.modules[module.__name__] = module
+
+        return module
+
+    else:
+        return imp.reload(module)
+
+
+def modules_from_import_statements(package, imports):
+    modules = []
+
+    for name, fromlist in imports:
+        level = 0
+
+        while level < len(name) and name[level] == '.':
+            level += 1
+
+        if level > 0:
+            steps = package.rsplit('.', level - 1)
+            if len(steps) < level:
+                raise ValueError('relative import reaches beyond top-level package')
+
+            suffix = name[level:]
+
+            if suffix:
+                fq_name = '{}.{}'.format(steps[0], name[level:])
+            else:
+                fq_name = steps[0]
+        else:
+            fq_name = name
+
+        path = None
+        steps = fq_name.split('.')
+
+        add_package = True
+
+        for i in range(len(steps)):
+            modname = '.'.join(steps[:i + 1])
+
+            loader = importlib.find_loader(modname, path=path)
+
+            if loader is None:
+                raise ValueError('could not find loader for module {}'.format(modname))
+
+            if not loader.is_package(modname):
+                break
+
+            modfile = loader.get_filename(modname)
+            # os.path.dirname(__file__) is a common importlib assumption for __path__
+            path = [os.path.dirname(modfile)]
+        else:
+            if fromlist:
+                add_package = False
+
+                for entry in fromlist:
+                    modname = '{}.{}'.format(fq_name, entry)
+                    entry_loader = importlib.find_loader(modname, path=path)
+
+                    if entry_loader is not None and entry_loader.path != loader.path:
+                        modules.append(modname)
+                    else:
+                        add_package = True
+
+        if add_package:
+            modules.append(fq_name)
+
+    return modules
