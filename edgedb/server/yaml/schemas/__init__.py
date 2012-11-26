d4f75109b89c1fa3f40a198dcb79b8c5b9c5836c
Elvis Pranskevichus <elprans@sprymix.com>
2012-11-26 11:24:26 -0500

    cais.backends.yaml: Announce Caos YAML schema as cacheable
    Caos protoschema now fully supports serialization, and is able to
    support module-level caching.

--- edgedb/server/yaml/schemas/__init__.py
+++ edgedb/server/yaml/schemas/__init__.py
@@ -8,15 +8,27 @@
 
 from semantix.caos import proto as caos_proto
 from semantix.utils.lang import protoschema
+from semantix.utils.lang.yaml.schema import CachingSchema
 
 from .semantics import Semantics
 from .delta import Delta
 
 
-class Semantics(Semantics):
+class Semantics(Semantics, CachingSchema):
     def get_import_context_class(self):
         return caos_proto.ImportContext
 
     @classmethod
     def get_module_class(cls):
         return caos_proto.SchemaModule
+
+    @classmethod
+    def normalize_code(cls, module_data, imports):
+        protomod = dict(module_data)['__sx_prototypes__']
+        schema = caos_proto.get_global_proto_schema()
+        protomod.normalize(imports)
+
+    @classmethod
+    def get_implicit_imports(cls):
+        # cls.get_module_class().get_schema_class().get_builtins_module()
+        return ('semantix.caos.builtins',)
