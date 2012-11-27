eb354a78edf20d6766d085beea3542b960351615
Yury Selivanov <yselivanov@sprymix.com>
2012-11-27 17:28:40 -0500

    utils.lang.python.convert: Copy 'lineno' and 'col_offset' properties

--- edgedb/lang/common/lang/python/convert.py
+++ edgedb/lang/common/lang/python/convert.py
@@ -37,6 +37,9 @@ class AstToPyAstConverter:
                 for item in value:
                     if isinstance(item, ast.AST):
                         new_item = cls.convert(item)
+                        if hasattr(item, 'lineno'):
+                            new_item.lineno = item.lineno
+                            new_item.col_offset = item.col_offset
                         new_item.parent = new_node
                         new_value.append(new_item)
                     else:
@@ -45,6 +48,9 @@ class AstToPyAstConverter:
             elif isinstance(value, ast.AST):
                 new_item = cls.convert(value)
                 new_item.parent = new_node
+                if hasattr(value, 'lineno'):
+                    new_item.lineno = value.lineno
+                    new_item.col_offset = value.col_offset
                 setattr(new_node, field, new_item)
             else:
                 setattr(new_node, field, value)
