889a287bbd0e5d769def3bcf582f8f1294d318cf
Elvis Pranskevichus <elprans@sprymix.com>
2012-12-11 17:06:28 -0500

    caos.backends.pgsql.dbops.schemas: Implement RenameSchema op

--- edgedb/server/pgsql/dbops/schemas.py
+++ edgedb/server/pgsql/dbops/schemas.py
@@ -34,6 +34,16 @@ class CreateSchema(ddl.DDLOperation):
         return '<caos.sync.%s %s>' % (self.__class__.__name__, self.name)
 
 
+class RenameSchema(ddl.SchemaObjectOperation):
+    def __init__(self, name, new_name):
+        super().__init__(name)
+        self.new_name = new_name
+
+    def code(self, context):
+        return '''ALTER SCHEMA {} RENAME TO {}'''.format(common.quote_ident(self.name),
+                                                         common.quote_ident(self.new_name)), []
+
+
 class DropSchema(ddl.DDLOperation):
     def __init__(self, name, *, conditions=None, neg_conditions=None, priority=0):
         super().__init__(conditions=conditions, neg_conditions=neg_conditions, priority=priority)
