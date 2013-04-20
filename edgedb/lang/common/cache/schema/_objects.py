0b1169f3ad9edcc30ce2275770f54e1f2ed6bed3
Yury Selivanov <yselivanov@sprymix.com>
2013-04-20 14:38:39 -0400

    node, utils.cache: Make cache a Node sub-system
    Now there is a standard way to configure cache system and plug it into
    Node.  Below is an example of YAML config file:
    
      %schema metamagic.node.schema.Node
      %from metamagic.utils.cache.schema import cache
      %from metamagic.utils.cache import Bucket, MemoryBackend
      ---
    
      !cache Cache:
          Bucket:
              backends:
                  - MemoryBackend
    
      DefaultNode:
          systems:
              - Cache
    
    The DefaultNode from the above example will automatically initialize
    cache during its start process.

---
+++ edgedb/lang/common/cache/schema/_objects.py
@@ -0,0 +1,22 @@
+##
+# Copyright (c) 2013 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from metamagic.node.schema._objects import ClassObject, resolve_name
+from metamagic.utils.cache import node
+
+
+class Cache(metaclass=ClassObject, baseclass=node.CacheSystem):
+    @classmethod
+    def _apply_data(cls, dct, *, data, context):
+        buckets = {}
+
+        for bucket, spec in data.items():
+            bucket_cls = resolve_name(context, bucket)
+            buckets[bucket_cls] = spec['backends']
+
+        dct['class_buckets'] = buckets
---
+++ edgedb/lang/common/cache/tests/test_node_cache.py
@@ -0,0 +1,23 @@
+##
+# Copyright (c) 2013 Sprymix Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+from metamagic.utils.cache.node import CacheSystem
+
+
+class TestNodeCacheSystem:
+    def test_utils_cache_node_1(self):
+        from . import node
+        from metamagic.utils.cache import Bucket, MemoryBackend
+
+        assert issubclass(node.Cache, CacheSystem)
+        assert node.Cache.class_buckets[Bucket][0].cls == MemoryBackend
+
+        c = node.Cache()
+        c.start()
+        assert len(Bucket.get_backends()) == 1
+        c.stop()
