3f96a8a916cbf6fb0079665d18a185f438dc61a5
Yury Selivanov <yselivanov@sprymix.com>
2013-05-07 13:48:53 -0400

    node, utils.cache: Application.get_cache -> node.CacheSystem.get_bucket
    All functionality related to cache buckets moved from Application to
    node's CacheSystem.  To get a cache bucket linked to the current
    deployment bucket, simply write 'CacheSystem.get_bucket(YourBucketCls)'

--- edgedb/lang/common/cache/nodesystem/__init__.py
+++ edgedb/lang/common/cache/nodesystem/__init__.py
@@ -6,11 +6,16 @@
 ##
 
 
+import weakref
+
 from metamagic import node
+from ..exceptions import CacheError
+from ..bucket import Bucket
 
 
 class CacheSystem(node.System):
     class_buckets = None
+    node_buckets_cache = weakref.WeakKeyDictionary()
 
     def configure(self):
         assert self.class_buckets
@@ -18,3 +23,57 @@ class CacheSystem(node.System):
         for bucket_cls, backend_classes in self.class_buckets.items():
             backends = [ctr.cls(**(ctr.args or {})) for ctr in backend_classes]
             bucket_cls.set_backends(*backends)
+
+    @classmethod
+    def get_bucket(cls, bucket_cls=None):
+        active_node = node.Node.active
+        if not active_node:
+            raise CacheError('An active node is required to get cache bucket {}'.format(bucket_cls))
+
+        try:
+            desc = cls.node_buckets_cache[active_node]
+        except KeyError:
+            desc = cls.node_buckets_cache[active_node] = _NodeCacheDescriptor(active_node)
+
+        return desc.get(bucket_cls)
+
+
+class NodeCache(Bucket):
+    pass
+
+
+class NodeDeploymentCache(NodeCache):
+    pass
+
+
+class PersistentNodeDeploymentCache(NodeDeploymentCache):
+    pass
+
+
+class _NodeCacheDescriptor:
+    __slots__ = ('root', 'depl', 'buckets')
+
+    def __init__(self, node):
+        try:
+            node.get_system(CacheSystem)
+        except node.NodeError as ex:
+            raise CacheError('Node does not have a configured cache system.') from ex
+
+        self.root = NodeCache('{}.{}'.format(node.__class__.__module__,
+                                             node.__class__.__name__))
+
+        self.depl = NodeDeploymentCache(node.deployment_name,
+                                        parent=self.root)
+
+        self.buckets = {NodeCache: self.root,
+                        NodeDeploymentCache: self.depl}
+
+    def get(self, bucket_cls):
+        if bucket_cls is None:
+            return self.depl
+
+        try:
+            return self.buckets[bucket_cls]
+        except KeyError:
+            bucket = self.buckets[bucket_cls] = bucket_cls(parent=self.depl)
+            return bucket
