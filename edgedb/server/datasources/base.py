45c2fa6a4a3967ead3566a037bf5e43ec274263f
Elvis Pranskevichus <elprans@sprymix.com>
2013-12-02 20:44:42 -0500

    caos.datasources.base: Add missing import

--- edgedb/server/datasources/base.py
+++ edgedb/server/datasources/base.py
@@ -12,6 +12,7 @@ import uuid
 
 from metamagic.caos import types as caos_types
 from metamagic.utils.lang import meta as lang_meta
+from metamagic.utils.lang.import_ import get_object
 
 
 def check_type(variable, type):
