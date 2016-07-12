1c25ba65f34faa2f9936e5ed3ad9a9c68c2ec3cf
Victor Petrovykh <vpetrovykh@sprymix.com>
2016-07-12 17:45:10 -0400

    Refactoring of parser tests.

--- edgedb/server/_testbase.py
+++ edgedb/server/_testbase.py
@@ -3,17 +3,12 @@ import atexit
 import functools
 import inspect
 import json
-import os
-import re
 import textwrap
 import unittest
 
 
 from edgedb.server import cluster as edgedb_cluster
 from edgedb.client import exceptions as edgeclient_exc
-from edgedb.lang.schema.parser import parser
-from edgedb.lang.schema import codegen
-from edgedb.lang.common import markup
 
 
 class TestCaseMeta(type(unittest.TestCase)):
@@ -217,41 +212,3 @@ class BaseParserTest(unittest.TestCase, metaclass=ParserTestMeta):
 
     def run_test(self, *, source, spec):
         raise NotImplementedError
-
-
-class ParserTest(BaseParserTest):
-    re_filter = re.compile(r'[\s\'"()]+|(#.*?\n)')
-    parser_cls = parser.EdgeSchemaParser
-
-    def get_parser(self, *, spec):
-        return self.__class__.parser_cls()
-
-    def assert_equal(self, expected, result):
-        expected_stripped = self.re_filter.sub('', expected).lower()
-        result_stripped = self.re_filter.sub('', result).lower()
-
-        assert expected_stripped == result_stripped, \
-            '[test]expected: {}\n[test] != returned: {}'.format(
-                expected, result)
-
-    def run_test(self, *, source, spec):
-        debug = bool(os.environ.get('DEBUG_ESCHEMA'))
-
-        if debug:
-            markup.dump_code(source, lexer='edgeschema')
-
-        p = self.get_parser(spec=spec)
-
-        esast = p.parse(source)
-
-        if debug:
-            markup.dump(esast)
-
-        processed_src = codegen.EdgeSchemaSourceGenerator.to_source(esast)
-
-        if debug:
-            markup.dump_code(processed_src, lexer='edgeschema')
-
-        expected_src = source
-
-        self.assert_equal(expected_src, processed_src)
--- tests/test_schema_parser.py
+++ tests/test_schema_parser.py
@@ -6,10 +6,54 @@
 ##
 
 
+import os
+import re
+
+from edgedb.lang.common import markup
+from edgedb.lang.schema import codegen
+from edgedb.lang.schema.parser import parser
 from edgedb.server import _testbase as tb
 
 
-class TestEdgeSchemaParser(tb.ParserTest):
+class ParserTest(tb.BaseParserTest):
+    re_filter = re.compile(r'[\s\'"()]+|(#.*?\n)')
+    parser_cls = parser.EdgeSchemaParser
+
+    def get_parser(self, *, spec):
+        return self.__class__.parser_cls()
+
+    def assert_equal(self, expected, result):
+        expected_stripped = self.re_filter.sub('', expected).lower()
+        result_stripped = self.re_filter.sub('', result).lower()
+
+        assert expected_stripped == result_stripped, \
+            '[test]expected: {}\n[test] != returned: {}'.format(
+                expected, result)
+
+    def run_test(self, *, source, spec):
+        debug = bool(os.environ.get('DEBUG_ESCHEMA'))
+
+        if debug:
+            markup.dump_code(source, lexer='edgeschema')
+
+        p = self.get_parser(spec=spec)
+
+        esast = p.parse(source)
+
+        if debug:
+            markup.dump(esast)
+
+        processed_src = codegen.EdgeSchemaSourceGenerator.to_source(esast)
+
+        if debug:
+            markup.dump_code(processed_src, lexer='edgeschema')
+
+        expected_src = source
+
+        self.assert_equal(expected_src, processed_src)
+
+
+class TestEdgeSchemaParser(ParserTest):
     def test_eschema_parser_concept01(self):
         """concept User extends builtins::NamedObject"""
 
