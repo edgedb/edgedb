f1b7cbde4f6de30249ad0967d4c29ddcdd3bb9f7
Elvis Pranskevichus <elvis@magic.io>
2016-09-04 02:50:17 -0400

    Precompile GraphQL parser on build

--- edgedb/lang/build.py
+++ edgedb/lang/build.py
@@ -16,11 +16,13 @@ class build(build.build):
         import edgedb.lang.edgeql.parser.grammar.block as edgeql_spec2
         import edgedb.server.pgsql.parser.pgsql as pgsql_spec
         import edgedb.lang.schema.parser.grammar.declarations as schema_spec
+        import edgedb.lang.graphql.parser.grammar.document as graphql_spec
 
         base_path = os.path.dirname(
             os.path.dirname(os.path.dirname(__file__)))
 
-        for spec in (edgeql_spec, edgeql_spec2, pgsql_spec, schema_spec):
+        for spec in (edgeql_spec, edgeql_spec2, pgsql_spec,
+                     schema_spec, graphql_spec):
             subpath = os.path.dirname(spec.__file__)[len(base_path) + 1:]
             cache = os.path.join(self.build_lib,
                                  subpath,
