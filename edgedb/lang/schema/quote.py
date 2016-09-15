fc6c7a743d4f177e76caa282c31c4cf11f2fb0bc
Victor Petrovykh <victor@magic.io>
2016-09-14 21:37:52 -0400

    Fix missing quoting module for schema.

---
+++ edgedb/lang/schema/quote.py
@@ -0,0 +1,40 @@
+##
+# Copyright (c) 2016 MagicStack Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+import re
+
+from .parser.grammar import keywords
+
+
+_re_ident = re.compile(r'(?:[^\W\d]|\$)(?:\w|\$)*')
+
+
+def quote_literal(text):
+    return "'" + text.replace("'", R"\'") + "'"
+
+
+def dollar_quote_literal(text):
+    quote = '$$'
+    qq = 0
+
+    while quote in text:
+        if qq % 16 < 10:
+            qq += 10 - qq % 16
+
+        quote = '${:x}$'.format(qq)[::-1]
+        qq += 1
+
+    return quote + text + quote
+
+
+def disambiguate_identifier(text):
+    if (keywords.edge_schema_keywords.get(text)
+            or not _re_ident.fullmatch(text)):
+        return '`{}`'.format(text)
+    else:
+        return text
