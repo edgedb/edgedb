4364e4ab0981e0f8d1ed49994e08390332b44d8f
Victor Petrovykh <victor@magic.io>
2016-09-21 20:54:36 -0400

    Add functional tests for UPDATE.

--- tests/test_edgeql_update.py
+++ tests/test_edgeql_update.py
@@ -6,12 +6,14 @@
 ##
 
 
+import unittest
 import uuid
 
+from edgedb.client import exceptions as exc
 from edgedb.server import _testbase as tb
 
 
-class TestDelete(tb.QueryTestCase):
+class TestUpdate(tb.QueryTestCase):
     SETUP = """
         CREATE DELTA test::d_update01 TO $$
             concept Status:
@@ -20,71 +22,302 @@ class TestDelete(tb.QueryTestCase):
             concept UpdateTest:
                 link name to str
                 link status to Status
+                link comment to str
         $$;
 
         COMMIT DELTA test::d_update01;
+
+        # populate the test DB
+
+        INSERT test::Status {
+            name := 'Open'
+        };
+
+        INSERT test::Status {
+            name := 'Closed'
+        };
     """
 
-    async def test_edgeql_update_simple01(self):
-        result = await self.con.execute(r"""
-            INSERT test::Status {
-                name := 'Open'
-            };
+    def setUp(self):
+        super().setUp()
+        self.loop.run_until_complete(self._setup_objects())
 
-            INSERT test::Status {
-                name := 'Closed'
+    def tearDown(self):
+        super().tearDown()
+        self.loop.run_until_complete(self.con.execute(r"""
+            DELETE test::UpdateTest;
+        """))
+
+    async def _setup_objects(self):
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            INSERT UpdateTest {
+                name := 'update-test1',
+                status := (SELECT Status WHERE Status.name = 'Open')
             };
 
             WITH MODULE test
             INSERT UpdateTest {
-                name := 'update-test',
+                name := 'update-test2',
+                comment := 'second',
                 status := (SELECT Status WHERE Status.name = 'Open')
             };
-        """)
 
-        result = await self.con.execute(r"""
+            WITH MODULE test
+            INSERT UpdateTest {
+                name := 'update-test3',
+                comment := 'third',
+                status := (SELECT Status WHERE Status.name = 'Closed')
+            };
+
             WITH MODULE test
             SELECT UpdateTest {
                 name,
+                comment,
                 status: {
                     name
                 }
-            } WHERE UpdateTest.name = 'update-test';
+            } ORDER BY UpdateTest.name;
         """)
 
-        self.assert_data_shape(result, [
-            [{
-                'id': uuid.UUID,
-                'name': 'update-test',
-                'status': {
-                    'name': 'Open'
-                }
-            }],
-        ])
+        self.original = res[-1]
 
-        result = await self.con.execute(r"""
+    async def test_edgeql_update_simple01(self):
+        orig1 = self.original[0]
+
+        res = await self.con.execute(r"""
             WITH MODULE test
             UPDATE UpdateTest {
                 status := (SELECT Status WHERE Status.name = 'Closed')
-            } WHERE UpdateTest.name = 'update-test';
-        """)
+            } WHERE UpdateTest.name = 'bad name';
 
-        result = await self.con.execute(r"""
             WITH MODULE test
             SELECT UpdateTest {
                 name,
+                comment,
                 status: {
                     name
                 }
-            } WHERE UpdateTest.name = 'update-test';
+            } WHERE UpdateTest.name = 'update-test1';
         """)
 
-        self.assert_data_shape(result, [
-            [{
-                'id': uuid.UUID,
-                'name': 'update-test',
+        self.assert_data_shape(res, [
+            [],
+            [orig1],
+        ])
+
+    async def test_edgeql_update_simple02(self):
+        orig1, orig2, orig3 = self.original
+
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            UPDATE UpdateTest {
+                status := (SELECT Status WHERE Status.name = 'Closed')
+            } WHERE UpdateTest.name = 'update-test1';
+
+            WITH MODULE test
+            SELECT UpdateTest {
+                name,
+                comment,
+                status: {
+                    name
+                }
+            } ORDER BY UpdateTest.name;
+        """)
+
+        self.assert_data_shape(res[-1], [
+            {
+                'id': orig1['id'],
+                'name': 'update-test1',
                 'status': {
                     'name': 'Closed'
                 }
-            }],
+            },
+            orig2,
+            orig3,
         ])
+
+    @unittest.expectedFailure
+    async def test_edgeql_update_simple03(self):
+        orig1, orig2, orig3 = self.original
+
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            UPDATE UpdateTest {
+                comment := 'updated ' + UpdateTest.comment
+            } WHERE UpdateTest.name = 'update-test2';
+
+            WITH MODULE test
+            SELECT UpdateTest {
+                name,
+                comment,
+            } ORDER BY UpdateTest.name;
+        """)
+
+        self.assert_data_shape(res[-1], [
+            {
+                'id': orig1['id'],
+                'name': orig1['name'],
+                'comment': orig1['comment'],
+            }, {
+                'id': orig2['id'],
+                'name': 'update-test2',
+                'comment': 'updated second',
+            }, {
+                'id': orig3['id'],
+                'name': orig3['name'],
+                'comment': orig3['comment'],
+            },
+        ])
+
+    async def test_edgeql_update_simple04(self):
+        orig1, orig2, orig3 = self.original
+
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            UPDATE UpdateTest {
+                comment := UpdateTest.comment + "!",
+                status := (SELECT Status WHERE Status.name = 'Closed')
+            };
+
+            WITH MODULE test
+            SELECT UpdateTest {
+                name,
+                comment,
+                status: {
+                    name
+                }
+            } ORDER BY UpdateTest.name;
+        """)
+
+        self.assert_data_shape(res[-1], [
+            {
+                'id': orig1['id'],
+                'name': 'update-test1',
+                'comment': None,
+                'status': {
+                    'name': 'Closed'
+                }
+            }, {
+                'id': orig2['id'],
+                'name': 'update-test2',
+                'comment': 'second!',
+                'status': {
+                    'name': 'Closed'
+                }
+            }, {
+                'id': orig3['id'],
+                'name': 'update-test3',
+                'comment': 'third!',
+                'status': {
+                    'name': 'Closed'
+                }
+            },
+        ])
+
+    @unittest.expectedFailure
+    async def test_edgeql_update_returning01(self):
+        orig1, orig2, orig3 = self.original
+
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            UPDATE UpdateTest {
+                comment := 'updated ' + UpdateTest.comment
+            } WHERE UpdateTest.name = 'update-test2'
+            RETURNING UpdateTest {
+                name,
+                comment,
+            };
+        """)
+
+        self.assert_data_shape(res[-1], [
+            {
+                'id': orig2['id'],
+                'name': 'update-test2',
+                'comment': 'updated second',
+            },
+        ])
+
+    @unittest.expectedFailure
+    async def test_edgeql_update_returning02(self):
+        orig1, orig2, orig3 = self.original
+
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            UPDATE UpdateTest {
+                comment := UpdateTest.comment + "!",
+                status := (SELECT Status WHERE Status.name = 'Closed')
+            } RETURNING UpdateTest {
+                name,
+                comment,
+                status: {
+                    name
+                }
+            };
+        """)
+
+        # XXX: this relies on the "natural" DB order of items
+        self.assert_data_shape(res[-1], [
+            {
+                'id': orig1['id'],
+                'name': 'update-test1',
+                'comment': None,
+                'status': {
+                    'name': 'Closed'
+                }
+            }, {
+                'id': orig2['id'],
+                'name': 'update-test2',
+                'comment': 'second!',
+                'status': {
+                    'name': 'Closed'
+                }
+            }, {
+                'id': orig3['id'],
+                'name': 'update-test3',
+                'comment': 'third!',
+                'status': {
+                    'name': 'Closed'
+                }
+            },
+        ])
+
+    @unittest.expectedFailure
+    async def test_edgeql_update_returning03(self):
+        orig1, orig2, orig3 = self.original
+
+        res = await self.con.execute(r"""
+            WITH MODULE test
+            UPDATE UpdateTest {
+                comment := 'updated ' + UpdateTest.comment
+            } WHERE UpdateTest.name = 'update-test2'
+            RETURNING SINGLE UpdateTest {
+                name,
+                comment,
+            };
+        """)
+
+        self.assert_data_shape(res[-1], [
+            {
+                'id': orig2['id'],
+                'name': 'update-test2',
+                'comment': 'updated second',
+            },
+        ])
+
+    @unittest.expectedFailure
+    async def test_edgeql_update_returning04(self):
+        # XXX: We're expecting an exception since the returning set is
+        # not a singleton set. The specific exception needs to be
+        # updated once it is implemented.
+        #
+        with self.assertRaises(exc._base.EdgeDBError):
+            res = await self.con.execute(r"""
+                WITH MODULE test
+                UPDATE UpdateTest {
+                    comment := 'updated ' + UpdateTest.comment
+                } RETURNING SINGLE UpdateTest {
+                    name,
+                    comment,
+                };
+            """)
