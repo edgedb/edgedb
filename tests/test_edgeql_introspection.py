51b207f5c067b2f0dd58e13c2524dc4e81b41bcd
Victor Petrovykh <victor@magic.io>
2016-10-14 19:24:18 -0400

    edgeql: Add introspection tests.

--- tests/test_edgeql_introspection.py
+++ tests/test_edgeql_introspection.py
@@ -147,6 +147,50 @@ class TestIntrospection(tb.QueryTestCase):
             }]
         ])
 
+    async def test_edgeql_introspection_concept05(self):
+        await self.assert_query_result(r"""
+            WITH MODULE schema
+            SELECT `Concept` {
+                name,
+                is_abstract,
+                links: {
+                    attributes: {
+                        name,
+                        @value
+                    } WHERE EXISTS `Concept`.links.attributes@value
+                      ORDER BY `Attribute`.name
+                } WHERE `Concept`.links.name LIKE 'test::%'
+                  ORDER BY `Link`.name
+            }
+            WHERE `Concept`.name = 'test::User';
+        """, [
+            [{
+                'name': 'test::User',
+                'is_abstract': False,
+                'links': [{
+                    'attributes': [
+                        {'name': 'stdattrs::is_abstract', '@value': 'false'},
+                        {'name': 'stdattrs::is_derived', '@value': 'false'},
+                        {'name': 'stdattrs::is_final', '@value': 'false'},
+                        {'name': 'stdattrs::mapping', '@value': '*1'},
+                        {'name': 'stdattrs::name', '@value': 'test::name'},
+                        {'name': 'stdattrs::readonly', '@value': 'false'},
+                        {'name': 'stdattrs::required', '@value': 'true'},
+                    ]
+                }, {
+                    'attributes': [
+                        {'name': 'stdattrs::is_abstract', '@value': 'false'},
+                        {'name': 'stdattrs::is_derived', '@value': 'false'},
+                        {'name': 'stdattrs::is_final', '@value': 'false'},
+                        {'name': 'stdattrs::mapping', '@value': '**'},
+                        {'name': 'stdattrs::name', '@value': 'test::todo'},
+                        {'name': 'stdattrs::readonly', '@value': 'false'},
+                        {'name': 'stdattrs::required', '@value': 'false'},
+                    ]
+                }]
+            }]
+        ])
+
     async def test_edgeql_introspection_link01(self):
         await self.assert_query_result(r"""
             WITH MODULE schema
@@ -182,3 +226,44 @@ class TestIntrospection(tb.QueryTestCase):
         """)
 
         self.assert_data_shape(res[0], [True] * len(res[0]))
+
+    async def test_edgeql_introspection_meta02(self):
+        await self.assert_query_result(r"""
+            WITH MODULE schema
+            SELECT Class {
+                name
+            }
+            WHERE Class.name ~ '^test::\w+$'
+            ORDER BY Class.name;
+        """, [
+            [
+                {'name': 'test::body'},
+                {'name': 'test::Comment'},
+                {'name': 'test::Dictionary'},
+                {'name': 'test::due_date'},
+                {'name': 'test::Issue'},
+                {'name': 'test::issue'},
+                {'name': 'test::issue_num_t'},
+                {'name': 'test::LogEntry'},
+                {'name': 'test::name'},
+                {'name': 'test::Named'},
+                {'name': 'test::number'},
+                {'name': 'test::Owned'},
+                {'name': 'test::owner'},
+                {'name': 'test::parent'},
+                {'name': 'test::Priority'},
+                {'name': 'test::priority'},
+                {'name': 'test::rank'},
+                {'name': 'test::related_to'},
+                {'name': 'test::spent_time'},
+                {'name': 'test::start_date'},
+                {'name': 'test::Status'},
+                {'name': 'test::status'},
+                {'name': 'test::Text'},
+                {'name': 'test::time_estimate'},
+                {'name': 'test::time_spent_log'},
+                {'name': 'test::todo'},
+                {'name': 'test::User'},
+                {'name': 'test::watchers'},
+            ]
+        ])
