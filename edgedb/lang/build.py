f940ef8b4b221bae3bd851301c27098e6982b556
Elvis Pranskevichus <elvis@magic.io>
2016-12-09 00:17:53 -0500

    setup: Build WKT parser at build stage

--- edgedb/lang/build.py
+++ edgedb/lang/build.py
@@ -17,12 +17,13 @@ class build(build.build):
         import edgedb.server.pgsql.parser.pgsql as pgsql_spec
         import edgedb.lang.schema.parser.grammar.declarations as schema_spec
         import edgedb.lang.graphql.parser.grammar.document as graphql_spec
+        import edgedb.lang.common.gis.serialization.wkt.wkt as wkt_spec
 
         base_path = os.path.dirname(
             os.path.dirname(os.path.dirname(__file__)))
 
         for spec in (edgeql_spec, edgeql_spec2, pgsql_spec,
-                     schema_spec, graphql_spec):
+                     schema_spec, graphql_spec, wkt_spec):
             subpath = os.path.dirname(spec.__file__)[len(base_path) + 1:]
             cache_dir = os.path.join(self.build_lib, subpath)
             os.makedirs(cache_dir, exist_ok=True)
