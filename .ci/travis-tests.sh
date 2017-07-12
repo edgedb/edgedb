2013dc08a671eaa5807a3c4199d0092c06858fa0
Elvis Pranskevichus <elvis@magic.io>
2017-07-12 18:54:17 -0400

    ci: Run flake8 as part of tests.

--- .ci/travis-tests.sh
+++ .ci/travis-tests.sh
@@ -7,11 +7,14 @@ export PIP_CACHE_DIR=$(pwd)/build/pip/
 pip --quiet install vex
 vex --python=python3 -m test pip install --quiet -U setuptools wheel pip
 vex test pip install --quiet -U -r requirements.txt
+vex test pip install --quiet -U -r .ci/requirements.txt
 vex test python setup.py -q build install
 cd /tmp
 export EDGEDB_MODPATH=$(vex test python -c \
     'import edgedb; print(next(iter(edgedb.__path__)))')
 vex test python -m unittest discover --verbose -s "${CI_PROJECT_DIR}/tests"
+cd "${CI_PROJECT_DIR}"
+vex test flake8
 cd "${CI_PROJECT_DIR}"
 find build -type f ! -name '*.pickle' ! -wholename 'build/pip/*' -delete && \
     find build -type d -empty -delete
