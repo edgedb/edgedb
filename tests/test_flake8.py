de3b7c6158a18d5767251886114d935206ab30c8
Yury Selivanov <yury@magic.io>
2017-07-13 13:24:28 -0400

    flake8: freeze pycodestyle dep; only check edgedb/ and tests/

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -20,13 +20,15 @@ class TestFlake8(unittest.TestCase):
         edgepath = list(edgedb.__path__)[0]
         edgepath = os.path.dirname(edgepath)
 
-        try:
-            subprocess.run(
-                [sys.executable, '-m', 'flake8'],
-                check=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                cwd=edgepath)
-        except subprocess.CalledProcessError as ex:
-            raise AssertionError(
-                f'flake8 validation failed:\n{ex.output.decode()}') from None
+        for subdir in ['edgedb', 'tests']:  # ignore any top-level test files
+            try:
+                subprocess.run(
+                    [sys.executable, '-m', 'flake8'],
+                    check=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    cwd=os.path.join(edgepath, subdir))
+            except subprocess.CalledProcessError as ex:
+                output = ex.output.decode()
+                raise AssertionError(
+                    f'flake8 validation failed:\n{output}') from None
