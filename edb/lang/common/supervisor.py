a973e39e8f926d21c836c7fd7c26b2085b09b39c
Yury Selivanov <yury@magic.io>
2019-01-04 15:35:13 -0500

    Add common.supervisor async abstraction
    Supervisor provides an easy to use interface to spawn asynchronous
    tasks and to waiti for them or cancel them efficiently.

---
+++ edb/lang/common/supervisor.py
@@ -0,0 +1,159 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2018-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import asyncio
+import itertools
+
+from . import taskgroup
+
+
+class Supervisor:
+
+    def __init__(self, *, _name, _loop, _private):
+        if _name is None:
+            self._name = f'sup#{_name_counter()}'
+        else:
+            self._name = str(_name)
+
+        self._loop = _loop
+        self._unfinished_tasks = 0
+        self._cancelled = False
+        self._tasks = set()
+        self._errors = []
+        self._base_error = None
+        self._on_completed_fut = None
+
+    @classmethod
+    async def create(cls, *, name: str=None):
+        loop = asyncio.get_running_loop()
+        return cls(_loop=loop, _name=name, _private=True)
+
+    def __repr__(self):
+        msg = f'<Supervisor {self._name!r}'
+        if self._tasks:
+            msg += f' tasks:{len(self._tasks)}'
+        if self._unfinished_tasks:
+            msg += f' unfinished:{self._unfinished_tasks}'
+        if self._errors:
+            msg += f' errors:{len(self._errors)}'
+        if self._cancelled:
+            msg += ' cancelling'
+        msg += '>'
+        return msg
+
+    def create_task(self, coro):
+        if self._cancelled:
+            raise RuntimeError(
+                f'supervisor {self!r} has already been cancelled')
+
+        task = self._loop.create_task(coro)
+        task.add_done_callback(self._on_task_done)
+
+        self._unfinished_tasks += 1
+        self._tasks.add(task)
+
+        return task
+
+    async def cancel(self):
+        self._cancel()
+
+        if self._unfinished_tasks:
+            was_cancelled = await self._wait()
+            if was_cancelled:
+                raise asyncio.CancelledError()
+
+    async def wait(self):
+        if self._unfinished_tasks:
+            was_cancelled = await self._wait()
+            if was_cancelled:
+                raise asyncio.CancelledError()
+
+        if self._base_error is not None:
+            raise self._base_error
+
+        if self._errors:
+            # Exceptions are heavy objects that can have object
+            # cycles (bad for GC); let's not keep a reference to
+            # a bunch of them.
+            errors = self._errors
+            self._errors = None
+
+            me = taskgroup.TaskGroupError('unhandled errors in a Supervisor',
+                                          errors=errors)
+            raise me from None
+
+    async def _wait(self):
+        was_cancelled = False
+
+        # We use while-loop here because "self._on_completed_fut"
+        # can be cancelled multiple times if our parent task
+        # is being cancelled repeatedly (or even once, when
+        # our own cancellation is already in progress)
+        while self._unfinished_tasks:
+            if self._on_completed_fut is None:
+                self._on_completed_fut = self._loop.create_future()
+
+            try:
+                await self._on_completed_fut
+            except asyncio.CancelledError:
+                was_cancelled = True
+                self._cancel()
+
+            self._on_completed_fut = None
+
+        assert self._unfinished_tasks == 0
+        self._on_completed_fut = None  # no longer needed
+
+        return was_cancelled
+
+    def _on_task_done(self, task):
+        self._unfinished_tasks -= 1
+
+        assert self._unfinished_tasks >= 0
+
+        if self._on_completed_fut is not None and not self._unfinished_tasks:
+            if not self._on_completed_fut.done():
+                self._on_completed_fut.set_result(True)
+
+        if task.cancelled():
+            return
+
+        exc = task.exception()
+        if exc is None:
+            return
+
+        self._errors.append(exc)
+        if self._is_base_error(exc) and self._base_error is None:
+            self._base_error = exc
+
+        self._cancel()
+
+    def _cancel(self):
+        self._cancelled = True
+
+        for t in self._tasks:
+            if not t.done():
+                t.cancel()
+
+    def _is_base_error(self, exc):
+        assert isinstance(exc, BaseException)
+        return not isinstance(exc, Exception)
+
+
+_name_counter = itertools.count(1).__next__
---
+++ tests/common/test_supervisor.py
@@ -0,0 +1,288 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2018-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import asyncio
+
+from edb.lang.common import supervisor
+from edb.lang.common import taskgroup
+from edb.server import _testbase as tb
+
+
+class TestSupervisor(tb.TestCase):
+
+    async def test_supervisor_01(self):
+
+        async def foo1():
+            await asyncio.sleep(0.1)
+            return 42
+
+        async def foo2():
+            await asyncio.sleep(0.2)
+            return 11
+
+        g = await supervisor.Supervisor.create()
+        t1 = g.create_task(foo1())
+        t2 = g.create_task(foo2())
+        await g.wait()
+
+        self.assertEqual(t1.result(), 42)
+        self.assertEqual(t2.result(), 11)
+
+    async def test_supervisor_02(self):
+
+        async def foo1():
+            await asyncio.sleep(0.1)
+            return 42
+
+        async def foo2():
+            await asyncio.sleep(0.2)
+            return 11
+
+        g = await supervisor.Supervisor.create()
+        t1 = g.create_task(foo1())
+        await asyncio.sleep(0.15)
+        t2 = g.create_task(foo2())
+        await g.wait()
+
+        self.assertEqual(t1.result(), 42)
+        self.assertEqual(t2.result(), 11)
+
+    async def test_supervisor_03(self):
+
+        async def foo1():
+            await asyncio.sleep(1)
+            return 42
+
+        async def foo2():
+            await asyncio.sleep(0.2)
+            return 11
+
+        g = await supervisor.Supervisor.create()
+
+        t1 = g.create_task(foo1())
+        await asyncio.sleep(0.15)
+        # cancel t1 explicitly, i.e. everything should continue
+        # working as expected.
+        t1.cancel()
+
+        t2 = g.create_task(foo2())
+        await g.wait()
+
+        self.assertTrue(t1.cancelled())
+        self.assertEqual(t2.result(), 11)
+
+    async def test_supervisor_04(self):
+
+        NUM = 0
+        t2_cancel = False
+        t2 = None
+
+        async def foo1():
+            await asyncio.sleep(0.1)
+            1 / 0
+
+        async def foo2():
+            nonlocal NUM, t2_cancel
+            try:
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                t2_cancel = True
+                raise
+            NUM += 1
+
+        async def runner():
+            nonlocal NUM, t2
+
+            g = await supervisor.Supervisor.create()
+
+            g.create_task(foo1())
+            t2 = g.create_task(foo2())
+
+            await g.wait()
+
+            NUM += 10
+
+        with self.assertRaisesRegex(taskgroup.TaskGroupError,
+                                    r'1 sub errors: \(ZeroDivisionError\)'):
+            await self.loop.create_task(runner())
+
+        self.assertEqual(NUM, 0)
+        self.assertTrue(t2_cancel)
+        self.assertTrue(t2.cancelled())
+
+    async def test_supervisor_05(self):
+
+        NUM = 0
+
+        async def foo():
+            nonlocal NUM
+            try:
+                await asyncio.sleep(5)
+            except asyncio.CancelledError:
+                NUM += 1
+                raise
+
+        async def runner():
+            g = await supervisor.Supervisor.create()
+
+            for _ in range(5):
+                g.create_task(foo())
+
+            await g.wait()
+
+        r = self.loop.create_task(runner())
+        await asyncio.sleep(0.1)
+
+        self.assertFalse(r.done())
+        r.cancel()
+        with self.assertRaises(asyncio.CancelledError):
+            await r
+
+        self.assertEqual(NUM, 5)
+
+    async def test_supervisor_06(self):
+
+        async def foo1():
+            await asyncio.sleep(1)
+            return 42
+
+        async def foo2():
+            await asyncio.sleep(2)
+            return 11
+
+        async def runner():
+            g = await supervisor.Supervisor.create()
+
+            g.create_task(foo1())
+            g.create_task(foo2())
+
+            await g.wait()
+
+        r = self.loop.create_task(runner())
+        await asyncio.sleep(0.05)
+        r.cancel()
+
+        with self.assertRaises(asyncio.CancelledError):
+            await r
+
+    async def test_supervisor_07(self):
+        NUM = 0
+
+        async def foo1():
+            nonlocal NUM
+            NUM += 1
+            try:
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                NUM += 10
+                await asyncio.sleep(10)
+                NUM += 1000
+                raise
+            return 42
+
+        async def foo2():
+            nonlocal NUM
+            NUM += 1
+            await asyncio.sleep(2)
+            NUM += 1000
+            return 11
+
+        async def runner():
+            g = await supervisor.Supervisor.create()
+
+            g.create_task(foo1())
+            g.create_task(foo2())
+
+            await asyncio.sleep(0.1)
+
+            await g.cancel()
+
+        r = self.loop.create_task(runner())
+        await asyncio.sleep(0.5)
+        r.cancel()
+
+        with self.assertRaises(asyncio.CancelledError):
+            await r
+
+        self.assertEqual(NUM, 12)
+
+    async def test_supervisor_08(self):
+        NUM = 0
+
+        async def foo1():
+            nonlocal NUM
+            NUM += 1
+            await asyncio.sleep(1)
+            NUM += 1000
+            return 42
+
+        async def foo2():
+            nonlocal NUM
+            NUM += 1
+            await asyncio.sleep(2)
+            NUM += 1000
+            return 11
+
+        async def runner():
+            g = await supervisor.Supervisor.create()
+
+            g.create_task(foo1())
+            g.create_task(foo2())
+
+            await asyncio.sleep(0.1)
+
+            await g.cancel()
+
+        await runner()
+        self.assertEqual(NUM, 2)
+
+    async def test_supervisor_09(self):
+        NUM = 0
+
+        async def foo1():
+            nonlocal NUM
+            NUM += 1
+            try:
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                await asyncio.sleep(0.2)
+                NUM += 10
+                raise
+            NUM += 1000
+            return 42
+
+        async def foo2():
+            nonlocal NUM
+            NUM += 1
+            await asyncio.sleep(2)
+            NUM += 1000
+            return 11
+
+        async def runner():
+            g = await supervisor.Supervisor.create()
+
+            g.create_task(foo1())
+            g.create_task(foo2())
+
+            await asyncio.sleep(0.1)
+
+            await g.cancel()
+
+        await runner()
+        self.assertEqual(NUM, 12)
