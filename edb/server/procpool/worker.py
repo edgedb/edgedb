0ed3581c2e618803373ba8a0aeb6a4b612b31a2f
Yury Selivanov <yury@magic.io>
2019-01-18 20:32:29 -0500

    Restore error printing code in procpool/worker

--- edb/server/procpool/worker.py
+++ edb/server/procpool/worker.py
@@ -27,7 +27,9 @@ import traceback
 
 import uvloop
 
+from edb.common import debug
 from edb.common import devmode
+from edb.common import markup
 
 from . import amsg
 
@@ -64,6 +66,8 @@ async def worker(cls, cls_args, sockname):
                     res = await meth(*args)
                     data = (0, res)
                 except Exception as ex:
+                    if debug.flags.server:
+                        markup.dump(ex)
                     ex_str = str(ex)
                     data = (1, ex, traceback.format_exc())
 
