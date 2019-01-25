edgedb/edgedb

commit 89f49bd3d7d0bb0bbd8dd800bc4e402073c7abef

Fix encoding of nested array sets
A nested Set of an array type is now encoded as an array where every
element is wrapped into a 1-element record.  This is needed to sidestep
the fact that array_agg cannot aggregate arrays of different
size.

edb/pgsql/compiler/relgen.py
@@ -359,10 +359,17 @@ def set_to_array(
         pathctx.put_path_serialized_var(
             result, ir_set.path_id, val, force=True, env=ctx.env)
 
+    pg_type = output.get_pg_type(ir_set.typeref, ctx=ctx)
+
     if ir_set.path_id.is_objtype_path():
         val = output.prepare_tuple_for_aggregation(val, env=ctx.env)
-
-    pg_type = output.get_pg_type(ir_set.typeref, ctx=ctx)
+    elif (ir_set.path_id.is_array_path()
+            and ctx.env.output_format != context.OutputFormat.JSON):
+        # We cannot aggregate arrays straight away, as
+        # they be of different length, so we have to
+        # encase each element into a record.
+        val = pgast.RowExpr(args=[val], ser_safe=val.ser_safe)
+        pg_type = ('record',)
 
     array_agg = pgast.FuncCall(
         name=('array_agg',),

commit 9fbb99c5ef0cb0758d2b57e27654d715048df57f

Fix regression in encoding of nested array sets
This is a regression from `89f49bd3d7d` which broke NULL filtering.

edb/pgsql/compiler/relgen.py
@@ -360,6 +360,7 @@ def set_to_array(
             result, ir_set.path_id, val, force=True, env=ctx.env)
 
     pg_type = output.get_pg_type(ir_set.typeref, ctx=ctx)
+    orig_val = val
 
     if ir_set.path_id.is_objtype_path():
         val = output.prepare_tuple_for_aggregation(val, env=ctx.env)
@@ -375,8 +376,9 @@ def set_to_array(
         name=('array_agg',),
         args=[val],
         agg_filter=(
-            astutils.new_binop(val, pgast.NullConstant(), 'IS DISTINCT FROM')
-            if val.nullable else None
+            astutils.new_binop(orig_val, pgast.NullConstant(),
+                               'IS DISTINCT FROM')
+            if orig_val.nullable else None
         ),
         ser_safe=val.ser_safe,
     )

tests/test_edgeql_linkatoms.py
@@ -1258,7 +1258,6 @@ class TestEdgeQLLinkToScalarTypes(tb.QueryTestCase):
             }
         )
 
-    @test.xfail('does not work with binary serialization')
     async def test_edgeql_links_derived_array_07(self):
         await self.assert_query_result(
             r'''
@@ -1297,7 +1296,6 @@ class TestEdgeQLLinkToScalarTypes(tb.QueryTestCase):
             }
         )
 
-    @test.xfail('does not work with binary serialization')
     async def test_edgeql_links_derived_array_08(self):
         await self.assert_query_result(
             r'''