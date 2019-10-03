f1c0da949d4913e1a90575c7f16865d983fe3ae2
Elvis Pranskevichus <elvis@edgedb.com>
2019-10-02 21:25:32 -0400

    Actually use the HEAD version of edgedb-python in CI

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -2,13 +2,14 @@
 
 set -e -x
 
+pip install --quiet -U setuptools wheel pip
+pip download --dest=/tmp/deps .[test,docs]
+pip install -U --no-index --find-links=/tmp/deps /tmp/deps/*
+
 git clone https://github.com/edgedb/edgedb-python.git edgedb
 cd edgedb
 git submodule update --init --depth 50
 pip install --verbose -e .
 cd ../
 
-pip install --quiet -U setuptools wheel pip
-pip download --dest=/tmp/deps .[test,docs]
-pip install -U --no-index --find-links=/tmp/deps /tmp/deps/*
-pip install --verbose -e .
+pip install --no-deps --verbose -e .
