d531dd25eef2eda223ffa40c32476cb9b62ad5d4
Łukasz Langa <lukasz@langa.pl>
2019-10-23 18:09:47 +0200

    Add mypy to dev dependencies

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ EXTRA_DEPS = {
     'test': [
         'black~=19.3b0',
         'flake8~=3.7.5',
+        'mypy~=0.740',
         'pycodestyle~=2.5.0',
         'coverage~=4.5.2',
         'requests-xml~=0.2.3',
