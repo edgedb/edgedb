e7dd9cd2fd609f64b2bfe7c6873468f9f766468b
Elvis Pranskevichus <elvis@edgedb.com>
2019-11-04 14:08:31 -0500

    Unbreak Travis CI
    There is a transitive dependency on the wheel package through
    psutil, but it is specified in pyproject.toml there, and
    `pip download` DOES NOT pick it up, but fails on `pip install`.

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -4,6 +4,10 @@ set -e -x
 
 pip install --quiet -U setuptools wheel pip
 pip download --dest=/tmp/deps .[test,docs]
+# There is a transitive dependency on wheel through psutil,
+# but it is specified in pyproject.toml, and `pip download`
+# DOES NOT pick it up, but fails on `pip install` nonetheless.
+pip download --dest=/tmp/deps wheel
 pip install -U --no-index --find-links=/tmp/deps /tmp/deps/*
 
 git clone https://github.com/edgedb/edgedb-python.git edgedb
