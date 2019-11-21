sthagen/edgedb-edgedb

Implement std::IN as a derivative of std::=
The membership operators, `std::IN` and `std::NOT IN` must be applicable
to the same domain as `std::=`, since `[NOT] IN` is basically an
element-wise application of `=`.  Blindly copying all the `=`
definitions would be tedious now and in the future, so introduce a
mechanism to declare an operator as a "derivative" of another operator,
which would tell the compiler to use the origin operator when doing
call resolution.  For simplicity, derivative operators cannot be
polymorphic.

Also fixes an issue with tuple inputs to `IN`, where the right operand was
erroneously passed as a TupleVar instead of a value.

tests/test_edgeql_ddl.py
@@ -1332,6 +1332,49 @@ class TestEdgeQLDDL(tb.DDLTestCase):
                 };
             ''')
 
+    async def test_edgeql_ddl_operator_08(self):
+        with self.assertRaisesRegex(
+                edgedb.DuplicateOperatorDefinitionError,
+                r'cannot create the '
+                r'`test::IN\(l: std::int64, r: std::int64\)` operator: '
+                r'there exists a derivative operator of the same name'):
+            # create 2 operators in test: derivative first, then a
+            # non-derivative one
+            await self.con.execute('''
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::float64, r: std::float64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                    SET derivative_of := 'std::=';
+                };
+
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::int64, r: std::int64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                };
+            ''')
+
+    async def test_edgeql_ddl_operator_09(self):
+        with self.assertRaisesRegex(
+                edgedb.DuplicateOperatorDefinitionError,
+                r'cannot create '
+                r'`test::IN\(l: std::int64, r: std::int64\)` as a '
+                r'derivative operator: there already exists an operator '
+                r'of the same name'):
+            # create 2 operators in test: non-derivative first, then a
+            # derivative one
+            await self.con.execute('''
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::float64, r: std::float64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                };
+
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::int64, r: std::int64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                    SET derivative_of := 'std::=';
+                };
+            ''')
+
     async def test_edgeql_ddl_cast_01(self):
         await self.con.execute('''
             CREATE SCALAR TYPE test::type_a EXTENDING std::str;

edgedb/edgedb

tests/test_edgeql_ddl.py
@@ -1787,6 +1787,49 @@ class TestEdgeQLDDL(tb.DDLTestCase):
                 };
             ''')
 
+    async def test_edgeql_ddl_operator_10(self):
+        with self.assertRaisesRegex(
+                edgedb.DuplicateOperatorDefinitionError,
+                r'cannot create the '
+                r'`test::IN\(l: std::int64, r: std::int64\)` operator: '
+                r'there exists a derivative operator of the same name'):
+            # create 2 operators in test: derivative first, then a
+            # non-derivative one
+            await self.con.execute('''
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::float64, r: std::float64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                    SET derivative_of := 'std::=';
+                };
+
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::int64, r: std::int64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                };
+            ''')
+
+    async def test_edgeql_ddl_operator_11(self):
+        with self.assertRaisesRegex(
+                edgedb.DuplicateOperatorDefinitionError,
+                r'cannot create '
+                r'`test::IN\(l: std::int64, r: std::int64\)` as a '
+                r'derivative operator: there already exists an operator '
+                r'of the same name'):
+            # create 2 operators in test: non-derivative first, then a
+            # derivative one
+            await self.con.execute('''
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::float64, r: std::float64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                };
+
+                CREATE INFIX OPERATOR
+                test::`IN` (l: std::int64, r: std::int64) -> std::bool {
+                    FROM SQL EXPRESSION;
+                    SET derivative_of := 'std::=';
+                };
+            ''')
+
     async def test_edgeql_ddl_cast_01(self):
         await self.con.execute('''
             CREATE SCALAR TYPE test::type_a EXTENDING std::str;