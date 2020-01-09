b17f57b7a72ee0091edafa545729b837e317259e
Yury Selivanov <yury@edgedb.com>
2020-01-08 17:43:13 -0800

    Unbreak coverage tests

--- tests/test_type_coverage.py
+++ tests/test_type_coverage.py
@@ -197,7 +197,7 @@ class TypeCoverageTests(unittest.TestCase):
         self.assertFunctionCoverage(EDB_DIR / "cli", 40.62)
 
     def test_cqa_type_coverage_common(self) -> None:
-        self.assertFunctionCoverage(EDB_DIR / "common", 27.36)
+        self.assertFunctionCoverage(EDB_DIR / "common", 27.22)
 
     def test_cqa_type_coverage_common_ast(self) -> None:
         self.assertFunctionCoverage(EDB_DIR / "common" / "ast", 7.58)
