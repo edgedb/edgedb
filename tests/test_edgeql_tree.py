a22e5188434b4d181c29edb2a653a8c133cd1523
Victor Petrovykh <victor@edgedb.com>
2020-01-16 04:48:24 -0500

    tests: Add UPDATE tests for tree structures.
    Add various UPDATE tests for trees:
    - tests that update node values based on parent or children nodes
    - tests that move tree nodes around

--- tests/test_edgeql_tree.py
+++ tests/test_edgeql_tree.py
@@ -259,9 +259,9 @@ class TestTree(tb.QueryTestCase):
                             val,
                             children: {
                                 val,
-                            },
-                        },
-                    },
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
                 }
                 FILTER NOT EXISTS .parent
                 ORDER BY .val;
@@ -308,9 +308,9 @@ class TestTree(tb.QueryTestCase):
                             val,
                             children: {
                                 val,
-                            },
-                        },
-                    },
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
                 }
                 FILTER NOT EXISTS .parent
                 ORDER BY .val;
@@ -443,4 +443,538 @@ class TestTree(tb.QueryTestCase):
                 ORDER BY .val;
             """,
             [{'val': '0'}],
+        )
+
+    async def test_edgeql_tree_update_01(self):
+        # Update all the tree nodes to base their val on the children
+        # vals.
+        await self.con.execute(
+            r"""
+                WITH MODULE test
+                UPDATE Tree
+                SET {
+                    val := to_str(
+                        [.val, 'c'] ++ array_agg((
+                            SELECT _ := .children.val
+                            ORDER BY _
+                        )),
+                        '_'
+                    )
+                }
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Tree {
+                    val,
+                    children: {
+                        val,
+                        children: {
+                            val,
+                            children: {
+                                val,
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
+                }
+                FILTER NOT EXISTS .parent
+                ORDER BY .val;
+            """,
+            [
+                {
+                    'val': '0_c_00_01_02',
+                    'children': [
+                        {
+                            'val': '00_c_000',
+                            'children': [{'val': '000_c', 'children': []}]
+                        },
+                        {
+                            'val': '01_c_010',
+                            'children': [{'val': '010_c', 'children': []}]
+                        },
+                        {
+                            'val': '02_c',
+                            'children': []
+                        },
+                    ],
+                },
+                {
+                    'val': '1_c_10_11_12_13',
+                    'children': [
+                        {'val': '10_c', 'children': []},
+                        {'val': '11_c', 'children': []},
+                        {'val': '12_c', 'children': []},
+                        {'val': '13_c', 'children': []},
+                    ],
+                },
+            ],
+        )
+
+    async def test_edgeql_tree_update_02(self):
+        # Update all the tree nodes to base their val on the children
+        # vals.
+        await self.con.execute(
+            r"""
+                WITH MODULE test
+                UPDATE Eert
+                SET {
+                    val := to_str(
+                        [.val, 'c'] ++ array_agg((
+                            SELECT _ := .children.val
+                            ORDER BY _
+                        )),
+                        '_'
+                    )
+                }
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Eert {
+                    val,
+                    children: {
+                        val,
+                        children: {
+                            val,
+                            children: {
+                                val,
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
+                }
+                FILTER NOT EXISTS .parent
+                ORDER BY .val;
+            """,
+            [
+                {
+                    'val': '0_c_00_01_02',
+                    'children': [
+                        {
+                            'val': '00_c_000',
+                            'children': [{'val': '000_c', 'children': []}]
+                        },
+                        {
+                            'val': '01_c_010',
+                            'children': [{'val': '010_c', 'children': []}]
+                        },
+                        {
+                            'val': '02_c',
+                            'children': []
+                        },
+                    ],
+                },
+                {
+                    'val': '1_c_10_11_12_13',
+                    'children': [
+                        {'val': '10_c', 'children': []},
+                        {'val': '11_c', 'children': []},
+                        {'val': '12_c', 'children': []},
+                        {'val': '13_c', 'children': []},
+                    ],
+                },
+            ],
+        )
+
+    @test.xfail('''
+        This test fails with the following error:
+
+        edgedb.errors.MissingRequiredError:
+        missing value for required property test::Tree.val
+    ''')
+    async def test_edgeql_tree_update_03(self):
+        # Update all the tree nodes to base their val on the parent
+        # val.
+        await self.con.execute(
+            r"""
+                WITH MODULE test
+                UPDATE Tree
+                SET {
+                    val := .val ++ '_p' ++ (('_' ++ .parent.val) ?? '')
+                };
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Tree {val}
+                ORDER BY .val;
+            """,
+            [
+                {'val': '000_p_00'},
+                {'val': '00_p_0'},
+                {'val': '010_p_01'},
+                {'val': '01_p_0'},
+                {'val': '02_p_0'},
+                {'val': '0_p'},
+                {'val': '10_p_1'},
+                {'val': '11_p_1'},
+                {'val': '12_p_1'},
+                {'val': '13_p_1'},
+                {'val': '1_p'},
+            ]
+        )
+
+    @test.xfail('''
+        This test fails with the following error:
+
+        edgedb.errors.QueryError: possibly more than one element
+        returned by an expression where only singletons are allowed
+
+        Seems to be related to #1098.
+    ''')
+    async def test_edgeql_tree_update_04(self):
+        # Update all the tree nodes to base their val on the parent
+        # val.
+        await self.con.execute(
+            r"""
+                WITH MODULE test
+                UPDATE Eert
+                SET {
+                    val := .val ++ '_p' ++ (('_' ++ .parent.val) ?? '')
+                };
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Eert {val}
+                ORDER BY .val;
+            """,
+            [
+                {'val': '000_p_00'},
+                {'val': '00_p_0'},
+                {'val': '010_p_01'},
+                {'val': '01_p_0'},
+                {'val': '02_p_0'},
+                {'val': '0_p'},
+                {'val': '10_p_1'},
+                {'val': '11_p_1'},
+                {'val': '12_p_1'},
+                {'val': '13_p_1'},
+                {'val': '1_p'},
+            ]
+        )
+
+    async def test_edgeql_tree_update_05(self):
+        # Swap around a tree node and its first child as an atomic operation.
+        #
+        # The same basic principle might be used to swap around 2
+        # nodes in a linked-list, but we'll use trees since we have
+        # them already.
+        await self.con.execute(
+            r"""
+                WITH
+                    MODULE test,
+                    # start with node '00'
+                    T00 := (
+                        SELECT Tree
+                        FILTER .val = '00'
+                    ),
+                    # update its first child node ('000')
+                    TC := (
+                        UPDATE (SELECT T00.children
+                        ORDER BY .val
+                        LIMIT 1)
+                        SET {parent := T00.parent}
+                    ),
+                UPDATE T00
+                SET {parent := TC};
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Tree {
+                    val,
+                    children: {
+                        val,
+                        children: {
+                            val,
+                            children: {
+                                val,
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
+                }
+                FILTER .val = '0'
+                ORDER BY .val;
+            """,
+            [
+                {
+                    'val': '0',
+                    'children': [
+                        {
+                            'val': '000',
+                            'children': [{'val': '00', 'children': []}]
+                        },
+                        {
+                            'val': '01',
+                            'children': [{'val': '010', 'children': []}]
+                        },
+                        {
+                            'val': '02',
+                            'children': []
+                        },
+                    ],
+                },
+            ],
+        )
+
+    @test.xfail('''
+        This test fails with the following error:
+
+        InternalServerError: ON CONFLICT DO UPDATE command cannot
+        affect row a second time
+    ''')
+    async def test_edgeql_tree_update_06(self):
+        # Swap around a tree node and its first child as an atomic operation.
+        #
+        # The same basic principle might be used to swap around 2
+        # nodes in a linked-list, but we'll use trees since we have
+        # them already.
+        #
+        # With this particular structure swapping nodes is awkward
+        # because children lists for 3 nodes must be updated. The
+        # whole point of this exercise it to try and express this as a
+        # single command to make it atomic without needing to use
+        # transactions.
+        await self.con.execute(
+            r"""
+                WITH
+                    MODULE test,
+                    # start with node '00'
+                    T00 := (
+                        SELECT Eert
+                        FILTER .val = '00'
+                    ),
+                    # grab the parent of '00'
+                    TP := T00.parent,
+                    # update its first child node ('000')
+                    TC := (
+                        UPDATE (
+                            SELECT T00.children
+                            ORDER BY .val
+                            LIMIT 1
+                        )
+                        SET {
+                            children := {.children, T00}
+                        }
+                    ),
+                    T00_up := (
+                        UPDATE T00
+                        SET {
+                            children := (
+                                SELECT _ := T00.children
+                                FILTER _ != TC
+                            )
+                        }
+                    ),
+                # update the original parent of '00'
+                UPDATE TP
+                SET {
+                    children := (
+                        SELECT _ := {.children, T00_up}
+                        FILTER _ != T00
+                    )
+                }
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Eert {
+                    val,
+                    children: {
+                        val,
+                        children: {
+                            val,
+                            children: {
+                                val,
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
+                }
+                FILTER .val = '0'
+                ORDER BY .val;
+            """,
+            [
+                {
+                    'val': '0',
+                    'children': [
+                        {
+                            'val': '000',
+                            'children': [{'val': '00', 'children': []}]
+                        },
+                        {
+                            'val': '01',
+                            'children': [{'val': '010', 'children': []}]
+                        },
+                        {
+                            'val': '02',
+                            'children': []
+                        },
+                    ],
+                },
+            ],
+        )
+
+    async def test_edgeql_tree_update_07(self):
+        # Swap around a tree node and its parent as an atomic operation.
+        #
+        # The same basic principle might be used to swap around 2
+        # nodes in a linked-list, but we'll use trees since we have
+        # them already.
+        await self.con.execute(
+            r"""
+                WITH
+                    MODULE test,
+                    # start with node '000', get its parent
+                    TP := (
+                        SELECT Tree
+                        FILTER .val = '000'
+                    ).parent,
+                    # move the '000' node
+                    T000 := (
+                        UPDATE Tree
+                        FILTER .val = '000'
+                        SET {parent := .parent.parent}
+                    )
+                UPDATE TP
+                SET {parent := T000};
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Tree {
+                    val,
+                    children: {
+                        val,
+                        children: {
+                            val,
+                            children: {
+                                val,
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
+                }
+                FILTER .val = '0'
+                ORDER BY .val;
+            """,
+            [
+                {
+                    'val': '0',
+                    'children': [
+                        {
+                            'val': '000',
+                            'children': [{'val': '00', 'children': []}]
+                        },
+                        {
+                            'val': '01',
+                            'children': [{'val': '010', 'children': []}]
+                        },
+                        {
+                            'val': '02',
+                            'children': []
+                        },
+                    ],
+                },
+            ],
+        )
+
+    async def test_edgeql_tree_update_08(self):
+        # Swap around a tree node and its parent as an atomic operation.
+        #
+        # The same basic principle might be used to swap around 2
+        # nodes in a linked-list, but we'll use trees since we have
+        # them already.
+        #
+        # With this particular structure swapping nodes is awkward
+        # because children lists for 3 nodes must be updated. The
+        # whole point of this exercise it to try and express this as a
+        # single command to make it atomic without needing to use
+        # transactions.
+        await self.con.execute(
+            r"""
+                WITH
+                    MODULE test,
+                    # start with node '000'
+                    T000 := (
+                        SELECT Eert
+                        FILTER .val = '000'
+                    ),
+                    # update the parent of '000'
+                    TP := (
+                        UPDATE (SELECT T000.parent)
+                        SET {
+                            children := (
+                                SELECT _ := .children
+                                FILTER _ != T000
+                            )
+                        }
+                    ),
+                    # update the grand-parent of '000'
+                    TPP := (
+                        UPDATE (SELECT TP.parent)
+                        SET {
+                            children := (
+                                SELECT _ := {.children, T000}
+                                FILTER _ != TP
+                            )
+                        }
+                    )
+                # update node '000'
+                UPDATE (SELECT _ := TPP.children FILTER _ = T000)
+                SET {
+                    children := {.children, TP}
+                };
+            """
+        )
+
+        await self.assert_query_result(
+            r"""
+                WITH MODULE test
+                SELECT Eert {
+                    val,
+                    children: {
+                        val,
+                        children: {
+                            val,
+                            children: {
+                                val,
+                            } ORDER BY .val,
+                        } ORDER BY .val,
+                    } ORDER BY .val,
+                }
+                FILTER .val = '0'
+                ORDER BY .val;
+            """,
+            [
+                {
+                    'val': '0',
+                    'children': [
+                        {
+                            'val': '000',
+                            'children': [{'val': '00', 'children': []}]
+                        },
+                        {
+                            'val': '01',
+                            'children': [{'val': '010', 'children': []}]
+                        },
+                        {
+                            'val': '02',
+                            'children': []
+                        },
+                    ],
+                },
+            ],
         )
