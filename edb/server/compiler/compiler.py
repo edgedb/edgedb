edgedb/edgedb

commit a81203a6892303c8097b82437cbeec8916833c91

Treat databases as proper (global) schema objects
We already have the necessary infrastructure for global objects that is
used by Roles, so make Database objects benefit from that as well.

There is a small caveat: `CREATE DATABASE` and `DROP DATABASE` are
non-transactional, so add facilities to ensure that these statements
don't get wrapped into a transaction.

edb/server/compiler/compiler.py
@@ -509,15 +509,25 @@ class Compiler(BaseCompiler):
             new_types = frozenset(str(tid) for tid in plan.new_types)
 
         plan.generate(block)
-        sql = block.to_string().encode('utf-8')
+        is_transactional = block.is_transactional()
+        if not is_transactional:
+            sql = tuple(stmt.encode('utf-8')
+                        for stmt in block.get_statements())
+        else:
+            sql = (block.to_string().encode('utf-8'),)
 
         current_tx.update_schema(schema)
 
         if debug.flags.delta_execute:
             debug.header('Delta Script')
-            debug.dump_code(sql, lexer='sql')
+            debug.dump_code('\n'.join(sql), lexer='sql')
 
-        return dbstate.DDLQuery(sql=(sql,), new_types=new_types)
+        return dbstate.DDLQuery(
+            sql=sql,
+            is_transactional=is_transactional,
+            single_unit=not is_transactional,
+            new_types=new_types,
+        )
 
     def _compile_command(
             self, ctx: CompileContext, cmd) -> dbstate.BaseQuery:
@@ -914,8 +924,7 @@ class Compiler(BaseCompiler):
             comp: dbstate.BaseQuery = self._compile_dispatch_ql(ctx, stmt)
 
             if unit is not None:
-                if (isinstance(comp, dbstate.TxControlQuery) and
-                        comp.single_unit):
+                if comp.single_unit:
                     units.append(unit)
                     unit = None
 
@@ -927,6 +936,15 @@ class Compiler(BaseCompiler):
                     cardinality=default_cardinality)
             else:
                 unit.status = status.get_status(stmt)
+
+            if not comp.is_transactional:
+                if not comp.single_unit:
+                    raise errors.InternalServerError(
+                        'non-transactional compilation units must '
+                        'be single-unit'
+                    )
+
+                unit.is_transactional = False
 
             if isinstance(comp, dbstate.Query):
                 if single_stmt_mode:

edb/pgsql/datasources/schema/databases.py
@@ -19,16 +19,23 @@
 
 from __future__ import annotations
 
-from . import annos  # NOQA
-from . import casts  # NOQA
-from . import constraints  # NOQA
-from . import databases  # NOQA
-from . import functions  # NOQA
-from . import indexes  # NOQA
-from . import links  # NOQA
-from . import modules  # NOQA
-from . import objtypes  # NOQA
-from . import operators  # NOQA
-from . import roles  # NOQA
-from . import scalars  # NOQA
-from . import types  # NOQA
+import asyncpg
+from typing import *  # NoQA
+
+
+async def fetch(
+        conn: asyncpg.connection.Connection) -> List[asyncpg.Record]:
+    return await conn.fetch("""
+        SELECT
+            ((d.description)->>'id')::uuid              AS id,
+            datname                                     AS name
+        FROM
+            pg_database dat
+            CROSS JOIN LATERAL (
+                SELECT
+                    edgedb.shobj_metadata(dat.oid, 'pg_database')
+                        AS description
+            ) AS d
+        WHERE
+            (d.description)->>'id' IS NOT NULL
+    """)

edb/pgsql/dbops/base.py
@@ -67,6 +67,7 @@ class SQLBlock:
     def __init__(self):
         self.commands = []
         self.disable_ddl_triggers = True
+        self._transactional = True
 
     def add_block(self):
         block = PLTopBlock()
@@ -74,8 +75,11 @@ class SQLBlock:
         return block
 
     def to_string(self) -> str:
-        stmts = ((cmd if isinstance(cmd, str) else cmd.to_string()).rstrip()
-                 for cmd in self.commands)
+        if not self._transactional:
+            raise ValueError(
+                'block is non-transactional, please use .get_statements()'
+            )
+        stmts = self.get_statements()
         body = '\n\n'.join(stmt + ';' if stmt[-1] != ';' else stmt
                            for stmt in stmts).rstrip()
         if body and body[-1] != ';':
@@ -83,14 +87,26 @@ class SQLBlock:
 
         return body
 
+    def get_statements(self) -> List[str]:
+        return [(cmd if isinstance(cmd, str) else cmd.to_string()).rstrip()
+                for cmd in self.commands]
+
     def add_command(self, stmt) -> None:
-        if isinstance(stmt, PLBlock) and not stmt.has_declarations():
+        if (isinstance(stmt, PLBlock)
+                and not stmt.has_declarations()
+                and not isinstance(stmt, PLTopBlock)):
             self.commands.extend(stmt.commands)
         else:
             self.commands.append(stmt)
 
     def has_declarations(self) -> bool:
         return False
+
+    def set_non_transactional(self) -> None:
+        self._transactional = False
+
+    def is_transactional(self) -> bool:
+        return self._transactional
 
 
 class PLBlock(SQLBlock):

edb/pgsql/dbops/databases.py
@@ -40,6 +40,7 @@ class Database(base.DBObject):
         lc_collate: Optional[str] = None,
         lc_ctype: Optional[str] = None,
         template: Optional[str] = 'edgedb0',
+        metadata: Optional[Mapping[str, Any]] = None,
     ) -> None:
         super().__init__()
         self.name = name
@@ -49,6 +50,7 @@ class Database(base.DBObject):
         self.lc_collate = lc_collate
         self.lc_ctype = lc_ctype
         self.template = template
+        self.metadata = metadata
 
     def get_type(self):
         return 'DATABASE'
@@ -89,7 +91,7 @@ class DatabaseExists(base.Condition):
         ''')
 
 
-class CreateDatabase(ddl.CreateObject):
+class CreateDatabase(ddl.CreateObject, ddl.NonTransactionalDDLOperation):
     def __init__(
             self, db, *, conditions=None, neg_conditions=None, priority=0):
         super().__init__(
@@ -116,7 +118,8 @@ class CreateDatabase(ddl.CreateObject):
         return (f'CREATE DATABASE {self.object.get_id()} {extra}')
 
 
-class DropDatabase(ddl.SchemaObjectOperation):
+class DropDatabase(ddl.SchemaObjectOperation,
+                   ddl.NonTransactionalDDLOperation):
 
     def code(self, block: base.PLBlock) -> str:
         return f'DROP DATABASE {qi(self.name)}'

edb/pgsql/dbops/ddl.py
@@ -18,6 +18,7 @@
 
 
 from __future__ import annotations
+from typing import *  # NoQA
 
 import json
 import textwrap
@@ -95,6 +96,17 @@ class DDLOperation(base.Command):
                 trigger.generate_after(block, self)
 
 
+class NonTransactionalDDLOperation(DDLOperation):
+    def generate_self_block(
+        self,
+        block: base.PLBlock,
+    ) -> Optional[base.PLBlock]:
+        block.add_command(self.code(block))
+        block.set_non_transactional()
+        self_block = block.add_block()
+        return self_block
+
+
 class SchemaObjectOperation(DDLOperation):
     def __init__(
             self, name, *, conditions=None, neg_conditions=None, priority=0):
@@ -156,11 +168,11 @@ class GetMetadata(base.Command):
         is_shared = self.object.is_shared()
         if isinstance(oid, base.Query):
             qry = oid.text
-            objoid = block.declare_var('oid')
             classoid = block.declare_var('oid')
+            objoid = block.declare_var('oid')
             objsubid = block.declare_var('oid')
             block.add_command(
-                qry + f' INTO {objoid}, {classoid}, {objsubid}')
+                qry + f' INTO {classoid}, {objoid}, {objsubid}')
         else:
             objoid, classoid, objsubid = oid
 

edb/pgsql/delta.py
@@ -3647,14 +3647,23 @@ class DeleteModule(ModuleMetaCommand, adapts=s_mod.DeleteModule):
 
 class CreateDatabase(ObjectMetaCommand, adapts=s_db.CreateDatabase):
     def apply(self, schema, context):
-        schema, _ = s_db.CreateDatabase.apply(self, schema, context)
-        self.pgops.add(dbops.CreateDatabase(dbops.Database(self.classname)))
+        schema, db = s_db.CreateDatabase.apply(self, schema, context)
+        self.pgops.add(
+            dbops.CreateDatabase(
+                dbops.Database(
+                    self.classname,
+                    metadata=dict(
+                        id=str(db.id),
+                    ),
+                )
+            )
+        )
         return schema, None
 
 
 class DropDatabase(ObjectMetaCommand, adapts=s_db.DropDatabase):
     def apply(self, schema, context):
-        schema, _ = s_db.CreateDatabase.apply(self, schema, context)
+        schema, _ = s_db.DropDatabase.apply(self, schema, context)
         self.pgops.add(dbops.DropDatabase(self.classname))
         return schema, None
 

edb/pgsql/intromech.py
@@ -33,6 +33,7 @@ from edb import schema as so
 from edb.schema import abc as s_abc
 from edb.schema import annos as s_anno
 from edb.schema import casts as s_casts
+from edb.schema import database as s_db
 from edb.schema import scalars as s_scalars
 from edb.schema import objtypes as s_objtypes
 from edb.schema import constraints as s_constr
@@ -70,6 +71,8 @@ class IntrospectionMech:
             schema = so.Schema()
 
         schema = await self.read_roles(
+            schema)
+        schema = await self.read_databases(
             schema)
         schema = await self.read_modules(
             schema, only_modules=modules, exclude_modules=exclude_modules)
@@ -147,6 +150,18 @@ class IntrospectionMech:
                     schema,
                     'bases',
                     [schema.get_by_id(b) for b in bases])
+
+        return schema
+
+    async def read_databases(self, schema):
+        dbs = await datasources.schema.databases.fetch(self.connection)
+
+        for row in dbs:
+            schema, _ = s_db.Database.create_in_schema(
+                schema,
+                id=row['id'],
+                name=row['name'],
+            )
 
         return schema
 

edb/pgsql/metaschema.py
@@ -2396,17 +2396,19 @@ def _generate_database_view(schema):
 
     view_query = f'''
         SELECT
-            edgedb.uuid_generate_v5(
-                '{DATABASE_ID_NAMESPACE}'::uuid,
-                pg_database.oid::text)
-                            AS id,
+            ((d.description)->>'id')::uuid              AS id,
             datname         AS name,
             (SELECT id FROM edgedb.Object
                  WHERE name = 'sys::Database') AS __type__
         FROM
-            pg_database
+            pg_database dat
+            CROSS JOIN LATERAL (
+                SELECT
+                    edgedb.shobj_metadata(dat.oid, 'pg_database')
+                        AS description
+            ) AS d
         WHERE
-            datname NOT IN ('postgres', 'template0', 'template1')
+            (d.description)->>'id' IS NOT NULL
     '''
 
     return dbops.View(name=tabname(schema, Database), query=view_query)

commit 32e9df353ce5fd96a0b4aeff64dd09c7c4e8dc02

Fix bug of bootstrapping server with EDGEDB_DEBUG_DELTA_EXECUTE
This was broken by a81203a6892303c8097b82437cbeec8916833c91

edb/server/compiler/compiler.py
@@ -520,7 +520,7 @@ class Compiler(BaseCompiler):
 
         if debug.flags.delta_execute:
             debug.header('Delta Script')
-            debug.dump_code('\n'.join(sql), lexer='sql')
+            debug.dump_code(b'\n'.join(sql), lexer='sql')
 
         return dbstate.DDLQuery(
             sql=sql,