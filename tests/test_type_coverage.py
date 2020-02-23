7edc3b1014fcba5d970804a3a4a6dc72bdd36e78
Roberto Prevato <roberto.prevato@gmail.com>
2020-02-23 21:42:52 +0100

    Squashed commit of the following:
    commit 4c33bf71a4f0b7235b72517c46f2b24ac13af80e
    Author: Roberto Prevato <roberto.prevato@gmail.com>
    Date:   Sun Feb 23 21:39:43 2020 +0100
    
        corrections
    
    commit f2e669efcb9c31d4ea38a7c85d57e70e146a7246
    Author: Roberto Prevato <roberto.prevato@gmail.com>
    Date:   Sun Feb 23 21:30:15 2020 +0100
    
        annotations for constraints almost complete
    
    commit 37561ad129654f57f18ad6f1f3634bc1c6a61995
    Author: Roberto Prevato <roberto.prevato@gmail.com>
    Date:   Sun Feb 23 17:30:42 2020 +0100
    
        down to 6 errors
    
    commit 97f71d24cfc4dd3ee081ca2ea99541ad4e8e5b1f
    Author: Roberto Prevato <roberto.prevato@gmail.com>
    Date:   Sun Feb 23 16:52:52 2020 +0100
    
        twelve errors left
    
    commit 8c941f5740bb38f552d49731185dae7ea63c0ea8
    Author: Roberto Prevato <roberto.prevato@gmail.com>
    Date:   Sun Feb 23 11:32:40 2020 +0100
    
        assertions verified
    
    commit f47cf5df95db34741674b6da2ac6b0fe6b1ac335
    Author: Roberto Prevato <roberto.prevato@gmail.com>
    Date:   Sun Feb 23 10:56:07 2020 +0100
    
        work in progress annotating constraints.py

--- tests/test_type_coverage.py
+++ tests/test_type_coverage.py
@@ -245,7 +245,7 @@ class TypeCoverageTests(unittest.TestCase):
         self.assertFunctionCoverage(EDB_DIR / "repl", 100.0)
 
     def test_cqa_type_coverage_schema(self) -> None:
-        self.assertFunctionCoverage(EDB_DIR / "schema", 69.43)
+        self.assertFunctionCoverage(EDB_DIR / "schema", 71.99)
 
     def test_cqa_type_coverage_server(self) -> None:
         self.assertFunctionCoverage(EDB_DIR / "server", 14.23)
