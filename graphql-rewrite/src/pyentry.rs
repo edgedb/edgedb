938108c3535d68351890a62627bbef80c2303b6f
Paul Colomiets <paul@colomiets.name>
2020-03-20 18:21:42 +0200

    Decode strings in rust

--- graphql-rewrite/src/pyentry.rs
+++ graphql-rewrite/src/pyentry.rs
@@ -1,10 +1,11 @@
 use cpython::{Python, PyClone, ToPyObject, PythonObject, ObjectProtocol};
 use cpython::{PyString, PyResult, PyTuple, PyDict, PyList, PyObject};
 
+use crate::common::{unquote_string, unquote_block_string};
 use crate::position::Pos;
 use crate::pyerrors::{LexingError, SyntaxError, NotFoundError, AssertionError};
 use crate::entry_point::{Variable, Error};
-use crate::pytoken::{PyToken, PyTokenKind};
+use crate::pytoken::PyToken;
 use crate::entry_point;
 
 
@@ -20,22 +21,36 @@ py_class!(pub class Entry |py| {
         Ok(self._variables(py).clone_ref(py))
     }
     def tokens(&self, kinds: PyObject) -> PyResult<PyList> {
+        use crate::pytoken::PyTokenKind as K;
 
         let sof = kinds.get_item(py, "SOF")?;
         let eof = kinds.get_item(py, "EOF")?;
         let bang = kinds.get_item(py, "BANG")?;
+        let bang_v = "!".to_py_object(py).into_object();
         let dollar = kinds.get_item(py, "DOLLAR")?;
+        let dollar_v = "$".to_py_object(py).into_object();
         let paren_l = kinds.get_item(py, "PAREN_L")?;
+        let paren_l_v = "(".to_py_object(py).into_object();
         let paren_r = kinds.get_item(py, "PAREN_R")?;
+        let paren_r_v = ")".to_py_object(py).into_object();
         let spread = kinds.get_item(py, "SPREAD")?;
+        let spread_v = "...".to_py_object(py).into_object();
         let colon = kinds.get_item(py, "COLON")?;
+        let colon_v = ":".to_py_object(py).into_object();
         let equals = kinds.get_item(py, "EQUALS")?;
+        let equals_v = "=".to_py_object(py).into_object();
         let at = kinds.get_item(py, "AT")?;
+        let at_v = "@".to_py_object(py).into_object();
         let bracket_l = kinds.get_item(py, "BRACKET_L")?;
+        let bracket_l_v = "[".to_py_object(py).into_object();
         let bracket_r = kinds.get_item(py, "BRACKET_R")?;
+        let bracket_r_v = "]".to_py_object(py).into_object();
         let brace_l = kinds.get_item(py, "BRACE_L")?;
+        let brace_l_v = "{".to_py_object(py).into_object();
         let pipe = kinds.get_item(py, "PIPE")?;
+        let pipe_v = "|".to_py_object(py).into_object();
         let brace_r = kinds.get_item(py, "BRACE_R")?;
+        let brace_r_v = "}".to_py_object(py).into_object();
         let name = kinds.get_item(py, "NAME")?;
         let int = kinds.get_item(py, "INT")?;
         let float = kinds.get_item(py, "FLOAT")?;
@@ -53,29 +68,47 @@ py_class!(pub class Entry |py| {
             py.None(),
         ]).into_object());
         for el in tokens {
+            let (kind, value) = match el.kind {
+                K::Sof => (sof.clone_ref(py), py.None()),
+                K::Eof => (eof.clone_ref(py), py.None()),
+                K::Bang => (bang.clone_ref(py), bang_v.clone_ref(py)),
+                K::Dollar => (dollar.clone_ref(py), dollar_v.clone_ref(py)),
+                K::ParenL => (paren_l.clone_ref(py), paren_l_v.clone_ref(py)),
+                K::ParenR => (paren_r.clone_ref(py), paren_r_v.clone_ref(py)),
+                K::Spread => (spread.clone_ref(py), spread_v.clone_ref(py)),
+                K::Colon => (colon.clone_ref(py), colon_v.clone_ref(py)),
+                K::Equals => (equals.clone_ref(py), equals_v.clone_ref(py)),
+                K::At => (at.clone_ref(py), at_v.clone_ref(py)),
+                K::BracketL => (bracket_l.clone_ref(py),
+                                bracket_l_v.clone_ref(py)),
+                K::BracketR => (bracket_r.clone_ref(py),
+                                bracket_r_v.clone_ref(py)),
+                K::BraceL => (brace_l.clone_ref(py), brace_l_v.clone_ref(py)),
+                K::Pipe => (pipe.clone_ref(py), pipe_v.clone_ref(py)),
+                K::BraceR => (brace_r.clone_ref(py), brace_r_v.clone_ref(py)),
+                K::Name => (name.clone_ref(py),
+                            el.value.to_py_object(py).into_object()),
+                K::Int => (int.clone_ref(py),
+                           el.value.to_py_object(py).into_object()),
+                K::Float => (float.clone_ref(py),
+                           el.value.to_py_object(py).into_object()),
+                K::String => {
+                    // graphql-core 3 receives unescaped strings from the lexer
+                    let v = unquote_string(&el.value)
+                        .map_err(|e| LexingError::new(py, e.to_string()))?
+                        .to_py_object(py).into_object();
+                    (string.clone_ref(py), v)
+                }
+                K::BlockString => {
+                    // graphql-core 3 receives unescaped strings from the lexer
+                    let v = unquote_block_string(&el.value)
+                        .map_err(|e| LexingError::new(py, e.to_string()))?
+                        .to_py_object(py).into_object();
+                    (block_string.clone_ref(py), v)
+                }
+            };
             elems.push(PyTuple::new(py, &[
-                match el.kind {
-                    PyTokenKind::Sof => sof.clone_ref(py),
-                    PyTokenKind::Eof => eof.clone_ref(py),
-                    PyTokenKind::Bang => bang.clone_ref(py),
-                    PyTokenKind::Dollar => dollar.clone_ref(py),
-                    PyTokenKind::ParenL => paren_l.clone_ref(py),
-                    PyTokenKind::ParenR => paren_r.clone_ref(py),
-                    PyTokenKind::Spread => spread.clone_ref(py),
-                    PyTokenKind::Colon => colon.clone_ref(py),
-                    PyTokenKind::Equals => equals.clone_ref(py),
-                    PyTokenKind::At => at.clone_ref(py),
-                    PyTokenKind::BracketL => bracket_l.clone_ref(py),
-                    PyTokenKind::BracketR => bracket_r.clone_ref(py),
-                    PyTokenKind::BraceL => brace_l.clone_ref(py),
-                    PyTokenKind::Pipe => pipe.clone_ref(py),
-                    PyTokenKind::BraceR => brace_r.clone_ref(py),
-                    PyTokenKind::Name => name.clone_ref(py),
-                    PyTokenKind::Int => int.clone_ref(py),
-                    PyTokenKind::Float => float.clone_ref(py),
-                    PyTokenKind::String => string.clone_ref(py),
-                    PyTokenKind::BlockString => block_string.clone_ref(py),
-                },
+                kind,
                 el.position.map(|x| x.character)
                     .to_py_object(py).into_object(),
                 el.position.map(|x| x.character + el.value.chars().count())
@@ -84,7 +117,7 @@ py_class!(pub class Entry |py| {
                     .to_py_object(py).into_object(),
                 el.position.map(|x| x.column)
                     .to_py_object(py).into_object(),
-                el.value.to_py_object(py).into_object(),
+                value,
             ]).into_object());
         }
         let pos = self._end_pos(py);
