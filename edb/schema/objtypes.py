3c9ba631b41857e9c1923ba775571e7273116df4
Roberto Prevato <roberto.prevato@gmail.com>
2020-04-16 22:17:03 +0200

    corrects annotation

--- edb/schema/objtypes.py
+++ edb/schema/objtypes.py
@@ -257,7 +257,7 @@ def get_or_create_union_type(
         module=module,
     )
 
-    objtype = schema.get_by_id(type_id, None, type=so.Object)
+    objtype = schema.get_by_id(type_id, None, type=ObjectType)
     created = objtype is None
     if objtype is None:
         components = list(components)
@@ -283,7 +283,6 @@ def get_or_create_union_type(
                 modname=module,
             )
 
-    assert isinstance(objtype, ObjectType)
     return schema, objtype, created
 
 
