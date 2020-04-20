50a6d3b9731ab13d6a7054637696245f8d478b8e
Roberto Prevato <roberto.prevato@gmail.com>
2020-04-20 16:13:38 +0200

    Add annotations for schema indexes (#1349)

--- edb/schema/indexes.py
+++ edb/schema/indexes.py
@@ -35,6 +35,11 @@ from . import objects as so
 from . import referencing
 
 
+if TYPE_CHECKING:
+    from . import schema as s_schema
+    from . import types as s_types
+
+
 class Index(referencing.ReferencedInheritingObject, s_anno.AnnotationSubject):
 
     subject = so.SchemaField(so.Object)
@@ -48,15 +53,16 @@ class Index(referencing.ReferencedInheritingObject, s_anno.AnnotationSubject):
         str, default=None, coerce=True, allow_ddl_set=True,
         ephemeral=True)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         cls = self.__class__
         return '<{}.{} {!r} at 0x{:x}>'.format(
             cls.__module__, cls.__name__, self.id, id(self))
 
     __str__ = __repr__
 
-    def get_displayname(self, schema) -> str:
+    def get_displayname(self, schema: s_schema.Schema) -> str:
         expr = self.get_expr(schema)
+        assert expr.origtext is not None
         return expr.origtext
 
 
@@ -70,7 +76,11 @@ class IndexableSubject(so.InheritingObject):
         inheritable=False, ephemeral=True, coerce=True, compcoef=0.909,
         default=so.DEFAULT_CONSTRUCTOR)
 
-    def add_index(self, schema, index):
+    def add_index(
+        self,
+        schema: s_schema.Schema,
+        index: Index,
+    ) -> s_schema.Schema:
         return self.add_classref(schema, 'indexes', index)
 
 
@@ -78,28 +88,34 @@ class IndexSourceCommandContext:
     pass
 
 
-class IndexSourceCommand(inheriting.InheritingObjectCommand):
+class IndexSourceCommand(inheriting.InheritingObjectCommand[Index]):
     pass
 
 
-class IndexCommandContext(sd.ObjectCommandContext,
+class IndexCommandContext(sd.ObjectCommandContext[Index],
                           s_anno.AnnotationSubjectCommandContext):
     pass
 
 
 class IndexCommand(
-    referencing.ReferencedInheritingObjectCommand,
+    referencing.ReferencedInheritingObjectCommand[Index],
     schema_metaclass=Index,
     context_class=IndexCommandContext,
     referrer_context_class=IndexSourceCommandContext,
 ):
 
     @classmethod
-    def _classname_from_ast(cls, schema, astnode, context):
+    def _classname_from_ast(
+        cls,
+        schema: s_schema.Schema,
+        astnode: qlast.NamedDDL,
+        context: sd.CommandContext,
+    ) -> sn.Name:
         referrer_ctx = cls.get_referrer_context(context)
         if referrer_ctx is not None:
 
             referrer_name = referrer_ctx.op.classname
+            assert isinstance(referrer_name, sn.Name)
 
             shortname = sn.Name(
                 module='__',
@@ -123,24 +139,40 @@ class IndexCommand(
         return name
 
     @classmethod
-    def _classname_quals_from_ast(cls, schema, astnode, base_name,
-                                  referrer_name, context):
+    def _classname_quals_from_ast(
+        cls,
+        schema: s_schema.Schema,
+        astnode: qlast.NamedDDL,
+        base_name: str,
+        referrer_name: str,
+        context: sd.CommandContext,
+    ) -> Tuple[str, ...]:
+        assert isinstance(astnode, qlast.IndexOp)
         expr_text = cls.get_orig_expr_text(schema, astnode, 'expr')
+
         if expr_text is None:
             # if not, then use the origtext directly from the expression
             expr = s_expr.Expression.from_ast(
                 astnode.expr, schema, context.modaliases)
             expr_text = expr.origtext
 
+        assert expr_text is not None
         name = (cls._name_qual_from_exprs(schema, (expr_text,)),)
 
         return name
 
-    def get_object(self, schema, context, *, name=None):
+    # type ignore below, because parent class defines a compatible overload
+    def get_object(  # type: ignore
+        self,
+        schema: s_schema.Schema,
+        context: sd.CommandContext,
+        *,
+        name: Optional[str] = None,
+    ) -> Optional[Index]:
         try:
             return super().get_object(schema, context, name=name)
         except errors.InvalidReferenceError:
-            referrer_ctx = self.get_referrer_context(context)
+            referrer_ctx = self.get_referrer_context_or_die(context)
             referrer = referrer_ctx.scls
             expr = self.get_attribute_value('expr')
             raise errors.InvalidReferenceError(
@@ -157,9 +189,14 @@ class CreateIndex(
     referenced_astnode = qlast.CreateIndex
 
     @classmethod
-    def _cmd_tree_from_ast(cls, schema, astnode, context):
+    def _cmd_tree_from_ast(
+        cls,
+        schema: s_schema.Schema,
+        astnode: qlast.DDLOperation,
+        context: sd.CommandContext,
+    ) -> sd.Command:
         cmd = super()._cmd_tree_from_ast(schema, astnode, context)
-
+        assert isinstance(astnode, qlast.CreateIndex)
         orig_text = cls.get_orig_expr_text(schema, astnode, 'expr')
         cmd.set_attribute_value(
             'expr',
@@ -174,9 +211,17 @@ class CreateIndex(
         return cmd
 
     @classmethod
-    def as_inherited_ref_ast(cls, schema, context, name, parent):
+    def as_inherited_ref_ast(
+        cls,
+        schema: s_schema.Schema,
+        context: sd.CommandContext,
+        name: str,
+        parent: referencing.ReferencedObject,
+    ) -> qlast.ObjectDDL:
+        assert isinstance(parent, Index)
         nref = cls.get_inherited_ref_name(schema, context, parent, name)
         astnode_cls = cls.referenced_astnode
+
         expr = parent.get_expr(schema)
         if expr is not None:
             expr_ql = edgeql.parse_fragment(expr.origtext)
@@ -194,17 +239,33 @@ class CreateIndex(
         else:
             return None
 
-    def compile_expr_field(self, schema, context, field, value):
+    def compile_expr_field(
+        self,
+        schema: s_schema.Schema,
+        context: sd.CommandContext,
+        field: so.Field[Any],
+        value: s_expr.Expression,
+    ) -> s_expr.Expression:
+        from . import objtypes as s_objtypes
+
+        singletons: List[s_types.Type]
         if field.name == 'expr':
-            parent_ctx = context.get_ancestor(IndexSourceCommandContext, self)
+            # type ignore below, for the class is used as mixin
+            parent_ctx = context.get_ancestor(
+                IndexSourceCommandContext,  # type: ignore
+                self
+            )
+            assert parent_ctx is not None
             subject_name = parent_ctx.op.classname
             subject = schema.get(subject_name, default=None)
-            if not isinstance(subject, s_abc.Pointer):
-                singletons = [subject]
-                path_prefix_anchor = qlast.Subject().name
-            else:
+
+            if isinstance(subject, s_abc.Pointer):
                 singletons = []
                 path_prefix_anchor = None
+            else:
+                assert isinstance(subject, s_objtypes.ObjectType)
+                singletons = [subject]
+                path_prefix_anchor = qlast.Subject().name
 
             return type(value).compiled(
                 value,
@@ -214,7 +275,7 @@ class CreateIndex(
                     schema_object_context=self.get_schema_metaclass(),
                     anchors={qlast.Subject().name: subject},
                     path_prefix_anchor=path_prefix_anchor,
-                    singletons=singletons,
+                    singletons=frozenset(singletons),
                 ),
             )
         else:
@@ -242,7 +303,13 @@ class DeleteIndex(
     astnode = qlast.DropIndex
 
     @classmethod
-    def _cmd_tree_from_ast(cls, schema, astnode, context):
+    def _cmd_tree_from_ast(
+        cls,
+        schema: s_schema.Schema,
+        astnode: qlast.DDLOperation,
+        context: sd.CommandContext,
+    ) -> sd.Command:
+        assert isinstance(astnode, qlast.DropIndex)
         cmd = super()._cmd_tree_from_ast(schema, astnode, context)
 
         cmd.set_attribute_value(
