be763aeba1a5e4f7d79d1ccaa64712c70663768a
Paul Colomiets <paul@colomiets.name>
2020-04-20 13:45:39 +0300

    Make rust lib source of truth for keywords

--- edb/edgeql-rust/src/lib.rs
+++ edb/edgeql-rust/src/lib.rs
@@ -2,21 +2,27 @@
 
 use cpython::PyString;
 
-mod tokenizer;
 mod errors;
+mod keywords;
+mod tokenizer;
 
 use errors::TokenizerError;
 use tokenizer::{Token, tokenize, get_unpickle_fn};
+
 
 py_module_initializer!(
     _edgeql_rust, init_edgeql_rust, PyInit__edgeql_rust,
     |py, m| {
         tokenizer::init_module(py);
+        let keywords = keywords::get_keywords(py)?;
         m.add(py, "__doc__", "Rust enhancements for edgeql language parser")?;
 
         m.add(py, "tokenize", py_fn!(py, tokenize(data: &PyString)))?;
         m.add(py, "_unpickle_token", get_unpickle_fn(py))?;
         m.add(py, "Token", py.get_type::<Token>())?;
         m.add(py, "TokenizerError", py.get_type::<TokenizerError>())?;
+        m.add(py, "unreserved_keywords", keywords.unreserved)?;
+        m.add(py, "future_reserved_keywords", keywords.future)?;
+        m.add(py, "current_reserved_keywords", keywords.current)?;
         Ok(())
     });
