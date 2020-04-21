ceea0c5545928dc3db7dd021ba2c8218c502d63c
Paul Colomiets <paul@colomiets.name>
2020-04-21 11:41:53 +0300

    Add a comment

--- edb/server/mng_port/edgecon.pyx
+++ edb/server/mng_port/edgecon.pyx
@@ -1620,9 +1620,12 @@ cdef class EdgeConnection:
                 frb_read(&in_buf, 4)  # reserved
             in_len = hton.unpack_int32(frb_read(&in_buf, 4))
             out_buf.write_int32(in_len)
+
             if in_len < 0:
+                # This means argument value is NULL
                 name = query.query_unit.in_type_args[i]
                 raise errors.QueryError(f"parameter ${name} is required")
+
             if in_len > 0:
                 data = frb_read(&in_buf, in_len)
                 array_tid = array_tids and array_tids.get(i)
