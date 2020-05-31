da83bd1d06799435056d88564014a247553baea0
Łukasz Langa <lukasz@langa.pl>
2020-06-01 00:34:15 +0200

    Fix `IndexCommand.get_object()` (#1450)
    Without this change, altering a type to add an index to it results in:
    
    ```
    TypeError: get_object() got an unexpected keyword argument 'default'
    ```

--- edb/schema/indexes.py
+++ edb/schema/indexes.py
@@ -185,16 +185,40 @@ class IndexCommand(
         quals = sn.quals_from_fullname(name)
         return tuple(quals[-2:])
 
-    # type ignore below, because parent class defines a compatible overload
-    def get_object(  # type: ignore
+    @overload
+    def get_object(
         self,
         schema: s_schema.Schema,
         context: sd.CommandContext,
         *,
         name: Optional[str] = None,
+        default: Union[Index, so.NoDefaultT] = so.NoDefault,
+    ) -> Index:
+        ...
+
+    @overload
+    def get_object(  # NoQA: F811
+        self,
+        schema: s_schema.Schema,
+        context: sd.CommandContext,
+        *,
+        name: Optional[str] = None,
+        default: None = None,
+    ) -> Optional[Index]:
+        ...
+
+    def get_object(  # NoQA: F811
+        self,
+        schema: s_schema.Schema,
+        context: sd.CommandContext,
+        *,
+        name: Optional[str] = None,
+        default: Union[Index, so.NoDefaultT, None] = so.NoDefault,
     ) -> Optional[Index]:
         try:
-            return super().get_object(schema, context, name=name)
+            return super().get_object(
+                schema, context, name=name, default=default
+            )
         except errors.InvalidReferenceError:
             referrer_ctx = self.get_referrer_context_or_die(context)
             referrer = referrer_ctx.scls
