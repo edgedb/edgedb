edgedb/edgedb

commit 43c3b541f4d2304fd46bcd0a48577ae21766aaad

setup.py: Fix hardcoded `build` paths

setup.py
@@ -298,9 +298,9 @@ class develop(setuptools_develop.develop):
     def run(self, *args, **kwargs):
         _check_rust()
         build = self.get_finalized_command('build')
-        build_base = pathlib.Path('build').resolve()
         rust_tmp = pathlib.Path(build.build_temp) / 'rust' / 'cli'
-        rust_root = pathlib.Path(build.build_base) / 'cli'
+        build_base = pathlib.Path(build.build_base).resolve()
+        rust_root = build_base / 'cli'
         env = dict(os.environ)
         env['CARGO_TARGET_DIR'] = str(rust_tmp)
         env['PSQL_DEFAULT_PATH'] = build_base / 'postgres' / 'install' / 'bin'
@@ -334,7 +334,7 @@ class develop(setuptools_develop.develop):
 
         super().run(*args, **kwargs)
 
-        _compile_parsers(pathlib.Path('build/lib'), inplace=True)
+        _compile_parsers(build_base / 'lib', inplace=True)
         _compile_postgres(build_base)
 
 
@@ -385,7 +385,7 @@ class build_postgres(setuptools.Command):
 
     def run(self, *args, **kwargs):
         _compile_postgres(
-            pathlib.Path('build').resolve(),
+            pathlib.Path(build.build_base).resolve(),
             force_build=True,
             fresh_build=self.fresh_build,
             run_configure=self.configure,

commit e5ee38936a52a7e754cd378e112606de35ba85e1

Misc fixes (#1449)
* nightly: Allow skipping all targets of a platform class

* Unbreak setup.py build_postgres

This has been broken since 43c3b541f4d2.

* Bump Postgres submodule to 12.3

.github/workflows.src/nightly.tpl.yml
@@ -10,6 +10,7 @@ on:
       - nightly
 
 jobs:
+<% if targets.linux %>
   build-linux:
     runs-on: ubuntu-latest
     strategy:
@@ -48,7 +49,9 @@ jobs:
       with:
         name: builds-${{ matrix.target }}
         path: artifacts/${{ matrix.target }}
+<% endif %>
 
+<% if targets.macos %>
   build-macos:
     runs-on: macos-latest
 
@@ -91,7 +94,9 @@ jobs:
       with:
         name: builds-${{ matrix.target }}
         path: artifacts/${{ matrix.target }}
+<% endif %>
 
+<% if targets.linux %>
   publish-linux:
     needs: [build-linux]
     runs-on: ubuntu-latest
@@ -154,9 +159,11 @@ jobs:
         snapshot: true
         workdir: dockerfile
         buildargs: version=${{ steps.describe.outputs.version-slot }},subdist=.nightly
+<% endif %>
 
+<% if targets.macos %>
   publish-macos:
-    needs: [build-linux, build-macos]
+    needs: [build-macos]
     runs-on: macos-latest
     strategy:
       max-parallel: 4
@@ -197,3 +204,5 @@ jobs:
       run: |
         edgedb-pkg/integration/macos/publish.sh
 <% endfor %>
+
+<% endif %>

.github/workflows/nightly.yml
@@ -426,7 +426,7 @@ jobs:
         buildargs: version=${{ steps.describe.outputs.version-slot }},subdist=.nightly
 
   publish-macos:
-    needs: [build-linux, build-macos]
+    needs: [build-macos]
     runs-on: macos-latest
     strategy:
       max-parallel: 4

setup.py
@@ -390,6 +390,7 @@ class build_postgres(setuptools.Command):
         pass
 
     def run(self, *args, **kwargs):
+        build = self.get_finalized_command('build')
         _compile_postgres(
             pathlib.Path(build.build_base).resolve(),
             force_build=True,