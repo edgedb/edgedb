9a14756a2788bd39bc3946e0b4d786bd98dc455e
Paul Colomiets <paul@colomiets.name>
2020-06-12 13:04:38 +0300

    Fix rust tests

--- edb/edgeql-rust/tests/normalize.rs
+++ edb/edgeql-rust/tests/normalize.rs
@@ -24,7 +24,7 @@ fn test_int() {
     let entry = normalize(r###"
         SELECT 1 + 2
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT(<int64>$0)+(<int64>$1)");
+    assert_eq!(entry.key, "SELECT(<__std__::int64>$0)+(<__std__::int64>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Int(1),
@@ -40,7 +40,7 @@ fn test_str() {
     let entry = normalize(r###"
         SELECT "x" + "yy"
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT(<str>$0)+(<str>$1)");
+    assert_eq!(entry.key, "SELECT(<__std__::str>$0)+(<__std__::str>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Str("x".into()),
@@ -56,7 +56,8 @@ fn test_float() {
     let entry = normalize(r###"
         SELECT 1.5 + 23.25
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT(<float64>$0)+(<float64>$1)");
+    assert_eq!(entry.key,
+        "SELECT(<__std__::float64>$0)+(<__std__::float64>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Float(1.5),
@@ -72,7 +73,7 @@ fn test_bigint() {
     let entry = normalize(r###"
         SELECT 1n + 23n
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT(<bigint>$0)+(<bigint>$1)");
+    assert_eq!(entry.key, "SELECT(<__std__::bigint>$0)+(<__std__::bigint>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::BigInt(1.into()),
@@ -88,7 +89,7 @@ fn test_bigint_exponent() {
     let entry = normalize(r###"
         SELECT 1e10n + 23e13n
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT(<bigint>$0)+(<bigint>$1)");
+    assert_eq!(entry.key, "SELECT(<__std__::bigint>$0)+(<__std__::bigint>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::BigInt(10000000000u64.into()),
@@ -104,7 +105,8 @@ fn test_decimal() {
     let entry = normalize(r###"
         SELECT 1.33n + 23.77n
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT(<decimal>$0)+(<decimal>$1)");
+    assert_eq!(entry.key,
+        "SELECT(<__std__::decimal>$0)+(<__std__::decimal>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Decimal("1.33".parse().unwrap()),
@@ -120,7 +122,7 @@ fn test_positional() {
     let entry = normalize(r###"
         SELECT <int64>$0 + 2
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT<int64>$0+(<int64>$1)");
+    assert_eq!(entry.key, "SELECT<int64>$0+(<__std__::int64>$1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Int(2),
@@ -133,7 +135,8 @@ fn test_named() {
     let entry = normalize(r###"
         SELECT <int64>$test_var + 2
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT<int64>$test_var+(<int64>$__edb_arg_1)");
+    assert_eq!(entry.key,
+        "SELECT<int64>$test_var+(<__std__::int64>$__edb_arg_1)");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Int(2),
@@ -146,7 +149,7 @@ fn test_limit_1() {
     let entry = normalize(r###"
         SELECT User { one := 1 } LIMIT 1
     "###).unwrap();
-    assert_eq!(entry.key, "SELECT User{one:=(<int64>$0)}LIMIT 1");
+    assert_eq!(entry.key, "SELECT User{one:=(<__std__::int64>$0)}LIMIT 1");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Int(1),
@@ -160,7 +163,8 @@ fn test_tuple_access() {
         SELECT User { one := 2, two := .field.2, three := .field  . 3 }
     "###).unwrap();
     assert_eq!(entry.key,
-        "SELECT User{one:=(<int64>$0),two:=.field.2,three:=.field.3}");
+        "SELECT User{one:=(<__std__::int64>$0),\
+                     two:=.field.2,three:=.field.3}");
     assert_eq!(entry.variables, vec![
         Variable {
             value: Value::Int(2),
