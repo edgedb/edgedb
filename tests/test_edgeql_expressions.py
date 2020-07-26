edgedb/edgedb

commit 3bbfda45d8683883a8059dd320fbd2e3ef0ce4d2

Avoid running cardinality inference callbacks in temporary scopes
Since temporary scopes are thrown away upon exit from the context block,
any delayed cardinality inference scheduled while inside the block must
not proceed, because the scope is gone.  There is some protection from
this currently, but it's a porous patchwork, so replace it with a proper
solution and add explicit assertions that dangling scope pointers are
never referenced.

edb/edgeql/compiler/setgen.py
@@ -229,13 +229,16 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
 
                 view_set = ctx.view_sets.get(stype)
                 if view_set is not None:
-                    path_scope, path_scope_ns = ctx.path_scope_map[view_set]
+                    view_scope_info = ctx.path_scope_map[view_set]
                     path_tip = new_set_from_set(
                         view_set,
-                        preserve_scope_ns=path_scope_ns is not None,
+                        preserve_scope_ns=(
+                            view_scope_info.pinned_path_id_ns is not None
+                        ),
                         ctx=ctx,
                     )
-                    extra_scopes[path_tip] = path_scope.copy()
+
+                    extra_scopes[path_tip] = view_scope_info
                 else:
                     path_tip = class_set(stype, ctx=ctx)
 
@@ -297,23 +300,22 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
             else:
                 source = get_set_type(path_tip, ctx=ctx)
 
-            with ctx.newscope(fenced=True, temporary=True) as subctx:
-                if isinstance(source, s_types.Tuple):
-                    path_tip = tuple_indirection_set(
-                        path_tip, source=source, ptr_name=ptr_name,
-                        source_context=step.context, ctx=subctx)
+            if isinstance(source, s_types.Tuple):
+                path_tip = tuple_indirection_set(
+                    path_tip, source=source, ptr_name=ptr_name,
+                    source_context=step.context, ctx=ctx)
 
-                else:
-                    path_tip = ptr_step_set(
-                        path_tip, source=source, ptr_name=ptr_name,
-                        direction=direction,
-                        ignore_computable=True,
-                        source_context=step.context, ctx=subctx)
+            else:
+                path_tip = ptr_step_set(
+                    path_tip, source=source, ptr_name=ptr_name,
+                    direction=direction,
+                    ignore_computable=True,
+                    source_context=step.context, ctx=ctx)
 
-                    ptrcls = typegen.ptrcls_from_ptrref(
-                        path_tip.rptr.ptrref, ctx=ctx)
-                    if _is_computable_ptr(ptrcls, ctx=ctx):
-                        computables.append(path_tip)
+                ptrcls = typegen.ptrcls_from_ptrref(
+                    path_tip.rptr.ptrref, ctx=ctx)
+                if _is_computable_ptr(ptrcls, ctx=ctx):
+                    computables.append(path_tip)
 
         elif isinstance(step, qlast.TypeIntersection):
             arg_type = inference.infer_type(path_tip, ctx.env)
@@ -354,7 +356,14 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
                 else:
                     scope_set = path_tip
 
-                extra_scopes[scope_set] = subctx.path_scope
+                extra_scopes[scope_set] = context.ScopeInfo(
+                    path_scope=subctx.path_scope,
+                    tentative_work=[
+                        cb
+                        for cb in subctx.tentative_work
+                        if cb not in ctx.tentative_work
+                    ],
+                )
 
         for key_path_id in path_tip.path_id.iter_weak_namespace_prefixes():
             mapped = ctx.view_map.get(key_path_id)
@@ -417,6 +426,30 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
     for fence in fences:
         fence.fenced = True
 
+    for ir_set, scope_info in extra_scopes.items():
+        nodes = tuple(
+            node for node in ctx.path_scope.find_descendants(ir_set.path_id)
+            # if node.parent_fence not in fences
+        )
+
+        if not nodes:
+            # The path portion not being a descendant means
+            # that is is already present in the scope above us,
+            # along with the view scope.
+            continue
+
+        assert len(nodes) == 1
+
+        nodes[0].fuse_subtree(scope_info.path_scope.copy())
+
+        for cb in scope_info.tentative_work:
+            stmtctx.at_stmt_fini(cb, ctx=ctx)
+
+        scope_info.tentative_work[:] = []
+
+        if ir_set.path_scope_id is None:
+            pathctx.assign_set_scope(ir_set, nodes[0], ctx=ctx)
+
     for ir_set in computables:
         scope = ctx.path_scope.find_descendant(ir_set.path_id)
         if scope is None:
@@ -433,24 +466,6 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
             else:
                 path_tip = comp_ir_set
             path_sets[i] = comp_ir_set
-
-    for ir_set, scope in extra_scopes.items():
-        nodes = tuple(
-            node for node in ctx.path_scope.find_descendants(ir_set.path_id)
-            if node.parent_fence not in fences
-        )
-
-        if not nodes:
-            # The path portion not being a descendant means
-            # that is is already present in the scope above us,
-            # along with the view scope.
-            continue
-
-        assert len(nodes) == 1
-
-        nodes[0].fuse_subtree(scope)
-        if ir_set.path_scope_id is None:
-            pathctx.assign_set_scope(ir_set, nodes[0], ctx=ctx)
 
     for fence in fences:
         fence.fenced = False

edb/edgeql/compiler/context.py
@@ -24,10 +24,9 @@ from typing import *
 from typing_extensions import Protocol  # type: ignore
 
 import collections
+import dataclasses
 import enum
 import uuid
-
-from dataclasses import dataclass
 
 from edb.common import compiler
 from edb.common import parsing
@@ -85,10 +84,19 @@ class ViewRPtr:
         self.is_update = is_update
 
 
-@dataclass
+@dataclasses.dataclass
 class StatementMetadata:
     is_unnest_fence: bool = False
     iterator_target: bool = False
+
+
+@dataclasses.dataclass
+class ScopeInfo:
+    path_scope: irast.ScopeTreeNode
+    pinned_path_id_ns: Optional[FrozenSet[str]] = None
+    tentative_work: List[CompletionWorkCallback] = (
+        dataclasses.field(default_factory=list)
+    )
 
 
 class CompletionWorkCallback(Protocol):
@@ -408,14 +416,8 @@ class ContextLevel(compiler.ContextLevel):
     path_scope: irast.ScopeTreeNode
     """Path scope tree, with per-lexical-scope levels."""
 
-    path_scope_map: Dict[
-        irast.Set,
-        Tuple[irast.ScopeTreeNode, Optional[FrozenSet[str]]],
-    ]
-    """A dictionary of scope trees that are appropriate for a given view.
-    The second element in the value tuple is an optional pinned path id
-    namespace that must be used for all references to the view.
-    """
+    path_scope_map: Dict[irast.Set, ScopeInfo]
+    """A dictionary of scope info that are appropriate for a given view."""
 
     iterator_ctx: Optional[ContextLevel]
     """The context of the statement where all iterators should be placed."""
@@ -458,6 +460,12 @@ class ContextLevel(compiler.ContextLevel):
 
     in_conditional: Optional[parsing.ParserContext]
     """Whether currently in a conditional branch."""
+
+    in_temp_scope: bool
+    """Whether currently in a temporary scope."""
+
+    tentative_work: List[CompletionWorkCallback]
+    """Callbacks that rely on scope and scheduled tentatively."""
 
     def __init__(
         self,
@@ -516,6 +524,8 @@ class ContextLevel(compiler.ContextLevel):
             self.empty_result_type_hint = None
             self.defining_view = None
             self.in_conditional = None
+            self.in_temp_scope = False
+            self.tentative_work = []
 
         else:
             self.env = prevlevel.env
@@ -557,6 +567,8 @@ class ContextLevel(compiler.ContextLevel):
             self.empty_result_type_hint = prevlevel.empty_result_type_hint
             self.defining_view = prevlevel.defining_view
             self.in_conditional = prevlevel.in_conditional
+            self.in_temp_scope = prevlevel.in_temp_scope
+            self.tentative_work = prevlevel.tentative_work
 
             if mode == ContextSwitchMode.SUBQUERY:
                 self.anchors = prevlevel.anchors.copy()
@@ -618,6 +630,8 @@ class ContextLevel(compiler.ContextLevel):
                     prevlevel.path_scope = self.env.path_scope
 
                 self.path_scope = prevlevel.path_scope.copy()
+                self.in_temp_scope = True
+                self.tentative_work = list(prevlevel.tentative_work)
 
             if mode in {ContextSwitchMode.NEWFENCE,
                         ContextSwitchMode.NEWFENCE_TEMP}:

edb/edgeql/compiler/inference/cardinality.py
@@ -104,10 +104,14 @@ def _get_set_scope(
         ir_set: irast.Set,
         scope_tree: irast.ScopeTreeNode) -> irast.ScopeTreeNode:
 
-    new_scope = None
     if ir_set.path_scope_id:
         new_scope = scope_tree.root.find_by_unique_id(ir_set.path_scope_id)
-    if new_scope is None:
+        if new_scope is None:
+            raise errors.InternalServerError(
+                f'dangling scope pointer to node with uid'
+                f':{ir_set.path_scope_id} in {ir_set!r}'
+            )
+    else:
         new_scope = scope_tree
 
     return new_scope

edb/edgeql/compiler/pathctx.py
@@ -115,12 +115,20 @@ def assign_set_scope(
 
 
 def get_set_scope(
-        ir_set: irast.Set, *,
-        ctx: context.ContextLevel) -> Optional[irast.ScopeTreeNode]:
+    ir_set: irast.Set,
+    *,
+    ctx: context.ContextLevel,
+) -> Optional[irast.ScopeTreeNode]:
     if ir_set.path_scope_id is None:
         return None
     else:
-        return ctx.path_scope.root.find_by_unique_id(ir_set.path_scope_id)
+        scope = ctx.path_scope.root.find_by_unique_id(ir_set.path_scope_id)
+        if scope is None:
+            raise errors.InternalServerError(
+                f'dangling scope pointer to node with uid'
+                f':{ir_set.path_scope_id} in {ir_set!r}'
+            )
+        return scope
 
 
 def mark_path_as_optional(

edb/edgeql/compiler/stmt.py
@@ -134,7 +134,8 @@ def compile_ForQuery(
 
             stmt.iterator_stmt = iterator_stmt
 
-            iterator_scope, _ = scopectx.path_scope_map[iterator_view]
+            view_scope_info = scopectx.path_scope_map[iterator_view]
+            iterator_scope = view_scope_info.path_scope
 
         pathctx.register_set_in_scope(
             iterator_stmt,
@@ -703,14 +704,9 @@ def compile_result_clause(
             shape = None
 
         if result_alias:
-            # `SELECT foo := expr` is largely equivalent to
-            # `WITH foo := expr SELECT foo` with one important exception:
-            # the scope namespace does not get added to the current query
-            # path scope.  This is needed to handle FOR queries correctly.
-            with sctx.newscope(temporary=True, fenced=True) as scopectx:
-                stmtctx.declare_view(
-                    result_expr, alias=result_alias,
-                    temporary_scope=False, ctx=scopectx)
+            # `SELECT foo := expr` is equivalent to
+            # `WITH foo := expr SELECT foo`
+            stmtctx.declare_view(result_expr, alias=result_alias, ctx=sctx)
 
             result_expr = qlast.Path(
                 steps=[qlast.ObjectRef(name=result_alias)]

edb/edgeql/compiler/stmtctx.py
@@ -350,16 +350,18 @@ def populate_anchors(
 
 
 def declare_view(
-        expr: qlast.Expr, alias: str, *,
-        fully_detached: bool=False,
-        temporary_scope: bool=True,
-        must_be_used: bool=False,
-        path_id_namespace: Optional[FrozenSet[str]]=None,
-        ctx: context.ContextLevel) -> irast.Set:
+    expr: qlast.Expr,
+    alias: str,
+    *,
+    fully_detached: bool=False,
+    must_be_used: bool=False,
+    path_id_namespace: Optional[FrozenSet[str]]=None,
+    ctx: context.ContextLevel,
+) -> irast.Set:
 
     pinned_pid_ns = path_id_namespace
 
-    with ctx.newscope(temporary=temporary_scope, fenced=True) as subctx:
+    with ctx.newscope(temporary=True, fenced=True) as subctx:
         if path_id_namespace is not None:
             subctx.path_id_namespace = path_id_namespace
 
@@ -399,7 +401,15 @@ def declare_view(
         view_set = dispatch.compile(astutils.ensure_qlstmt(expr), ctx=subctx)
         assert isinstance(view_set, irast.Set)
 
-        ctx.path_scope_map[view_set] = subctx.path_scope, pinned_pid_ns
+        ctx.path_scope_map[view_set] = context.ScopeInfo(
+            path_scope=subctx.path_scope,
+            pinned_path_id_ns=pinned_pid_ns,
+            tentative_work=[
+                cb
+                for cb in subctx.tentative_work
+                if cb not in ctx.tentative_work
+            ],
+        )
 
         if not fully_detached:
             # The view path id _itself_ should not be in the nested namespace.
@@ -591,8 +601,10 @@ def get_pointer_cardinality_later(
             ptrcls=ptrcls,
             irexpr=irexpr,
             specified_card=specified_card,
-            source_ctx=source_ctx),
-        ctx=ctx)
+            source_ctx=source_ctx,
+        ),
+        ctx=ctx,
+    )
 
 
 def get_expr_cardinality_later(
@@ -606,9 +618,7 @@ def get_expr_cardinality_later(
         card = infer_expr_cardinality(irexpr=irexpr, ctx=ctx)
         setattr(target, field, card)
 
-    at_stmt_fini(
-        functools.partial(cb, irexpr=irexpr),
-        ctx=ctx)
+    at_stmt_fini(functools.partial(cb, irexpr=irexpr), ctx=ctx)
 
 
 def ensure_ptrref_cardinality(
@@ -661,20 +671,14 @@ def enforce_singleton_now(
 
 
 def enforce_singleton(
-        irexpr: irast.Base, *,
-        ctx: context.ContextLevel) -> None:
-
-    if not ctx.defining_view:
-        # We cannot reliably defer cardinality inference operations
-        # because the current scope is temporary and will not be
-        # accessible when the scheduled inference will run.
-        at_stmt_fini(
-            functools.partial(
-                enforce_singleton_now,
-                irexpr=irexpr
-            ),
-            ctx=ctx
-        )
+    irexpr: irast.Base,
+    *,
+    ctx: context.ContextLevel,
+) -> None:
+    at_stmt_fini(
+        functools.partial(enforce_singleton_now, irexpr=irexpr),
+        ctx=ctx,
+    )
 
 
 def enforce_pointer_cardinality(
@@ -685,16 +689,25 @@ def enforce_pointer_cardinality(
     ctx: context.ContextLevel,
 ) -> None:
 
-    if not ctx.defining_view:
-        def _enforce_singleton(ctx: context.ContextLevel) -> None:
-            if ptrcls.singular(ctx.env.schema):
-                enforce_singleton_now(irexpr, singletons=singletons, ctx=ctx)
+    def _enforce_singleton(ctx: context.ContextLevel) -> None:
+        if ptrcls.singular(ctx.env.schema):
+            enforce_singleton_now(irexpr, singletons=singletons, ctx=ctx)
 
-        at_stmt_fini(_enforce_singleton, ctx=ctx)
+    at_stmt_fini(_enforce_singleton, ctx=ctx)
 
 
 def at_stmt_fini(
-    cb: context.CompletionWorkCallback, *,
+    cb: context.CompletionWorkCallback,
+    *,
     ctx: context.ContextLevel,
 ) -> None:
-    ctx.completion_work.append(cb)
+
+    if ctx.in_temp_scope:
+        # We cannot reliably defer cardinality inference operations
+        # because the current scope is temporary and will not be
+        # accessible when the scheduled inference will run.
+        # If, however, the temp scope is later merged into the main
+        # scope, we need to schedule the inference operations properly.
+        ctx.tentative_work.append(cb)
+    else:
+        ctx.completion_work.append(cb)

tests/test_edgeql_ir_scopetree.py
@@ -397,22 +397,21 @@ class TestEdgeQLIRScopeTree(tb.BaseEdgeQLCompilerTest):
                     "(__derived__::__derived__|U@@w~1)\
 .>cards[IS test::Card]": {
                         "BRANCH": {
-                            "(__derived__::__derived__|U@@w~1)"
+                            "(__derived__::__derived__|U@@w~1)": {
+                                "FENCE": {
+                                    "(test::User)",
+                                    "FENCE": {
+                                        "(test::User).>name[IS std::str]"
+                                    }
+                                }
+                            }
                         },
                         "FENCE": {
                             "(test::Card)",
                             "FENCE": {
                                 "(__derived__::__derived__|U@@w~1)\
 .>deck[IS test::Card]": {
-                                    "(__derived__::__derived__|U@@w~1)": {
-                                        "FENCE": {
-                                            "(test::User)",
-                                            "FENCE": {
-                                                "(test::User)\
-.>name[IS std::str]"
-                                            }
-                                        }
-                                    }
+                                    "(__derived__::__derived__|U@@w~1)"
                                 }
                             }
                         }

commit 602cca6ecc691601f6d2b72b9bd59af270080b1c

Make sure delayed inference work is not missed in WITH/FOR aliases
The bug was introduced in 3bbfda45d86 by placing the `tentative_work`
iteration in a branch that may not always execute.

Per report from @Fogapod.

Fixes: #1594.

edb/edgeql/compiler/setgen.py
@@ -454,9 +454,13 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
         fence.fenced = True
 
     for ir_set, scope_info in extra_scopes.items():
+        for cb in scope_info.tentative_work:
+            stmtctx.at_stmt_fini(cb, ctx=ctx)
+
+        scope_info.tentative_work[:] = []
+
         nodes = tuple(
             node for node in ctx.path_scope.find_descendants(ir_set.path_id)
-            # if node.parent_fence not in fences
         )
 
         if not nodes:
@@ -468,11 +472,6 @@ def compile_path(expr: qlast.Path, *, ctx: context.ContextLevel) -> irast.Set:
         assert len(nodes) == 1
 
         nodes[0].fuse_subtree(scope_info.path_scope.copy())
-
-        for cb in scope_info.tentative_work:
-            stmtctx.at_stmt_fini(cb, ctx=ctx)
-
-        scope_info.tentative_work[:] = []
 
         if ir_set.path_scope_id is None:
             pathctx.assign_set_scope(ir_set, nodes[0], ctx=ctx)

tests/test_edgeql_expressions.py
@@ -3979,6 +3979,15 @@ aa \
             [['a', 'miss'], ['c', 'hit'], ['t', 'miss']],
         )
 
+    async def test_edgeql_expr_if_else_07(self):
+        await self.assert_query_result(
+            r"""
+                FOR x IN {<str>{} IF false ELSE <str>{'1'}}
+                UNION (SELECT x);
+            """,
+            ['1'],
+        )
+
     async def test_edgeql_expr_setop_01(self):
         await self.assert_query_result(
             r"""SELECT EXISTS <str>{};""",