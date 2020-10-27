edgedb/edgedb

commit c1d346c9b383dda4d83c06470393fca6df7676c4

Make sure configuration sources do not get mixed up on config mutation
Currently, when `CONFIGURE SYSTEM` mutates a configuration variable,
_all_ configuration variables switch to `system override` status, which
is totally wrong.  This patch modifies the system so that the source of
each configuration option is tracked separately in a consistent manner.

Additionally, the current `DESCRIBE SYSTEM CONFIG` implementation is
wrong, because it describes the configuration as a whole without regard
for the source of the configuration values.  The correct way is to
consider `system override` values only.  While at it, I rewrote the
generation of the EdgeQL query that emits config description to make it
generic and to support configuration type subclassing properly.

edb/server/bootstrap.py
@@ -678,7 +678,7 @@ async def _init_stdlib(cluster, conn, testmode, global_ids):
     )
 
     await metaschema.generate_more_support_functions(
-        conn, compiler, stdlib.reflschema)
+        conn, compiler, stdlib.reflschema, testmode)
 
     return schema, stdlib.reflschema, compiler
 
@@ -764,15 +764,21 @@ async def _configure(
     *,
     insecure: bool = False,
 ) -> None:
+    config_spec = config.get_settings()
+
     scripts = []
 
     if cluster.is_managed() and not devmode.is_in_dev_mode():
         memory_kb = psutil.virtual_memory().total // 1024
-        settings: Mapping[str, str] = {
-            'shared_buffers': f'"{int(memory_kb * 0.2)}kB"',
-            'effective_cache_size': f'"{int(memory_kb * 0.5)}kB"',
-            'query_work_mem': f'"{6 * (2 ** 10)}kB"',
-        }
+        settings = config.from_dict(
+            config_spec,
+            {
+                'shared_buffers': f'"{int(memory_kb * 0.2)}kB"',
+                'effective_cache_size': f'"{int(memory_kb * 0.5)}kB"',
+                'query_work_mem': f'"{6 * (2 ** 10)}kB"',
+            },
+            source='dynamic default',
+        )
 
         for setting, value in settings.items():
             scripts.append(f'''
@@ -789,8 +795,6 @@ async def _configure(
             };
         ''')
 
-    config_spec = config.get_settings()
-
     for script in scripts:
         _, sql = compile_bootstrap_script(
             compiler,
@@ -806,10 +810,10 @@ async def _configure(
         config_op_data = await conn.fetchval(sql)
         if config_op_data is not None and isinstance(config_op_data, str):
             config_op = config.Operation.from_json(config_op_data)
-            storage: Mapping[str, str] = immutables.Map()
+            storage: Mapping[str, config.SettingValue] = immutables.Map()
             settings = config_op.apply(config_spec, storage)
 
-    config_json = config.to_json(config_spec, settings)
+    config_json = config.to_json(config_spec, settings, include_source=False)
     block = dbops.PLTopBlock()
     dbops.UpdateMetadata(
         dbops.Database(name=edbdef.EDGEDB_TEMPLATE_DB),
@@ -822,12 +826,10 @@ async def _configure(
 async def _compile_sys_queries(schema, compiler, cluster):
     queries = {}
 
-    cfg_query = config.generate_config_query(schema)
-
     schema, sql = compile_bootstrap_script(
         compiler,
         schema,
-        cfg_query,
+        'SELECT cfg::get_config_json()',
         expected_cardinality_one=True,
         single_statement=True,
     )

edb/edgeql/compiler/config.py
@@ -241,7 +241,7 @@ def _validate_op(
         )
 
     name = expr.name.name
-    cfg_host_type = ctx.env.get_track_schema_type('cfg::Config')
+    cfg_host_type = ctx.env.get_track_schema_type('cfg::AbstractConfig')
     assert isinstance(cfg_host_type, s_objtypes.ObjectType)
     cfg_type = None
 
@@ -281,8 +281,11 @@ def _validate_op(
                 ptr_candidate = pointer_link
                 break
 
-        if (ptr_candidate is None
-                or ptr_candidate.get_source(ctx.env.schema) != cfg_host_type):
+        if (
+            ptr_candidate is None
+            or (ptr_source := ptr_candidate.get_source(ctx.env.schema)) is None
+            or not ptr_source.issubclass(ctx.env.schema, cfg_host_type)
+        ):
             raise errors.ConfigurationError(
                 f'{name!r} cannot be configured directly'
             )

edb/edgeql/compiler/stmt.py
@@ -593,7 +593,7 @@ def compile_DescribeStmt(
         stmt = irast.SelectStmt()
         init_stmt(stmt, ql, ctx=ictx, parent_ctx=ctx)
 
-        if ql.object == qlast.DescribeGlobal.Schema:
+        if ql.object is qlast.DescribeGlobal.Schema:
             if ql.language is qltypes.DescribeLanguage.DDL:
                 # DESCRIBE SCHEMA
                 text = s_ddl.ddl_text_from_schema(
@@ -613,7 +613,7 @@ def compile_DescribeStmt(
                 ctx=ictx,
             )
 
-        elif ql.object == qlast.DescribeGlobal.SystemConfig:
+        elif ql.object is qlast.DescribeGlobal.SystemConfig:
             if ql.language is qltypes.DescribeLanguage.DDL:
                 function_call = dispatch.compile(
                     qlast.FunctionCall(
@@ -625,7 +625,8 @@ def compile_DescribeStmt(
             else:
                 raise errors.QueryError(
                     f'cannot describe config as {ql.language}')
-        elif ql.object == qlast.DescribeGlobal.Roles:
+
+        elif ql.object is qlast.DescribeGlobal.Roles:
             if ql.language is qltypes.DescribeLanguage.DDL:
                 function_call = dispatch.compile(
                     qlast.FunctionCall(
@@ -637,6 +638,7 @@ def compile_DescribeStmt(
             else:
                 raise errors.QueryError(
                     f'cannot describe roles as {ql.language}')
+
         else:
             assert isinstance(ql.object, qlast.ObjectRef), ql.object
             modules = []

edb/lib/_testmode.edgeql
@@ -20,7 +20,7 @@
 # These definitions are picked up if the EdgeDB instance is bootstrapped
 # with --testmode.
 
-CREATE TYPE cfg::SessionConfig {
+CREATE TYPE cfg::TestSessionConfig {
     CREATE REQUIRED PROPERTY name -> std::str {
         CREATE CONSTRAINT std::exclusive;
     }
@@ -42,7 +42,7 @@ CREATE TYPE cfg::Subclass2 EXTENDING cfg::Base {
 };
 
 
-CREATE TYPE cfg::SystemConfig {
+CREATE TYPE cfg::TestSystemConfig {
     CREATE REQUIRED PROPERTY name -> std::str {
         CREATE CONSTRAINT std::exclusive;
     };
@@ -51,9 +51,13 @@ CREATE TYPE cfg::SystemConfig {
 };
 
 
-ALTER TYPE cfg::Config {
-    CREATE MULTI LINK sessobj -> cfg::SessionConfig;
-    CREATE MULTI LINK sysobj -> cfg::SystemConfig;
+ALTER TYPE cfg::AbstractConfig {
+    CREATE MULTI LINK sessobj -> cfg::TestSessionConfig {
+        CREATE ANNOTATION cfg::internal := 'true';
+    };
+    CREATE MULTI LINK sysobj -> cfg::TestSystemConfig {
+        CREATE ANNOTATION cfg::internal := 'true';
+    };
 
     CREATE PROPERTY __internal_testvalue -> std::int64 {
         CREATE ANNOTATION cfg::internal := 'true';

edb/lib/cfg.edgeql
@@ -19,10 +19,10 @@
 
 CREATE MODULE cfg;
 
-CREATE ABSTRACT ANNOTATION cfg::backend_setting;
-CREATE ABSTRACT ANNOTATION cfg::internal;
-CREATE ABSTRACT ANNOTATION cfg::requires_restart;
-CREATE ABSTRACT ANNOTATION cfg::system;
+CREATE ABSTRACT INHERITABLE ANNOTATION cfg::backend_setting;
+CREATE ABSTRACT INHERITABLE ANNOTATION cfg::internal;
+CREATE ABSTRACT INHERITABLE ANNOTATION cfg::requires_restart;
+CREATE ABSTRACT INHERITABLE ANNOTATION cfg::system;
 
 CREATE ABSTRACT TYPE cfg::ConfigObject EXTENDING std::BaseObject;
 
@@ -81,7 +81,7 @@ CREATE TYPE cfg::Auth EXTENDING cfg::ConfigObject {
 };
 
 
-CREATE TYPE cfg::Config extending cfg::ConfigObject {
+CREATE ABSTRACT TYPE cfg::AbstractConfig extending cfg::ConfigObject {
     CREATE REQUIRED PROPERTY listen_port -> std::int16 {
         CREATE ANNOTATION cfg::system := 'true';
         SET default := 5656;
@@ -132,12 +132,25 @@ CREATE TYPE cfg::Config extending cfg::ConfigObject {
 };
 
 
+CREATE TYPE cfg::Config EXTENDING cfg::AbstractConfig;
+CREATE TYPE cfg::SystemConfig EXTENDING cfg::AbstractConfig;
+CREATE TYPE cfg::DatabaseConfig EXTENDING cfg::AbstractConfig;
+
+
 CREATE FUNCTION
-cfg::get_config_json() -> std::json
+cfg::get_config_json(
+    NAMED ONLY sources: OPTIONAL array<std::str> = {},
+    NAMED ONLY min_source: OPTIONAL std::str = {}
+) -> std::json
 {
     USING SQL $$
-    SELECT jsonb_object_agg(cfg.name, cfg)
-    FROM edgedb._read_sys_config() AS cfg
+    SELECT
+        jsonb_object_agg(cfg.name, cfg)
+    FROM
+        edgedb._read_sys_config(
+            sources::edgedb._sys_config_source_t[],
+            min_source::edgedb._sys_config_source_t
+        ) AS cfg
     $$;
 };
 
@@ -151,130 +164,10 @@ cfg::_quote(text: std::str) -> std::str
 };
 
 CREATE FUNCTION
-cfg::_name_value(
-    prefix: std::str,
-    suffix: std::str,
-    name: std::str,
-    value: OPTIONAL std::int16,
-    default: OPTIONAL std::int16
-) -> std::str
-{
-    SET internal := true;
-    USING (
-        SELECT '' IF EXISTS default AND default = value
-            ELSE prefix ++ name ++ ' := ' ++ <str>value ?? '{}' ++ suffix
-    )
-};
-
-CREATE FUNCTION
-cfg::_name_value(
-    prefix: std::str,
-    suffix: std::str,
-    name: std::str,
-    value: OPTIONAL std::int32,
-    default: OPTIONAL std::int32
-) -> std::str
-{
-    SET internal := true;
-    USING (
-        SELECT '' IF EXISTS default AND default = value
-            ELSE prefix ++ name ++ ' := ' ++ <str>value ?? '{}' ++ suffix
-    )
-};
-
-CREATE FUNCTION
-cfg::_name_value(
-    prefix: std::str,
-    suffix: std::str,
-    name: std::str,
-    value: OPTIONAL std::int64,
-    default: OPTIONAL std::int64
-) -> std::str
-{
-    SET internal := true;
-    USING (
-        SELECT '' IF EXISTS default AND default = value
-            ELSE prefix ++ name ++ ' := ' ++ <str>value ?? '{}' ++ suffix
-    )
-};
-
-CREATE FUNCTION
-cfg::_name_value(
-    prefix: std::str,
-    suffix: std::str,
-    name: std::str,
-    value: OPTIONAL std::bool,
-    default: OPTIONAL std::bool
-) -> std::str
-{
-    SET internal := true;
-    USING (
-        SELECT '' IF EXISTS default AND default = value
-            ELSE prefix ++ name ++ ' := ' ++ <str>value ?? '{}' ++ suffix
-    )
-};
-
-CREATE FUNCTION
-cfg::_name_value(
-    prefix: std::str,
-    suffix: std::str,
-    name: std::str,
-    value: OPTIONAL std::str,
-    default: OPTIONAL std::str
-) -> std::str
-{
-    SET internal := true;
-    USING (
-        SELECT '' IF EXISTS default AND default = value
-            ELSE prefix ++
-                name ++ ' := ' ++ cfg::_quote(value) ?? '{}'
-                ++ suffix
-    )
-};
-
-CREATE FUNCTION
-cfg::_name_value_array(
-    prefix: std::str,
-    suffix: std::str,
-    name: std::str,
-    value: array<std::str>
-) -> std::str
-{
-    SET internal := true;
-    USING SQL $$
-        SELECT
-            CASE WHEN array_length(value, 1) > 0 THEN
-                (SELECT concat(prefix, name, ' := {',
-                    string_agg(quote_literal(item), ', '), '}', suffix)
-                 FROM unnest(value) as item)
-            ELSE ''
-            END
-    $$
-};
-
-CREATE FUNCTION
 cfg::_describe_system_config_as_ddl() -> str
 {
     # The results won't change within a single statement.
     SET volatility := 'STABLE';
     SET internal := true;
     USING SQL FUNCTION 'edgedb._describe_system_config_as_ddl';
-};
-
-CREATE FUNCTION
-cfg::_config_insert_all_ports() -> str
-{
-    # The results won't change within a single statement.
-    SET volatility := 'STABLE';
-    SET internal := true;
-    USING SQL FUNCTION 'edgedb._config_insert_all_ports';
-};
-
-CREATE FUNCTION
-cfg::_config_insert_all_auth() -> str
-{
-    # The results won't change within a single statement.
-    SET volatility := 'STABLE';
-    SET internal := true;
-    USING SQL FUNCTION 'edgedb._config_insert_all_auth';
 };

edb/pgsql/compiler/config.py
@@ -478,17 +478,17 @@ def top_output_as_config_op(
             ]
         )
 
-        result = pgast.FuncCall(
+        array = pgast.FuncCall(
             name=('jsonb_build_array',),
             args=result_row.args,
             null_safe=True,
             ser_safe=True,
         )
 
-        return pgast.SelectStmt(
+        result = pgast.SelectStmt(
             target_list=[
                 pgast.ResTarget(
-                    val=result,
+                    val=array,
                 ),
             ],
             from_clause=[
@@ -496,6 +496,11 @@ def top_output_as_config_op(
             ],
         )
 
+        result.ctes = stmt.ctes
+        result.argnames = stmt.argnames
+        stmt.ctes = []
+
+        return result
     else:
         raise errors.InternalServerError(
             f'CONFIGURE {ir_set.expr.scope} INSERT is not supported')

edb/pgsql/dbops/enums.py
@@ -48,14 +48,22 @@ class EnumExists(base.Condition):
         ''')
 
 
+class Enum(base.DBObject):
+
+    def __init__(self, name, values, *, metadata=None):
+        self.name = name
+        self.values = values
+        super().__init__(metadata=metadata)
+
+
 class CreateEnum(ddl.SchemaObjectOperation):
     def __init__(
-            self, name, values, *, conditions=None, neg_conditions=None,
+            self, enum, *, conditions=None, neg_conditions=None,
             priority=0):
         super().__init__(
-            name, conditions=conditions, neg_conditions=neg_conditions,
+            enum.name, conditions=conditions, neg_conditions=neg_conditions,
             priority=priority)
-        self.values = values
+        self.values = enum.values
 
     def code(self, block: base.PLBlock) -> str:
         vals = ', '.join(ql(v) for v in self.values)

edb/pgsql/delta.py
@@ -1252,7 +1252,7 @@ class CreateScalarType(ScalarTypeMetaCommand,
             new_enum_name = common.get_backend_name(
                 schema, scalar, catenate=False)
             self.pgops.add(dbops.CreateEnum(
-                name=new_enum_name, values=enum_values))
+                dbops.Enum(name=new_enum_name, values=enum_values)))
             base = q(*new_enum_name)
 
         else:

commit bdcddbce8247637a0c92a3954080745220870f38

Fix non-devmode bootstrap
The config rework in c1d346c9b38 broke non-devmode bootstrap due to an
oversight.  Fix this.

edb/server/bootstrap.py
@@ -782,7 +782,7 @@ async def _configure(
 
         for setting, value in settings.items():
             scripts.append(f'''
-                CONFIGURE SYSTEM SET {setting} := {value};
+                CONFIGURE SYSTEM SET {setting} := {value.value};
             ''')
     else:
         settings = {}