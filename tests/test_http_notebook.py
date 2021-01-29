4919b6763e02897caf6b94f50ab452b628d204d1
Paul Colomiets <paul@colomiets.name>
2021-01-29 18:10:55 +0200

    Fixup

--- tests/test_http_notebook.py
+++ tests/test_http_notebook.py
@@ -112,6 +112,12 @@ class TestHttpNotebook(tb.BaseHttpTest, tb.server.QueryTestCase):
                                 '65524': '8',
                                 '65521': '7',
                                 '65522': '17',
+                                '65525': '7',
+                                '65526': '1',
+                                '65527': '18',
+                                '65528': '17',
+                                '65529': '7',
+                                '65530': '17',
                                 '1': 'Consider using an explicit type '
                                      'cast or a conversion function.'
                             }
