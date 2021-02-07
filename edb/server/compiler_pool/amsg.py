9b95e5141b1fe0c767ba5671d8eb726654f7bb9f
Yury Selivanov <yury@edgedb.com>
2021-02-07 13:28:52 -0800

    copmpiler-pool: add request_id to the protocol to synchronize it

--- edb/server/compiler_pool/amsg.py
+++ edb/server/compiler_pool/amsg.py
@@ -24,8 +24,8 @@ import os
 import struct
 
 
-_len_unpacker = struct.Struct('!I').unpack
-_len_packer = struct.Struct('!I').pack
+_uint64_unpacker = struct.Struct('!Q').unpack
+_uint64_packer = struct.Struct('!Q').pack
 
 
 class PoolClosedError(Exception):
@@ -50,9 +50,9 @@ class BaseFramedProtocol(asyncio.Protocol):
         self._buffer += data
         while self._buffer:
             if self._curmsg_len == -1:
-                if len(self._buffer) >= 4:
-                    self._curmsg_len = _len_unpacker(self._buffer[:4])[0]
-                    self._buffer = self._buffer[4:]
+                if len(self._buffer) >= 8:
+                    self._curmsg_len = _uint64_unpacker(self._buffer[:8])[0]
+                    self._buffer = self._buffer[8:]
                 else:
                     return
 
@@ -86,26 +86,36 @@ class HubProtocol(BaseFramedProtocol):
 
     def __init__(self, *, loop, on_pid):
         super().__init__(loop=loop)
-        self._msg_waiter = None
+        self._resp_waiter = None
+        self._resp_expected_id = -1
         self._on_pid = on_pid
         self._pid = None
 
-    def send(self, waiter, payload: bytes):
-        if self._msg_waiter is not None and not self._msg_waiter.done():
+    def send(self, req_id: int, waiter: asyncio.Future, payload: bytes):
+        if self._resp_waiter is not None and not self._resp_waiter.done():
             raise RuntimeError('FramedProtocol: another send() is in progress')
-        self._msg_waiter = waiter
-        self._transport.writelines((_len_packer(len(payload)), payload))
+        self._resp_waiter = waiter
+        self._resp_expected_id = req_id
+        self._transport.writelines(
+            (_uint64_packer(len(payload) + 8), _uint64_packer(req_id), payload)
+        )
 
     def process_message(self, msg):
-        if self._msg_waiter is not None and not self._msg_waiter.done():
-            self._msg_waiter.set_result(msg)
-            self._msg_waiter = None
+        msgview = memoryview(msg)
+        req_id = _uint64_unpacker(msgview[:8])[0]
+        if req_id != self._resp_expected_id:
+            # This could have happened if the previous request got cancelled.
+            return
+        if self._resp_waiter is not None and not self._resp_waiter.done():
+            self._resp_waiter.set_result(msgview[8:])
+            self._resp_waiter = None
+            self._resp_expected_id = -1
 
     def data_received(self, data):
         if self._pid is None:
-            pid_data = data[:4]
-            data = data[4:]
-            self._pid = _len_unpacker(pid_data)[0]
+            pid_data = data[:8]
+            data = data[8:]
+            self._pid = _uint64_unpacker(pid_data)[0]
             self._on_pid(self, self._transport, self._pid)
             if data:
                 super().data_received(data)
@@ -115,13 +125,13 @@ class HubProtocol(BaseFramedProtocol):
     def connection_lost(self, exc):
         super().connection_lost(exc)
 
-        if self._msg_waiter is not None:
+        if self._resp_waiter is not None:
             if exc is not None:
-                self._msg_waiter.set_exception(exc)
+                self._resp_waiter.set_exception(exc)
             else:
-                self._msg_waiter.set_exception(ConnectionError(
+                self._resp_waiter.set_exception(ConnectionError(
                     'lost connection to the worker during a call'))
-            self._msg_waiter = None
+            self._resp_waiter = None
 
 
 class WorkerProtocol(BaseFramedProtocol):
@@ -131,15 +141,17 @@ class WorkerProtocol(BaseFramedProtocol):
         self._con = con
         super().__init__(loop=loop, con_waiter=con_waiter)
 
-    def reply(self, payload: bytes):
-        self._transport.writelines((_len_packer(len(payload)), payload))
+    def reply(self, req_id: int, payload: bytes):
+        self._transport.writelines(
+            (_uint64_packer(len(payload) + 8), _uint64_packer(req_id), payload)
+        )
 
     def process_message(self, msg):
         self._con._on_message(msg)
 
     def connection_made(self, tr):
         super().connection_made(tr)
-        tr.write(_len_packer(os.getpid()))
+        tr.write(_uint64_packer(os.getpid()))
 
     def connection_lost(self, exc):
         super().connection_lost(exc)
@@ -152,13 +164,17 @@ class HubConnection:
         self._transport = transport
         self._protocol = protocol
         self._loop = loop
+        self._req_id_cnt = 0
 
     def is_closed(self):
         return self._protocol._closed
 
     async def request(self, data: bytes) -> bytes:
+        self._req_id_cnt += 1
+        req_id = self._req_id_cnt
+
         waiter = self._loop.create_future()
-        self._protocol.send(waiter, data)
+        self._protocol.send(req_id, waiter, data)
         return await waiter
 
     def abort(self):
@@ -178,15 +194,17 @@ class WorkerConnection:
         return self._protocol._closed
 
     def _on_message(self, msg: bytes):
-        self._msgs.put_nowait(msg)
+        msgview = memoryview(msg)
+        req_id = _uint64_unpacker(msgview[:8])[0]
+        self._msgs.put_nowait((req_id, msgview[8:]))
 
     def _on_connection_lost(self, exc):
         self._con_lost_fut.set_exception(
             PoolClosedError('connection to the pool is closed'))
         self._con_lost_fut._log_traceback = False
 
-    async def reply(self, data):
-        self._protocol.reply(data)
+    async def reply(self, req_id, data):
+        self._protocol.reply(req_id, data)
 
     async def next_request(self) -> bytes:
         getter = self._loop.create_task(self._msgs.get())
