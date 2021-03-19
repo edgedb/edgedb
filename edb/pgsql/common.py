197092dcbd22f5fea6f08b22814229fbf5354863
Michael J. Sullivan <sully@msully.net>
2021-03-19 11:34:59 -0700

    Fix use of union pointers with mixed storage type (#2385)
    We need to be able to handle union pointers where one only is stored
    inline and one is only stored outline.
    
    Fixes #2360.

--- edb/pgsql/common.py
+++ edb/pgsql/common.py
@@ -186,6 +186,18 @@ def convert_name(name, suffix='', catenate=True):
         return schema, dbname
 
 
+def update_aspect(name, aspect):
+    """Update the aspect on a non catenated name.
+
+    It also needs to be from an object that uses ids for names"""
+    suffix = get_aspect_suffix(aspect)
+    stripped = name[1].rsplit("_", 1)[0]
+    if suffix:
+        return (name[0], f'{stripped}_{suffix}')
+    else:
+        return (name[0], stripped)
+
+
 def get_scalar_backend_name(id, module_name, catenate=True, *, aspect=None):
     if aspect is None:
         aspect = 'domain'
