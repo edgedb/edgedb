edgedb/edgedb

commit d8b1f5a3efba76ecbf3b110c9f21e5a70f724d91

Increase the confidence of simple create pointer migrations (#2561)
The slight churn in the produced deltas caused us to produce worse
migration prompts in some cases (a "did you alter <parent>?") instead
of a useful prompt about what was really done, so I hacked the code
for DESCRIBE MIGRATION AS JSON to generate better prompts by
rebuilding the delta from the AST. Sorry. This improves our prompts in
some existing cases too.

Fixes #2547.

edb/server/compiler/compiler.py
@@ -1026,12 +1026,27 @@ class Compiler:
                         mstate.target_schema,
                         guided_diff,
                     )
-
                     proposed_steps = []
 
                     if proposed_ddl:
-                        for ddl_text, top_op in proposed_ddl:
-                            prompt_id, prompt_text = top_op.get_user_prompt()
+                        for ddl_text, ast, top_op in proposed_ddl:
+                            # get_ast has a lot of logic for figuring
+                            # out when an op is implicit in a parent
+                            # op. get_user_prompt does not have any of
+                            # that sort of logic, which makes it
+                            # susceptible to producing overly broad
+                            # messages. To avoid duplicating that sort
+                            # of logic, we recreate the delta from the
+                            # AST, and extract a user prompt from
+                            # *that*.
+                            # This is stupid, and it is slow.
+                            top_op2 = s_ddl.cmd_from_ddl(
+                                ast,
+                                schema=schema,
+                                modaliases=current_tx.get_modaliases(),
+                            )
+
+                            prompt_id, prompt_text = top_op2.get_user_prompt()
                             confidence = top_op.get_annotation('confidence')
                             assert confidence is not None
 

tests/test_edgeql_data_migration.py
@@ -4928,7 +4928,6 @@ class TestEdgeQLDataMigration(tb.DDLTestCase):
             "'test::OrgUniquelyNamedResource'?",
 
             "did you alter object type 'test::OrgUniquelyNamedResource'?",
-            "did you alter object type 'test::OrgUniquelyNamedResource'?",
         ])
 
     async def test_edgeql_migration_rename_01(self):
@@ -8240,6 +8239,39 @@ class TestEdgeQLDataMigration(tb.DDLTestCase):
             },
         })
 
+    async def test_edgeql_migration_confidence_03(self):
+        await self.con.execute('''
+            START MIGRATION TO {
+                module test {
+                    type Obj1;
+                };
+            };
+        ''')
+        await self.fast_forward_describe_migration()
+
+        await self.con.execute('''
+            START MIGRATION TO {
+                module test {
+                    type Obj1 {
+                        property x -> str;
+                    }
+                };
+            };
+        ''')
+
+        await self.assert_describe_migration({
+            'confirmed': [],
+            'complete': False,
+            'proposed': {
+                'statements': [{
+                    'text':
+                        'ALTER TYPE test::Obj1 {\n    '
+                        'CREATE PROPERTY x -> std::str;\n};'
+                }],
+                'confidence': 1.0,
+            },
+        })
+
     async def test_edgeql_migration_data_safety_01(self):
         await self.start_migration('''
             type Obj1;
@@ -8576,7 +8608,7 @@ class TestEdgeQLDataMigration(tb.DDLTestCase):
 
         await self.assert_describe_migration({
             'proposed': {
-                'prompt_id': 'CreateObjectType TYPE test::Bar',
+                'prompt_id': 'CreateLink LINK test::`__|spam@test|Bar`',
                 'statements': [{
                     'text': """
                         ALTER TYPE test::Bar {

edb/schema/ddl.py
@@ -699,7 +699,7 @@ def statements_from_delta(
     sdlmode: bool = False,
     descriptive_mode: bool = False,
     limit_ref_classes: Iterable[so.ObjectMeta] = tuple(),
-) -> Tuple[Tuple[str, sd.Command], ...]:
+) -> Tuple[Tuple[str, qlast.DDLOperation, sd.Command], ...]:
 
     stmts = ddlast_from_delta(
         schema_a,
@@ -759,7 +759,7 @@ def statements_from_delta(
             descmode=descriptive_mode,
             limit_ref_classes=ql_classes,
         )
-        text.append((stmt_text + ';', cmd))
+        text.append((stmt_text + ';', stmt_ast, cmd))
 
     return tuple(text)
 
@@ -781,7 +781,7 @@ def text_from_delta(
         descriptive_mode=descriptive_mode,
         limit_ref_classes=limit_ref_classes,
     )
-    return '\n'.join(text for text, _ in stmts)
+    return '\n'.join(text for text, _, _ in stmts)
 
 
 def ddl_text_from_delta(

edb/schema/delta.py
@@ -195,6 +195,8 @@ def delta_objects(
             s, y = comparison_map[x]
             x_name = x.get_name(new_schema)
             y_name = y.get_name(old_schema)
+
+            already_has = x_name == y_name and x_name not in renames_x
             if (
                 0.6 < s < 1.0
                 or (
@@ -204,7 +206,8 @@ def delta_objects(
                 or x_name in renames_x
             ):
                 if (
-                    (x_alter_variants[x] > 1 or can_create(x_name))
+                    (x_alter_variants[x] > 1 or (
+                        not already_has and can_create(x_name)))
                     and parent_confidence != 1.0
                 ):
                     confidence = s
@@ -1638,9 +1641,11 @@ class ObjectCommand(Command, Generic[so.Object_T]):
                 )
             )
         ):
+            from . import referencing as s_referencing
+
             subcommands = self.get_subcommands(
                 type=ObjectCommand,
-                exclude=AlterObjectProperty,
+                exclude=(AlterObjectProperty, s_referencing.AlterOwned),
             )
             if len(subcommands) == 1:
                 subcommand = subcommands[0]

edb/schema/inheriting.py
@@ -1123,10 +1123,14 @@ class RebaseInheritingObject(
                 continue
 
             if isinstance(pos, tuple):
+                typ = utils.typeref_to_ast(schema, pos[1])
+                assert isinstance(typ, qlast.TypeName)
+
                 pos_node = qlast.Position(
                     position=pos[0],
-                    ref=utils.typeref_to_ast(schema, pos[1]),
+                    ref=typ.maintype,
                 )
+
             else:
                 pos_node = qlast.Position(position=pos)
 

commit 061ac141f054407e41c53400036a0bf4bb83c6ad

Fix a migration regression that results in extra prompts
I broke this in #2561; we had a test for this,
test_edgeql_migration_prompt_id_01, but I just updated the test to
want the regressed behavior. Sorry.

Fixes #2591.

edb/server/compiler/compiler.py
@@ -1045,8 +1045,12 @@ class Compiler:
                                 schema=schema,
                                 modaliases=current_tx.get_modaliases(),
                             )
+                            _, prompt_text = top_op2.get_user_prompt()
 
-                            prompt_id, prompt_text = top_op2.get_user_prompt()
+                            # The prompt_id still needs to come from
+                            # the original op, though, since
+                            # orig_cmd_class is lost in ddl.
+                            prompt_id, _ = top_op.get_user_prompt()
                             confidence = top_op.get_annotation('confidence')
                             assert confidence is not None
 

tests/test_edgeql_data_migration.py
@@ -8608,9 +8608,13 @@ class TestEdgeQLDataMigration(tb.DDLTestCase):
 
         await self.fast_forward_describe_migration(limit=1)
 
+        # N.B: It is important that the prompt_id here match the
+        # prompt_id in the first migration, so that the migration tool
+        # will automatically apply this proposal as part of the
+        # earlier action.
         await self.assert_describe_migration({
             'proposed': {
-                'prompt_id': 'CreateLink LINK test::`__|spam@test|Bar`',
+                'prompt_id': 'CreateObjectType TYPE test::Bar',
                 'statements': [{
                     'text': """
                         ALTER TYPE test::Bar {