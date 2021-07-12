edgedb/edgedb

commit a9d76f5a2987917d4d05bbe182fd80e1a45241bb

Turn MarkupCapableMeta into MarkupCapableMixin (#2653)

edb/common/markup/__init__.py
@@ -30,21 +30,15 @@ from .serializer.base import Context  # noqa
 from .elements.base import Markup  # noqa
 
 
-class MarkupCapableMeta(type):
+class MarkupCapableMixin:
 
-    def __new__(mcls, name, bases, dct, **kwargs):
-        cls = super().__new__(mcls, name, bases, dct, **kwargs)
-        if 'as_markup' in dct:
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if hasattr(cls, 'as_markup'):
             serializer.serializer.register(cls)(cls.as_markup)
-        return cls
 
 
-class MarkupExceptionContextMeta(MarkupCapableMeta, abc.ABCMeta):
-    pass
-
-
-class MarkupExceptionContext(exceptions.ExceptionContext,
-                             metaclass=MarkupExceptionContextMeta):
+class MarkupExceptionContext(exceptions.ExceptionContext):
 
     @abc.abstractclassmethod
     def as_markup(cls, *, ctx):

edb/common/ast/base.py
@@ -190,7 +190,9 @@ class AST:
 
         return cls
 
-    def __init_subclass__(cls):
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
         cls._collect_direct_fields()
 
         fields = collections.OrderedDict()

edb/pgsql/dbops/base.py
@@ -261,7 +261,7 @@ class PLTopBlock(PLBlock):
         return self
 
 
-class BaseCommand(metaclass=markup.MarkupCapableMeta):
+class BaseCommand(markup.MarkupCapableMixin):
     def generate(self, block):
         raise NotImplementedError
 

edb/schema/delta.py
@@ -310,7 +310,6 @@ CommandMeta_T = TypeVar("CommandMeta_T", bound="CommandMeta")
 class CommandMeta(
     adapter.Adapter,
     struct.MixedStructMeta,
-    markup.MarkupCapableMeta,
 ):
 
     _astnode_map: Dict[Type[qlast.DDLOperation], Type[Command]] = {}
@@ -376,7 +375,11 @@ Command_T = TypeVar("Command_T", bound="Command")
 Command_T_co = TypeVar("Command_T_co", bound="Command", covariant=True)
 
 
-class Command(struct.MixedStruct, metaclass=CommandMeta):
+class Command(
+    struct.MixedStruct,
+    markup.MarkupCapableMixin,
+    metaclass=CommandMeta,
+):
     source_context = struct.Field(parsing.ParserContext, default=None)
     canonical = struct.Field(bool, default=False)
 
@@ -1510,7 +1513,7 @@ class ObjectCommand(Command, Generic[so.Object_T]):
         # Check if the command subclass has been parametrized with
         # a concrete schema object class, and if so, record the
         # argument to be made available via get_schema_metaclass().
-        super().__init_subclass__(*args, **kwargs)  # type: ignore
+        super().__init_subclass__(*args, **kwargs)
         generic_bases = typing_inspect.get_generic_bases(cls)
         mcls: Optional[Type[so.Object]] = None
         for gb in generic_bases:

commit 0fa1726895f59e60ef796d3ca46c8866210898ed

Unbreak parser source context dumps in server tracebacks
This is a regression from #2653, where `MarkupExceptionContext` was
erroneously left without the `MarkupCapableMixin` in its bases.

edb/common/markup/__init__.py
@@ -38,7 +38,10 @@ class MarkupCapableMixin:
             serializer.serializer.register(cls)(cls.as_markup)
 
 
-class MarkupExceptionContext(exceptions.ExceptionContext):
+class MarkupExceptionContext(
+    exceptions.ExceptionContext,
+    MarkupCapableMixin,
+):
 
     @abc.abstractclassmethod
     def as_markup(cls, *, ctx):