edgedb/edgedb

commit 1063fbe1fc5c333ff841430df1319e860f5da4a8

Allow generate cert with --bootstrap-only (#2689)
Also generate certs in the internal_runstate_dir:

If there is no data_dir when --generate-self-signed-cert is set, the
generated cert files are now stored in the internal_runstate_dir rather
than another temporary directory.

Also fixed 2 admin query issues in the test (edgedb PATH and timeout)

edb/server/main.py
@@ -175,7 +175,6 @@ async def _run_server(
     args: srvargs.ServerConfig,
     runstate_dir,
     internal_runstate_dir,
-    tls_cert_gen_dir,
     *,
     do_setproctitle: bool
 ):
@@ -196,15 +195,21 @@ async def _run_server(
         )
         await sc.wait_for(ss.init())
 
-        if args.bootstrap_only:
+        if args.generate_self_signed_cert:
+            ss.init_tls(
+                *_generate_cert(
+                    args.data_dir or pathlib.Path(internal_runstate_dir),
+                    ss.get_listen_host(),
+                )
+            )
+
+        if args.startup_script:
             await sc.wait_for(ss.run_startup_script_and_exit())
+
+        if args.bootstrap_only:
             return
 
-        if tls_cert_gen_dir:
-            ss.init_tls(
-                *_generate_cert(tls_cert_gen_dir, ss.get_listen_host())
-            )
-        else:
+        if not args.generate_self_signed_cert:
             ss.init_tls(args.tls_cert_file, args.tls_key_file)
 
         try:
@@ -301,7 +306,6 @@ def run_server(args: srvargs.ServerConfig, *, do_setproctitle: bool=False):
 
     pg_cluster_init_by_us = False
     pg_cluster_started_by_us = False
-    cert_temp_dir = None
 
     if args.tenant_id is None:
         tenant_id = buildmeta.get_default_tenant_id()
@@ -407,6 +411,7 @@ def run_server(args: srvargs.ServerConfig, *, do_setproctitle: bool=False):
                 not args.bootstrap_only
                 or args.bootstrap_script
                 or args.bootstrap_command
+                or args.generate_self_signed_cert
             ):
                 if args.data_dir:
                     cluster.set_connection_params(
@@ -419,22 +424,12 @@ def run_server(args: srvargs.ServerConfig, *, do_setproctitle: bool=False):
                         ),
                     )
 
-                if args.tls_cert_file:
-                    tls_cert_gen_dir = None
-                else:
-                    if args.data_dir:
-                        tls_cert_gen_dir = args.data_dir
-                    else:
-                        cert_temp_dir = tempfile.TemporaryDirectory()
-                        tls_cert_gen_dir = pathlib.Path(cert_temp_dir.name)
-
                 asyncio.run(
                     _run_server(
                         cluster,
                         args,
                         runstate_dir,
                         internal_runstate_dir,
-                        tls_cert_gen_dir,
                         do_setproctitle=do_setproctitle,
                     )
                 )
@@ -459,8 +454,6 @@ def run_server(args: srvargs.ServerConfig, *, do_setproctitle: bool=False):
 
         elif pg_cluster_started_by_us:
             cluster.stop()
-        if cert_temp_dir is not None:
-            cert_temp_dir.cleanup()
 
 
 def bump_rlimit_nofile() -> None:

edb/server/args.py
@@ -93,6 +93,7 @@ class ServerConfig(NamedTuple):
 
     tls_cert_file: Optional[pathlib.Path]
     tls_key_file: Optional[pathlib.Path]
+    generate_self_signed_cert: bool
     allow_cleartext_connections: bool
 
 
@@ -484,7 +485,7 @@ def parse_args(**kwargs: Any):
                 if (tls_key_file := data_dir / TLS_KEY_FILE_NAME).exists():
                     kwargs['tls_key_file'] = tls_key_file
     if (
-        not kwargs.pop('generate_self_signed_cert')
+        not kwargs['generate_self_signed_cert']
         and not kwargs['tls_cert_file']
         and not kwargs['bootstrap_only']
     ):

edb/server/cluster.py
@@ -245,7 +245,7 @@ class BaseCluster:
             asyncio.run(test())
             left -= (time.monotonic() - started)
 
-        if self._admin_query("SELECT ();", f"{min(1, int(left))}s"):
+        if self._admin_query("SELECT ();", f"{max(1, int(left))}s"):
             raise ClusterError(
                 f'could not connect to edgedb-server '
                 f'within {timeout} seconds') from None
@@ -253,9 +253,7 @@ class BaseCluster:
     def _admin_query(self, query, wait_until_available="0s"):
         return subprocess.call(
             [
-                sys.executable,
-                "-m",
-                "edb.cli",
+                "edgedb",
                 "--host",
                 str(self._runstate_dir),
                 "--port",

commit b5c9ac1634bfcb70548b540445cef0e7cff33f35

Fix issue that --bootstrap-command is executed twice
A bug was introduced in #2689 that if --bootstrap-command or
--bootstrap-script is set without --bootstrap-only, the startup script
will be executed twice.

edb/server/main.py
@@ -203,10 +203,9 @@ async def _run_server(
                 )
             )
 
-        if args.startup_script:
-            await sc.wait_for(ss.run_startup_script_and_exit())
-
         if args.bootstrap_only:
+            if args.startup_script:
+                await sc.wait_for(ss.run_startup_script_and_exit())
             return
 
         if not args.generate_self_signed_cert:

tests/test_server_ops.py
@@ -132,6 +132,23 @@ class TestServerOps(tb.TestCase):
                     'Unexpected server error output:\n' + stderr.decode()
                 )
 
+    async def test_server_ops_bootstrap_script_server(self):
+        # Test that "edgedb-server" works as expected with the
+        # following arguments:
+        #
+        # * "--bootstrap-command"
+
+        async with tb.start_edgedb_server(
+            auto_shutdown=True,
+            bootstrap_command='CREATE SUPERUSER ROLE test_bootstrap2 '
+                              '{ SET password := "tbs2" };'
+        ) as sd:
+            con = await sd.connect(user='test_bootstrap2', password='tbs2')
+            try:
+                self.assertEqual(await con.query_one('SELECT 1'), 1)
+            finally:
+                await con.aclose()
+
     async def test_server_ops_emit_server_status_to_file(self):
         debug = False
 