55858b30c8dd9ade8acc80bafdb3cd42e60073ec
Fantix King <fantix.king@gmail.com>
2022-01-16 19:10:18 -0500

    Bump Managed Postgres versions to 13

--- .github/aws-rds/main.tf
+++ .github/aws-rds/main.tf
@@ -1,12 +1,12 @@
 resource "aws_db_instance" "default" {
   allocated_storage = 10
   engine = "postgres"
-  engine_version = "12.8"
+  engine_version = "13.4"
   instance_class = "db.m6g.large"
   name = "edbtest"
   username = "edbtest"
   password = var.password
-  parameter_group_name = "default.postgres12"
+  parameter_group_name = "default.postgres13"
   skip_final_snapshot = true
   auto_minor_version_upgrade = false
   publicly_accessible = true
