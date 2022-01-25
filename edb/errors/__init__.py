6d547d014fb1ac11584e630a5b969fec64742458
Michael J. Sullivan <sully@msully.net>
2022-01-25 12:14:35 -0800

    Handle negative OFFSET and LIMIT
    Nothing important here, but might as well not produce an ISE.

--- edb/errors/__init__.py
+++ edb/errors/__init__.py
@@ -70,6 +70,8 @@ __all__ = base.__all__ + (  # type: ignore
     'InvalidValueError',
     'DivisionByZeroError',
     'NumericOutOfRangeError',
+    'InvalidLimitError',
+    'InvalidOffsetError',
     'IntegrityError',
     'ConstraintViolationError',
     'CardinalityViolationError',
@@ -332,6 +334,14 @@ class DivisionByZeroError(InvalidValueError):
 
 class NumericOutOfRangeError(InvalidValueError):
     _code = 0x_05_01_00_02
+
+
+class InvalidLimitError(InvalidValueError):
+    _code = 0x_05_01_00_03
+
+
+class InvalidOffsetError(InvalidValueError):
+    _code = 0x_05_01_00_04
 
 
 class IntegrityError(ExecutionError):
