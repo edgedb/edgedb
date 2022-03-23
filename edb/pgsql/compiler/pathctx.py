5dfcc2fd2616d4b6bb40e51d3899ba6c05a44d26
Michael J. Sullivan <sully@msully.net>
2022-03-22 18:50:52 -0700

    Search the source_rvar for materialized refs
    This is able to find materialized rvars that didn't need to be
    unpacked to resolve the source.

--- edb/pgsql/compiler/pathctx.py
+++ edb/pgsql/compiler/pathctx.py
@@ -1020,7 +1020,8 @@ def _get_rel_path_output(
             raise ValueError(
                 f'could not resolve trailing pointer class for {path_id}')
 
-        assert not ptrref.is_computable
+        if ptrref.is_computable:
+            raise LookupError("can't lookup computable ptrref")
 
         if ptr_info is None:
             ptr_info = pg_types.get_ptrref_storage_info(
