4119114a2b302c54af7921f7a2334ab249690a2a
Paul Colomiets <paul@edgedb.com>
2022-06-29 13:37:04 +0300

    Document different headers
    * Binary headers now called `KeyValue`
    * JSON headers are now called `Annotations`

--- edb/protocol/protocol.pyx
+++ edb/protocol/protocol.pyx
@@ -55,7 +55,7 @@ cdef class Connection:
     async def execute(self, query):
         await self.send(
             messages.Execute(
-                headers=[],
+                annotations=[],
                 command_text=query,
                 output_format=messages.OutputFormat.NONE,
                 expected_cardinality=messages.Cardinality.MANY,
