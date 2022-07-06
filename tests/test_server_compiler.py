390e58079440d218442b5be953e28fe9af0cc976
Elvis Pranskevichus <elvis@edgedb.com>
2022-07-06 15:19:42 -0700

    Bump timeout in compiler pool tests
    Should fix spurious failures on slow CI boxes

--- tests/test_server_compiler.py
+++ tests/test_server_compiler.py
@@ -279,7 +279,7 @@ class TestServerCompilerPool(tbs.TestCase):
                 f"in {timeout} second(s)"
             )
 
-    def _get_worker_pids(self, sd, least_num=2, timeout=1):
+    def _get_worker_pids(self, sd, least_num=2, timeout=5):
         rv = []
         start = time.monotonic()
         while time.monotonic() - start < timeout and len(rv) < least_num:
