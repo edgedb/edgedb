f0f154e41b69cc71578ef3afb913c595ff83e75d
Fantix King <fantix.king@gmail.com>
2022-07-25 14:06:46 -0500

    Update edb/server/server.py
    Co-authored-by: Elvis Pranskevichus <elvis@edgedb.com>

--- edb/server/server.py
+++ edb/server/server.py
@@ -1468,7 +1468,7 @@ class Server(ha_base.ClusterProtocol):
 
         # Fail if none of the servers can be started, except when the admin
         # server on a UNIX domain socket will be started.
-        if not servers and not (admin and port):
+        if not servers and (not admin or port == 0):
             raise StartupError("could not create any listen sockets")
 
         addrs = []
