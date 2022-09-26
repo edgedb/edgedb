93ec0c9a66973bb43dafed5e386a9f9b9ad93508
Michael J. Sullivan <sully@msully.net>
2022-09-26 10:29:29 -0700

    tpl_ctx.conn

--- edb/server/bootstrap.py
+++ edb/server/bootstrap.py
@@ -1659,10 +1659,7 @@ async def _bootstrap(ctx: BootstrapContext) -> None:
         schema = s_schema.FlatSchema()
         schema = await _init_defaults(schema, compiler, tpl_ctx.conn)
 
-        if backend_params.has_create_database:
-            await conn.sql_execute(
-                b"ANALYZE",
-            )
+        await tpl_ctx.conn.sql_execute(b"ANALYZE")
 
     finally:
         if in_dev_mode:
