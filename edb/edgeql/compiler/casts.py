57e972c2c16379cc965fbf9f39351c4e14545e31
Aljaž Mur Eržen <aljaz.erzen@gmail.com>
2022-10-03 16:14:08 +0200

    refactor

--- edb/edgeql/compiler/casts.py
+++ edb/edgeql/compiler/casts.py
@@ -69,7 +69,7 @@ def compile_cast(
             ctx=ctx,
             srcctx=ir_expr.context)
 
-    elif irutils.is_untyped_empty_array_expr(ir_expr):
+    if irutils.is_untyped_empty_array_expr(ir_expr):
         # Ditto for empty arrays.
         new_typeref = typegen.type_to_typeref(new_stype, ctx.env)
         return setgen.ensure_set(
@@ -81,7 +81,7 @@ def compile_cast(
     if (orig_stype == new_stype and
             cardinality_mod is not qlast.CardinalityModifier.Required):
         return ir_set
-    elif orig_stype.is_object_type() and new_stype.is_object_type():
+    if orig_stype.is_object_type() and new_stype.is_object_type():
         # Object types cannot be cast between themselves,
         # as cast is a _constructor_ operation, and the only
         # valid way to construct an object is to INSERT it.
@@ -99,27 +99,23 @@ def compile_cast(
         return _cast_array_literal(
             ir_set, orig_stype, new_stype, srcctx=srcctx, ctx=ctx)
 
-    elif orig_stype.is_tuple(ctx.env.schema):
+    if orig_stype.is_tuple(ctx.env.schema):
         return _cast_tuple(
             ir_set, orig_stype, new_stype, srcctx=srcctx, ctx=ctx)
 
-    elif (
-        orig_stype.is_array()
-        and not s_types.is_type_compatible(
-            orig_stype, new_stype, schema=ctx.env.schema)
+    if orig_stype.is_array() and not s_types.is_type_compatible(
+        orig_stype, new_stype, schema=ctx.env.schema
     ):
         return _cast_array(
             ir_set, orig_stype, new_stype, srcctx=srcctx, ctx=ctx)
 
-    elif (
-        orig_stype.is_range()
-        and not s_types.is_type_compatible(
-            orig_stype, new_stype, schema=ctx.env.schema)
+    if orig_stype.is_range() and not s_types.is_type_compatible(
+        orig_stype, new_stype, schema=ctx.env.schema
     ):
         return _cast_range(
             ir_set, orig_stype, new_stype, srcctx=srcctx, ctx=ctx)
 
-    elif orig_stype.issubclass(ctx.env.schema, new_stype):
+    if orig_stype.issubclass(ctx.env.schema, new_stype):
         # The new type is a supertype of the old type,
         # and is always a wider domain, so we simply reassign
         # the stype.
@@ -127,61 +123,96 @@ def compile_cast(
             ir_set, orig_stype, new_stype,
             cardinality_mod=cardinality_mod, ctx=ctx)
 
-    elif new_stype.issubclass(ctx.env.schema, orig_stype):
+    if new_stype.issubclass(ctx.env.schema, orig_stype):
         # The new type is a subtype, so may potentially have
         # a more restrictive domain, generate a cast call.
         return _inheritance_cast_to_ir(
             ir_set, orig_stype, new_stype,
             cardinality_mod=cardinality_mod, ctx=ctx)
 
+    if (
+        new_stype.issubclass(ctx.env.schema, json_t)
+        and ir_set.path_id.is_objtype_path()
+    ):
+        # JSON casts of objects are special: we want the full shape
+        # and not just an identity.
+        with ctx.new() as subctx:
+            subctx.implicit_id_in_shapes = False
+            subctx.implicit_tid_in_shapes = False
+            subctx.implicit_tname_in_shapes = False
+            viewgen.late_compile_view_shapes(ir_set, ctx=subctx)
     else:
-        if (new_stype.issubclass(ctx.env.schema, json_t) and
-                ir_set.path_id.is_objtype_path()):
-            # JSON casts of objects are special: we want the full shape
-            # and not just an identity.
-            with ctx.new() as subctx:
-                subctx.implicit_id_in_shapes = False
-                subctx.implicit_tid_in_shapes = False
-                subctx.implicit_tname_in_shapes = False
-                viewgen.late_compile_view_shapes(ir_set, ctx=subctx)
-        elif (orig_stype.issubclass(ctx.env.schema, json_t)
-              and new_stype.is_enum(ctx.env.schema)):
+        if orig_stype.issubclass(ctx.env.schema, json_t) and new_stype.is_enum(
+            ctx.env.schema
+        ):
             # Casts from json to enums need some special handling
             # here, where we have access to the enum type. Just turn
             # it into json->str and str->enum.
-            str_typ = ctx.env.get_track_schema_type(
-                sn.QualName('std', 'str'))
+            str_typ = ctx.env.get_track_schema_type(sn.QualName('std', 'str'))
             str_ir = compile_cast(ir_expr, str_typ, srcctx=srcctx, ctx=ctx)
-            return compile_cast(str_ir, new_stype,
-                                cardinality_mod=cardinality_mod,
-                                srcctx=srcctx, ctx=ctx)
-        elif (orig_stype.issubclass(ctx.env.schema, json_t)
-              and isinstance(new_stype, s_types.Array)
-              and not new_stype.get_subtypes(ctx.env.schema)[0].issubclass(
-                  ctx.env.schema, json_t)):
+            return compile_cast(
+                str_ir,
+                new_stype,
+                cardinality_mod=cardinality_mod,
+                srcctx=srcctx,
+                ctx=ctx,
+            )
+
+        if (
+            orig_stype.issubclass(ctx.env.schema, json_t)
+            and isinstance(new_stype, s_types.Array)
+            and not new_stype.get_subtypes(ctx.env.schema)[0].issubclass(
+                ctx.env.schema, json_t
+            )
+        ):
             # Turn casts from json->array<T> into json->array<json>
             # and array<json>->array<T>.
             ctx.env.schema, json_array_typ = s_types.Array.from_subtypes(
-                ctx.env.schema, [json_t])
+                ctx.env.schema, [json_t]
+            )
             json_array_ir = compile_cast(
-                ir_expr, json_array_typ, cardinality_mod=cardinality_mod,
-                srcctx=srcctx, ctx=ctx)
+                ir_expr,
+                json_array_typ,
+                cardinality_mod=cardinality_mod,
+                srcctx=srcctx,
+                ctx=ctx,
+            )
             return compile_cast(
-                json_array_ir, new_stype, srcctx=srcctx, ctx=ctx)
-        elif (orig_stype.issubclass(ctx.env.schema, json_t)
-              and isinstance(new_stype, s_types.Tuple)):
-            return _cast_json_to_tuple(
-                ir_set, orig_stype, new_stype, cardinality_mod,
-                srcctx=srcctx, ctx=ctx)
-        elif (orig_stype.issubclass(ctx.env.schema, json_t)
-              and isinstance(new_stype, s_types.Range)):
-            return _cast_json_to_range(
-                ir_set, orig_stype, new_stype, cardinality_mod,
-                srcctx=srcctx, ctx=ctx)
+                json_array_ir, new_stype, srcctx=srcctx, ctx=ctx
+            )
 
-        return _compile_cast(
-            ir_expr, orig_stype, new_stype, cardinality_mod=cardinality_mod,
-            srcctx=srcctx, ctx=ctx)
+        if orig_stype.issubclass(ctx.env.schema, json_t) and isinstance(
+            new_stype, s_types.Tuple
+        ):
+            return _cast_json_to_tuple(
+                ir_set,
+                orig_stype,
+                new_stype,
+                cardinality_mod,
+                srcctx=srcctx,
+                ctx=ctx,
+            )
+
+        if orig_stype.issubclass(ctx.env.schema, json_t) and isinstance(
+            new_stype, s_types.Range
+        ):
+            return _cast_json_to_range(
+                ir_set,
+                orig_stype,
+                new_stype,
+                cardinality_mod,
+                srcctx=srcctx,
+                ctx=ctx,
+            )
+
+    return _compile_cast(
+        ir_expr,
+        orig_stype,
+        new_stype,
+        cardinality_mod=cardinality_mod,
+        srcctx=srcctx,
+        ctx=ctx,
+    )
 
 
 def _compile_cast(
