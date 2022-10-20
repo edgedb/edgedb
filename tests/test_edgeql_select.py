19158a50de00eea19fbade91d01ec64b6d8002a5
Aljaž Mur Eržen <aljaz.erzen@gmail.com>
2022-10-20 22:01:21 +0200

    fmt

--- tests/test_edgeql_select.py
+++ tests/test_edgeql_select.py
@@ -1424,7 +1424,6 @@ class TestEdgeQLSelect(tb.QueryTestCase):
                 };
             ''')
 
-
     async def test_edgeql_select_polymorphic_06(self):
         await self.assert_query_result(
             r'''
