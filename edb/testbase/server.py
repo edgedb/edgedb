2b426dccf6f2c4af60915a8ee47a41eff1f6a00c
Michael J. Sullivan <sully@msully.net>
2022-11-12 00:25:57 -0500

    Pass password argument if present for SQLQueryTestCase (#4656)

--- edb/testbase/server.py
+++ edb/testbase/server.py
@@ -1232,10 +1232,13 @@ class SQLQueryTestCase(BaseQueryTestCase):
         if pgaddr is None:
             raise unittest.SkipTest('SQL tests requires devmode')
         pgaddr = json.loads(pgaddr)
-        pgdsn = (
-            f'postgres:///{pgaddr["database"]}?user={pgaddr["user"]}'
-            f'&port={pgaddr["port"]}&host={pgaddr["host"]}'
-        )
+
+        pgdsn = os.environ.get('EDGEDB_TEST_BACKEND_DSN')
+        if not pgdsn:
+            pgdsn = (
+                f'postgres:///{pgaddr["database"]}?user={pgaddr["user"]}'
+                f'&port={pgaddr["port"]}&host={pgaddr["host"]}'
+            )
 
         cls.scon = cls.loop.run_until_complete(
             asyncpg.connect(pgdsn))
