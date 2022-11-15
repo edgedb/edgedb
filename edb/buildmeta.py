f2dcfaff358bd25fde065a5122eb2d56e66724d1
Victor Petrovykh <victor@edgedb.com>
2022-11-14 19:41:26 -0500

    Fix `max_ex_value` constraint.
    The constraint was not exclusive. This is now fixed.

--- edb/buildmeta.py
+++ edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2022_11_04_00_00
+EDGEDB_CATALOG_VERSION = 2022_11_15_00_00
 EDGEDB_MAJOR_VERSION = 3
 
 
