94208869c959c0cec052f4ab7d33b395ec62fd1d
Devon Campbell <devon@edgedb.com>
2022-12-01 09:06:26 -0800

    Hide Heroku guide
    Temporarily hiding this while the buildpack is broken due to a recent
    Heroku change. See https://github.com/edgedb/edgedb/issues/4744

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['guides/deployment/heroku.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
