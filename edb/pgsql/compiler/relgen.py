71583236acc5c31e3a33fa29006244900b59e63b
Michael J. Sullivan <sully@msully.net>
2023-01-23 11:03:18 -0800

    Fix two issues with DML in free objects (#4902)
    Fix an ISE where the volatility ref from the free object will
    get referenced in the CTE for a for loop that contains DML
    inside the free object. Fix this by not inserting volatility
    refs for free objects, where they shouldn't be needed.
    Fixes #4875.
    
    Also fix an issue where DML in a non-exposed free object would not be
    executed. Fix this by only allowing free object DML in exposed
    positions.

--- edb/pgsql/compiler/relgen.py
+++ edb/pgsql/compiler/relgen.py
@@ -1312,7 +1312,10 @@ def process_set_as_subquery(
         semi_join = False
 
         if ir_source is not None:
-            if ir_source.path_id != ctx.current_insert_path_id:
+            if (
+                ir_source.path_id != ctx.current_insert_path_id
+                and not irutils.is_trivial_free_object(ir_source)
+            ):
                 # This is a computable pointer.  In order to ensure that
                 # the volatile functions in the pointer expression are called
                 # the necessary number of times, we must inject a
@@ -1322,6 +1325,17 @@ def process_set_as_subquery(
                 # If the source is an insert that we are in the middle
                 # of doing, we don't have a volatility ref to add, so
                 # skip it based on the current_insert_path_id check.
+
+                # Note also that we skip this when the source is a
+                # trivial free object reference. A trivial free object
+                # reference is always executed exactly once (if there
+                # is an outer iterator of some kind, we'll pick up
+                # *that* volatility ref) and, unlike other shapes, may
+                # contain DML. We disable the volatility ref for
+                # trival free objects then both as a minor
+                # optimization and to avoid it interfering with DML in
+                # the object (since the volatility ref would not be
+                # visible in DML CTEs).
                 path_id = ir_source.path_id
                 newctx.volatility_ref += (
                     lambda _stmt, xctx: relctx.maybe_get_path_var(
