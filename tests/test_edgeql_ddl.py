edgedb/edgedb

commit a21c48f24d9fab6cbc45ff0bbb80d2f9c660acec

DML in fill_expr & conv_expr (#4873)
* pg.delta extract CTEs to top level for fill_expr and conv_expr

* wrap conv_expr into for loop via IR

* *Always* register the tgt_path_id identity as being present

* Refactor external_rvar logic to only live in relctx helpers

---------

Co-authored-by: Michael J. Sullivan <sully@msully.net>

tests/test_edgeql_ddl.py
@@ -2839,6 +2839,144 @@ class TestEdgeQLDDL(tb.DDLTestCase):
                 ALTER TYPE Foo ALTER LINK l SET TYPE Spam USING (SELECT Spam)
             """)
 
+    async def test_edgeql_ddl_ptr_using_dml_01(self):
+        await self.con.execute(
+            r"""
+            CREATE TYPE Hello;
+            CREATE TYPE World {
+                CREATE LINK hell -> Hello;
+                CREATE MULTI LINK heaven -> Hello;
+            };
+            INSERT World {
+                heaven := {
+                    (INSERT Hello),
+                    (INSERT Hello),
+                    (INSERT Hello),
+                }
+            };
+            INSERT World {
+                hell := (INSERT Hello),
+                heaven := {
+                    (INSERT Hello),
+                    (INSERT Hello),
+                }
+            };
+            """
+        )
+        self.assertEqual(len(await self.con.query("SELECT Hello")), 6)
+
+        await self.con.execute(
+            r"""
+            ALTER TYPE World {
+                ALTER LINK hell SET REQUIRED USING (INSERT Hello);
+                ALTER LINK heaven SET SINGLE USING (INSERT Hello);
+            }
+            """
+        )
+
+        self.assertEqual(len(await self.con.query("SELECT Hello")), 9)
+
+        res = await self.con.query(
+            "SELECT World { hell: { id }, heaven: { id } }"
+        )
+        self.assertEqual(len(res), 2)
+        set_of_hellos = {
+            world.hell.id for world in res
+        }.union({
+            world.heaven.id for world in res
+        })
+        self.assertEqual(len(set_of_hellos), 4)
+
+    async def test_edgeql_ddl_ptr_using_dml_02(self):
+        await self.con.execute(
+            r"""
+            CREATE TYPE Hello;
+            CREATE TYPE World {
+                CREATE LINK hell -> Hello;
+                CREATE MULTI LINK heaven -> Hello;
+            };
+            """
+        )
+
+        await self.con.execute(
+            r"""
+            ALTER TYPE World {
+                ALTER LINK hell SET REQUIRED USING (INSERT Hello);
+                ALTER LINK heaven SET SINGLE USING (INSERT Hello);
+            }
+            """
+        )
+
+    async def test_edgeql_ddl_ptr_using_dml_03(self):
+        async with self.assertRaisesRegexTx(
+            edgedb.SchemaError, r"cannot include mutating statements"
+        ):
+            await self.con.execute(
+                r"""
+                CREATE TYPE Hello;
+                CREATE TYPE Goodbye;
+                CREATE TYPE World {
+                    CREATE LINK hell -> Hello;
+                };
+                INSERT World { hell:= (INSERT Hello) };
+                INSERT World {};
+                ALTER TYPE World {
+                    ALTER LINK hell SET TYPE Goodbye USING (INSERT Goodbye);
+                }
+                """
+            )
+
+            await self.assert_query_result(
+                'select Goodbye',
+                [{}, {}]
+            )
+
+    async def test_edgeql_ddl_ptr_using_dml_04(self):
+        await self.con.execute(
+            r"""
+            CREATE TYPE Box {
+                CREATE PROPERTY chance_of_success -> str;
+            };
+            INSERT Box { chance_of_success := 'low' };
+            INSERT Box { chance_of_success := 'high' };
+            INSERT Box { chance_of_success := 'none' };
+            ALTER TYPE Box {
+                ALTER PROPERTY chance_of_success
+                    SET TYPE float64
+                    USING (random());
+            }
+            """
+        )
+
+        res = await self.con.query('select Box { chance_of_success }')
+        self.assertEqual(len(res), 3)
+        self.assertNotEqual(res[0].chance_of_success, res[1].chance_of_success)
+        self.assertNotEqual(res[1].chance_of_success, res[2].chance_of_success)
+        self.assertNotEqual(res[2].chance_of_success, res[0].chance_of_success)
+
+    async def test_edgeql_ddl_ptr_using_dml_05(self):
+        await self.con.execute(
+            r"""
+            CREATE TYPE Hello {
+                CREATE PROPERTY bar -> str;
+            };
+            CREATE TYPE World {
+                CREATE PROPERTY foo -> str;
+                CREATE LINK hell -> Hello;
+            };
+            INSERT World { foo := 'hello' };
+            """
+        )
+
+        await self.con.execute(
+            r"""
+            ALTER TYPE World {
+                ALTER LINK hell SET REQUIRED
+                    USING (INSERT Hello { bar := World.foo });
+            }
+            """
+        )
+
     async def test_edgeql_ddl_ptr_set_cardinality_01(self):
         await self.con.execute(r'''
             CREATE TYPE Foo {

edb/pgsql/codegen.py
@@ -111,13 +111,18 @@ class SQLSourceGenerator(codegen.SourceGenerator):
             exceptions.add_context(e, ctx)
             raise
 
+    @classmethod
+    def ctes_to_source(cls, ctes: List[pgast.CommonTableExpr]) -> str:
+        generator = cls()
+        generator.gen_ctes(ctes)
+        return ''.join(generator.result)
+
     def generic_visit(self, node):  # type: ignore
         raise SQLSourceGeneratorError(
             'No method to generate code for %s' % node.__class__.__name__
         )
 
     def gen_ctes(self, ctes: List[pgast.CommonTableExpr]) -> None:
-        self.write('WITH')
         count = len(ctes)
         for i, cte in enumerate(ctes):
             self.new_lines = 1
@@ -205,6 +210,7 @@ class SQLSourceGenerator(codegen.SourceGenerator):
                     self.indentation += 1
 
         if node.ctes:
+            self.write('WITH ')
             self.gen_ctes(node.ctes)
 
         # If reordered is True, we try to put the FROM clause *before* SELECT,
@@ -332,6 +338,7 @@ class SQLSourceGenerator(codegen.SourceGenerator):
 
     def visit_InsertStmt(self, node: pgast.InsertStmt) -> None:
         if node.ctes:
+            self.write('WITH ')
             self.gen_ctes(node.ctes)
 
         self.write('INSERT INTO ')
@@ -391,6 +398,7 @@ class SQLSourceGenerator(codegen.SourceGenerator):
 
     def visit_UpdateStmt(self, node: pgast.UpdateStmt) -> None:
         if node.ctes:
+            self.write('WITH ')
             self.gen_ctes(node.ctes)
 
         self.write('UPDATE ')
@@ -433,6 +441,7 @@ class SQLSourceGenerator(codegen.SourceGenerator):
 
     def visit_DeleteStmt(self, node: pgast.DeleteStmt) -> None:
         if node.ctes:
+            self.write('WITH ')
             self.gen_ctes(node.ctes)
 
         self.write('DELETE FROM ')

edb/pgsql/compiler/__init__.py
@@ -56,8 +56,11 @@ def compile_ir_to_sql_tree(
     external_rvars: Optional[
         Mapping[Tuple[irast.PathId, str], pgast.PathRangeVar]
     ] = None,
+    external_rels: Optional[
+        Mapping[irast.PathId, pgast.BaseRelation]
+    ] = None,
     backend_runtime_params: Optional[pgparams.BackendRuntimeParams]=None,
-) -> pgast.Base:
+) -> Tuple[pgast.Base, context.Environment]:
     try:
         # Transform to sql tree
         query_params = []
@@ -100,6 +103,7 @@ def compile_ir_to_sql_tree(
             singleton_mode=singleton_mode,
             scope_tree_nodes=scope_tree_nodes,
             external_rvars=external_rvars,
+            external_rels=external_rels,
             backend_runtime_params=backend_runtime_params,
         )
 
@@ -136,7 +140,7 @@ def compile_ir_to_sql_tree(
             args = []
         raise errors.InternalServerError(*args) from e
 
-    return qtree
+    return (qtree, env)
 
 
 def compile_ir_to_sql(
@@ -152,7 +156,7 @@ def compile_ir_to_sql(
     backend_runtime_params: Optional[pgparams.BackendRuntimeParams]=None,
 ) -> Tuple[str, Dict[str, pgast.Param]]:
 
-    qtree = compile_ir_to_sql_tree(
+    qtree, _ = compile_ir_to_sql_tree(
         ir_expr,
         output_format=output_format,
         ignore_shapes=ignore_shapes,
@@ -239,21 +243,10 @@ def new_external_rvar(
     resulting range var as a ``(path_id, tuple-of-aspects): attribute name``
     mapping.
     """
-    if len(rel_name) == 1:
-        table_name = rel_name[0]
-        schema_name = None
-    elif len(rel_name) == 2:
-        schema_name, table_name = rel_name
-    else:
-        raise AssertionError(f'unexpected rvar name: {rel_name}')
+    rel = new_external_rel(rel_name=rel_name, path_id=path_id)
+    assert rel.name
 
-    rel = pgast.Relation(
-        name=table_name,
-        schemaname=schema_name,
-        path_id=path_id,
-    )
-
-    alias = pgast.Alias(aliasname=table_name)
+    alias = pgast.Alias(aliasname=rel.name)
 
     if not path_id.is_ptr_path():
         rvar = pgast.RelRangeVar(
@@ -268,3 +261,23 @@ def new_external_rvar(
             rel.path_outputs[output_pid, aspect] = var
 
     return rvar
+
+
+def new_external_rel(
+    *,
+    rel_name: Tuple[str, ...],
+    path_id: irast.PathId,
+) -> pgast.Relation:
+    if len(rel_name) == 1:
+        table_name = rel_name[0]
+        schema_name = None
+    elif len(rel_name) == 2:
+        schema_name, table_name = rel_name
+    else:
+        raise AssertionError(f'unexpected rvar name: {rel_name}')
+
+    return pgast.Relation(
+        name=table_name,
+        schemaname=schema_name,
+        path_id=path_id,
+    )

edb/pgsql/compiler/clauses.py
@@ -192,13 +192,18 @@ def compile_iterator_expr(
         # If the iterator value is nullable, add a null test. This
         # makes sure that we don't spuriously produce output when
         # iterating over options pointers.
-        assert isinstance(iterator_query, pgast.SelectStmt)
-        iterator_var = pathctx.get_path_value_var(
-            iterator_query, path_id=iterator_expr.path_id, env=ctx.env)
-        if iterator_var.nullable:
-            iterator_query.where_clause = astutils.extend_binop(
-                iterator_query.where_clause,
-                pgast.NullTest(arg=iterator_var, negated=True))
+        if isinstance(iterator_query, pgast.SelectStmt):
+            iterator_var = pathctx.get_path_value_var(
+                iterator_query, path_id=iterator_expr.path_id, env=ctx.env)
+            if iterator_var.nullable:
+                iterator_query.where_clause = astutils.extend_binop(
+                    iterator_query.where_clause,
+                    pgast.NullTest(arg=iterator_var, negated=True))
+        elif isinstance(iterator_query, pgast.Relation):
+            # will never be null
+            pass
+        else:
+            raise NotImplementedError()
 
         # Regardless of result type, we use transient identity,
         # for path identity of the iterator expression.  This is

edb/pgsql/compiler/context.py
@@ -386,6 +386,7 @@ class Environment:
     type_rewrites: Dict[RewriteKey, irast.Set]
     scope_tree_nodes: Dict[int, irast.ScopeTreeNode]
     external_rvars: Mapping[Tuple[irast.PathId, str], pgast.PathRangeVar]
+    external_rels: Mapping[irast.PathId, pgast.BaseRelation]
     materialized_views: Dict[uuid.UUID, irast.Set]
     backend_runtime_params: pgparams.BackendRuntimeParams
 
@@ -409,6 +410,9 @@ class Environment:
         external_rvars: Optional[
             Mapping[Tuple[irast.PathId, str], pgast.PathRangeVar]
         ] = None,
+        external_rels: Optional[
+            Mapping[irast.PathId, pgast.BaseRelation]
+        ] = None,
         backend_runtime_params: pgparams.BackendRuntimeParams,
     ) -> None:
         self.aliases = aliases.AliasGenerator()
@@ -424,6 +428,7 @@ class Environment:
         self.type_rewrites = type_rewrites
         self.scope_tree_nodes = scope_tree_nodes
         self.external_rvars = external_rvars or {}
+        self.external_rels = external_rels or {}
         self.materialized_views = {}
         self.check_ctes = []
         self.backend_runtime_params = backend_runtime_params

edb/pgsql/compiler/pathctx.py
@@ -857,10 +857,10 @@ def maybe_get_path_rvar(
         stmt: pgast.Query, path_id: irast.PathId, *, aspect: str,
         flavor: str='normal',
         env: context.Environment) -> Optional[pgast.PathRangeVar]:
-    rvar = env.external_rvars.get((path_id, aspect))
+    rvar = None
     path_rvar_map = stmt.maybe_get_rvar_map(flavor)
     if path_rvar_map is not None:
-        if rvar is None and path_rvar_map:
+        if path_rvar_map:
             rvar = path_rvar_map.get((path_id, aspect))
         if rvar is None and aspect == 'identity':
             rvar = path_rvar_map.get((path_id, 'value'))

edb/pgsql/compiler/relctx.py
@@ -260,11 +260,7 @@ def include_specific_rvar(
     """
 
     if not has_rvar(stmt, rvar, ctx=ctx):
-        if not (
-            ctx.env.external_rvars
-            and has_external_rvar(rvar, ctx=ctx)
-        ):
-            rel_join(stmt, rvar, ctx=ctx)
+        rel_join(stmt, rvar, ctx=ctx)
         # Make sure that the path namespace of *rvar* is mapped
         # onto the path namespace of *stmt*.
         if pull_namespace:
@@ -298,6 +294,9 @@ def has_rvar(
 
     curstmt: Optional[pgast.Query] = stmt
 
+    if ctx.env.external_rvars and has_external_rvar(rvar, ctx=ctx):
+        return True
+
     while curstmt is not None:
         if pathctx.has_rvar(curstmt, rvar, env=ctx.env):
             return True
@@ -322,6 +321,10 @@ def _maybe_get_path_rvar(
     aspect: str,
     ctx: context.CompilerContextLevel,
 ) -> Optional[Tuple[pgast.PathRangeVar, irast.PathId]]:
+    rvar = ctx.env.external_rvars.get((path_id, aspect))
+    if rvar:
+        return rvar, path_id
+
     qry: Optional[pgast.Query] = stmt
     while qry is not None:
         rvar = pathctx.maybe_get_path_rvar(

commit 011d4e5595870cba3b4cc5256f54fac6e8a50a15

Fix creation of a new link property with a default value
This was a regression from 2.9 to 2.10, introduced by #4873.
`_alter_pointer_optionality` is also (ab)used to populate newly
created pointers with their default values, and #4873 broke that flow
for link properties (which don't have an `id` field).

Link properties *shouldn't* be running the
`_alter_pointer_optionality` explicit flow for explicitly populating a
field, though! Defaults on link properties are (unlike defaults on
object properties) currently represented using SQL defaults. (Though I
want to change this eventually since it places more limitations on
what can be done.)

Fixes #5060.

edb/pgsql/delta.py
@@ -5253,6 +5253,11 @@ class PropertyMetaCommand(PointerMetaCommand[s_props.Property]):
                 (default := prop.get_default(schema))
                 and not prop.is_pure_computable(schema)
                 and not fills_required
+                # link properties use SQL defaults and shouldn't need
+                # us to do it explicitly (which is good, since
+                # _alter_pointer_optionality doesn't currently work on
+                # linkprops)
+                and not prop.is_link_property(schema)
             ):
                 self._alter_pointer_optionality(
                     schema, schema, context, fill_expr=default)

tests/test_edgeql_ddl.py
@@ -3438,6 +3438,27 @@ class TestEdgeQLDDL(tb.DDLTestCase):
             json_only=True,
         )
 
+    async def test_edgeql_ddl_link_property_10(self):
+        await self.con.execute("""
+            CREATE TYPE default::User;
+            CREATE TYPE default::Survey {
+                CREATE MULTI LINK recipients -> default::User;
+            };
+            insert Survey { recipients := (insert User) };
+        """)
+
+        await self.con.execute("""
+            alter type Survey alter link recipients
+            create property destination -> str { set default := "email" };
+        """)
+
+        await self.assert_query_result(
+            r"""
+                select Survey { recipients: {@destination} };
+            """,
+            [{"recipients": [{"@destination": "email"}]}]
+        )
+
     async def test_edgeql_ddl_bad_01(self):
         with self.assertRaisesRegex(
                 edgedb.InvalidReferenceError,