edgedb/edgedb

commit 61b90a0801591d2b746bc32ad50e6189cbacd2f6

Optimize trivial WITH-bound `group` uses (#4978)
If the last alias in a WITH block is a GROUP query that is immediately
used in a shape or a for loop, inline it so that our other
optimizations apply.

This probably covers 95% of WITH-bound group use cases, despite being
a "hyper-local hack", as I said in #4243.

Fixes #4243. (Mostly.)

edb/edgeql/desugar_group.py
@@ -27,6 +27,7 @@ from __future__ import annotations
 
 from typing import *
 
+from edb.common import ast
 from edb.common import ordered
 from edb.common.compiler import AliasGenerator
 
@@ -165,6 +166,20 @@ def desugar_group(
     )
 
 
+def _count_alias_uses(
+    node: qlast.Expr,
+    alias: str,
+) -> int:
+    uses = 0
+    for child in ast.find_children(node, qlast.Path):
+        match child:
+            case qlast.Path(steps=[
+                qlast.ObjectRef(module=None, name=alias2)
+            ]) if alias == alias2:
+                uses += 1
+    return uses
+
+
 def try_group_rewrite(
     node: qlast.Query,
     aliases: AliasGenerator,
@@ -199,7 +214,40 @@ def try_group_rewrite(
         )
     """
 
-    # TODO: would Python's new pattern matching fit well here???
+    # Inline trivial uses of aliases bound to a group and then
+    # immediately used, so that we can apply the other optimizations.
+    match node:
+        case qlast.SelectQuery(
+            aliases=[
+                *_,
+                qlast.AliasedExpr(alias=alias, expr=qlast.GroupQuery() as grp)
+            ] as qaliases,
+            result=qlast.Shape(
+                expr=qlast.Path(steps=[
+                    qlast.ObjectRef(module=None, name=alias2)
+                ]),
+                elements=elements,
+            ) as result,
+        ) if alias == alias2 and _count_alias_uses(result, alias) == 1:
+            node = node.replace(
+                aliases=qaliases[:-1],
+                result=qlast.Shape(expr=grp, elements=elements),
+            )
+
+        case qlast.ForQuery(
+            aliases=[
+                *_,
+                qlast.AliasedExpr(alias=alias, expr=qlast.GroupQuery() as grp)
+            ] as qaliases,
+            iterator=qlast.Path(steps=[
+                qlast.ObjectRef(module=None, name=alias2)
+            ]),
+            result=result,
+        ) if alias == alias2 and _count_alias_uses(result, alias) == 0:
+            node = node.replace(
+                aliases=qaliases[:-1],
+                iterator=grp,
+            )
 
     # Sink shapes into the GROUP
     if (

tests/test_edgeql_sql_codegen.py
@@ -209,6 +209,20 @@ class TestEdgeQLSQLCodegen(tb.BaseEdgeQLCompilerTest):
             "group has unnecessary uuid_generate",
         )
 
+    def test_codegen_group_binding(self):
+        sql = self._compile('''
+        with g := (group Issue by .status)
+        select g {
+            name := .key.status.name,
+            num := count(.elements),
+        } order by .name
+        ''')
+
+        self.assertNotIn(
+            "array_agg", sql,
+            "group has unnecessary array_agg",
+        )
+
     def test_codegen_in_array_unpack_no_dupe(self):
         sql = self._compile('''
             select 1 in array_unpack(

commit 57ac83d815174af831a0c002b824fa3cdafe4fea

Fix GROUP regression with some query-builder queries
The optimization introduced in #4978 to optimize trivial with-bound
queries had a bug in the code where it tried to make sure the alias
was only used exactly once: it failed to consider when it appeared
at the head of a path. Fix that oversight.

Fixes #5070.

edb/edgeql/compiler/astutils.py
@@ -152,13 +152,16 @@ def find_parameters(
     return v.params
 
 
-class alias_view(view_patterns.ViewPattern[str], targets=(qlast.Base,)):
+class alias_view(
+    view_patterns.ViewPattern[tuple[str, list[qlast.PathElement]]],
+    targets=(qlast.Base,),
+):
     @staticmethod
-    def match(obj: object) -> str:
+    def match(obj: object) -> tuple[str, list[qlast.PathElement]]:
         match obj:
             case qlast.Path(
-                steps=[qlast.ObjectRef(module=None, name=alias)],
+                steps=[qlast.ObjectRef(module=None, name=alias), *rest],
                 partial=False,
             ):
-                return alias
+                return alias, rest
         raise view_patterns.NoMatch

edb/edgeql/desugar_group.py
@@ -174,7 +174,7 @@ def _count_alias_uses(
     uses = 0
     for child in ast.find_children(node, qlast.Path):
         match child:
-            case astutils.alias_view(alias2) if alias == alias2:
+            case astutils.alias_view((alias2, _)) if alias == alias2:
                 uses += 1
     return uses
 
@@ -222,7 +222,7 @@ def try_group_rewrite(
                 qlast.AliasedExpr(alias=alias, expr=qlast.GroupQuery() as grp)
             ] as qaliases,
             result=qlast.Shape(
-                expr=astutils.alias_view(alias2),
+                expr=astutils.alias_view((alias2, [])),
                 elements=elements,
             ) as result,
         ) if alias == alias2 and _count_alias_uses(result, alias) == 1:
@@ -236,7 +236,7 @@ def try_group_rewrite(
                 *_,
                 qlast.AliasedExpr(alias=alias, expr=qlast.GroupQuery() as grp)
             ] as qaliases,
-            iterator=astutils.alias_view(alias2),
+            iterator=astutils.alias_view((alias2, [])),
             result=result,
         ) if alias == alias2 and _count_alias_uses(result, alias) == 0:
             node = node.replace(

tests/test_edgeql_group.py
@@ -1329,3 +1329,19 @@ class TestEdgeQLGroup(tb.QueryTestCase):
                 }
             ]),
         )
+
+    async def test_edgeql_group_uses_name_01(self):
+        # Make sure that our crappy optimizations don't break anything
+        await self.con.query(
+            r'''
+            WITH g := (GROUP cards::Card BY .cost)
+            SELECT g {
+              key: {cost},
+              grouping,
+              elements: {
+                name,
+                multi owners := g.elements.owners { name },
+              }
+            };
+            ''',
+        )