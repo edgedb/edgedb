1c80bad9d1ef1f36d9b263a1f079b4cbd7df4c95
Aljaž Mur Eržen <aljaz.erzen@gmail.com>
2023-02-22 19:37:33 +0100

    refactor ShapeContext

--- edb/edgeql/compiler/stmt.py
+++ edb/edgeql/compiler/stmt.py
@@ -1442,7 +1442,6 @@ def compile_query_subject(
             view_rptr=view_rptr,
             view_name=view_name,
             exprtype=exprtype,
-            parser_context=expr.context,
             ctx=ctx,
         )
 
