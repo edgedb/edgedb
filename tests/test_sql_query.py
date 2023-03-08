edgedb/edgedb

commit 051afcc4a6eb7005f940dced5a9e6c52cb091af0

Forward ParameterStatus in simple query

edb/server/pgcon/pgcon.pyx
@@ -1559,13 +1559,26 @@ cdef class PGConnection:
                     else:
                         self.buffer.redirect_messages(buf, mtype, 0)
 
-            # CloseComplete is skipped in wait_for_sync()
-            msg_buf = WriteBuffer.new_message(b'Z')
-            msg_buf.write_byte(await self.wait_for_sync())
-            buf.write_buffer(msg_buf.end_message())
+            while True:
+                if not self.buffer.take_message():
+                    if buf.len() > 0:
+                        fe_conn.write(buf)
+                        fe_conn.flush()
+                        buf = WriteBuffer.new()
+                    await self.wait_for_message()
+                mtype = self.buffer.get_message_type()
+                if mtype == b'3':  # CloseComplete
+                    self.buffer.discard_message()
+                elif mtype == b'Z':  # ReadyForQuery
+                    self.buffer.redirect_messages(buf, mtype, 0)
+                    break
+                else:
+                    # Other messages like ParameterStatus should be forwarded
+                    self.buffer.redirect_messages(buf, mtype, 0)
 
-            fe_conn.write(buf)
-            fe_conn.flush()
+            if buf.len() > 0:
+                fe_conn.write(buf)
+                fe_conn.flush()
         finally:
             await self.after_command()
             dbv.end_implicit()

commit d194f4b1e1be8873ff1e73f3f74e2d7be2929567

pg-ext: fix bug in simple query sync count
Bug was introduced in https://github.com/edgedb/edgedb/pull/4957/commits/051afcc4a6eb7005f940dced5a9e6c52cb091af0

Also address a CR to speed up server start

edb/server/pgcon/pgcon.pyx
@@ -103,7 +103,7 @@ cdef dict POSTGRES_SHUTDOWN_ERR_CODES = {
 }
 
 cdef bytes INIT_CON_SCRIPT = None
-cdef object EMPTY_SQL_STATE = json.dumps({}).encode('utf-8')
+cdef object EMPTY_SQL_STATE = b"{}"
 
 cdef object logger = logging.getLogger('edb.server')
 
@@ -1623,7 +1623,9 @@ cdef class PGConnection:
                 if mtype == b'3':  # CloseComplete
                     self.buffer.discard_message()
                 elif mtype == b'Z':  # ReadyForQuery
-                    self.buffer.redirect_messages(buf, mtype, 0)
+                    msg_buf = WriteBuffer.new_message(b'Z')
+                    msg_buf.write_byte(self.parse_sync_message())
+                    buf.write_buffer(msg_buf.end_message())
                     break
                 else:
                     # Other messages like ParameterStatus should be forwarded

tests/test_sql_query.py
@@ -791,3 +791,14 @@ class TestSQL(tb.SQLQueryTestCase):
             COMMIT;
             """,
         )
+
+    async def test_sql_transaction_02(self):
+        await self.scon.execute("BEGIN")
+        await self.scon.execute(
+            "SET TRANSACTION ISOLATION LEVEL read uncommitted"
+        )
+        v1 = await self.scon.fetchval("SHOW transaction_isolation")
+        self.assertEqual(v1, "read uncommitted")
+        await self.scon.execute("ROLLBACK")
+        v2 = await self.scon.fetchval("SHOW transaction_isolation")
+        self.assertNotEqual(v1, v2)