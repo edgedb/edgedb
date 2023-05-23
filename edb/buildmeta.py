930463c420d579b16225771a3806dff2ce07df0b
Michael J. Sullivan <sully@msully.net>
2023-05-23 11:16:45 -0700

    catalog

--- edb/buildmeta.py
+++ edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2023_05_04_00_02
+EDGEDB_CATALOG_VERSION = 2023_05_23_00_00
 EDGEDB_MAJOR_VERSION = 4
 
 
