e89236c2beb752b64698a9a148bc4b6269ece174
Michael J. Sullivan <sully@msully.net>
2023-06-05 15:54:24 -0700

    Extend the patch system so we can backport extension package changes (#5589)
    * Add a new patch mode, `metaschema-sql`, to recreate a function
      defined in metaschema. We need this to update a function whose
      contents are partially generated, but it's also just a much nicer
      way to update metaschema functions in general.
    * Support modifying and regenerating the views for `sys` extension
      views.  To avoid just throwing every possible change into
      `edgeql+schema`, gate it on adding `+ext` to the patch type.
    * Add a mechanism for repairing unpickled schemas. Schemas pickled and
      cached on old patch versions will have data tuples that are too
      short. When we load an old pickled schema, we need to fix it up. (We
      got away without this before because the only `edgeql+schema` patch
      we've done added a new type that obviously then couldn't appear in
      pickled schemas.)
    
    With these changes, we can succesfully cherry-pick #5479 to 3.x and
    patch it in.

--- edb/pgsql/patches.py
+++ edb/pgsql/patches.py
@@ -36,7 +36,8 @@ def get_version_key(num_patches: int):
     the key based on the number of schema layout patches that we can
     *see*, we still compute the right key.
     """
-    num_major = sum(p == 'edgeql+schema' for p, _ in PATCHES[:num_patches])
+    num_major = sum(
+        p.startswith('edgeql+schema') for p, _ in PATCHES[:num_patches])
     if num_major == 0:
         return ''
     else:
@@ -55,7 +56,7 @@ def _setup_patches(patches: list[tuple[str, str]]) -> list[tuple[str, str]]:
     npatches = []
     for kind, patch in patches:
         npatches.append((kind, patch))
-        if kind == 'edgeql+schema' and seen_repair:
+        if kind.startswith('edgeql+schema') and seen_repair:
             npatches.append(('repair', ''))
         seen_repair |= kind == 'repair'
     return npatches
@@ -66,6 +67,7 @@ The actual list of patches. The patches are (kind, script) pairs.
 
 There are currently 4 kinds:
  * sql - simply runs a SQL script
+ * metaschema-sql - create a function from metaschema
  * edgeql - runs an edgeql DDL command
  * edgeql+schema - runs an edgeql DDL command and updates the std schemas
  * repair - fix up inconsistencies in *user* schemas
