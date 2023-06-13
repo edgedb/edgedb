ae753628f77210d0738af587ecd7473250f6711c
zchenb <zhibo@edgedb.com>
2023-06-13 01:24:20 -0400

    let's try this

--- edb/edgeql/compiler/setgen.py
+++ edb/edgeql/compiler/setgen.py
@@ -728,7 +728,7 @@ def resolve_ptr_with_intersections(
             if track_ref is not False:
                 # When ptr is a computed property, it is still referenced!
                 # We need to add this to ensure the correct order in DDL.
-                if ref != ptr:
+                if ref != ptr and ptr.is_property(ctx.env.schema):
                     ctx.env.add_schema_ref(ptr, track_ref)
                 ctx.env.add_schema_ref(ref, track_ref)
                 _add_target_schema_refs(
