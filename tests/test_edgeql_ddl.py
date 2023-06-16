edgedb/edgedb

commit 45987a4436fa282b1c22f21897c431e6673e07f4

Partial paths in defaults (#4851)

edb/edgeql/compiler/viewgen.py
@@ -27,7 +27,9 @@ import functools
 from typing import *
 
 from edb import errors
+from edb.common import ast
 from edb.common import context as pctx
+from edb.common import topological
 from edb.common.typeutils import not_none
 
 from edb.ir import ast as irast
@@ -196,7 +198,7 @@ def _process_view(
             transparent=True, ctx=ctx)
 
     pointers: List[s_pointers.Pointer] = []
-    pointer_entries = []
+    pointer_entries: List[Tuple[s_pointers.Pointer, irast.Set | None]] = []
 
     elements = elements or ()
     for shape_el in elements:
@@ -246,70 +248,15 @@ def _process_view(
 
     if exprtype.is_insert():
         explicit_ptrs = {
-            ptrcls.get_local_name(ctx.env.schema)
-            for ptrcls in pointers
+            ptrcls.get_local_name(ctx.env.schema) for ptrcls in pointers
         }
-        scls_pointers = stype.get_pointers(ctx.env.schema)
-        for pn, ptrcls in scls_pointers.items(ctx.env.schema):
-            if (pn in explicit_ptrs or
-                    ptrcls.is_pure_computable(ctx.env.schema)):
-                continue
 
-            default_expr = ptrcls.get_default(ctx.env.schema)
-            if not default_expr:
-                if (
-                    ptrcls.get_required(ctx.env.schema)
-                    and pn != sn.UnqualName('__type__')
-                ):
-                    if ptrcls.is_property(ctx.env.schema):
-                        # If the target is a sequence, there's no need
-                        # for an explicit value.
-                        ptrcls_target = ptrcls.get_target(ctx.env.schema)
-                        assert ptrcls_target is not None
-                        if ptrcls_target.issubclass(
-                                ctx.env.schema,
-                                ctx.env.schema.get(
-                                    'std::sequence',
-                                    type=s_objects.SubclassableObject)):
-                            continue
-                    vn = ptrcls.get_verbosename(
-                        ctx.env.schema, with_parent=True)
-                    raise errors.MissingRequiredError(
-                        f'missing value for required {vn}')
-                else:
-                    continue
-
-            ptrcls_sn = ptrcls.get_shortname(ctx.env.schema)
-            default_ql = qlast.ShapeElement(
-                expr=qlast.Path(
-                    steps=[
-                        qlast.Ptr(
-                            ptr=qlast.ObjectRef(
-                                name=ptrcls_sn.name,
-                                module=ptrcls_sn.module,
-                            ),
-                        ),
-                    ],
-                ),
-                compexpr=qlast.DetachedExpr(
-                    expr=default_expr.qlast,
-                ),
+        pointer_entries.extend(
+            gen_pointers_from_defaults(
+                explicit_ptrs, view_scls, ir_set, stype, path_id_namespace,
+                view_rptr, exprtype, ctx
             )
-
-            with ctx.new() as scopectx:
-                pointer_entries.append(
-                    _normalize_view_ptr_expr(
-                        ir_set,
-                        default_ql,
-                        view_scls,
-                        path_id=path_id,
-                        path_id_namespace=path_id_namespace,
-                        exprtype=exprtype,
-                        from_default=True,
-                        view_rptr=view_rptr,
-                        ctx=scopectx,
-                    ),
-                )
+        )
 
     set_shape = []
     shape_ptrs: List[ShapePtr] = []
@@ -394,6 +341,111 @@ def _process_view(
     return view_scls, ir_set
 
 
+def gen_pointers_from_defaults(
+    explicit_ptrs: Set[sn.UnqualName],
+    view_scls: s_objtypes.ObjectType,
+    ir_set: irast.Set,
+    stype: s_objtypes.ObjectType,
+    path_id_namespace: Optional[irast.Namespace],
+    view_rptr: Optional[context.ViewRPtr],
+    exprtype: s_types.ExprType,
+    ctx: context.ContextLevel,
+) -> List[Tuple[s_pointers.Pointer, irast.Set | None]]:
+    path_id = ir_set.path_id
+    result: List[Tuple[s_pointers.Pointer, irast.Set | None]] = []
+
+    scls_pointers = stype.get_pointers(ctx.env.schema)
+    for pn, ptrcls in scls_pointers.items(ctx.env.schema):
+        if pn in explicit_ptrs or ptrcls.is_pure_computable(ctx.env.schema):
+            continue
+
+        default_expr = ptrcls.get_default(ctx.env.schema)
+        if not default_expr:
+            if ptrcls.get_required(ctx.env.schema) and pn != sn.UnqualName(
+                "__type__"
+            ):
+                if ptrcls.is_property(ctx.env.schema):
+                    # If the target is a sequence, there's no need
+                    # for an explicit value.
+                    ptrcls_target = ptrcls.get_target(ctx.env.schema)
+                    assert ptrcls_target is not None
+                    if ptrcls_target.issubclass(
+                        ctx.env.schema,
+                        ctx.env.schema.get(
+                            "std::sequence", type=s_objects.SubclassableObject
+                        ),
+                    ):
+                        continue
+                vn = ptrcls.get_verbosename(ctx.env.schema, with_parent=True)
+                raise errors.MissingRequiredError(
+                    f"missing value for required {vn}"
+                )
+            else:
+                continue
+
+        ptrcls_sn = ptrcls.get_shortname(ctx.env.schema)
+        default_ql = qlast.ShapeElement(
+            expr=qlast.Path(
+                steps=[
+                    qlast.Ptr(
+                        ptr=qlast.ObjectRef(
+                            name=ptrcls_sn.name,
+                            module=ptrcls_sn.module,
+                        ),
+                    ),
+                ],
+            ),
+            compexpr=qlast.DetachedExpr(
+                expr=default_expr.qlast,
+                preserve_path_prefix=True,
+            ),
+        )
+
+        with ctx.new() as scopectx:
+            result.append(
+                _normalize_view_ptr_expr(
+                    ir_set,
+                    default_ql,
+                    view_scls,
+                    path_id=path_id,
+                    path_id_namespace=path_id_namespace,
+                    exprtype=exprtype,
+                    from_default=True,
+                    view_rptr=view_rptr,
+                    ctx=scopectx,
+                ),
+            )
+
+    schema = ctx.env.schema
+
+    # Toposort defaults
+    # This is required because defaults may reference each other
+    # (and even contain cyclical dependencies).
+    # We cannot check or preprocess this at migration time, because some
+    # defaults may not be used for some inserts.
+    pointer_indexes = {}
+    for (index, (pointer, _)) in enumerate(result):
+        p = pointer.get_nearest_non_derived_parent(schema)
+        pointer_indexes[p.get_name(schema).name] = index
+    graph = {}
+    for (index, (_, irset)) in enumerate(result):
+        assert irset
+        dep_pointers = ast.find_children(irset, irast.Pointer)
+        dep_rptrs = (
+            pointer.target.path_id.rptr() for pointer in dep_pointers
+            if pointer.source.typeref.id == stype.id
+        )
+        deps = {
+            pointer_indexes[rpts.name.name] for rpts in dep_rptrs
+            if rpts and rpts.name.name in pointer_indexes
+        }
+        graph[index] = topological.DepGraphEntry(
+            item=index, deps=deps, extra=False,
+        )
+
+    return [result[i] for i in topological.sort(graph, allow_unresolved=True)]
+
+
 def _maybe_fixup_lprop(
     path_id: irast.PathId,
     ptrcls: s_pointers.Pointer,
@@ -453,7 +505,6 @@ def _compile_qlexpr(
     *,
     ptrcls: Optional[s_pointers.Pointer],
     ptrsource: s_sources.Source,
-    path_id: irast.PathId,
     ptr_name: sn.QualName,
     exprtype: s_types.ExprType = s_types.ExprType.Select,
     is_linkprop: bool,
@@ -698,7 +749,7 @@ def _normalize_view_ptr_expr(
             irexpr, _ = _compile_qlexpr(
                 ir_source, qlexpr, view_scls,
                 ptrcls=qlptrcls, ptrsource=qlptrsource,
-                path_id=path_id, ptr_name=ptr_name, is_linkprop=is_linkprop,
+                ptr_name=ptr_name, is_linkprop=is_linkprop,
                 exprtype=exprtype, ctx=ctx)
             materialized = setgen.should_materialize(
                 irexpr, ptrcls=ptrcls,
@@ -770,7 +821,7 @@ def _normalize_view_ptr_expr(
 
         irexpr, sub_view_rptr = _compile_qlexpr(
             ir_source, qlexpr, view_scls, ptrcls=ptrcls, ptrsource=ptrsource,
-            path_id=path_id, ptr_name=ptr_name, is_linkprop=is_linkprop,
+            ptr_name=ptr_name, is_linkprop=is_linkprop,
             exprtype=exprtype, ctx=ctx)
         materialized = setgen.should_materialize(
             irexpr, ptrcls=ptrcls,

tests/test_edgeql_ddl.py
@@ -559,17 +559,14 @@ class TestEdgeQLDDL(tb.DDLTestCase):
             """)
 
     async def test_edgeql_ddl_14(self):
-        with self.assertRaisesRegex(
-                edgedb.QueryError,
-                f'__source__ cannot be used in this expression'):
-            await self.con.execute("""
-                CREATE TYPE TestSelfLink1 {
-                    CREATE PROPERTY foo1 -> std::str;
-                    CREATE PROPERTY bar1 -> std::str {
-                        SET default := __source__.foo1;
-                    };
+        await self.con.execute("""
+            CREATE TYPE TestSelfLink1 {
+                CREATE PROPERTY foo1 -> std::str;
+                CREATE PROPERTY bar1 -> std::str {
+                    SET default := __source__.foo1;
                 };
-            """)
+            };
+        """)
 
     async def test_edgeql_ddl_15(self):
         await self.con.execute(r"""
@@ -607,18 +604,14 @@ class TestEdgeQLDDL(tb.DDLTestCase):
         )
 
     async def test_edgeql_ddl_16(self):
-        with self.assertRaisesRegex(
-                edgedb.SchemaDefinitionError,
-                'possibly more than one element'):
-            await self.con.execute(r"""
-                CREATE TYPE TestSelfLink3 {
-                    CREATE PROPERTY foo3 -> std::str;
-                    CREATE PROPERTY bar3 -> std::str {
-                        # NOTE: this is a set of all TestSelfLink3.foo3
-                        SET default := TestSelfLink3.foo3;
-                    };
+        await self.con.execute(r"""
+            CREATE TYPE TestSelfLink3 {
+                CREATE PROPERTY foo3 -> std::str;
+                CREATE PROPERTY bar3 -> std::str {
+                    SET default := TestSelfLink3.foo3;
                 };
-            """)
+            };
+        """)
 
     async def test_edgeql_ddl_18(self):
         await self.con.execute("""

edb/edgeql/ast.py
@@ -267,6 +267,7 @@ class Subject(SpecialAnchor):  # __subject__
 
 class DetachedExpr(Expr):  # DETACHED Expr
     expr: Expr
+    preserve_path_prefix: bool = False
 
 
 class GlobalExpr(Expr):  # GLOBAL Name

edb/edgeql/compiler/expr.py
@@ -173,6 +173,9 @@ def compile_DetachedExpr(
     ctx: context.ContextLevel,
 ) -> irast.Set:
     with ctx.detached() as subctx:
+        if expr.preserve_path_prefix:
+            subctx.partial_path_prefix = ctx.partial_path_prefix
+
         ir = dispatch.compile(expr.expr, ctx=subctx)
     # Wrap the result in another set, so that the inner namespace
     # doesn't leak out into any shapes (since computable computation

edb/schema/pointers.py
@@ -27,6 +27,8 @@ from edb import errors
 
 from edb.common import enum as s_enum
 from edb.common import struct
+from edb.common import parsing
+from edb.common import ast
 
 from edb.edgeql import ast as qlast
 from edb.edgeql import compiler as qlcompiler
@@ -1323,6 +1325,7 @@ class PointerCommandOrFragment(
         target_as_singleton: bool = False,
         expr_description: Optional[str] = None,
         no_query_rewrites: bool = False,
+        source_context: Optional[parsing.ParserContext] = None,
     ) -> s_expr.CompiledExpression:
         singletons: List[Union[s_types.Type, Pointer]] = []
 
@@ -1368,9 +1371,8 @@ class PointerCommandOrFragment(
             else:
                 singletons.append(self.scls)
 
-        compiled = expr.compiled(
-            schema=schema,
-            options=qlcompiler.CompilerOptions(
+        try:
+            options = qlcompiler.CompilerOptions(
                 modaliases=context.modaliases,
                 schema_object_context=self.get_schema_metaclass(),
                 anchors={qlast.Source().name: source},
@@ -1381,20 +1383,28 @@ class PointerCommandOrFragment(
                 ),
                 track_schema_ref_exprs=track_schema_ref_exprs,
                 in_ddl_context_name=in_ddl_context_name,
-            ),
-        )
-
-        if singleton_result_expected and compiled.cardinality.is_multi():
-            if expr_description is None:
-                expr_description = 'an expression'
-
-            raise errors.SchemaError(
-                f'possibly more than one element returned by '
-                f'{expr_description}, while a singleton is expected',
-                context=expr.qlast.context,
             )
 
-        return compiled
+            compiled = expr.compiled(schema=schema, options=options)
+
+            if singleton_result_expected and compiled.cardinality.is_multi():
+                if expr_description is None:
+                    expr_description = 'an expression'
+
+                raise errors.SchemaError(
+                    f'possibly more than one element returned by '
+                    f'{expr_description}, while a singleton is expected',
+                    context=expr.qlast.context,
+                )
+
+            return compiled
+
+        except errors.QueryError as e:
+            if source_context:
+                e.set_source_context(source_context)
+            if not e.has_source_context():
+                e.set_source_context(expr.qlast.context)
+            raise
 
     def compile_expr_field(
         self,
@@ -1584,6 +1594,8 @@ class PointerCommand(
         context: sd.CommandContext,
     ) -> None:
         """Check that pointer definition is sound."""
+        from edb.ir import ast as irast
+
         referrer_ctx = self.get_referrer_context(context)
         if referrer_ctx is None:
             return
@@ -1598,18 +1610,17 @@ class PointerCommand(
 
         if default_expr is not None:
 
-            source_context = self.get_attribute_source_context('default')
-
-            try:
-                default_expr = default_expr.ensure_compiled(schema)
-            except errors.QueryError as e:
-                e.set_source_context(source_context)
-                raise
+            if not default_expr.irast:
+                default_expr = self._compile_expr(
+                    schema, context, default_expr
+                )
+                assert default_expr.irast
 
             if scls.is_id_pointer(schema):
                 self._check_id_default(
                     schema, context, default_expr.irast.expr)
 
+            source_context = self.get_attribute_source_context('default')
             default_schema = default_expr.irast.schema
             default_type = default_expr.irast.stype
             assert default_type is not None
@@ -1645,6 +1656,34 @@ class PointerCommand(
                     f"'single'",
                     context=source_context,
                 )
+
+            # prevent references to local links, only properties
+            pointers = ast.find_children(default_expr.irast, irast.Pointer)
+            scls_source = scls.get_source(schema)
+            assert scls_source
+            for pointer in pointers:
+                if pointer.source.typeref.id != scls_source.id:
+                    continue
+                if not isinstance(pointer.ptrref, irast.PointerRef):
+                    continue
+                s_pointer = schema.get_by_id(pointer.ptrref.id, type=Pointer)
+                card = s_pointer.get_cardinality(schema)
+
+                if s_pointer.is_property(schema) and card.is_multi():
+                    raise errors.SchemaDefinitionError(
+                        f"default expression cannot refer to multi properties "
+                        "of insterted object",
+                        context=source_context,
+                        hint="this is a temporary implementation restriction",
+                    )
+
+                if not s_pointer.is_property(schema):
+                    raise errors.SchemaDefinitionError(
+                        f"default expression cannot refer to links "
+                        "of insterted object",
+                        context=source_context,
+                        hint='this is a temporary implementation restriction'
+                    )
 
     def _check_id_default(
         self,

tests/test_edgeql_insert.py
@@ -1779,6 +1779,106 @@ class TestInsert(tb.QueryTestCase):
         # The result should not include the default param
         assert not hasattr(obj, 'num')
 
+    async def test_edgeql_insert_default_07(self):
+        await self.con.query(
+            r"""
+            create type Foo {
+                create property n -> int32;
+                create property a -> str;
+                create property b -> str;
+                create property c -> str;
+            };
+
+            alter type Foo {
+                alter property a { set default := 'a=' ++ .b };
+                alter property b { set default := 'b=' ++ .c };
+                alter property c { set default := 'c=' ++ .a };
+            };
+        """
+        )
+        await self.con.query("insert Foo { n := 0, a := 'given' };")
+        await self.con.query("insert Foo { n := 1, b := 'given' };")
+        await self.con.query("insert Foo { n := 2, c := 'given' };")
+        await self.assert_query_result(
+            "select Foo { a, b, c } order by .n",
+            [
+                {"a": "given", "b": "b=c=given", "c": "c=given"},
+                {"a": "a=given", "b": "given", "c": "c=a=given"},
+                {"a": "a=b=given", "b": "b=given", "c": "given"},
+            ],
+        )
+
+    async def test_edgeql_insert_default_08(self):
+        await self.con.query(
+            r"""
+            create type Bar {
+                create property f -> float64;
+                create property g -> float64 {
+                    set default := .f
+                };
+            };
+            """
+        )
+        await self.con.query("insert Bar { f := random() };")
+        res = await self.con.query("select Bar { f, g }")
+        assert res[0].f == res[0].g
+
+    async def test_edgeql_insert_default_09(self):
+        async with self.assertRaisesRegexTx(
+            edgedb.SchemaDefinitionError,
+            r"default expression cannot refer to multi properties",
+        ):
+            await self.migrate(
+                r"""
+                type Hello {
+                    multi property b -> int32;
+                    property a -> int32 {
+                        default := count(.b);
+                    };
+                }
+            """
+            )
+
+        async with self.assertRaisesRegexTx(
+            edgedb.SchemaDefinitionError,
+            r"default expression cannot refer to links",
+        ):
+            await self.migrate(
+                r"""
+                type World {
+                    property w -> int32;
+                }
+
+                type Hello {
+                    link world -> World;
+
+                    property hello -> int32 {
+                        default := .world.w;
+                    };
+                }
+            """
+            )
+
+        async with self.assertRaisesRegexTx(
+            edgedb.SchemaDefinitionError,
+            r"default expression cannot refer to links",
+        ):
+            await self.migrate(
+                r"""
+                type World {
+                    property w -> int32;
+                }
+
+                type Hello {
+                    multi link world -> World;
+
+                    property hello -> int32 {
+                        default := count(.world);
+                    };
+                }
+            """
+            )
+
     async def test_edgeql_insert_as_expr_01(self):
         await self.con.execute(r'''
             # insert several objects, then annotate one of the inserted batch

commit 15a54027bb156fa651b152d2340e578d59505acd

Fix __source__ in defaults
Probably broken in #4851

edb/edgeql/compiler/viewgen.py
@@ -751,6 +751,15 @@ def _gen_pointers_from_defaults(
         with ctx.new() as scopectx:
             scopectx.active_defaults |= {stype}
 
+            # add __source__ to anchors
+            source_set = ir_set
+            scopectx.path_scope.attach_path(
+                source_set.path_id, context=None,
+                optional=False,
+            )
+            scopectx.iterator_path_ids |= {source_set.path_id}
+            scopectx.anchors['__source__'] = source_set
+
             pointer, ptr_set = _normalize_view_ptr_expr(
                 ir_set,
                 default_ql_desc,

tests/test_edgeql_ddl.py
@@ -568,6 +568,12 @@ class TestEdgeQLDDL(tb.DDLTestCase):
             };
         """)
 
+        await self.con.execute(
+            """
+            INSERT TestSelfLink1 { foo1 := 'hello' };
+            """
+        )
+
     async def test_edgeql_ddl_15(self):
         await self.con.execute(r"""
             CREATE TYPE TestSelfLink2 {