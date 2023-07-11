d63b57d8c6594a5f8f10f27a214a200b0124790e
Aljaž Mur Eržen <aljaz@edgedb.com>
2023-07-11 12:16:44 +0200

    Indexed __fts_document__ column, used in fts::test

--- edb/ir/ast.py
+++ edb/ir/ast.py
@@ -361,6 +361,11 @@ class TupleIndirectionPointerRef(BasePointerRef):
     pass
 
 
+class SpecialPointerRef(BasePointerRef):
+    """Pointer ref used for internal columns, such as __fts_document__"""
+    pass
+
+
 class TypeIntersectionLink(s_pointers.PseudoPointer):
     """A Link-alike that can be used in type intersection path ids."""
 
