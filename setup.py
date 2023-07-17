00c5e4c246737cc96ab22e3f3d2251bb4cc0c692
Scott Trinh <scott@scotttrinh.com>
2023-07-17 17:31:31 -0400

    Remove auth module from cython build

--- setup.py
+++ setup.py
@@ -1057,14 +1057,6 @@ setuptools.setup(
         ),
 
         setuptools_extension.Extension(
-            "edb.server.protocol.ext.auth",
-            ["edb/server/protocol/ext/auth.py"],
-            extra_compile_args=EXT_CFLAGS,
-            extra_link_args=EXT_LDFLAGS,
-            include_dirs=EXT_INC_DIRS,
-        ),
-
-        setuptools_extension.Extension(
             "edb.server.protocol.frontend",
             ["edb/server/protocol/frontend.pyx"],
             extra_compile_args=EXT_CFLAGS,
