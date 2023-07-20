385222cd64e950332db17340149e33ed94b9dcd6
Michael J. Sullivan <sully@msully.net>
2023-07-19 17:26:57 -0700

    Get rid of the config spec stored in a global variable
    This is in preparation for having some per-database configuration
    settings via extensions.

--- edb/server/bootstrap.py
+++ edb/server/bootstrap.py
@@ -1213,7 +1213,6 @@ async def _init_stdlib(
     await metaschema.create_pg_extensions(conn, backend_params)
 
     config_spec = config.load_spec_from_schema(stdlib.stdschema)
-    config.set_settings(config_spec)
 
     if tpldbdump is None:
         logger.info('Populating internal SQL structures...')
@@ -1324,7 +1323,6 @@ async def _init_stdlib(
         # _testmode includes extra config settings, so make sure
         # those are picked up.
         config_spec = config.load_spec_from_schema(stdlib.stdschema)
-        config.set_settings(config_spec)
 
     # Make sure that schema backend_id properties are in sync with
     # the database.
@@ -2001,7 +1999,7 @@ async def _pg_ensure_database_not_connected(
             f'database {dbname!r} is being accessed by other users')
 
 
-async def _start(ctx: BootstrapContext) -> None:
+async def _start(ctx: BootstrapContext) -> edbcompiler.CompilerState:
     conn = await _check_catalog_compatibility(ctx)
 
     try:
@@ -2010,7 +2008,7 @@ async def _start(ctx: BootstrapContext) -> None:
         ctx.cluster.overwrite_capabilities(struct.Struct('!Q').unpack(caps)[0])
         _check_capabilities(ctx)
 
-        await edbcompiler.new_compiler_from_pg(conn)
+        return (await edbcompiler.new_compiler_from_pg(conn)).state
 
     finally:
         conn.terminate()
@@ -2035,7 +2033,7 @@ async def _bootstrap_edgedb_super_roles(ctx: BootstrapContext) -> uuid.UUID:
     return superuser_uid
 
 
-async def _bootstrap(ctx: BootstrapContext) -> None:
+async def _bootstrap(ctx: BootstrapContext) -> edbcompiler.CompilerState:
     args = ctx.args
     cluster = ctx.cluster
     backend_params = cluster.get_runtime_params()
@@ -2232,15 +2230,17 @@ async def _bootstrap(ctx: BootstrapContext) -> None:
             args.default_database_user or edbdef.EDGEDB_SUPERUSER,
         )
 
+    return compiler.state
+
 
 async def ensure_bootstrapped(
     cluster: pgcluster.BaseCluster,
     args: edbargs.ServerConfig,
-) -> bool:
+) -> tuple[bool, edbcompiler.CompilerState]:
     """Bootstraps EdgeDB instance if it hasn't been bootstrapped already.
 
     Returns True if bootstrap happened and False if the instance was already
-    bootstrapped.
+    bootstrapped, along with the bootstrap compiler state.
     """
     pgconn = PGConnectionProxy(cluster)
     ctx = BootstrapContext(cluster=cluster, conn=pgconn, args=args)
@@ -2249,10 +2249,10 @@ async def ensure_bootstrapped(
         mode = await _get_cluster_mode(ctx)
         ctx = dataclasses.replace(ctx, mode=mode)
         if mode == ClusterMode.pristine:
-            await _bootstrap(ctx)
-            return True
+            state = await _bootstrap(ctx)
+            return True, state
         else:
-            await _start(ctx)
-            return False
+            state = await _start(ctx)
+            return False, state
     finally:
         pgconn.terminate()
