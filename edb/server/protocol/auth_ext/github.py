d5c5afbf8098342b24e4397a2eb493ec77153c3f
Scott Trinh <scott@scotttrinh.com>
2023-07-25 21:43:49 -0400

    Add basic routing and provider redirect for GitHub (#5816)

---
+++ edb/server/protocol/auth_ext/github.py
@@ -0,0 +1,65 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2016-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import httpx
+import urllib.parse
+
+from . import base
+
+
+class GitHubProvider(base.BaseProvider):
+    def __init__(self, *args, **kwargs):
+        super().__init__("github", *args, **kwargs)
+
+    def get_code_url(
+        self, state: str, redirect_uri: str, scope: str = "read:user"
+    ) -> str:
+        params = {
+            "client_id": self.client_id,
+            "scope": scope,
+            "state": state,
+            "redirect_uri": redirect_uri,
+        }
+        encoded = urllib.parse.urlencode(params)
+        return f"https://github.com/login/oauth/authorize?{encoded}"
+
+    async def exchange_access_token(
+        self, code: str, state: str, redirect_uri: str
+    ):
+        # TODO: Check state value
+        data = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "state": state,
+            "redirect_uri": redirect_uri,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+        }
+
+        headers = {"Content-Type": "application/json"}
+
+        async with httpx.AsyncClient() as client:
+            resp = await client.post(
+                "https://github.com/login/oauth/access_token",
+                json=data,
+                headers=headers,
+            )
+            token = resp.json()["access_token"]
+
+            return token
---
+++ tests/test_http_ext_auth.py
@@ -0,0 +1,83 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2016-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import urllib.parse
+import uuid
+import json
+import base64
+
+from jwcrypto import jwt, jwk
+from edb.testbase import http as tb
+
+
+class TestHttpExtAuth(tb.ExtAuthTestCase):
+    SETUP = [
+        f"""
+        CONFIGURE CURRENT DATABASE
+        SET xxx_auth_signing_key := <str>'{"a" * 32}';
+        """,
+        f"""
+        CONFIGURE CURRENT DATABASE
+        SET xxx_github_client_secret := <str>'{"b" * 32}';
+        """,
+        f"""
+        CONFIGURE CURRENT DATABASE
+        SET xxx_github_client_id := <str>'{uuid.uuid4()}';
+        """,
+    ]
+
+    async def test_http_auth_ext_github_authorize_01(self):
+        with self.http_con() as http_con:
+            client_id = await self.con.query_single(
+                """SELECT assert_single(cfg::Config.xxx_github_client_id);"""
+            )
+
+            auth_signing_key = await self.con.query_single(
+                """SELECT assert_single(cfg::Config.xxx_auth_signing_key);"""
+            )
+
+            _, headers, status = self.http_con_request(
+                http_con, {"provider": "github"}, path="authorize"
+            )
+
+            self.assertEqual(status, 302)
+
+            location = headers.get("location")
+            assert location is not None
+            url = urllib.parse.urlparse(location)
+            qs = urllib.parse.parse_qs(url.query, keep_blank_values=True)
+            self.assertEqual(url.scheme, "https")
+            self.assertEqual(url.hostname, "github.com")
+            self.assertEqual(url.path, "/login/oauth/authorize")
+            self.assertEqual(qs.get("scope"), ["read:user"])
+
+            state = qs.get("state")
+            assert state is not None
+
+            key_bytes = base64.b64encode(auth_signing_key.encode())
+            key = jwk.JWK(k=key_bytes.decode(), kty="oct")
+            signed_token = jwt.JWT(key=key, algs=["HS256"], jwt=state[0])
+            claims = json.loads(signed_token.claims)
+            self.assertEqual(claims.get("provider"), "github")
+            self.assertEqual(claims.get("iss"), self.http_addr)
+
+            self.assertEqual(
+                qs.get("redirect_uri"), [f"{self.http_addr}/callback"]
+            )
+            self.assertEqual(qs.get("client_id"), [client_id])
