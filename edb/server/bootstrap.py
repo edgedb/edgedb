edgedb/edgedb

commit 18f2047e7c4fa7fa6fd59da0f6af707527caedac

Refactor loading of parsers (#5856)
We have recently switched to a new parser in Rust.  This parser does not
need the heavy `parsing` Spec instances to parse things as it translates
them into its own internal representation.  Hence, keeping the `Spec`
instances around is pure memory waste.  Additionally, the current parser
loading abstractions are unnecessarily complex and provide for
customization that we don't actually use.

This rectifies both issues by simplifying the whole thing to
`parse(grammar_module: types.ModuleType, source: str)`.  The Rust parser
still loads and caches its internal spec and there is still `preload()`,
so there should be no behavioral changes.

Finally, reduce the list of parsers loaded by the I/O process to just
`fragment`, which is used for the processing of config spec.  There are
no other uses.

This brings the resident set of the I/O process down from 307MiB to
160MiB, a nearly 50% reduction!  Compiler processes get a more modest
reduction from 251MiB to 196MiB (22%), however this figure slightly
favors the old implementation as it currently forgets to preload the
`migration_body` and `extension_package_body` grammars.

edb/server/main.py
@@ -153,14 +153,17 @@ async def _init_cluster(
 
 
 def _init_parsers():
-    # Initialize all parsers, rebuilding grammars if
-    # necessary.  Do it earlier than later so that we don't
-    # end up in a situation where all our compiler processes
-    # are building parsers in parallel.
-
+    # Initialize parsers that are used in the server process.
     from edb.edgeql import parser as ql_parser
+    from edb.edgeql.parser import grammar as ql_grammar
 
-    ql_parser.preload(allow_rebuild=devmode.is_in_dev_mode(), paralellize=True)
+    ql_parser.preload(
+        allow_rebuild=devmode.is_in_dev_mode(),
+        paralellize=True,
+        grammars=[
+            ql_grammar.fragment,
+        ]
+    )
 
 
 async def _run_server(

edb/common/parsing.py
@@ -21,6 +21,7 @@ from __future__ import annotations
 from typing import *  # NoQA
 
 import functools
+import json
 import logging
 import os
 import sys
@@ -282,60 +283,106 @@ class Precedence(parsing.Precedence, assoc='fail', metaclass=PrecedenceMeta):
     pass
 
 
-class ParserSpec:
-    parser_spec: ClassVar[parsing.Spec | None]
+def load_parser_spec(
+    mod: types.ModuleType,
+    allow_rebuild: bool = False,
+) -> parsing.Spec:
+    return parsing.Spec(
+        mod,
+        pickleFile=_localpath(mod, "pickle"),
+        skinny=not debug.flags.edgeql_parser,
+        logFile=_localpath(mod, "log"),
+        verbose=bool(debug.flags.edgeql_parser),
+        unpickleHook=functools.partial(
+            _on_spec_unpickle, mod, allow_rebuild)
+    )
 
-    def __init__(self, **parser_data):
-        self.parser_data = parser_data
 
-    def get_debug(self):
-        return debug.flags.edgeql_parser
-
-    def get_parser_spec_module(self) -> types.ModuleType:
-        raise NotImplementedError
-
-    def get_parser_spec(self, allow_rebuild: bool = True) -> parsing.Spec:
-        cls = self.__class__
-
-        try:
-            spec = cls.__dict__['parser_spec']
-        except KeyError:
-            pass
+def _on_spec_unpickle(
+    mod: types.ModuleType,
+    allow_rebuild: bool,
+    spec: parsing.Spec,
+    compatibility: str,
+) -> None:
+    if compatibility != "compatible":
+        if allow_rebuild:
+            logger.info(f'rebuilding grammar for {mod.__name__}...')
         else:
-            if spec is not None:
-                return spec
+            raise ParserSpecIncompatibleError(
+                f'parser tables for {mod.__name__} are missing or '
+                f'incompatible with parser source'
+            )
 
-        mod = self.get_parser_spec_module()
-        spec = parsing.Spec(
-            mod,
-            pickleFile=self.localpath(mod, "pickle"),
-            skinny=not self.get_debug(),
-            logFile=self.localpath(mod, "log"),
-            verbose=self.get_debug(),
-            unpickleHook=functools.partial(
-                self.on_spec_unpickle, mod, allow_rebuild)
-        )
 
-        self.__class__.parser_spec = spec
-        return spec
+def _localpath(mod, type):
+    return os.path.join(
+        os.path.dirname(mod.__file__),
+        mod.__name__.rpartition('.')[2] + '.' + type)
 
-    def on_spec_unpickle(
-        self,
-        mod: types.ModuleType,
-        allow_rebuild: bool,
-        spec: parsing.Spec,
-        compatibility: str,
-    ) -> None:
-        if compatibility != "compatible":
-            if allow_rebuild:
-                logger.info(f'rebuilding grammar for {mod.__name__}...')
+
+def spec_to_json(spec: parsing.Spec) -> tuple[str, list[Callable]]:
+    # Converts a ParserSpec into JSON. Called from edgeql-parser Rust crate.
+    assert spec.pureLR
+
+    token_map: Dict[str, str] = {
+        v._token: c for (_, c), v in TokenMeta.token_map.items()
+    }
+
+    # productions
+    productions: list[Any] = []
+    production_ids: dict[Any, int] = {}
+    inlines: list[tuple[int, int]] = []
+
+    def get_production_id(prod: Any) -> int:
+        if prod in production_ids:
+            return production_ids[prod]
+
+        id = len(productions)
+        productions.append(prod)
+        production_ids[prod] = id
+
+        inline = getattr(prod.method, 'inline_index', None)
+        if inline is not None:
+            assert isinstance(inline, int)
+            inlines.append((id, inline))
+
+        return id
+
+    actions = []
+    for st_actions in spec.actions():
+        out_st_actions = []
+        for tok, acts in st_actions.items():
+            act = cast(Any, acts[0])
+
+            str_tok = token_map.get(str(tok), str(tok))
+            if 'ShiftAction' in str(type(act)):
+                action_obj: Any = int(act.nextState)
             else:
-                raise ParserSpecIncompatibleError(
-                    f'parser tables for {mod.__name__} are missing or '
-                    f'incompatible with parser source'
-                )
+                prod = act.production
+                action_obj = {
+                    'production_id': get_production_id(prod),
+                    'non_term': str(prod.lhs),
+                    'cnt': len(prod.rhs),
+                }
 
-    def localpath(self, mod, type):
-        return os.path.join(
-            os.path.dirname(mod.__file__),
-            mod.__name__.rpartition('.')[2] + '.' + type)
+            out_st_actions.append((str_tok, action_obj))
+
+        actions.append(out_st_actions)
+
+    # goto
+    goto = []
+    for st_goto in spec.goto():
+        out_goto = []
+        for nterm, action in st_goto.items():
+            out_goto.append((str(nterm), action))
+
+        goto.append(out_goto)
+
+    res = {
+        'actions': actions,
+        'goto': goto,
+        'start': str(spec.start_sym()),
+        'inlines': inlines,
+    }
+    res_json = json.dumps(res)
+    return (res_json, productions)

edb/edgeql-parser/edgeql-parser-python/src/lib.rs
@@ -13,7 +13,7 @@ mod pynormalize;
 mod tokenizer;
 
 use errors::{SyntaxError, ParserResult};
-use parser::{parse, CSTNode, Production};
+use parser::{parse, cache_spec, CSTNode, Production};
 use position::{offset_of_line, SourcePoint};
 use pynormalize::normalize;
 use tokenizer::{get_fn_unpickle_token, tokenize, OpaqueToken};
@@ -53,6 +53,11 @@ py_module_initializer!(
             py,
             "parse",
             py_fn!(py, parse(parser_name: &PyString, data: PyObject)),
+        )?;
+        m.add(
+            py,
+            "cache_spec",
+            py_fn!(py, cache_spec(grammar_name: &PyString, py_spec: &PyObject)),
         )?;
         m.add(py, "CSTNode", py.get_type::<CSTNode>())?;
         m.add(py, "Production", py.get_type::<Production>())?;

edb/edgeql-parser/edgeql-parser-python/src/parser.rs
@@ -3,7 +3,7 @@ use std::sync::Mutex;
 
 use cpython::{
     ObjectProtocol, PyClone, PyInt, PyList, PyObject, PyResult, PyString, PyTuple, Python,
-    PythonObject, PythonObjectWithCheckedDowncast, ToPyObject,
+    PythonObject, PythonObjectWithCheckedDowncast, ToPyObject, PyNone,
 };
 
 use edgeql_parser::parser;
@@ -13,11 +13,20 @@ use crate::errors::{parser_error_into_tuple, ParserResult};
 use crate::pynormalize::value_to_py_object;
 use crate::tokenizer::OpaqueToken;
 
-pub fn parse(py: Python, parser_name: &PyString, tokens: PyObject) -> PyResult<PyTuple> {
-    let mut parser_specs = PARSER_SPECS.lock().unwrap();
+pub fn parse(py: Python, grammar_name: &PyString, tokens: PyObject) -> PyResult<PyTuple> {
+    let mut spec_cache = PARSER_SPECS.lock().unwrap();
 
-    let (spec, productions) =
-        load_spec(py, &mut parser_specs, parser_name.to_string(py)?.as_ref())?;
+    let grammar_name_str = grammar_name.to_string(py)?;
+    let (spec, productions) = match spec_cache.get(grammar_name_str.as_ref()) {
+        Some(spec) => spec,
+        None => {
+            let parsing_mod = py.import("edb.common.parsing")?;
+            let load_parser_spec = parsing_mod.get(py, "load_parser_spec")?;
+            let grammar_mod = py.import(grammar_name_str.as_ref())?;
+            let py_spec = load_parser_spec.call(py, (grammar_mod,), None)?;
+            _load_spec(py, &mut spec_cache, grammar_name_str.as_ref(), &py_spec)?
+        },
+    };
 
     let tokens = downcast_tokens(py, tokens)?;
 
@@ -98,33 +107,36 @@ fn downcast_tokens<'a>(py: Python, token_list: PyObject) -> PyResult<Vec<parser:
     Ok(buf)
 }
 
-fn load_spec<'a>(
+pub fn cache_spec(
+    py: Python,
+    grammar_name: &PyString,
+    py_spec: &PyObject,
+) -> PyResult<PyNone> {
+    let mut parser_specs = PARSER_SPECS.lock().unwrap();
+    _load_spec(py, &mut parser_specs, grammar_name.to_string(py)?.as_ref(), py_spec)?;
+    Ok(PyNone)
+}
+
+fn _load_spec<'a>(
     py: Python,
     specs: &'a mut ParserSpecs,
-    parser_name: &str,
+    grammar_name: &str,
+    py_spec: &PyObject,
 ) -> PyResult<&'a (parser::Spec, PyObject)> {
-    if !specs.contains_key(parser_name) {
-        let parser_mod = py.import("edb.edgeql.parser.parser")?;
+    let spec_to_json = py.import("edb.common.parsing")?.get(py, "spec_to_json")?;
 
-        let process_spec = py.import("edb.edgeql.parser")?.get(py, "process_spec")?;
+    let res = spec_to_json.call(py, (py_spec,), None)?;
+    let res = PyTuple::downcast_from(py, res)?;
 
-        let parser_cls = parser_mod.get(py, parser_name)?;
-        let parser = parser_cls.call(py, PyTuple::new(py, &[]), None)?;
+    let spec_json = PyString::downcast_from(py, res.get_item(py, 0))?;
+    let spec_json = spec_json.to_string(py).unwrap();
+    let spec = parser::Spec::from_json(&spec_json).unwrap();
+    let productions = res.get_item(py, 1);
+    let result = (spec, productions);
 
-        let res = process_spec.call(py, (parser,), None)?;
-        let res = PyTuple::downcast_from(py, res)?;
+    specs.insert(grammar_name.to_string(), result);
 
-        let spec_json = PyString::downcast_from(py, res.get_item(py, 0))?;
-        let spec_json = spec_json.to_string(py).unwrap();
-
-        let productions = res.get_item(py, 1);
-
-        let spec = parser::Spec::from_json(&spec_json).unwrap();
-
-        specs.insert(parser_name.to_string(), (spec, productions));
-    }
-
-    Ok(specs.get(parser_name).unwrap())
+    Ok(specs.get(grammar_name).unwrap())
 }
 
 fn to_py_cst<'a>(cst: &'a parser::CSTNode<'a>, py: Python) -> PyResult<CSTNode> {

edb/edgeql/__init__.py
@@ -22,6 +22,6 @@ from __future__ import annotations
 from . import ast  # NOQA
 from .tokenizer import Source, NormalizedSource  # NOQA
 from .codegen import generate_source  # NOQA
-from .parser import parse_fragment, parse_single, parse_block  # NOQA
+from .parser import parse_fragment, parse_block  # NOQA
 from .parser import parse_query, parse_command  # NOQA
 from .parser.grammar import keywords  # NOQA

edb/edgeql/parser/__init__.py
@@ -20,17 +20,19 @@
 from __future__ import annotations
 from typing import *
 
+import importlib
 import multiprocessing
-import json
+import types
 
 from edb import errors
 from edb.common import parsing
 
-from . import parser as qlparser
+import edb._edgeql_parser as rust_parser
+
+from . import grammar as qlgrammar
+
 from .. import ast as qlast
 from .. import tokenizer as qltokenizer
-
-EdgeQLParserBase = qlparser.EdgeQLParserSpec
 
 
 def append_module_aliases(tree, aliases):
@@ -51,19 +53,8 @@ def parse_fragment(
     source: Union[qltokenizer.Source, str],
     filename: Optional[str] = None,
 ) -> qlast.Expr:
-    parser = qlparser.EdgeQLExpressionSpec().get_parser()
-    res = parser.parse(source, filename=filename)
+    res = parse(qlgrammar.fragment, source, filename=filename)
     assert isinstance(res, qlast.Expr)
-    return res
-
-
-def parse_single(
-    source: Union[qltokenizer.Source, str],
-    filename: Optional[str] = None,
-) -> qlast.Statement:
-    parser = qlparser.EdgeQLSingleSpec().get_parser()
-    res = parser.parse(source, filename=filename)
-    assert isinstance(res, (qlast.Query | qlast.Command))
     return res
 
 
@@ -93,7 +84,7 @@ def parse_command(
 ) -> qlast.Command:
     """Parse some EdgeQL command potentially adding some module aliases."""
 
-    tree = parse_single(source)
+    tree = parse(qlgrammar.single, source)
     assert isinstance(tree, qlast.Command)
 
     if module_aliases:
@@ -102,9 +93,8 @@ def parse_command(
     return tree
 
 
-def parse_block(source: Union[qltokenizer.Source, str]) -> List[qlast.Base]:
-    parser = qlparser.EdgeQLBlockSpec().get_parser()
-    return parser.parse(source)
+def parse_block(source: qltokenizer.Source | str) -> list[qlast.Base]:
+    return parse(qlgrammar.block, source)
 
 
 def parse_migration_body_block(
@@ -115,10 +105,7 @@ def parse_migration_body_block(
     # (without braces)", so we just hack around this by adding braces.
     # This is only really workable because we only use this in a place
     # where the source contexts don't matter anyway.
-    source = '{' + source + '}'
-
-    parser = qlparser.EdgeQLMigrationBodySpec().get_parser()
-    return parser.parse(source)
+    return parse(qlgrammar.migration_body, f"{{{source}}}")
 
 
 def parse_extension_package_body_block(
@@ -129,125 +116,169 @@ def parse_extension_package_body_block(
     # (without braces)", so we just hack around this by adding braces.
     # This is only really workable because we only use this in a place
     # where the source contexts don't matter anyway.
-    source = '{' + source + '}'
-
-    parser = qlparser.EdgeQLExtensionPackageBodySpec().get_parser()
-    return parser.parse(source)
+    return parse(qlgrammar.extension_package_body, f"{{{source}}}")
 
 
 def parse_sdl(expr: str):
-    parser = qlparser.EdgeSDLSpec().get_parser()
-    return parser.parse(expr)
+    return parse(qlgrammar.sdldocument, expr)
 
 
-def _load_parser(parser: qlparser.EdgeQLParserSpec) -> None:
-    parser.get_parser_spec(allow_rebuild=True)
+def parse(
+    grammar: types.ModuleType,
+    source: Union[str, qltokenizer.Source],
+    filename: Optional[str] = None,
+):
+    if isinstance(source, str):
+        source = qltokenizer.Source.from_string(source)
+
+    result, productions = rust_parser.parse(grammar.__name__, source.tokens())
+
+    if len(result.errors()) > 0:
+        # TODO: emit multiple errors
+
+        # Heuristic to pick the error:
+        # - first encountered,
+        # - Unexpected before Missing,
+        # - original order.
+        errs: List[Tuple[str, Tuple[int, Optional[int]]]] = result.errors()
+        errs.sort(key=lambda e: (e[1][0], -ord(e[0][1])))
+        error = errs[0]
+
+        message, span = error
+        position = qltokenizer.inflate_position(source.text(), span)
+
+        raise errors.EdgeQLSyntaxError(message, position=position)
+
+    return _cst_to_ast(
+        result.out(),
+        productions,
+        source,
+        filename,
+    ).val
+
+
+def _cst_to_ast(
+    cst: rust_parser.CSTNode,
+    productions: list[Callable],
+    source: qltokenizer.Source,
+    filename: Optional[str],
+) -> Any:
+    # Converts CST into AST by calling methods from the grammar classes.
+    #
+    # This function was originally written as a simple recursion.
+    # Then I had to unfold it, because it was hitting recursion limit.
+    # Stack here contains all remaining things to do:
+    # - CST node means the node has to be processed and pushed onto the
+    #   result stack,
+    # - production means that all args of production have been processed
+    #   are are ready to be passed to the production method. The result is
+    #   obviously pushed onto the result stack
+
+    stack: List[rust_parser.CSTNode | rust_parser.Production] = [cst]
+    result: List[Any] = []
+
+    while len(stack) > 0:
+        node = stack.pop()
+
+        if isinstance(node, rust_parser.CSTNode):
+            # this would be the body of the original recursion function
+
+            if terminal := node.terminal():
+                # Terminal is simple: just convert to parsing.Token
+                context = parsing.ParserContext(
+                    name=filename,
+                    buffer=source.text(),
+                    start=terminal.start(),
+                    end=terminal.end(),
+                )
+                result.append(
+                    parsing.Token(
+                        terminal.text(), terminal.value(), context
+                    )
+                )
+
+            elif production := node.production():
+                # Production needs to first process all args, then
+                # call the appropriate method.
+                # (this is all in reverse, because stacks)
+                stack.append(production)
+                args = list(production.args())
+                args.reverse()
+                stack.extend(args)
+            else:
+                raise NotImplementedError(node)
+
+        elif isinstance(node, rust_parser.Production):
+            # production args are done, get them out of result stack
+            len_args = len(node.args())
+            split_at = len(result) - len_args
+            args = result[split_at:]
+            result = result[0:split_at]
+
+            # find correct method to call
+            production_id = node.id()
+            production = productions[production_id]
+
+            sym = production.lhs.nontermType()
+            assert len(args) == len(production.rhs)
+            production.method(sym, *args)
+
+            # push into result stack
+            result.append(sym)
+
+    return result.pop()
+
+
+def _load_parser(grammar: str) -> None:
+    specmod = importlib.import_module(grammar)
+    parsing.load_parser_spec(specmod, allow_rebuild=True)
 
 
 def preload(
     allow_rebuild: bool = True,
     paralellize: bool = False,
-    parsers: Optional[List[qlparser.EdgeQLParserSpec]] = None,
+    grammars: Optional[list[types.ModuleType]] = None,
 ) -> None:
-    if parsers is None:
-        parsers = [
-            qlparser.EdgeQLBlockSpec(),
-            qlparser.EdgeQLSingleSpec(),
-            qlparser.EdgeQLExpressionSpec(),
-            qlparser.EdgeSDLSpec(),
+    if grammars is None:
+        grammars = [
+            qlgrammar.block,
+            qlgrammar.fragment,
+            qlgrammar.sdldocument,
+            qlgrammar.extension_package_body,
+            qlgrammar.migration_body,
         ]
 
     if not paralellize:
         try:
-            for parser in parsers:
-                parser.get_parser_spec(allow_rebuild)
+            for grammar in grammars:
+                spec = parsing.load_parser_spec(
+                    grammar, allow_rebuild=allow_rebuild)
+                rust_parser.cache_spec(grammar.__name__, spec)
         except parsing.ParserSpecIncompatibleError as e:
             raise errors.InternalServerError(e.args[0]) from None
     else:
         parsers_to_rebuild = []
 
-        for parser in parsers:
+        for grammar in grammars:
             try:
-                parser.get_parser_spec(allow_rebuild=False)
+                spec = parsing.load_parser_spec(grammar, allow_rebuild=False)
+                rust_parser.cache_spec(grammar.__name__, spec)
             except parsing.ParserSpecIncompatibleError:
-                parsers_to_rebuild.append(parser)
+                parsers_to_rebuild.append(grammar)
 
         if len(parsers_to_rebuild) == 0:
             pass
         elif len(parsers_to_rebuild) == 1:
-            parsers_to_rebuild[0].get_parser_spec(allow_rebuild=True)
+            spec = parsing.load_parser_spec(
+                parsers_to_rebuild[0], allow_rebuild=True)
+            rust_parser.cache_spec(parsers_to_rebuild[0].__name__, spec)
         else:
             with multiprocessing.Pool(len(parsers_to_rebuild)) as pool:
-                pool.map(_load_parser, parsers_to_rebuild)
+                pool.map(
+                    _load_parser,
+                    [mod.__name__ for mod in parsers_to_rebuild],
+                )
 
-            preload(parsers=parsers, allow_rebuild=False)
-
-
-def process_spec(parser: parsing.ParserSpec) -> Tuple[str, List[Any]]:
-    # Converts a ParserSpec into JSON. Called from edgeql-parser Rust crate.
-
-    spec = parser.get_parser_spec()
-    assert spec.pureLR
-
-    token_map: Dict[str, str] = {
-        v._token: c for (_, c), v in parsing.TokenMeta.token_map.items()
-    }
-
-    # productions
-    productions: List[Any] = []
-    production_ids: Dict[Any, int] = {}
-    inlines: List[Tuple[int, int]] = []
-
-    def get_production_id(prod: Any) -> int:
-        if prod in production_ids:
-            return production_ids[prod]
-
-        id = len(productions)
-        productions.append(prod)
-        production_ids[prod] = id
-
-        inline = getattr(prod.method, 'inline_index', None)
-        if inline is not None:
-            assert isinstance(inline, int)
-            inlines.append((id, inline))
-
-        return id
-
-    actions = []
-    for st_actions in spec.actions():
-        out_st_actions = []
-        for tok, acts in st_actions.items():
-            act = cast(Any, acts[0])
-
-            str_tok = token_map.get(str(tok), str(tok))
-            if 'ShiftAction' in str(type(act)):
-                action_obj: Any = int(act.nextState)
-            else:
-                prod = act.production
-                action_obj = {
-                    'production_id': get_production_id(prod),
-                    'non_term': str(prod.lhs),
-                    'cnt': len(prod.rhs),
-                }
-
-            out_st_actions.append((str_tok, action_obj))
-
-        actions.append(out_st_actions)
-
-    # goto
-    goto = []
-    for st_goto in spec.goto():
-        out_goto = []
-        for nterm, action in st_goto.items():
-            out_goto.append((str(nterm), action))
-
-        goto.append(out_goto)
-
-    res = {
-        'actions': actions,
-        'goto': goto,
-        'start': str(spec.start_sym()),
-        'inlines': inlines,
-    }
-    res_json = json.dumps(res)
-    return (res_json, productions)
+            for grammar in parsers_to_rebuild:
+                spec = parsing.load_parser_spec(grammar, allow_rebuild=False)
+                rust_parser.cache_spec(grammar.__name__, spec)

edb/edgeql/parser/grammar/__init__.py
@@ -6,3 +6,11 @@
 ##
 
 from __future__ import annotations
+
+
+from . import block as block  # noqa
+from . import extension_package_body as extension_package_body  # noqa
+from . import fragment as fragment  # noqa
+from . import migration_body as migration_body  # noqa
+from . import single as single  # noqa
+from . import sdldocument as sdldocument  # noqa

commit c72f51f36954e8b98dc77e88284b54c41318f95a

Make sure block parser is loaded when building stdlib
This is a regression from #5856.  The `block` parser is needed to
compile the stdlib when the cache isn't there.

edb/server/bootstrap.py
@@ -30,6 +30,7 @@ import pickle
 import re
 import struct
 import textwrap
+import types
 
 from edb import buildmeta
 from edb import errors
@@ -953,6 +954,10 @@ async def _make_stdlib(
     testmode: bool,
     global_ids: Mapping[str, uuid.UUID],
 ) -> StdlibBits:
+    from edb.edgeql.parser import grammar as ql_grammar
+
+    init_parsers(grammars=[ql_grammar.block])
+
     schema: s_schema.Schema = s_schema.ChainedSchema(
         s_schema.EMPTY_SCHEMA,
         s_schema.EMPTY_SCHEMA,
@@ -1194,6 +1199,17 @@ def _get_cache_dir() -> pathlib.Path | None:
         return pathlib.Path(specified_cache_dir)
     else:
         return None
+
+
+def init_parsers(grammars: list[types.ModuleType]) -> None:
+    # Initialize parsers that are used in the server process.
+    from edb.edgeql import parser as ql_parser
+
+    ql_parser.preload(
+        allow_rebuild=devmode.is_in_dev_mode(),
+        paralellize=True,
+        grammars=grammars,
+    )
 
 
 def read_data_cache(

edb/server/main.py
@@ -177,20 +177,6 @@ async def _init_cluster(
     return new_instance
 
 
-def _init_parsers():
-    # Initialize parsers that are used in the server process.
-    from edb.edgeql import parser as ql_parser
-    from edb.edgeql.parser import grammar as ql_grammar
-
-    ql_parser.preload(
-        allow_rebuild=devmode.is_in_dev_mode(),
-        paralellize=True,
-        grammars=[
-            ql_grammar.fragment,
-        ]
-    )
-
-
 async def _run_server(
     cluster,
     args: srvargs.ServerConfig,
@@ -393,6 +379,8 @@ async def run_server(
     do_setproctitle: bool=False,
     runstate_dir: pathlib.Path,
 ) -> None:
+    from edb.server import bootstrap as server_bootstrap
+    from edb.edgeql.parser import grammar as ql_grammar
     from . import server as server_mod
     global server
     server = server_mod
@@ -424,7 +412,11 @@ async def run_server(
 
         debugpy.listen(38782)
 
-    _init_parsers()
+    server_bootstrap.init_parsers(
+        grammars=[
+            ql_grammar.fragment,
+        ]
+    )
 
     pg_cluster_init_by_us = False
 