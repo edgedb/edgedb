84115ef24d94462b55f38e88a34aed5199a97ae6
Aljaž Mur Eržen <aljaz@edgedb.com>
2023-09-20 10:58:28 +0200

    bump catalog version

--- edb/buildmeta.py
+++ edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2023_09_05_00_00
+EDGEDB_CATALOG_VERSION = 2023_09_20_00_00
 EDGEDB_MAJOR_VERSION = 4
 
 
