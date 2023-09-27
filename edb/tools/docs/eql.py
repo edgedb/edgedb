520eb4ab0a0ad0d1e57943f444b695eadfb81a73
Aljaž Mur Eržen <aljaz@edgedb.com>
2023-09-27 15:18:05 +0200

    fix docs

--- edb/tools/docs/eql.py
+++ edb/tools/docs/eql.py
@@ -726,7 +726,7 @@ class EQLFunctionDirective(BaseEQLDirective):
 
         try:
             astnode = edgeql_parser.parse(
-                edgeql_grammar.start,
+                edgeql_grammar.tokens.T_STARTBLOCK,
                 f'create function {sig} using SQL function "xxx";')[0]
         except Exception as ex:
             raise self.error(
@@ -800,7 +800,8 @@ class EQLConstraintDirective(BaseEQLDirective):
 
         try:
             astnode = edgeql_parser.parse(
-                edgeql_grammar.start, f'create abstract constraint {sig};'
+                edgeql_grammar.tokens.T_STARTBLOCK,
+                f'create abstract constraint {sig};'
             )[0]
         except Exception as ex:
             raise self.error(
