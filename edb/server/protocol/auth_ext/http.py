100badbcf18182719af6fbfc208308f43facfaca
Scott Trinh <scott@scotttrinh.com>
2023-10-05 21:52:02 -0400

    Revert lots of Black formatting

--- edb/server/protocol/auth_ext/http.py
+++ edb/server/protocol/auth_ext/http.py
@@ -82,7 +82,7 @@ class Router:
                     oauth_client = oauth.Client(
                         db=self.db,
                         provider_name=provider_name,
-                        base_url=test_url,
+                        base_url=test_url
                     )
                     await pkce.create(self.db, challenge)
                     authorize_url = await oauth_client.get_authorize_url(
@@ -304,7 +304,7 @@ class Router:
                             redirect_params = urllib.parse.urlencode(
                                 {
                                     "error": str(ex),
-                                    "email": data.get('email', ''),
+                                    "email": data.get('email', '')
                                 }
                             )
                             redirect_url = (
@@ -381,7 +381,7 @@ class Router:
                         else:
                             raise ex
 
-                case ('send_reset_email',):
+                case ('send_reset_email', ):
                     data = self._get_data_from_request(request)
 
                     local_provider_name = data.get("provider")
@@ -400,18 +400,16 @@ class Router:
                                 "Missing 'reset_url' in data"
                             )
 
-                        (
-                            identity,
-                            secret,
-                        ) = await local_client.get_identity_and_secret(data)
+                        identity, secret = (
+                            await local_client.get_identity_and_secret(data))
 
                         new_reset_token = self._make_reset_token(
                             identity.id, secret
                         )
 
-                        reset_token_params = urllib.parse.urlencode(
-                            {"reset_token": new_reset_token}
-                        )
+                        reset_token_params = urllib.parse.urlencode({
+                            "reset_token": new_reset_token
+                        })
                         reset_url = f"{data['reset_url']}?{reset_token_params}"
 
                         from_addr = util.get_config(
@@ -450,9 +448,11 @@ class Router:
                         if task.done():
                             await task
 
-                        return_data = {
-                            "email_sent": data.get('email'),
-                        }
+                        return_data = (
+                            {
+                                "email_sent": data.get('email'),
+                            }
+                        )
 
                         if data.get("redirect_to") is not None:
                             response.status = http.HTTPStatus.FOUND
@@ -466,7 +466,9 @@ class Router:
                         else:
                             response.status = http.HTTPStatus.OK
                             response.content_type = b"application/json"
-                            response.body = json.dumps(return_data).encode()
+                            response.body = json.dumps(
+                                return_data
+                            ).encode()
                     except aiosmtplib.SMTPException as ex:
                         if not debug.flags.server:
                             logger.warning(
@@ -515,8 +517,8 @@ class Router:
                             )
                         reset_token = data['reset_token']
 
-                        identity_id, secret = self._get_data_from_reset_token(
-                            reset_token
+                        identity_id, secret = (
+                            self._get_data_from_reset_token(reset_token)
                         )
 
                         identity = await local_client.update_password(
@@ -568,10 +570,7 @@ class Router:
                         else:
                             raise ex
 
-                case (
-                    'ui',
-                    'signin',
-                ):
+                case ('ui', 'signin'):
                     ui_config = self._get_ui_config()
 
                     if ui_config is None:
@@ -581,13 +580,13 @@ class Router:
                         providers = util.maybe_get_config(
                             self.db,
                             "ext::auth::AuthConfig::providers",
-                            frozenset,
+                            frozenset
                         )
 
                         if providers is None or len(providers) == 0:
                             raise errors.MissingConfiguration(
                                 'ext::auth::AuthConfig::providers',
-                                'No providers are configured',
+                                'No providers are configured'
                             )
 
                         query = urllib.parse.parse_qs(
@@ -615,10 +614,7 @@ class Router:
                             brand_color=ui_config.brand_color,
                         )
 
-                case (
-                    'ui',
-                    'signup',
-                ):
+                case ('ui', 'signup'):
                     ui_config = self._get_ui_config()
                     password_provider = (
                         self._get_password_provider()
@@ -630,8 +626,7 @@ class Router:
                         response.status = http.HTTPStatus.NOT_FOUND
                         response.body = (
                             b'Password provider not configured'
-                            if ui_config
-                            else b'Auth UI not enabled'
+                            if ui_config else b'Auth UI not enabled'
                         )
                     else:
                         query = urllib.parse.parse_qs(
@@ -656,10 +651,7 @@ class Router:
                             brand_color=ui_config.brand_color,
                         )
 
-                case (
-                    'ui',
-                    'forgot-password',
-                ):
+                case ('ui', 'forgot-password'):
                     ui_config = self._get_ui_config()
                     password_provider = (
                         self._get_password_provider()
@@ -671,8 +663,7 @@ class Router:
                         response.status = http.HTTPStatus.NOT_FOUND
                         response.body = (
                             b'Password provider not configured'
-                            if ui_config
-                            else b'Auth UI not enabled'
+                            if ui_config else b'Auth UI not enabled'
                         )
                     else:
                         query = urllib.parse.parse_qs(
@@ -699,10 +690,7 @@ class Router:
                             brand_color=ui_config.brand_color,
                         )
 
-                case (
-                    'ui',
-                    'reset-password',
-                ):
+                case ('ui', 'reset-password'):
                     ui_config = self._get_ui_config()
                     password_provider = (
                         self._get_password_provider()
@@ -714,8 +702,7 @@ class Router:
                         response.status = http.HTTPStatus.NOT_FOUND
                         response.body = (
                             b'Password provider not configured'
-                            if ui_config
-                            else b'Auth UI not enabled'
+                            if ui_config else b'Auth UI not enabled'
                         )
                     else:
                         query = urllib.parse.parse_qs(
@@ -725,26 +712,24 @@ class Router:
                         )
 
                         reset_token = _maybe_get_search_param(
-                            query, 'reset_token'
-                        )
+                            query, 'reset_token')
 
                         if reset_token is not None:
                             try:
-                                (
-                                    identity_id,
-                                    secret,
-                                ) = self._get_data_from_reset_token(reset_token)
+                                identity_id, secret = (
+                                    self._get_data_from_reset_token(
+                                        reset_token
+                                    )
+                                )
 
                                 local_client = local.Client(
                                     db=self.db,
-                                    provider_name=password_provider.name,
+                                    provider_name=password_provider.name
                                 )
 
-                                is_valid = (
-                                    await (
-                                        local_client.validate_reset_secret(
-                                            identity_id, secret
-                                        )
+                                is_valid = await (
+                                    local_client.validate_reset_secret(
+                                        identity_id, secret
                                     )
                                 )
                             except Exception:
@@ -771,7 +756,8 @@ class Router:
 
                 case ('ui', '_static', filename):
                     filepath = os.path.join(
-                        os.path.dirname(__file__), '_static', filename
+                        os.path.dirname(__file__),
+                        '_static', filename
                     )
                     try:
                         with open(filepath, 'rb') as f:
@@ -853,9 +839,9 @@ class Router:
         self, provider: str, redirect_to: str, challenge: str
     ) -> str:
         signing_key = self._get_auth_signing_key()
-        expires_at = datetime.datetime.now(
-            datetime.timezone.utc
-        ) + datetime.timedelta(minutes=5)
+        expires_at = (
+            datetime.datetime.now(datetime.timezone.utc) +
+            datetime.timedelta(minutes=5))
 
         state_claims = {
             "iss": self.base_path,
@@ -970,15 +956,19 @@ class Router:
 
     def _get_ui_config(self):
         return util.maybe_get_config(
-            self.db, "ext::auth::AuthConfig::ui", CompositeConfigType
+            self.db, "ext::auth::AuthConfig::ui",
+            CompositeConfigType
         )
 
     def _get_password_provider(self):
         providers = util.get_config(
-            self.db, "ext::auth::AuthConfig::providers", frozenset
+            self.db,
+            "ext::auth::AuthConfig::providers",
+            frozenset
         )
         password_providers = [
-            p for p in providers if (p.name == 'builtin::local_emailpassword')
+            p for p in providers
+            if (p.name == 'builtin::local_emailpassword')
         ]
 
         return password_providers[0] if len(password_providers) == 1 else None
