f6d163514a520004f49f2fd57b5f863edfd5f5df
Michael J. Sullivan <sully@msully.net>
2023-10-11 13:45:20 -0700

    Fix compilation of explicit ids in inserts that can be empty
    Explicit ids in inserts that have nontrivial possibly empty
    expressions would fail, because an interaction with optional wrapping
    caused the id path to fall back to the identity of the source, which
    the fallback rvar happily provided as NULL.
    
    I ran across this bug in #3834, but hacked around it instead of fixing
    it (the fix would have been totally different then, also), since
    explicitly-specified `id`s were purely internal, so it wasn't
    high-priority. Of course, two months later we added explicitly
    specified ids.

--- tests/test_edgeql_insert.py
+++ tests/test_edgeql_insert.py
@@ -5708,7 +5708,7 @@ class TestInsert(tb.QueryTestCase):
 
         await self.con.execute('''
             INSERT Person {
-                id := <uuid>'ffffffff-ffff-ffff-ffff-ffffffffffff',
+                id := <uuid>to_json('"ffffffff-ffff-ffff-ffff-ffffffffffff"'),
                 name := "test",
              }
         ''')
