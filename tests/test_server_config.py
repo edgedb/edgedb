edgedb/edgedb

commit cc5958c48e74b92dbedadfaf9915bdc7966e79dc

Allow configuration values to have an enum type
Also, remove some of the special-handling of `cfg::memory` and
`std::duration` in config op compilation and use a staeval-ed value
instead.

edb/pgsql/compiler/config.py
@@ -21,8 +21,6 @@ from __future__ import annotations
 
 from edb import errors
 from edb.ir import ast as irast
-from edb.ir import statypes
-from edb.schema import objects as s_obj
 
 from edb.edgeql import qltypes
 
@@ -436,26 +434,13 @@ def _compile_config_value(
     *,
     ctx: context.CompilerContextLevel,
 ) -> pgast.BaseExpr:
-    op_expr = op.expr.expr
-
-    if (op.backend_setting and
-            isinstance(op_expr, irast.TypeCast) and
-            isinstance(op_expr.expr.expr, irast.StringConstant) and
-            op_expr.from_type.id == s_obj.get_known_type_id('std::str')):
-
-        if op_expr.to_type.id == s_obj.get_known_type_id('std::duration'):
-            dur_value = statypes.Duration(op_expr.expr.expr.value)
-            return pgast.StringConstant(
-                val=f'{dur_value.to_microseconds()}us'
-            )
-
-        if op_expr.to_type.id == s_obj.get_known_type_id('cfg::memory'):
-            mem_value = statypes.ConfigMemory(op_expr.expr.expr.value)
-            return pgast.StringConstant(
-                val=mem_value.to_pg_memory()
-            )
-
     val: pgast.BaseExpr
+
+    if op.backend_setting:
+        assert op.backend_expr is not None
+        expr = op.backend_expr
+    else:
+        expr = op.expr
 
     with ctx.new() as subctx:
         if op.backend_setting:
@@ -464,7 +449,7 @@ def _compile_config_value(
             output_format = context.OutputFormat.JSONB
 
         with context.output_format(ctx, output_format):
-            if isinstance(op.expr, irast.EmptySet):
+            if isinstance(expr, irast.EmptySet):
                 # Special handling for empty sets, because we want a
                 # singleton representation of the value and not an empty rel
                 # in this context.
@@ -485,17 +470,17 @@ def _compile_config_value(
                         ),
                     )
             else:
-                val = dispatch.compile(op.expr, ctx=subctx)
+                val = dispatch.compile(expr, ctx=subctx)
                 assert isinstance(val, pgast.SelectStmt), "expected SelectStmt"
 
                 pathctx.get_path_serialized_output(
-                    val, op.expr.path_id, env=ctx.env)
+                    val, expr.path_id, env=ctx.env)
 
                 if op.cardinality is qltypes.SchemaCardinality.Many:
                     val = output.aggregate_json_output(
-                        val, op.expr, env=ctx.env)
+                        val, expr, env=ctx.env)
 
-    if op.backend_setting:
+    if op.backend_setting and op.scope is qltypes.ConfigScope.INSTANCE:
         assert isinstance(val, pgast.SelectStmt) and len(val.target_list) == 1
         val = val.target_list[0].val
         if isinstance(val, pgast.TypeCast):

tests/test_server_config.py
@@ -972,11 +972,15 @@ class TestServerConfig(tb.QueryTestCase):
             conf4 = "CONFIGURE INSTANCE SET memprop := <cfg::memory>'100MiB';"
             await self.con.execute(conf4)
 
+            conf5 = "CONFIGURE INSTANCE SET enumprop := <cfg::TestEnum>'Two';"
+            await self.con.execute(conf5)
+
             res = await self.con.query_single('DESCRIBE INSTANCE CONFIG;')
             self.assertIn(conf1, res)
             self.assertIn(conf2, res)
             self.assertNotIn(conf3, res)
             self.assertIn(conf4, res)
+            self.assertIn(conf5, res)
 
         finally:
             await self.con.execute('''
@@ -1042,6 +1046,16 @@ class TestServerConfig(tb.QueryTestCase):
             await self.con.execute('''
                 configure instance set
                     durprop := '12 seconds'
+            ''')
+
+    async def test_server_proto_configure_invalid_enum(self):
+        with self.assertRaisesRegex(
+            edgedb.InvalidValueError,
+            "invalid input value for enum 'cfg::TestEnum': \"foo\"",
+        ):
+            await self.con.execute('''
+                configure session set
+                    enumprop := <cfg::TestEnum>'foo'
             ''')
 
     async def test_server_proto_configure_compilation(self):

edb/edgeql/compiler/config.py
@@ -31,6 +31,7 @@ from edb.edgeql import qltypes
 
 from edb.ir import ast as irast
 from edb.ir import staeval as ireval
+from edb.ir import statypes as statypes
 from edb.ir import typeutils as irtyputils
 
 from edb.schema import links as s_links
@@ -42,6 +43,7 @@ from edb.schema import utils as s_utils
 
 from edb.edgeql import ast as qlast
 
+from . import casts
 from . import context
 from . import dispatch
 from . import inference
@@ -64,25 +66,37 @@ def compile_ConfigSet(
 ) -> irast.Set:
 
     info = _validate_op(expr, ctx=ctx)
-    param_val = setgen.ensure_set(
-        dispatch.compile(expr.expr, ctx=ctx), ctx=ctx)
-
-    actual_param_type = inference.infer_type(param_val, ctx.env)
-    if not actual_param_type.assignment_castable_to(
-            info.param_type, ctx.env.schema):
-        raise errors.ConfigurationError(
-            f'invalid setting value type for {info.param_name}: '
-            f'{actual_param_type.get_displayname(ctx.env.schema)!r} '
-            f'(expecting {info.param_type.get_displayname(ctx.env.schema)!r})'
-        )
+    param_val = dispatch.compile(expr.expr, ctx=ctx)
+    param_type = info.param_type
+    val_type = inference.infer_type(param_val, ctx.env)
+    compatible = s_types.is_type_compatible(
+        val_type, param_type, schema=ctx.env.schema)
+    if not compatible:
+        if not val_type.assignment_castable_to(param_type, ctx.env.schema):
+            raise errors.ConfigurationError(
+                f'invalid setting value type for {info.param_name}: '
+                f'{val_type.get_displayname(ctx.env.schema)!r} '
+                f'(expecting {param_type.get_displayname(ctx.env.schema)!r})'
+            )
+        else:
+            param_val = casts.compile_cast(
+                param_val, param_type, srcctx=None, ctx=ctx)
 
     try:
-        ireval.evaluate(param_val, schema=ctx.env.schema)
+        val = ireval.evaluate_to_python_val(param_val, schema=ctx.env.schema)
     except ireval.UnsupportedExpressionError as e:
         raise errors.QueryError(
             f'non-constant expression in CONFIGURE {expr.scope} SET',
             context=expr.expr.context
         ) from e
+    else:
+        if isinstance(val, statypes.ScalarType) and info.backend_setting:
+            backend_expr = dispatch.compile(
+                qlast.StringConstant.from_python(val.to_backend_str()),
+                ctx=ctx,
+            )
+        else:
+            backend_expr = None
 
     config_set = irast.ConfigSet(
         name=info.param_name,
@@ -92,6 +106,7 @@ def compile_ConfigSet(
         backend_setting=info.backend_setting,
         context=expr.context,
         expr=param_val,
+        backend_expr=backend_expr,
     )
     return setgen.ensure_set(config_set, ctx=ctx)
 

edb/ir/ast.py
@@ -921,6 +921,7 @@ class ConfigCommand(Command, Expr):
 class ConfigSet(ConfigCommand):
 
     expr: Set
+    backend_expr: typing.Optional[Set] = None
 
 
 class ConfigReset(ConfigCommand):

edb/ir/staeval.py
@@ -103,22 +103,11 @@ def evaluate_TypeCast(
     schema, from_type = irtyputils.ir_typeref_to_type(
         schema, ir_cast.from_type)
     schema, to_type = irtyputils.ir_typeref_to_type(
-        schema, ir_cast.to_type)
-
-    str_t = cast(s_scalars.ScalarType, schema.get('std::str'))
-    dur_t = cast(s_scalars.ScalarType, schema.get('std::duration'))
-    mem_t = cast(s_scalars.ScalarType, schema.get('cfg::memory'))
-
-    if (from_type.issubclass(schema, str_t) and
-            to_type.issubclass(schema, dur_t)):
-        return ir_cast
-
-    if (from_type.issubclass(schema, str_t) and
-            to_type.issubclass(schema, mem_t)):
-        return ir_cast
-
-    raise UnsupportedExpressionError(
-        f'no static IR evaluation handler for {ir_cast.__class__}')
+        schema, ir_cast.from_type)
+    schema_type_to_python_type(from_type, schema)
+    schema_type_to_python_type(to_type, schema)
+    evaluate(ir_cast.expr, schema)
+    return ir_cast
 
 
 @evaluate.register(irast.EmptySet)
@@ -319,39 +308,43 @@ def schema_type_to_python_type(
             f'{stype.get_displayname(schema)} is not representable in Python')
 
 
+typemap = {
+    'std::str': str,
+    'std::anyint': int,
+    'std::anyfloat': float,
+    'std::decimal': decimal.Decimal,
+    'std::bigint': decimal.Decimal,
+    'std::bool': bool,
+    'std::json': str,
+    'std::uuid': uuidgen.UUID,
+    'std::duration': statypes.Duration,
+    'cfg::memory': statypes.ConfigMemory,
+}
+
+
 def scalar_type_to_python_type(
-        stype: s_types.Type,
-        schema: s_schema.Schema) -> type:
-
-    typemap = {
-        'std::str': str,
-        'std::anyint': int,
-        'std::anyfloat': float,
-        'std::decimal': decimal.Decimal,
-        'std::bigint': decimal.Decimal,
-        'std::bool': bool,
-        'std::json': str,
-        'std::uuid': uuidgen.UUID,
-        'std::duration': statypes.Duration,
-        'cfg::memory': statypes.ConfigMemory,
-    }
-
-    for basetype_name, python_type in typemap.items():
+    stype: s_types.Type,
+    schema: s_schema.Schema,
+) -> type:
+    for basetype_name, pytype in typemap.items():
         basetype = schema.get(basetype_name, type=s_obj.InheritingObject)
         if stype.issubclass(schema, basetype):
-            return python_type
+            return pytype
+
+    if stype.is_enum(schema):
+        return str
 
     raise UnsupportedExpressionError(
         f'{stype.get_displayname(schema)} is not representable in Python')
 
 
 def object_type_to_python_type(
-        objtype: s_objtypes.ObjectType,
-        schema: s_schema.Schema, *,
-        base_class: Optional[type] = None,
-        _memo: Optional[Dict[s_types.Type, type]]=None
+    objtype: s_objtypes.ObjectType,
+    schema: s_schema.Schema,
+    *,
+    base_class: Optional[type] = None,
+    _memo: Optional[Dict[s_types.Type, type]] = None,
 ) -> type:
-
     if _memo is None:
         _memo = {}
     default: Any

edb/ir/statypes.py
@@ -25,7 +25,15 @@ import re
 from edb import errors
 
 
-class Duration:
+class ScalarType:
+    def __init__(self, val: str, /) -> None:
+        raise NotImplementedError
+
+    def to_backend_str(self) -> str:
+        raise NotImplementedError
+
+
+class Duration(ScalarType):
 
     _pg_simple_parser = re.compile(r'''
         ^
@@ -281,11 +289,14 @@ class Duration:
             ret.append('0S')
         return ''.join(ret)
 
+    def to_backend_str(self) -> str:
+        return f'{self.to_microseconds()}us'
+
     def __repr__(self) -> str:
         return f'<statypes.Duration {self.to_iso8601()!r}>'
 
 
-class ConfigMemory:
+class ConfigMemory(ScalarType):
 
     PiB = 1024 * 1024 * 1024 * 1024 * 1024
     TiB = 1024 * 1024 * 1024 * 1024
@@ -362,7 +373,7 @@ class ConfigMemory:
 
         return f'{self._value}B'
 
-    def to_pg_memory(self) -> str:
+    def to_backend_str(self) -> str:
         if self._value >= self.TiB and self._value % self.TiB == 0:
             return f'{self._value // self.TiB}TB'
 

edb/lib/_testmode.edgeql
@@ -51,6 +51,9 @@ CREATE TYPE cfg::TestInstanceConfig {
 };
 
 
+CREATE SCALAR TYPE cfg::TestEnum extending enum<One, Two, Three>;
+
+
 ALTER TYPE cfg::AbstractConfig {
     CREATE MULTI LINK sessobj -> cfg::TestSessionConfig {
         CREATE ANNOTATION cfg::internal := 'true';
@@ -108,6 +111,9 @@ ALTER TYPE cfg::AbstractConfig {
         SET default := <std::duration>'0 seconds';
     };
 
+    CREATE PROPERTY enumprop -> cfg::TestEnum {
+        SET default := cfg::TestEnum.One;
+    };
 
     CREATE PROPERTY __pg_max_connections -> std::int64 {
         CREATE ANNOTATION cfg::internal := 'true';

edb/pgsql/compiler/__init__.py
@@ -136,6 +136,7 @@ def compile_ir_to_sql(
     output_format: Optional[OutputFormat]=None,
     ignore_shapes: bool=False,
     explicit_top_cast: Optional[irast.TypeRef]=None,
+    singleton_mode: bool=False,
     use_named_params: bool=False,
     expected_cardinality_one: bool=False,
     pretty: bool=True,
@@ -147,6 +148,7 @@ def compile_ir_to_sql(
         output_format=output_format,
         ignore_shapes=ignore_shapes,
         explicit_top_cast=explicit_top_cast,
+        singleton_mode=singleton_mode,
         use_named_params=use_named_params,
         expected_cardinality_one=expected_cardinality_one,
         backend_runtime_params=backend_runtime_params,

commit 63665b6990162e6d0d4cc2b5152acf0976909654

Fix configuring effective_io_concurrency
Configuring actual postgres backend settings only works for nontrivial
types like duration and memory, not simple ones like int.

I think the problem was introduced by cc5958c48.

Fixes #6109.

edb/pgsql/compiler/config.py
@@ -625,11 +625,7 @@ def _compile_config_value(
 ) -> pgast.BaseExpr:
     val: pgast.BaseExpr
 
-    if op.backend_setting:
-        assert op.backend_expr is not None
-        expr = op.backend_expr
-    else:
-        expr = op.expr
+    expr = op.backend_expr or op.expr
 
     with ctx.new() as subctx:
         if op.backend_setting or op.scope == qltypes.ConfigScope.GLOBAL:

tests/test_server_config.py
@@ -1726,6 +1726,14 @@ class TestSeparateCluster(tb.TestCase):
                     cur_shared[0]
                 )
 
+                cur_eff = await c1.query_single('''
+                    select assert_single(cfg::Config.effective_io_concurrency)
+                ''')
+                await c1.query(f'''
+                    configure instance set
+                        effective_io_concurrency := {cur_eff}
+                ''')
+
                 await c1.aclose()
                 await c2.aclose()
                 await t1.aclose()