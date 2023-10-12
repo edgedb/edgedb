edgedb/edgedb

commit 3eefe84aa570a6989ea2a211b4aa5a0ed3ba5855

Don't unpickle schema in I/O process (#5921)
* Add pickled user schema in dbview
* Add pickled global schema in dbview
* Use pickled user/global schema in compiler client
* Move inteprete_backend_error back to the compiler
* Move introspect_db_config() to Server and split parsing
* Use JSON schema and db_config in dump
* Load extension config settings in the compiler
* Rename and add missing usage of drop_tenant
* Construct compiler with override-able args
* Move multi-tenant-only arg to multitenant.py
* Revamp compiler pool client initialization
* Parse global schema JSON in the compiler
* Parse JSON of user schema and db config in Compiler
* Use pickled schemas in restore
* Move construction of state serializer to the compiler
* Make state serializer during introspection
* Drop unpickled schema in dbview

edb/server/protocol/binary.pxd
@@ -77,8 +77,6 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
     cdef inline dbview.DatabaseConnectionView get_dbview(self)
 
-    cdef interpret_backend_error(self, exc)
-
     cdef dbview.QueryRequestInfo parse_execute_request(self)
     cdef parse_output_format(self, bytes mode)
     cdef parse_cardinality(self, bytes card)

edb/server/protocol/binary.pyx
@@ -310,6 +310,8 @@ cdef class EdgeConnection(frontend.FrontendConnection):
         msg_buf.end_message()
         buf.write_buffer(msg_buf)
 
+        if self.get_dbview().get_state_serializer() is None:
+            await self.get_dbview().reload_state_serializer()
         buf.write_buffer(self.make_state_data_description_msg())
 
         self.write(buf)
@@ -875,6 +877,7 @@ cdef class EdgeConnection(frontend.FrontendConnection):
         cdef:
             bytes eql
             dbview.QueryRequestInfo query_req
+            dbview.DatabaseConnectionView _dbview
             WriteBuffer parse_complete
             WriteBuffer buf
 
@@ -882,6 +885,9 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
         self.ignore_headers()
 
+        _dbview = self.get_dbview()
+        if _dbview.get_state_serializer() is None:
+            await _dbview.reload_state_serializer()
         query_req = self.parse_execute_request()
         compiled = await self._parse(query_req)
 
@@ -908,14 +914,15 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
         self.ignore_headers()
 
+        _dbview = self.get_dbview()
+        if _dbview.get_state_serializer() is None:
+            await _dbview.reload_state_serializer()
         query_req = self.parse_execute_request()
         in_tid = self.buffer.read_bytes(16)
         out_tid = self.buffer.read_bytes(16)
         args = self.buffer.read_len_prefixed_bytes()
 
         self.buffer.finish_message()
-
-        _dbview = self.get_dbview()
 
         if (
             self._last_anon_compiled is not None and
@@ -1084,6 +1091,9 @@ cdef class EdgeConnection(frontend.FrontendConnection):
             self.get_dbview().tx_error()
             self.buffer.finish_message()
 
+            if isinstance(ex, pgerror.BackendError):
+                ex = await self.interpret_backend_error(ex)
+
             self.write_error(ex)
             self.flush()
 
@@ -1148,9 +1158,6 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
         exc_code = None
 
-        if isinstance(exc, pgerror.BackendError):
-            exc = self.interpret_backend_error(exc)
-
         fields = {}
         if (isinstance(exc, errors.EdgeDBError) and
                 type(exc) is not errors.EdgeDBError):
@@ -1195,17 +1202,14 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
         self.write(buf)
 
-    cdef interpret_backend_error(self, exc):
+    async def interpret_backend_error(self, exc):
         try:
             static_exc = errormech.static_interpret_backend_error(
                 exc.fields)
 
             # only use the backend if schema is required
             if static_exc is errormech.SchemaRequired:
-                exc = errormech.interpret_backend_error(
-                    self.get_dbview().get_schema(),
-                    exc.fields
-                )
+                exc = await self.get_dbview().interpret_backend_error(exc)
             elif isinstance(static_exc, (
                     errors.DuplicateDatabaseDefinitionError,
                     errors.UnknownDatabaseError)):
@@ -1355,16 +1359,18 @@ cdef class EdgeConnection(frontend.FrontendConnection):
                 ''',
             )
 
-            user_schema = await tenant.introspect_user_schema(pgcon)
-            global_schema = await tenant.introspect_global_schema(pgcon)
-            db_config = await tenant.introspect_db_config(pgcon, user_schema)
+            user_schema_json = await server.introspect_user_schema_json(pgcon)
+            global_schema_json = (
+                await server.introspect_global_schema_json(pgcon)
+            )
+            db_config_json = await server.introspect_db_config(pgcon)
             dump_protocol = self.max_protocol
 
             schema_ddl, schema_dynamic_ddl, schema_ids, blocks = (
                 await compiler_pool.describe_database_dump(
-                    user_schema,
-                    global_schema,
-                    db_config,
+                    user_schema_json,
+                    global_schema_json,
+                    db_config_json,
                     dump_protocol,
                 )
             )
@@ -1518,6 +1524,8 @@ cdef class EdgeConnection(frontend.FrontendConnection):
             raise errors.ProtocolError(
                 'RESTORE must not be executed while in transaction'
             )
+        if _dbview.get_state_serializer() is None:
+            await _dbview.reload_state_serializer()
 
         self.reject_headers()
         self.buffer.read_int16()  # discard -j level
@@ -1527,8 +1535,8 @@ cdef class EdgeConnection(frontend.FrontendConnection):
         server = self.server
         compiler_pool = server.get_compiler_pool()
 
-        global_schema = _dbview.get_global_schema()
-        user_schema = _dbview.get_user_schema()
+        global_schema_pickle = _dbview.get_global_schema_pickle()
+        user_schema_pickle = _dbview.get_user_schema_pickle()
 
         dump_server_ver_str = None
         cat_ver = None
@@ -1596,8 +1604,8 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
             schema_sql_units, restore_blocks, tables = \
                 await compiler_pool.describe_database_restore(
-                    user_schema,
-                    global_schema,
+                    user_schema_pickle,
+                    global_schema_pickle,
                     dump_server_ver_str,
                     cat_ver,
                     schema_ddl,
@@ -1873,7 +1881,7 @@ async def run_script(
         else:
             await conn._execute(compiled, b'', use_prep_stmt=0)
     except pgerror.BackendError as e:
-        exc = conn.interpret_backend_error(e)
+        exc = await conn.interpret_backend_error(e)
         if isinstance(exc, errors.EdgeDBError):
             raise exc from None
         else:

edb/server/protocol/execute.pyx
@@ -208,12 +208,13 @@ async def execute_script(
         bytes state = None, orig_state = None
         ssize_t sent = 0
         bint in_tx, sync, no_sync
-        object user_schema, extensions, cached_reflection, global_schema
+        object user_schema, extensions, ext_config_settings, cached_reflection
+        object global_schema, roles
         WriteBuffer bind_data
         int dbver = dbv.dbver
         bint parse
 
-    user_schema = extensions = cached_reflection = None
+    user_schema = extensions = ext_config_settings = cached_reflection = None
     global_schema = roles = None
     unit_group = compiled.query_unit_group
 
@@ -288,6 +289,7 @@ async def execute_script(
                 if query_unit.user_schema:
                     user_schema = query_unit.user_schema
                     extensions = query_unit.extensions
+                    ext_config_settings = query_unit.ext_config_settings
                     cached_reflection = query_unit.cached_reflection
 
                 if query_unit.global_schema:
@@ -354,6 +356,7 @@ async def execute_script(
             side_effects = dbv.commit_implicit_tx(
                 user_schema,
                 extensions,
+                ext_config_settings,
                 global_schema,
                 roles,
                 cached_reflection,

edb/graphql/extension.pyx
@@ -186,14 +186,12 @@ async def handle_request(
 
             # only use the backend if schema is required
             if static_exc is errormech.SchemaRequired:
-                ex = errormech.interpret_backend_error(
-                    s_schema.ChainedSchema(
-                        tenant.server.get_std_schema(),
-                        db.user_schema,
-                        tenant.get_global_schema(),
-                    ),
+                compiler_pool = tenant.server.get_compiler_pool()
+                ex = await compiler_pool.interpret_backend_error(
+                    db.user_schema_pickle,
+                    tenant.get_global_schema_pickle(),
                     ex.fields,
-                    from_graphql=True,
+                    True,
                 )
             else:
                 ex = static_exc
@@ -227,8 +225,8 @@ async def compile(
     compiler_pool = server.get_compiler_pool()
     return await compiler_pool.compile_graphql(
         db.name,
-        db.user_schema,
-        tenant.get_global_schema(),
+        db.user_schema_pickle,
+        tenant.get_global_schema_pickle(),
         db.reflection_cache,
         db.db_config,
         db._index.get_compilation_system_config(),

edb/server/compiler/compiler.py
@@ -387,6 +387,8 @@ class CompilerState:
 
     @functools.cached_property
     def state_serializer_factory(self) -> sertypes.StateSerializerFactory:
+        # TODO: This factory will probably need to become per-db once
+        # config spec differs between databases. See also #5836.
         return sertypes.StateSerializerFactory(
             self.std_schema, self.config_spec
         )
@@ -919,13 +921,115 @@ class Compiler:
 
         return compile(ctx=ctx, source=source), ctx.state
 
+    def interpret_backend_error(
+        self,
+        user_schema: bytes,
+        global_schema: bytes,
+        error_fields: dict[str, str],
+        from_graphql: bool,
+    ) -> errors.EdgeDBError:
+        from . import errormech
+
+        schema = s_schema.ChainedSchema(
+            self.state.std_schema,
+            pickle.loads(user_schema),
+            pickle.loads(global_schema),
+        )
+        rv: errors.EdgeDBError = errormech.interpret_backend_error(
+            schema, error_fields, from_graphql=from_graphql
+        )
+        return rv
+
+    def parse_json_schema(
+        self,
+        schema_json: bytes,
+        base_schema: s_schema.Schema | None,
+    ) -> s_schema.Schema:
+        if base_schema is None:
+            base_schema = self.state.std_schema
+        else:
+            base_schema = s_schema.ChainedSchema(
+                self.state.std_schema,
+                s_schema.EMPTY_SCHEMA,
+                base_schema,
+            )
+
+        return s_refl.parse_into(
+            base_schema=base_schema,
+            schema=s_schema.EMPTY_SCHEMA,
+            data=schema_json,
+            schema_class_layout=self.state.schema_class_layout,
+        )
+
+    def parse_db_config(
+        self, db_config_json: bytes, user_schema: s_schema.Schema
+    ) -> immutables.Map[str, config.SettingValue]:
+        spec = config.ChainedSpec(
+            self.state.config_spec,
+            config.load_ext_spec_from_schema(
+                user_schema,
+                self.state.std_schema,
+            ),
+        )
+        return config.from_json(spec, db_config_json)
+
+    def parse_global_schema(self, global_schema_json: bytes) -> bytes:
+        global_schema = self.parse_json_schema(global_schema_json, None)
+        return pickle.dumps(global_schema, -1)
+
+    def parse_user_schema_db_config(
+        self,
+        user_schema_json: bytes,
+        db_config_json: bytes,
+        global_schema_pickle: bytes,
+    ) -> dbstate.ParsedDatabase:
+        global_schema = pickle.loads(global_schema_pickle)
+        user_schema = self.parse_json_schema(user_schema_json, global_schema)
+        db_config = self.parse_db_config(db_config_json, user_schema)
+        ext_config_settings = config.load_ext_settings_from_schema(
+            s_schema.ChainedSchema(
+                self.state.std_schema,
+                user_schema,
+                s_schema.EMPTY_SCHEMA,
+            )
+        )
+        state_serializer = self.state.state_serializer_factory.make(
+            user_schema,
+            global_schema,
+            defines.CURRENT_PROTOCOL,
+        )
+        return dbstate.ParsedDatabase(
+            user_schema_pickle=pickle.dumps(user_schema, -1),
+            database_config=db_config,
+            ext_config_settings=ext_config_settings,
+            protocol_version=defines.CURRENT_PROTOCOL,
+            state_serializer=state_serializer,
+        )
+
+    def make_state_serializer(
+        self,
+        protocol_version: defines.ProtocolVersion,
+        user_schema_pickle: bytes,
+        global_schema_pickle: bytes,
+    ) -> sertypes.StateSerializer:
+        user_schema = pickle.loads(user_schema_pickle)
+        global_schema = pickle.loads(global_schema_pickle)
+        return self.state.state_serializer_factory.make(
+            user_schema,
+            global_schema,
+            protocol_version,
+        )
+
     def describe_database_dump(
         self,
-        user_schema: s_schema.Schema,
-        global_schema: s_schema.Schema,
-        database_config: immutables.Map[str, config.SettingValue],
+        user_schema_json: bytes,
+        global_schema_json: bytes,
+        db_config_json: bytes,
         protocol_version: defines.ProtocolVersion,
     ) -> DumpDescriptor:
+        global_schema = self.parse_json_schema(global_schema_json, None)
+        user_schema = self.parse_json_schema(user_schema_json, global_schema)
+        database_config = self.parse_db_config(db_config_json, user_schema)
         schema = s_schema.ChainedSchema(
             self.state.std_schema,
             user_schema,
@@ -1001,8 +1105,8 @@ class Compiler:
 
     def describe_database_restore(
         self,
-        user_schema: s_schema.Schema,
-        global_schema: s_schema.Schema,
+        user_schema_pickle: bytes,
+        global_schema_pickle: bytes,
         dump_server_ver_str: str,
         dump_catalog_version: Optional[int],
         schema_ddl: bytes,
@@ -1025,8 +1129,8 @@ class Compiler:
         dump_catalog_version = dump_catalog_version or 0
 
         state = dbstate.CompilerConnectionState(
-            user_schema=user_schema,
-            global_schema=global_schema,
+            user_schema=pickle.loads(user_schema_pickle),
+            global_schema=pickle.loads(global_schema_pickle),
             modaliases=DEFAULT_MODULE_ALIASES_MAP,
             session_config=EMPTY_MAP,
             database_config=EMPTY_MAP,
@@ -2234,7 +2338,9 @@ def _try_compile(
             if comp.user_schema is not None:
                 final_user_schema = comp.user_schema
                 unit.user_schema = pickle.dumps(comp.user_schema, -1)
-                unit.extensions = _extract_extension_names(comp.user_schema)
+                unit.extensions, unit.ext_config_settings = (
+                    _extract_extensions(ctx, comp.user_schema)
+                )
             if comp.cached_reflection is not None:
                 unit.cached_reflection = \
                     pickle.dumps(comp.cached_reflection, -1)
@@ -2255,7 +2361,9 @@ def _try_compile(
             if comp.user_schema is not None:
                 final_user_schema = comp.user_schema
                 unit.user_schema = pickle.dumps(comp.user_schema, -1)
-                unit.extensions = _extract_extension_names(comp.user_schema)
+                unit.extensions, unit.ext_config_settings = (
+                    _extract_extensions(ctx, comp.user_schema)
+                )
             if comp.cached_reflection is not None:
                 unit.cached_reflection = \
                     pickle.dumps(comp.cached_reflection, -1)
@@ -2289,7 +2397,9 @@ def _try_compile(
             if comp.user_schema is not None:
                 final_user_schema = comp.user_schema
                 unit.user_schema = pickle.dumps(comp.user_schema, -1)
-                unit.extensions = _extract_extension_names(comp.user_schema)
+                unit.extensions, unit.ext_config_settings = (
+                    _extract_extensions(ctx, comp.user_schema)
+                )
             if comp.cached_reflection is not None:
                 unit.cached_reflection = \
                     pickle.dumps(comp.cached_reflection, -1)
@@ -2802,11 +2912,22 @@ def _hash_sql(sql: bytes, **kwargs: bytes) -> bytes:
     return h.hexdigest().encode('latin1')
 
 
-def _extract_extension_names(user_schema: s_schema.Schema) -> set[str]:
-    return {
+def _extract_extensions(
+    ctx: CompileContext, user_schema: s_schema.Schema
+) -> tuple[set[str], list[config.Setting]]:
+    # XXX: Do we need to return None if extensions/config_spec didn't change?
+    names = {
         ext.get_name(user_schema).name
         for ext in user_schema.get_objects(type=s_ext.Extension)
     }
+    if names:
+        schema = s_schema.ChainedSchema(
+            ctx.compiler_state.std_schema, user_schema, s_schema.EMPTY_SCHEMA
+        )
+        settings = config.load_ext_settings_from_schema(schema)
+    else:
+        settings = []
+    return names, settings
 
 
 def _extract_roles(

edb/server/compiler/dbstate.py
@@ -315,6 +315,7 @@ class QueryUnit:
     user_schema: Optional[bytes] = None
     cached_reflection: Optional[bytes] = None
     extensions: Optional[set[str]] = None
+    ext_config_settings: Optional[list[config.Setting]] = None
 
     # If present, represents the future global schema state
     # after the command is run. The schema is pickled.
@@ -467,6 +468,16 @@ class SQLQueryUnit:
 
     command_tag: bytes = b""
     """If frontend_only is True, only issue CommandComplete with this tag."""
+
+
+@dataclasses.dataclass
+class ParsedDatabase:
+    user_schema_pickle: bytes
+    database_config: immutables.Map[str, config.SettingValue]
+    ext_config_settings: list[config.Setting]
+
+    protocol_version: defines.ProtocolVersion
+    state_serializer: sertypes.StateSerializer
 
 
 SQLSettings = immutables.Map[Optional[str], Optional[str | list[str]]]

edb/server/compiler_pool/server.py
@@ -194,27 +194,26 @@ class MultiSchemaPool(pool_mod.FixedPool):
     _clients: typing.Dict[int, ClientSchema]
 
     def __init__(self, cache_size, *, secret, **kwargs):
-        super().__init__(
-            backend_runtime_params=None,
-            std_schema=None,
-            refl_schema=None,
-            schema_class_layout=None,
-            **kwargs,
-        )
+        super().__init__(**kwargs)
         self._catalog_version = None
         self._inited = asyncio.Event()
         self._cache_size = cache_size
         self._clients = {}
         self._secret = secret
 
-    def _get_init_args_uncached(self):
+    def _init(self, kwargs: dict[str, typing.Any]) -> None:
+        # this is deferred to _init_server()
+        pass
+
+    @functools.cache
+    def _get_init_args(self):
         init_args = (
             self._backend_runtime_params,
             self._std_schema,
             self._refl_schema,
             self._schema_class_layout,
         )
-        return init_args
+        return init_args, pickle.dumps(init_args, -1)
 
     async def _attach_worker(self, pid: int):
         if not self._running:
@@ -234,7 +233,7 @@ class MultiSchemaPool(pool_mod.FixedPool):
         (
             std_args_pickled,
             client_args_pickled,
-            global_schema_pickled,
+            global_schema_pickle,
             system_config_pickled,
         ) = init_args_pickled
         dbs, backend_runtime_params = pickle.loads(client_args_pickled)
@@ -264,14 +263,14 @@ class MultiSchemaPool(pool_mod.FixedPool):
                 (
                     dbname,
                     PickledState(
-                        pickle.dumps(state.user_schema, -1),
+                        state.user_schema_pickle,
                         pickle.dumps(state.reflection_cache, -1),
                         pickle.dumps(state.database_config, -1),
                     ),
                 )
                 for dbname, state in dbs.items()
             ),
-            global_schema_pickled,
+            global_schema_pickle,
             system_config_pickled,
             (),
         )

commit 044127a4effc3dcc30f6c4f0a400d09b0a79bf47

Fix errors.IdleTransactionTimeoutError error code
This is a regression in 4.0, probably introduced by #5921 moving
interpret_error out of write_error. Some code paths, purely involving
protocol things, can't call interpret_error before calling
write_error, so make write_error at least do static error
interpretation.

Refactor ISE handling while we're at it.

edb/server/protocol/binary.pxd
@@ -100,6 +100,7 @@ cdef class EdgeConnection(frontend.FrontendConnection):
     cdef dict parse_headers(self)
 
     cdef write_status(self, bytes name, bytes value)
+    cdef write_edgedb_error(self, exc)
 
     cdef write_log(self, EdgeSeverity severity, uint32_t code, str message)
 

edb/server/protocol/binary.pyx
@@ -226,7 +226,7 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
     def close_for_idling(self):
         try:
-            self.write_error(
+            self.write_edgedb_error(
                 errors.IdleSessionTimeoutError(
                     'closing the connection due to idling')
             )
@@ -1127,7 +1127,7 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
             ex = await self.interpret_error(ex)
 
-            self.write_error(ex)
+            self.write_edgedb_error(ex)
 
             if isinstance(
                 ex,
@@ -1174,15 +1174,12 @@ cdef class EdgeConnection(frontend.FrontendConnection):
                 self.buffer.discard_message()
 
     cdef write_error(self, exc):
+        self.write_edgedb_error(execute.interpret_simple_error(exc))
+
+    cdef write_edgedb_error(self, exc):
         cdef:
             WriteBuffer buf
             int16_t fields_len
-
-        exc_type = type(exc)
-        # Not all calls to write_error went through interpret_error, so we
-        # do this check here also.
-        if not issubclass(exc_type, errors.EdgeDBError):
-            exc_type = errors.InternalServerError
 
         if self.debug and not isinstance(exc, errors.BackendUnavailableError):
             self.debug_print('EXCEPTION', type(exc).__name__, exc)
@@ -1201,21 +1198,9 @@ cdef class EdgeConnection(frontend.FrontendConnection):
                 'transport': self._transport,
         })
 
-        exc_code = None
-
         fields = {}
         if isinstance(exc, errors.EdgeDBError):
             fields.update(exc._attrs)
-
-        exc_code = exc_type.get_code()
-        if (exc_type is errors.InternalServerError
-                and not fields.get(base_errors.FIELD_HINT)):
-            fields[base_errors.FIELD_HINT] = (
-                f'This is most likely a bug in EdgeDB. '
-                f'Please consider opening an issue ticket '
-                f'at https://github.com/edgedb/edgedb/issues/new'
-                f'?template=bug_report.md'
-            )
 
         try:
             formatted_error = exc.__formatted_error__
@@ -1232,7 +1217,7 @@ cdef class EdgeConnection(frontend.FrontendConnection):
 
         buf = WriteBuffer.new_message(b'E')
         buf.write_byte(<char><uint8_t>EdgeSeverity.EDGE_SEVERITY_ERROR)
-        buf.write_int32(<int32_t><uint32_t>exc_code)
+        buf.write_int32(<int32_t><uint32_t>exc.get_code())
         buf.write_len_prefixed_utf8(str(exc))
         buf.write_int16(len(fields))
         for k, v in fields.items():

edb/server/protocol/execute.pyx
@@ -614,6 +614,25 @@ cdef bytes _encode_args(list args):
     return bytes(out_buf)
 
 
+cdef _check_for_ise(exc):
+    if not isinstance(exc, errors.EdgeDBError):
+        nexc = errors.InternalServerError(
+            f'{type(exc).__name__}: {exc}',
+            hint=(
+                f'This is most likely a bug in EdgeDB. '
+                f'Please consider opening an issue ticket '
+                f'at https://github.com/edgedb/edgedb/issues/new'
+                f'?template=bug_report.md'
+            ),
+        ).with_traceback(exc.__traceback__)
+        formatted = getattr(exc, '__formatted_error__', None)
+        if formatted:
+            nexc.__formatted_error__ = formatted
+        exc = nexc
+
+    return exc
+
+
 async def interpret_error(
     exc: Exception,
     db: dbview.Database,
@@ -672,12 +691,17 @@ async def interpret_error(
                 'unhandled error while calling interpret_backend_error(); '
                 'run with EDGEDB_DEBUG_SERVER to debug.')
 
-    if not isinstance(exc, errors.EdgeDBError):
-        nexc = errors.InternalServerError(
-            f'{type(exc).__name__}: {exc}').with_traceback(exc.__traceback__)
-        formatted = getattr(exc, '__formatted_error__', None)
-        if formatted:
-            nexc.__formatted_error__ = formatted
-        exc = nexc
+    return _check_for_ise(exc)
 
-    return exc
+
+def interpret_simple_error(
+    exc: Exception,
+) -> Exception:
+    """Intepret a protocol error not associated with a query or schema"""
+
+    if isinstance(exc, pgerror.BackendError):
+        static_exc = errormech.static_interpret_backend_error(exc.fields)
+        if static_exc is not errormech.SchemaRequired:
+            exc = static_exc
+
+    return _check_for_ise(exc)

tests/test_server_config.py
@@ -1493,10 +1493,15 @@ class TestSeparateCluster(tb.TestCase):
 
             await asyncio.sleep(1)
 
-            await conn.recv_match(
+            msg = await conn.recv_match(
                 protocol.ErrorResponse,
                 message='closing the connection due to idling'
             )
+
+            # Resolve error code before comparing for better error messages
+            errcls = errors.EdgeDBError.get_error_class_from_code(
+                msg.error_code)
+            self.assertEqual(errcls, errors.IdleSessionTimeoutError)
 
     async def test_server_config_db_config(self):
         async with tb.start_edgedb_server(
@@ -1791,11 +1796,16 @@ class TestSeparateCluster(tb.TestCase):
                 state=state_msg.state_data,
             )
 
-            await asyncio.wait_for(conn.recv_match(
+            msg = await asyncio.wait_for(conn.recv_match(
                 protocol.ErrorResponse,
                 message='terminating connection due to '
                         'idle-in-transaction timeout'
             ), 8)
+
+            # Resolve error code before comparing for better error messages
+            errcls = errors.EdgeDBError.get_error_class_from_code(
+                msg.error_code)
+            self.assertEqual(errcls, errors.IdleTransactionTimeoutError)
 
             with self.assertRaises((
                 edgedb.ClientConnectionFailedTemporarilyError,