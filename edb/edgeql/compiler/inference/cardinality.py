dda89425243667fad7c9753d6ba6bb6871a27ed6
Michael J. Sullivan <sully@msully.net>
2023-10-13 10:00:52 -0700

    Fix unused WITH DML when body is an INSERT (#6287)
    The problem was cardinality inference wasn't getting run on it.  If
    the body of the WITH was something other than an INSERT, cardinality
    inference happened somewhat by accident, since multiplicity inference
    was done properly.
    
    Fixes #6279.

--- edb/edgeql/compiler/inference/cardinality.py
+++ edb/edgeql/compiler/inference/cardinality.py
@@ -1152,6 +1152,9 @@ def _infer_stmt_cardinality(
     scope_tree: irast.ScopeTreeNode,
     ctx: inference_context.InfCtx,
 ) -> qltypes.Cardinality:
+    for part in (ir.bindings or []):
+        infer_cardinality(part, scope_tree=scope_tree, ctx=ctx)
+
     result = ir.subject if isinstance(ir, irast.MutatingStmt) else ir.result
     result_card = infer_cardinality(
         result,
@@ -1278,6 +1281,8 @@ def __infer_insert_stmt(
     scope_tree: irast.ScopeTreeNode,
     ctx: inference_context.InfCtx,
 ) -> qltypes.Cardinality:
+    for part in (ir.bindings or []):
+        infer_cardinality(part, scope_tree=scope_tree, ctx=ctx)
 
     infer_cardinality(
         ir.subject, is_mutation=True, scope_tree=scope_tree, ctx=ctx
