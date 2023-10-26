5f108884f49bee243e8a80e78e014670fba9e78f
Michael J. Sullivan <sully@msully.net>
2023-10-25 22:25:42 -0700

    Preload the parser for docs tests (#6359)
    Tested with
    ```
    edb test tests/test_docs.py
    ```

--- edb/tools/toy_eval_model.py
+++ edb/tools/toy_eval_model.py
@@ -66,6 +66,7 @@ from edb.common.compiler import AliasGenerator
 from edb.edgeql import ast as qlast
 from edb.edgeql import qltypes as ft
 from edb.edgeql import desugar_group
+from edb.edgeql import parser as qlparser
 
 
 from dataclasses import dataclass, replace, field
@@ -1924,6 +1925,8 @@ parser.add_argument('commands', metavar='cmd', type=str, nargs='*',
 
 
 def main() -> None:
+    qlparser.preload_spec()
+
     db = mk_DB1()
 
     args = parser.parse_args()
