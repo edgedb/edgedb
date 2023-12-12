e18e71ea35868f46e6d91e4fc23418eafa3d1be8
Michael J. Sullivan <sully@msully.net>
2023-12-12 13:20:56 -0800

    partial restore

--- edb/pgsql/compiler/output.py
+++ edb/pgsql/compiler/output.py
@@ -526,6 +526,19 @@ def output_as_value(
     return val
 
 
+def add_null_test(expr: pgast.BaseExpr, query: pgast.SelectStmt) -> None:
+    if not expr.nullable:
+        return
+
+    while isinstance(expr, pgast.TupleVar) and expr.elements:
+        expr = expr.elements[0].val
+
+    query.where_clause = astutils.extend_binop(
+        query.where_clause,
+        pgast.NullTest(arg=expr, negated=True)
+    )
+
+
 def serialize_expr_if_needed(
         expr: pgast.BaseExpr, *,
         path_id: irast.PathId,
