f6c1f0fad6f641494dfcf6a7e7f937657e183a64
Michael J. Sullivan <sully@msully.net>
2023-12-13 15:57:54 -0800

    Fix DML-containing FOR when the iterator is an object set with duplicates
    DML-containing FOR loops get compiled into CTEs, and after the DML is
    performed, the CTEs need to get joined back with the DML CTEs in order
    to produce the output. Currently we do these joins on the 'identity'
    of the iterator set; for non-object sets, we generate a transient
    identity with uuid_generate_v4, and for object sets we ues the actual
    object id.
    
    That works ok if all the objects in the iterator set are unique, but
    if they aren't we produce a lot of duplicate output rows.
    
    To solve this, we need to use transient ids for object types too, but
    overriding object identity with something transient causes problems:
    if we need to actually join the object against some *actual* table, we
    are in trouble, since we've overriden the identity.
    
    Solve this by introducing a new 'iterator' aspect and distinguishing
    between whether we want to join on an iterator aspect or a real identity
    when making path bonds.
    
    Fixes #6608

--- edb/pgsql/compiler/dml.py
+++ edb/pgsql/compiler/dml.py
@@ -160,8 +160,7 @@ def init_dml_stmt(
         dml_rvar = relctx.rvar_for_rel(dml_cte, ctx=ctx)
         else_cte = (dml_cte, dml_rvar)
 
-    if ctx.enclosing_cte_iterator:
-        pathctx.put_path_bond(ctx.rel, ctx.enclosing_cte_iterator.path_id)
+    put_iterator_bond(ctx.enclosing_cte_iterator, ctx.rel)
 
     ctx.dml_stmt_stack.append(ir_stmt)
 
@@ -210,8 +209,7 @@ def gen_dml_union(
             )
 
         assert qry.larg
-        if ctx.enclosing_cte_iterator:
-            pathctx.put_path_bond(qry.larg, ctx.enclosing_cte_iterator.path_id)
+        put_iterator_bond(ctx.enclosing_cte_iterator, qry.larg)
 
         union_cte = pgast.CommonTableExpr(
             query=qry.larg,
@@ -350,6 +348,15 @@ def wrap_dml_cte(
     return dml_rvar
 
 
+def put_iterator_bond(
+    iterator: Optional[pgast.IteratorCTE],
+    select: pgast.Query,
+) -> None:
+    if iterator:
+        pathctx.put_path_bond(
+            select, iterator.path_id, iterator=iterator.iterator_bond)
+
+
 def merge_iterator_scope(
     iterator: Optional[pgast.IteratorCTE],
     select: pgast.SelectStmt,
@@ -372,7 +379,7 @@ def merge_iterator(
     if iterator:
         iterator_rvar = relctx.rvar_for_rel(iterator.cte, ctx=ctx)
 
-        pathctx.put_path_bond(select, iterator.path_id)
+        put_iterator_bond(iterator, select)
         relctx.include_rvar(
             select, iterator_rvar,
             path_id=iterator.path_id,
@@ -537,7 +544,7 @@ def compile_iterator_cte(
         iterator_cte = ctx.dml_stmts[iterator_set]
         return pgast.IteratorCTE(
             path_id=iterator_set.path_id, cte=iterator_cte,
-            parent=last_iterator)
+            parent=last_iterator, iterator_bond=True)
 
     with ctx.newrel() as ictx:
         ictx.scope_tree = ctx.scope_tree
@@ -551,18 +558,21 @@ def compile_iterator_cte(
         if iterator_set.path_id.is_objtype_path():
             relgen.ensure_source_rvar(iterator_set, ictx.rel, ctx=ictx)
         ictx.rel.path_id = iterator_set.path_id
-        pathctx.put_path_bond(ictx.rel, iterator_set.path_id)
+        pathctx.put_path_bond(ictx.rel, iterator_set.path_id, iterator=True)
         iterator_cte = pgast.CommonTableExpr(
             query=ictx.rel,
-            name=ctx.env.aliases.get('iter')
+            name=ctx.env.aliases.get('iter'),
         )
         ictx.toplevel_stmt.append_cte(iterator_cte)
 
     ctx.dml_stmts[iterator_set] = iterator_cte
 
     return pgast.IteratorCTE(
-        path_id=iterator_set.path_id, cte=iterator_cte,
-        parent=last_iterator)
+        path_id=iterator_set.path_id,
+        cte=iterator_cte,
+        parent=last_iterator,
+        iterator_bond=True,
+    )
 
 
 def _mk_dynamic_get_path(
@@ -1006,7 +1016,9 @@ def process_insert_shape(
             subctx.path_scope = ctx.path_scope.new_child()
             merge_iterator(inner_iterator, select, ctx=subctx)
             inner_iterator_id = relctx.get_path_var(
-                select, inner_iterator.path_id, aspect='identity', ctx=ctx)
+                select, inner_iterator.path_id,
+                aspect=inner_iterator.aspect,
+                ctx=ctx)
 
         # Process the Insert IR and separate links that go
         # into the main table from links that are inserted into
@@ -1050,8 +1062,7 @@ def process_insert_shape(
             if link_ptr_info and link_ptr_info.table_type == 'link':
                 external_inserts.append(element)
 
-        if iterator is not None:
-            pathctx.put_path_bond(select, iterator.path_id)
+        put_iterator_bond(iterator, select)
 
     for aspect in ('value', 'identity'):
         pathctx._put_path_output_var(
@@ -1491,7 +1502,7 @@ def compile_insert_else_body(
             dummy_pathid = irast.PathId.new_dummy(ctx.env.aliases.get('dummy'))
             with ictx.subrel() as dctx:
                 dummy_q = dctx.rel
-                relctx.ensure_transient_identity_for_path(
+                relctx.create_iterator_identity_for_path(
                     dummy_pathid, dummy_q, ctx=dctx)
             dummy_rvar = relctx.rvar_for_rel(
                 dummy_q, lateral=True, ctx=ictx)
@@ -1507,7 +1518,12 @@ def compile_insert_else_body(
             iter_path_id = (
                 enclosing_cte_iterator.path_id if
                 enclosing_cte_iterator else None)
-            relctx.anti_join(ictx.rel, subrel, iter_path_id, ctx=ctx)
+            aspect = (
+                enclosing_cte_iterator.aspect if enclosing_cte_iterator
+                else 'identity'
+            )
+            relctx.anti_join(ictx.rel, subrel, iter_path_id,
+                             aspect=aspect, ctx=ctx)
 
             # Package it up as a CTE
             anti_cte = pgast.CommonTableExpr(
@@ -1517,7 +1533,9 @@ def compile_insert_else_body(
             ictx.toplevel_stmt.append_cte(anti_cte)
             anti_cte_iterator = pgast.IteratorCTE(
                 path_id=dummy_pathid, cte=anti_cte,
-                parent=ictx.enclosing_cte_iterator)
+                parent=ictx.enclosing_cte_iterator,
+                iterator_bond=True
+            )
 
     return anti_cte_iterator
 
@@ -1602,9 +1620,7 @@ def process_update_body(
     contents_select = update_cte.query
     toplevel = ctx.toplevel_stmt
 
-    if ctx.enclosing_cte_iterator:
-        pathctx.put_path_bond(
-            contents_select, ctx.enclosing_cte_iterator.path_id)
+    put_iterator_bond(ctx.enclosing_cte_iterator, contents_select)
 
     assert dml_parts.range_cte
     iterator = pgast.IteratorCTE(
