62e31848a28ec56c196049a4d1c0cd5b3643c13a
Michael J. Sullivan <sully@msully.net>
2023-12-19 01:00:21 -0800

    Remove handling for 'releases/*' in get_version_from_scm (#6619)

--- edb/buildmeta.py
+++ edb/buildmeta.py
@@ -427,15 +427,6 @@ def get_version_from_scm(root: pathlib.Path) -> str:
     )
 
     proc = subprocess.run(
-        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
-        stdout=subprocess.PIPE,
-        universal_newlines=True,
-        check=True,
-        cwd=root,
-    )
-    branch = proc.stdout.strip()
-
-    proc = subprocess.run(
         ['git', 'tag', '--list', 'v*'],
         stdout=subprocess.PIPE,
         universal_newlines=True,
@@ -480,17 +471,9 @@ def get_version_from_scm(root: pathlib.Path) -> str:
         ver = f'{major}.{minor}{microkind}{micro}{prekind}{preval}'
     else:
         # Dev/nightly build.
-        if branch.startswith("releases/"):
-            if prekind and preval:
-                pass
-            elif micro:
-                micro = str(int(micro) + 1)
-            else:
-                minor = str(int(minor) + 1)
-        else:
-            microkind = ''
-            micro = ''
-            minor = '0'
+        microkind = ''
+        micro = ''
+        minor = '0'
 
         incremented_ver = f'{major}.{minor}{microkind}{micro}'
 
