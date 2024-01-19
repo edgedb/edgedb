c0d8e3f4a04837d24b484f736da482428905f734
Zhibo Chen <zhibo@edgedb.com>
2024-01-19 14:54:54 -0500

    skipping more tests

--- tests/test_edgeql_select.py
+++ tests/test_edgeql_select.py
@@ -2780,6 +2780,7 @@ class TestEdgeQLSelect(tb.QueryTestCase):
                 """
             )
 
+    @test.decorators.experimental_interpreter_exclude("This seems fine, we allow computed properties to override in the interpreter")
     async def test_edgeql_select_setops_15(self):
         with self.assertRaisesRegex(
             edgedb.SchemaError,
