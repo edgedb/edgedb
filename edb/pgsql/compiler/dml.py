6bbce1f31a7b8db19a4e95efbd982df55b7e01ac
Michael J. Sullivan <sully@msully.net>
2024-01-31 08:33:14 -0800

    be less sloppy

--- edb/pgsql/compiler/dml.py
+++ edb/pgsql/compiler/dml.py
@@ -382,7 +382,9 @@ def merge_iterator(
         put_iterator_bond(iterator, select)
         relctx.include_rvar(
             select, iterator_rvar,
-            aspects=('source', 'value', iterator.aspect),
+            aspects=('value', iterator.aspect) + (
+                ('source',) if iterator.path_id.is_objtype_path() else ()
+            ),
             path_id=iterator.path_id,
             overwrite_path_rvar=True,
             ctx=ctx)
