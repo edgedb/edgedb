f04237c39205981e1f0939ceb387f4e05808055c
Fantix King <fantix.king@gmail.com>
2024-03-07 16:22:05 -0500

    Revert "Re-enable persistent cache"
    This reverts commit 9befc56bc1c285e4eba850b48b50bd38e4f01643.

--- edb/common/debug.py
+++ edb/common/debug.py
@@ -176,7 +176,7 @@ class flags(metaclass=FlagsMeta):
 
     zombodb = Flag(doc="Enabled zombodb and disables postgres FTS")
 
-    disable_persistent_cache = Flag(doc="Don't use persistent cache")
+    persistent_cache = Flag(doc="Use persistent cache")
 
     # Function cache is an experimental feature that may not fully work
     func_cache = Flag(doc="Use stored functions for persistent cache")
