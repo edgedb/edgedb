ae8f3be26cdfa63566d4490c75961c00c9187afb
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-03-25 16:56:42 +0100

    viewgen 1

--- edb/edgeql/compiler/viewgen.py
+++ edb/edgeql/compiler/viewgen.py
@@ -251,9 +251,9 @@ def _process_view(
     # Maybe rematerialize the set. The old ir_set might have already
     # been materialized, but the new version would be missing from the
     # use_sets.
-    if ir_set.rptr:
+    if isinstance(ir_set.expr, irast.Pointer):
         ctx.env.schema, remat_ptrcls = typeutils.ptrcls_from_ptrref(
-            ir_set.rptr.ptrref, schema=ctx.env.schema
+            ir_set.expr.ptrref, schema=ctx.env.schema
         )
         setgen.maybe_materialize(remat_ptrcls, ir_set, ctx=ctx)
 
@@ -2311,14 +2311,16 @@ def _get_late_shape_configuration(
     is_objtype = ir_set.path_id.is_objtype_path()
 
     if rptr is None:
-        rptr = ir_set.rptr
-    # If we have a specified rptr but no rptr on the set itself,
-    # construct a version of the set with the rptr added to use
-    # as the path tip for applying pointers. This ensures that
-    # we can find link properties on late shapes.
-    elif ir_set.rptr is None and ir_set.expr:
+        if isinstance(ir_set.expr, irast.Pointer):
+            rptr = ir_set.expr
+    elif ir_set.expr and not isinstance(ir_set.expr, irast.Pointer):
+        # If we have a specified rptr but set is not a pointer itself,
+        # construct a version of the set that is pointer so it can be used
+        # as the path tip for applying pointers. This ensures that
+        # we can find link properties on late shapes.
         ir_set = setgen.new_set_from_set(
-            ir_set, expr=rptr.replace(expr=ir_set.expr, is_phony=True), ctx=ctx)
+            ir_set, expr=rptr.replace(expr=ir_set.expr, is_phony=True), ctx=ctx
+        )
 
     rptrcls: Optional[s_pointers.PointerLike]
     if rptr is not None:
@@ -2370,8 +2372,8 @@ def late_compile_view_shapes(
 @late_compile_view_shapes.register(irast.Set)
 def _late_compile_view_shapes_in_set(
         ir_set: irast.Set, *,
-        rptr: Optional[irast.Pointer]=None,
-        parent_view_type: Optional[s_types.ExprType]=None,
+        rptr: Optional[irast.Pointer] = None,
+        parent_view_type: Optional[s_types.ExprType] = None,
         ctx: context.ContextLevel) -> None:
 
     shape_ptrs = _get_late_shape_configuration(
@@ -2476,8 +2478,8 @@ def _late_compile_view_shapes_in_set(
         else:
             late_compile_view_shapes(expr, ctx=ctx)
 
-    elif isinstance(ir_set.rptr, irast.TupleIndirectionPointer):
-        late_compile_view_shapes(ir_set.rptr.source, ctx=ctx)
+    elif isinstance(ir_set.expr, irast.TupleIndirectionPointer):
+        late_compile_view_shapes(ir_set.expr.source, ctx=ctx)
 
 
 @late_compile_view_shapes.register(irast.SelectStmt)
