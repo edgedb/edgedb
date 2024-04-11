edgedb/edgedb

commit b1461c00f51843ff81b4d123301adfea3ef0dd20

Replace flake8 with ruff (#6900)

edb/tools/toy_eval_model.py
@@ -49,13 +49,23 @@ against the real DB.
 
 from __future__ import annotations
 
-from pathlib import Path
-import sys
-EDB_DIR = Path(__file__).parent.parent.parent.resolve()
-sys.path.insert(0, str(EDB_DIR))
-
-from typing import *
-
+from typing import (
+    Any,
+    Callable,
+    Optional,
+    Protocol,
+    Tuple,
+    TypeVar,
+    Union,
+    Iterable,
+    Iterator,
+    Sequence,
+    Collection,
+    Dict,
+    List,
+    Set,
+    NamedTuple,
+)
 
 from edb.common import debug
 from edb.common.compiler import SimpleCounter
@@ -66,7 +76,6 @@ from edb.common.compiler import AliasGenerator
 from edb.edgeql import ast as qlast
 from edb.edgeql import qltypes as ft
 from edb.edgeql import desugar_group
-
 
 from dataclasses import dataclass, replace, field
 from collections import defaultdict
@@ -82,6 +91,11 @@ import random
 import statistics
 import traceback
 import uuid
+import sys
+from pathlib import Path
+
+EDB_DIR = Path(__file__).parent.parent.parent.resolve()
+sys.path.insert(0, str(EDB_DIR))
 
 
 DESUGARING_GROUP = True

commit e161929b819d84a18a939713d4807aa5edd0270b

Fix toy_eval_model.py so we can run it directly
It got broken by #6900

edb/tools/toy_eval_model.py
@@ -49,6 +49,16 @@ against the real DB.
 
 from __future__ import annotations
 
+# We need to adjust the path so we can run this directly from the
+# source tree.
+# flake8: noqa: E402
+import sys
+from pathlib import Path
+
+EDB_DIR = Path(__file__).parent.parent.parent.resolve()
+sys.path.insert(0, str(EDB_DIR))
+
+
 from typing import (
     Any,
     Callable,
@@ -91,11 +101,6 @@ import random
 import statistics
 import traceback
 import uuid
-import sys
-from pathlib import Path
-
-EDB_DIR = Path(__file__).parent.parent.parent.resolve()
-sys.path.insert(0, str(EDB_DIR))
 
 
 DESUGARING_GROUP = True