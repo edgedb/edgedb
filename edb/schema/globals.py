ba310df56a8c536391730e059fad1e824cebe67a
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-04-12 12:07:05 +0200

    fix

--- edb/schema/globals.py
+++ edb/schema/globals.py
@@ -589,12 +589,15 @@ class SetGlobalType(
         else:
             assert isinstance(set_field, qlast.SetField)
             assert not isinstance(set_field.value, qlast.Expr)
+
+            case_expr = None
+            if self.cast_expr:
+                assert isinstance(self.cast_expr, s_expr.Expression)
+                case_expr = self.cast_expr.qlast
+
             return qlast.SetGlobalType(
                 value=set_field.value,
-                cast_expr=(
-                    cast(s_expr.Expression, self.cast_expr).qlast
-                    if self.cast_expr is not None else None
-                ),
+                cast_expr=case_expr,
                 reset_value=self.reset_value,
             )
 
