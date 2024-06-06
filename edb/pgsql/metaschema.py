3b98dc53d8bb064afff8408d2a699c15fb3e6c21
Michael J. Sullivan <sully@msully.net>
2024-06-06 15:35:03 -0700

    DEBUG WRITE

--- edb/pgsql/metaschema.py
+++ edb/pgsql/metaschema.py
@@ -7417,6 +7417,12 @@ async def execute_sql_script(
 ) -> None:
     from edb.server import pgcon
 
+    try:
+        with open('bootstrap_script.sql', 'a') as f:
+            f.write(sql_text)
+    except Exception:
+        pass
+
     if debug.flags.bootstrap:
         debug.header('Bootstrap Script')
         if len(sql_text) > 102400:
