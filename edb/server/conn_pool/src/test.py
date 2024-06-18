3aeaa9d3345c9a362ebb4bb31d7da4f251666ce8
Matt Mastracci <matthew@mastracci.com>
2024-06-18 08:42:50 -0600

    docs

--- edb/server/conn_pool/src/test.py
+++ edb/server/conn_pool/src/test.py
@@ -1,79 +1,104 @@
 import edb.server._conn_pool
 import asyncio
 import threading
-import time
 import typing
-
-class ConnectionFactory(typing.Protocol):
-    async def connect(self, db: str):
-        ...
-    async def disconnect(self, conn):
-        ...
-    async def reconnect(self, conn, db: str):
-        ...
 
 C = typing.TypeVar('C')
 
-class ConnPool(typing.Generic[C]):
-    connection_factory: ConnectionFactory
-    pool: edb.server._conn_pool.ConnPool
-    loop: asyncio.BaseEventLoop
-    completion: asyncio.Future
-    _ready: asyncio.Future
+class ConnectionFactory(typing.Protocol[C]):
+    """The async interface to create and destroy database connections."""
+    async def connect(self, db: str) -> C:
+        """Create a new connection asynchronously.
 
-    def __init__(self, connection_factory: C):
-        self.connection_factory = connection_factory
-        self.loop = asyncio.get_event_loop()
-        self.pool = None
-        self.completion = None
-        self._ready = self.loop.create_future()
+        This method must retry exceptions internally. If an exception is thrown
+        from this method, the database is considered to be failed."""
+        ...
+    async def disconnect(self, conn: C):
+        """Gracefully disconnect a connection asynchronously.
 
-    def _callback(self, kind: int, response_id: int, *args):
+        If an exception is thrown from this method, the connection is simply
+        forgotten."""
+        ...
+    async def reconnect(self, conn: C, db: str) -> C:
+        """Reconnects a connection to the given database. If this is not possible,
+        it is permissable to return a new connection and gracefully disconnect
+        the other connection.
+
+        This method must retry exceptions internally. If an exception is thrown
+        from this method, the database is considered to be failed."""
+        ...
+
+CF = typing.TypeVar('CF', bound=ConnectionFactory[typing.Any])
+
+class ConnPool(typing.Generic[CF, C]):
+    _connection_factory: ConnectionFactory
+    _pool: edb.server._conn_pool.ConnPool
+    _loop: asyncio.AbstractEventLoop
+    _completion: asyncio.Future[bool]
+    _ready: asyncio.Future[bool]
+
+    def __init__(self, connection_factory: CF):
+        self._connection_factory = connection_factory
+        self._loop = asyncio.get_event_loop()
+        self._pool = None
+        self._completion = self._loop.create_future()
+        self._ready = self._loop.create_future()
+
+    def _callback(self, kind: int, response_id: int, *args) -> bool:
         """Receives the callback from the Rust connection pool.
 
         Required to call pool._respond with the result of this callback
         """
-        if self.loop.is_closed():
+        if self._loop.is_closed():
             return False
         else:
-            self.loop.call_soon_threadsafe(self.loop.create_task, self._perform_async(kind, response_id, *args))
+            self._loop.call_soon_threadsafe(self._loop.create_task, self._perform_async(kind, response_id, *args))
             return True
 
     async def _perform_async(self, kind: int, response_id: int, *args):
+        """Delegates the callback from Rust to the appropriate connection factory method.
+        """
         if kind == 0:
-            response = await self.connection_factory.connect(*args)
+            response = await self._connection_factory.connect(*args)
         elif kind == 1:
-            response = await self.connection_factory.disconnect(*args)
+            response = await self._connection_factory.disconnect(*args)
         elif kind == 2:
-            response = await self.connection_factory.reconnect(*args)
-        self.pool._respond(response_id, response)
+            response = await self._connection_factory.reconnect(*args)
+        self._pool._respond(response_id, response)
 
     def _thread_main(self):
-        self.loop.call_soon_threadsafe(self._ready.set_result, True)
-        self.pool.run_and_block()
-        if not self.loop.is_closed():
-            self.loop.call_soon_threadsafe(self.completion.set_result, True)
+        self._loop.call_soon_threadsafe(self._ready.set_result, True)
+        self._pool.run_and_block()
+        if not self._loop.is_closed():
+            self._loop.call_soon_threadsafe(self._completion.set_result, True)
 
     async def run(self):
-        if self.pool != None:
+        """Creates a long-lived task that manages the connection pool. Required
+        before any connections may be acquired."""
+        if self._pool != None:
             raise RuntimeError(
                 f'pool already started'
             ) from None
 
-        self.pool = edb.server._conn_pool.ConnPool(self._callback)
-        self.completion = self.loop.create_future()
+        self._pool = edb.server._conn_pool.ConnPool(self._callback)
         threading.Thread(target = self._thread_main).start()
         try:
-            await self.completion
+            await self._completion
         except asyncio.exceptions.CancelledError:
-            self.pool.halt()
-        self.pool = None
+            self._pool.halt()
+        self._pool = None
 
-    async def acquire(self, db):
+    async def acquire(self, db) -> C:
+        """Acquire a connection from the database. This connection must be released."""
         await self._ready
-        future = self.loop.create_future()
-        self.pool.acquire(db, lambda res: self.loop.call_soon_threadsafe(future.set_result, res))
+        future = self._loop.create_future()
+        self._pool.acquire(db, lambda res: self._loop.call_soon_threadsafe(future.set_result, res))
         return (await future)
+
+    async def release(self, db, conn, discard=False):
+        """Releases a connection back into the pool."""
+        pass
+
 
 async def main():
     class Factory:
