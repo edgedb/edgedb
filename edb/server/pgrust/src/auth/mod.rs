151f43fcf7ac23422b39d8dc625bdaf2142a3d8d
Matt Mastracci <matthew@mastracci.com>
2024-08-19 10:39:01 -0400

    Back out larger bits

--- edb/server/pgrust/src/auth/mod.rs
+++
@@ -1,4 +0,0 @@
-mod scram;
-pub use scram::{
-    generate_salted_password, ClientEnvironment, ClientTransaction, SCRAMError, Sha256Out,
-};
--- edb/server/pgrust/src/lib.rs
+++ edb/server/pgrust/src/lib.rs
@@ -1,10 +1,5 @@
-mod auth;
-mod conn;
 mod conn_string;
-mod connection;
-pub mod protocol;
 
-pub use conn::{Client, ConnectionParameters};
 pub use conn_string::{parse_postgres_url, Host, ParseError};
 
 #[cfg(feature = "python_extension")]
