38990a84e1f3331619ce17caf403056bbe0c963d
Matt Mastracci <matthew@mastracci.com>
2024-08-26 12:36:04 -0600

    rebase

--- edb/server/pgrust/src/python.rs
+++ edb/server/pgrust/src/python.rs
@@ -4,12 +4,12 @@ use crate::conn_string::{self, EnvVar};
 use pyo3::{
     exceptions::PyException,
     pyfunction, pymodule,
-    types::{PyByteArray, PyModule},
-    wrap_pyfunction, PyAny, PyResult, Python,
+    types::{PyAnyMethods, PyByteArray, PyModule, PyModuleMethods},
+    wrap_pyfunction, Bound, PyAny, PyResult, Python,
 };
 use serde_pickle::SerOptions;
 
-impl EnvVar for (String, &PyAny) {
+impl EnvVar for (String, Bound<'_, PyAny>) {
     fn read(&self, name: &'static str) -> Option<std::borrow::Cow<str>> {
         // os.environ[name], or the default user if not
         let py_str = self.1.get_item(name).ok();
@@ -22,10 +22,10 @@ impl EnvVar for (String, &PyAny) {
 }
 
 #[pyfunction]
-fn parse_dsn(py: Python, username: String, home_dir: String, s: String) -> PyResult<&PyAny> {
-    let pickle = py.import("pickle")?;
+fn parse_dsn(py: Python, username: String, home_dir: String, s: String) -> PyResult<Bound<PyAny>> {
+    let pickle = py.import_bound("pickle")?;
     let loads = pickle.getattr("loads")?;
-    let os = py.import("os")?;
+    let os = py.import_bound("os")?;
     let environ = os.getattr("environ")?;
     match conn_string::parse_postgres_url(&s, (username, environ)) {
         Ok(mut res) => {
@@ -33,20 +33,20 @@ fn parse_dsn(py: Python, username: String, home_dir: String, s: String) -> PyRes
                 res.password
                     .resolve(Path::new(&home_dir), &res.hosts, &res.database, &res.user)?
             {
-                let warnings = py.import("warnings")?;
+                let warnings = py.import_bound("warnings")?;
                 warnings.call_method1("warn", (warning.to_string(),))?;
             }
             let paths = res.ssl.resolve(Path::new(&home_dir))?;
             // Use serde_pickle to get a python-compatible representation of the result
             let vec = serde_pickle::to_vec(&(res, paths), SerOptions::new()).unwrap();
-            loads.call1((PyByteArray::new(py, &vec),))
+            loads.call1((PyByteArray::new_bound(py, &vec),))
         }
         Err(err) => Err(PyException::new_err(err.to_string())),
     }
 }
 
 #[pymodule]
-fn _pg_rust(_py: Python, m: &PyModule) -> PyResult<()> {
+fn _pg_rust(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parse_dsn, m)?)?;
     Ok(())
 }
