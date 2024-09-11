982cfb3a59681e038691a96f52187df4f2077942
Michael J. Sullivan <sully@msully.net>
2024-09-11 12:12:56 -0700

    Support querying pg_config to figure out where to put things
    This probably doesn't really matter but.

--- edb/load_ext/main.py
+++ edb/load_ext/main.py
@@ -23,6 +23,7 @@ from __future__ import annotations
 
 import pathlib
 import shutil
+import subprocess
 import sys
 import tomllib
 import zipfile
@@ -32,11 +33,18 @@ import click
 from edb import buildmeta
 from edb.common import typeutils
 
+# Directories that we map to config values in pg_config.
+CONFIG_PATHS = {
+    'share': 'sharedir',
+    'lib': 'libdir',
+    'include': 'includedir',
+}
+
 
 def install(
     pkg: pathlib.Path,
     ext_dir: pathlib.Path,
-    pg_dir: pathlib.Path,
+    pg_config: dict[str, str],
 ) -> None:
     print("Installing", ext_dir / pkg.name)
     shutil.copyfile(pkg, ext_dir / pkg.name)
@@ -47,29 +55,58 @@ def install(
 
         if 'postgres_files' in manifest:
             dir = manifest['postgres_files']
+            pdir = pathlib.Path(dir)
 
             for entry in z.infolist():
-                # Dirs should already exist
                 if entry.is_dir():
                     continue
                 fpath = pathlib.Path(entry.filename)
+
                 if fpath.parts[0] != dir:
                     continue
-                fpath = fpath.relative_to(dir)
+                # If the path is too short or isn't one of the
+                # directories we know about, skip it.
+                if (
+                    len(fpath.parts) < 2
+                    or not (config_field := CONFIG_PATHS.get(fpath.parts[1]))
+                ):
+                    print("Skipping", fpath)
+                    continue
 
-                print("Installing", pg_dir / fpath)
+                config_dir = pg_config[config_field]
+                fpath = fpath.relative_to(pdir / fpath.parts[1])
+
+                target_file = config_dir / fpath
+                print("Installing", target_file)
                 with z.open(entry) as f:
                     data = f.read()
-                with open(pg_dir / fpath, "wb") as f:
+                with open(target_file, "wb") as f:
                     f.write(data)
 
 
-def load_ext_main(package: pathlib.Path):
-    pg_config = buildmeta.get_pg_config_path()
-    ext_dir = buildmeta.get_extension_dir_path()
-    pg_dir = pg_config.parent.parent
+def get_pg_config() -> dict[str, str]:
+    output = subprocess.run(
+        buildmeta.get_pg_config_path(),
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    stdout_lines = output.stdout.split('\n')
 
-    install(package, ext_dir, pg_dir)
+    config = {}
+    for line in stdout_lines:
+        k, eq, v = line.partition('=')
+        if eq:
+            config[k.strip().lower()] = v.strip()
+
+    return config
+
+
+def load_ext_main(package: pathlib.Path):
+    ext_dir = buildmeta.get_extension_dir_path()
+    pg_config = get_pg_config()
+
+    install(package, ext_dir, pg_config)
 
 
 # Option is pulled out like this so that an edb tool can reuse it.
