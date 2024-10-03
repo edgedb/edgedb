f7b15799ab9dab2601f4e2a7e838d385513d890c
Matt Mastracci <matthew@mastracci.com>
2024-10-03 16:04:40 -0600

    Server-side SCRAM should work with stored keys, not hashed keys

--- edb/server/pgrust/src/auth/mod.rs
+++ edb/server/pgrust/src/auth/mod.rs
@@ -5,7 +5,6 @@ mod stringprep_table;
 
 pub use md5::md5_password;
 pub use scram::{
-    generate_salted_password, generate_stored_key, ClientEnvironment, ClientTransaction,
-    SCRAMError, Sha256Out,
+    generate_salted_password, ClientEnvironment, ClientTransaction, SCRAMError, Sha256Out,
 };
 pub use stringprep::{sasl_normalize_password, sasl_normalize_password_bytes};
