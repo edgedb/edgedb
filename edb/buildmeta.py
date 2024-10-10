3b53c5c472c6f01b9df0e809fd5a84bbf301251d
Michael J. Sullivan <sully@msully.net>
2024-10-10 12:49:26 -0700

    Put catalog version in the schema versions (#7849)
    This should enable inplace upgrades between nightlies and alphas.

--- edb/buildmeta.py
+++ edb/buildmeta.py
@@ -60,7 +60,7 @@ from edb.common import verutils
 # The merge conflict there is a nice reminder that you probably need
 # to write a patch in edb/pgsql/patches.py, and then you should preserve
 # the old value.
-EDGEDB_CATALOG_VERSION = 2024_10_02_00_00
+EDGEDB_CATALOG_VERSION = 2024_10_10_00_00
 EDGEDB_MAJOR_VERSION = 6
 
 
