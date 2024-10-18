177a0a4fc0b064cbfe2e65e13af08b17bd77692d
Michael J. Sullivan <sully@msully.net>
2024-10-18 10:49:47 -0700

    More NO_FACTOR and WARN_FACTOR on tests (#7884)

--- tests/test_edgeql_policies.py
+++ tests/test_edgeql_policies.py
@@ -28,6 +28,8 @@ from edb.testbase import server as tb
 class TestEdgeQLPolicies(tb.QueryTestCase):
     '''Tests for policies.'''
 
+    NO_FACTOR = True
+
     SCHEMA = os.path.join(os.path.dirname(__file__), 'schemas',
                           'issues.esdl')
 
