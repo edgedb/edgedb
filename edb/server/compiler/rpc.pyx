78cfcb14dac39aa4fd76c21aa5d3bd026b64053c
Elvis Pranskevichus <elvis@edgedb.com>
2024-10-21 14:08:23 -0700

    Move serialization out as well

--- edb/server/compiler/rpc.pyx
+++ edb/server/compiler/rpc.pyx
@@ -178,9 +178,11 @@ cdef class CompilationRequest:
         query_text: str,
         compilation_config_serializer: sertypes.CompilationConfigSerializer,
     ) -> CompilationRequest:
+        buf = ReadBuffer.new_message_parser(data)
+
         if data[0] == 0:
-            return _deserialize_v0_comp_req(
-                data, query_text, compilation_config_serializer)
+            return _deserialize_comp_req_v0(
+                buf, query_text, compilation_config_serializer)
         else:
             raise errors.UnsupportedProtocolVersionError(
                 f"unsupported compile cache: version {data[0]}"
@@ -197,78 +199,9 @@ cdef class CompilationRequest:
         return self.cache_key
 
     cdef _serialize(self):
-        # Please see _deserialize_v0 for the format doc
-
-        cdef:
-            char version = 0, flags
-            WriteBuffer out = WriteBuffer.new()
-
-        out.write_byte(version)
-
-        flags = (
-            (MASK_JSON_PARAMETERS if self.input_format is IN_FMT_JSON else 0) |
-            (MASK_EXPECT_ONE if self.expect_one else 0) |
-            (MASK_INLINE_TYPEIDS if self.inline_typeids else 0) |
-            (MASK_INLINE_TYPENAMES if self.inline_typenames else 0) |
-            (MASK_INLINE_OBJECTIDS if self.inline_objectids else 0)
-        )
-        out.write_byte(flags)
-
-        out.write_int16(self.protocol_version[0])
-        out.write_int16(self.protocol_version[1])
-        out.write_byte(serialize_output_format(self.output_format))
-        out.write_int64(self.implicit_limit)
-
-        if self.modaliases is None:
-            out.write_int32(-1)
-        else:
-            out.write_int32(len(self.modaliases))
-            for k, v in sorted(
-                self.modaliases.items(),
-                key=lambda i: (0, i[0]) if i[0] is None else (1, i[0])
-            ):
-                if k is None:
-                    out.write_byte(0)
-                else:
-                    out.write_byte(1)
-                    out.write_str(k, "utf-8")
-                out.write_str(v, "utf-8")
-
-        type_id, desc = self._serializer.describe()
-        out.write_bytes(type_id.bytes)
-        out.write_len_prefixed_bytes(desc)
-
-        hash_obj = hashlib.blake2b(memoryview(out), digest_size=16)
-        hash_obj.update(self.source.cache_key())
-
-        if self.session_config is None:
-            session_config = b""
-        else:
-            session_config = self._serializer.encode_configs(
-                self.session_config
-            )
-        out.write_len_prefixed_bytes(session_config)
-
-        # Build config that affects compilation: session -> database -> system.
-        # This is only used for calculating cache_key, while session
-        # config itself is separately stored above in the serialized format.
-        serialized_comp_config = self._serializer.encode_configs(
-            self.system_config, self.database_config, self.session_config
-        )
-        hash_obj.update(serialized_comp_config)
-
-        # Must set_schema_version() before serializing compilation request
-        assert self.schema_version is not None
-        hash_obj.update(self.schema_version.bytes)
-
-        cache_key_bytes = hash_obj.digest()
-        self.cache_key = uuidgen.from_bytes(cache_key_bytes)
-
-        out.write_len_prefixed_bytes(self.source.serialize())
-        out.write_bytes(cache_key_bytes)
-        out.write_bytes(self.schema_version.bytes)
-
-        self.serialized_cache = bytes(out)
+        cache_key, buf = _serialize_comp_req_v0(self)
+        self.cache_key = cache_key
+        self.serialized_cache = bytes(buf)
 
     def __hash__(self):
         return hash(self.get_cache_key())
@@ -287,8 +220,8 @@ cdef class CompilationRequest:
         )
 
 
-cdef _deserialize_v0_comp_req(
-    data: bytes,
+cdef _deserialize_comp_req_v0(
+    buf: ReadBuffer,
     query_text: str,
     compilation_config_serializer: sertypes.CompilationConfigSerializer,
 ):
@@ -329,8 +262,6 @@ cdef _deserialize_v0_comp_req(
     #    we stick it on the end where it will be ignored by old versions.
 
     cdef char flags
-
-    buf = ReadBuffer.new_message_parser(data)
 
     assert buf.read_byte() == 0  # version
 
@@ -414,3 +345,78 @@ cdef _deserialize_v0_comp_req(
     req.cache_key = cache_key
 
     return req
+
+
+cdef _serialize_comp_req_v0(req: CompilationRequest):
+    # Please see _deserialize_v0 for the format doc
+
+    cdef:
+        char version = 0, flags
+        WriteBuffer out = WriteBuffer.new()
+
+    out.write_byte(version)
+
+    flags = (
+        (MASK_JSON_PARAMETERS if req.input_format is IN_FMT_JSON else 0) |
+        (MASK_EXPECT_ONE if req.expect_one else 0) |
+        (MASK_INLINE_TYPEIDS if req.inline_typeids else 0) |
+        (MASK_INLINE_TYPENAMES if req.inline_typenames else 0) |
+        (MASK_INLINE_OBJECTIDS if req.inline_objectids else 0)
+    )
+    out.write_byte(flags)
+
+    out.write_int16(req.protocol_version[0])
+    out.write_int16(req.protocol_version[1])
+    out.write_byte(serialize_output_format(req.output_format))
+    out.write_int64(req.implicit_limit)
+
+    if req.modaliases is None:
+        out.write_int32(-1)
+    else:
+        out.write_int32(len(req.modaliases))
+        for k, v in sorted(
+            req.modaliases.items(),
+            key=lambda i: (0, i[0]) if i[0] is None else (1, i[0])
+        ):
+            if k is None:
+                out.write_byte(0)
+            else:
+                out.write_byte(1)
+                out.write_str(k, "utf-8")
+            out.write_str(v, "utf-8")
+
+    type_id, desc = req._serializer.describe()
+    out.write_bytes(type_id.bytes)
+    out.write_len_prefixed_bytes(desc)
+
+    hash_obj = hashlib.blake2b(memoryview(out), digest_size=16)
+    hash_obj.update(req.source.cache_key())
+
+    if req.session_config is None:
+        session_config = b""
+    else:
+        session_config = req._serializer.encode_configs(
+            req.session_config
+        )
+    out.write_len_prefixed_bytes(session_config)
+
+    # Build config that affects compilation: session -> database -> system.
+    # This is only used for calculating cache_key, while session
+    # config itreq is separately stored above in the serialized format.
+    serialized_comp_config = req._serializer.encode_configs(
+        req.system_config, req.database_config, req.session_config
+    )
+    hash_obj.update(serialized_comp_config)
+
+    # Must set_schema_version() before serializing compilation request
+    assert req.schema_version is not None
+    hash_obj.update(req.schema_version.bytes)
+
+    cache_key_bytes = hash_obj.digest()
+    cache_key = uuidgen.from_bytes(cache_key_bytes)
+
+    out.write_len_prefixed_bytes(req.source.serialize())
+    out.write_bytes(cache_key_bytes)
+    out.write_bytes(req.schema_version.bytes)
+
+    return cache_key, out
