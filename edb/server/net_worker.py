0ff0236533bf5eb98e58aa33bdc4ea8cb72c35e8
Michael J. Sullivan <sully@msully.net>
2024-10-23 10:30:22 -0700

    Rename net_http_max_connections to http_max_connections
    We're going to use it to cap other outgoing http connections also.

--- edb/server/net_worker.py
+++ edb/server/net_worker.py
@@ -47,10 +47,10 @@ NET_HTTP_REQUEST_TTL = statypes.Duration(
 
 
 async def _http_task(tenant: edbtenant.Tenant, http_client) -> None:
-    net_http_max_connections = tenant._server.config_lookup(
-        'net_http_max_connections', tenant.get_sys_config()
+    http_max_connections = tenant._server.config_lookup(
+        'http_max_connections', tenant.get_sys_config()
     )
-    http_client._update_limit(net_http_max_connections)
+    http_client._update_limit(http_max_connections)
     try:
         async with (asyncio.TaskGroup() as g,):
             for db in tenant.iter_dbs():
@@ -171,10 +171,10 @@ class HttpClient:
 
 
 def create_http(tenant: edbtenant.Tenant):
-    net_http_max_connections = tenant._server.config_lookup(
-        'net_http_max_connections', tenant.get_sys_config()
+    http_max_connections = tenant._server.config_lookup(
+        'http_max_connections', tenant.get_sys_config()
     )
-    return HttpClient(net_http_max_connections)
+    return HttpClient(http_max_connections)
 
 
 async def http(server: edbserver.BaseServer) -> None:
