386dbca9df7e6ea611e8f4349d6e6b84941e6cce
dnwpark <dnwpark@protonmail.com>
2024-11-19 15:15:56 -0500

    Fix aliases always being considered Immutable.

--- edb/edgeql/compiler/inference/volatility.py
+++ edb/edgeql/compiler/inference/volatility.py
@@ -209,7 +209,7 @@ def __infer_set(
             vol,
         ])
 
-    if ir.is_binding:
+    if ir.is_binding and ir.is_binding != irast.BindingKind.Schema:
         vol = IMMUTABLE
 
     return vol
