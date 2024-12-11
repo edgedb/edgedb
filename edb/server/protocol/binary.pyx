3ef196832cb3d29aaebdf6fcae06a0bcc362f739
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-12-11 11:27:48 +0100

    Disable normalization for SQL over EdgeDB proto
    Currently, there are queries that work without normalization
    but fail with normalization, so I'll disable it for now.

--- edb/server/protocol/binary.pyx
+++ edb/server/protocol/binary.pyx
@@ -474,10 +474,8 @@ cdef class EdgeConnection(frontend.FrontendConnection):
             else:
                 return edgeql.NormalizedSource.from_string(text)
         elif lang is LANG_SQL:
-            if debug.flags.edgeql_disable_normalization:
-                return pgparser.Source.from_string(text)
-            else:
-                return pgparser.NormalizedSource.from_string(text)
+            # TODO: enable normalization
+            return pgparser.Source.from_string(text)
         else:
             raise errors.UnsupportedFeatureError(
                 f"unsupported input language: {lang}")
