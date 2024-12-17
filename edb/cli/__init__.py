d89da8c8aef7cc70c155c471d44504a59756d377
Michael J. Sullivan <sully@msully.net>
2024-12-16 17:46:07 -0800

    Invoke the "gel" command instead of "edgedb" (#8130)
    This silences an annoying warning from the CLI

--- edb/cli/__init__.py
+++ edb/cli/__init__.py
@@ -31,4 +31,4 @@ def rustcli(*, args: Optional[List[str]]=None) -> NoReturn:
     if args is None:
         args = [*sys.argv]
 
-    os.execve(str(thisdir / 'edgedb'), args, os.environ)
+    os.execve(str(thisdir / 'gel'), args, os.environ)
