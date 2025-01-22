84ab4f62b8077dc6587304fa2597989dd6700baa
James Clarke <james@edgedb.com>
2025-01-22 20:06:20 +0000

    Make `release/6.x` branch build ui from edgedb-ui `6.x` branch (#8239)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ EDGEDBCLI_COMMIT = 'master'
 
 EDGEDBGUI_REPO = 'https://github.com/edgedb/edgedb-studio.git'
 # This can be a branch, tag, or commit
-EDGEDBGUI_COMMIT = 'main'
+EDGEDBGUI_COMMIT = '6.x'
 
 PGVECTOR_REPO = 'https://github.com/pgvector/pgvector.git'
 # This can be a branch, tag, or commit
