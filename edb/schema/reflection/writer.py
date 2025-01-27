00fe1c1957d73172f49e9868a663345b20f7d3c8
Michael J. Sullivan <sully@msully.net>
2025-01-27 10:46:50 -0800

    Remove some lingering debug code (#8253)
    *Most* of these breakpoint() calls were commented out...

--- edb/schema/reflection/writer.py
+++ edb/schema/reflection/writer.py
@@ -1108,8 +1108,6 @@ def write_meta_delete_object(
 
             parent_variables = {}
 
-            if not hasattr(target, 'id'):
-                breakpoint()
             parent_variables[f'__{target_link}'] = (
                 json.dumps(str(target.id))
             )
