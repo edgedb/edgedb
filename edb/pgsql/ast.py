edgedb/edgedb

commit df585393d518443ea4f4a16072ed38071f7eb264

[sql] Fix using `distinct`+`order by` and similar in native protocol (#8256)
It doesn't work in general to stick everything in the target list into
a call to ROW, because certain things in SQL only work when an
expression appears in the target list.

Preserve the shape of the original query and turn it into ROWs in an
outer query.

edb/pgsql/resolver/__init__.py
@@ -130,15 +130,29 @@ def resolve(
     if options.include_edgeql_io_format_alternative:
         edgeql_output_format_ast = copy.copy(resolved)
         if e := as_plain_select(edgeql_output_format_ast, resolved_table, ctx):
-            e.target_list = [
-                pgast.ResTarget(
-                    val=expr.construct_row_expr(
-                        (rt.val for rt in e.target_list),
-                        ctx=ctx,
+            # Turn the query into one that returns a ROW.
+            #
+            # We need to do this by injecting a new query and putting
+            # the old one in its FROM clause, since things like
+            # DISTINCT/ORDER BY care about what exact columns are in
+            # the target list.
+            columns = []
+            for i, target in enumerate(e.target_list):
+                if not target.name:
+                    e.target_list[i] = target = target.replace(name=f'__i~{i}')
+                    assert target.name
+                columns.append(pgast.ColumnRef(name=(target.name,)))
+
+            edgeql_output_format_ast = pgast.SelectStmt(
+                target_list=[
+                    pgast.ResTarget(
+                        val=expr.construct_row_expr(columns, ctx=ctx)
                     )
-                )
-            ]
-            edgeql_output_format_ast = e
+                ],
+                from_clause=[pgast.RangeSubselect(subquery=e)],
+                ctes=e.ctes,
+            )
+            e.ctes = []
     else:
         edgeql_output_format_ast = None
 

tests/test_sql_query.py
@@ -2934,6 +2934,19 @@ class TestSQLQuery(tb.SQLQueryTestCase):
             implicit_limit=3,
         )
 
+    async def test_sql_native_query_26(self):
+        await self.assert_sql_query_result(
+            """
+                select distinct title, pages from "Book"
+                order by title, pages;
+            """,
+            [
+                {'title': 'Chronicles of Narnia', 'pages': 206},
+                {'title': 'Hunger Games', 'pages': 374},
+            ],
+            apply_access_policies=False,
+        )
+
 
 class TestSQLQueryNonTransactional(tb.SQLQueryTestCase):
 

commit 55eec32892f25ba7f79ef7f6f18ecb9a726a0e10

[sql] Fix native SQL protocol on old postgres versions
I broke it in #8256 by adding a RangeSubselect without an alias.

edb/pgsql/ast.py
@@ -972,6 +972,11 @@ class WindowDef(ImmutableBase):
 class RangeSubselect(PathRangeVar):
     """Subquery appearing in FROM clauses."""
 
+    # Before postgres 16, an alias is always required on selects from
+    # a subquery. Try to catch that with the typechecker by getting
+    # rid of the default value.
+    alias: Alias
+
     lateral: bool = False
     subquery: Query
 

edb/pgsql/resolver/__init__.py
@@ -149,7 +149,10 @@ def resolve(
                         val=expr.construct_row_expr(columns, ctx=ctx)
                     )
                 ],
-                from_clause=[pgast.RangeSubselect(subquery=e)],
+                from_clause=[pgast.RangeSubselect(
+                    subquery=e,
+                    alias=pgast.Alias(aliasname='r'),
+                )],
                 ctes=e.ctes,
             )
             e.ctes = []