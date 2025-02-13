edgedb/edgedb

commit 300b877d661fffe9fbb48cb7838dfb4b9b040d7a

Expose `default_transaction_...` Postgres settings (#8276)
* `default_transaction_*` settings apply to implicit transactions
* `start transaction` will take transaction modifiers from the 
  `default_transaction_*` settings, unless explicitly specified
* When the effective `default_transaction_isolation` is lower than
  `Serializable`, the effective `default_transaction_access_mode`
  will always be `ReadOnly`
* Read-only implicit transactions are currently emulated in
  the Gel server to avoid internal complications
* `default_transaction_isolation` currently doesn't work on system/
  database level

edb/server/dbview/dbview.pyx
@@ -1182,17 +1182,22 @@ cdef class DatabaseConnectionView:
         self._reset_tx_state()
         return side_effects
 
+    cdef config_lookup(self, name):
+        return self.server.config_lookup(
+            name,
+            self.get_session_config(),
+            self.get_database_config(),
+            self.get_system_config(),
+        )
+
     async def recompile_cached_queries(self, user_schema, schema_version):
         compiler_pool = self.server.get_compiler_pool()
         compile_concurrency = max(1, compiler_pool.get_size_hint() // 2)
         concurrency_control = asyncio.Semaphore(compile_concurrency)
         rv = []
 
-        recompile_timeout = self.server.config_lookup(
+        recompile_timeout = self.config_lookup(
             "auto_rebuild_query_cache_timeout",
-            self.get_session_config(),
-            self.get_database_config(),
-            self.get_system_config(),
         )
 
         loop = asyncio.get_running_loop()
@@ -1418,9 +1423,6 @@ cdef class DatabaseConnectionView:
                     user_schema_version = unit.user_schema_version
             if user_schema and not self.server.config_lookup(
                 "auto_rebuild_query_cache",
-                self.get_session_config(),
-                self.get_database_config(),
-                self.get_system_config(),
             ):
                 user_schema = None
             if user_schema:
@@ -1684,6 +1686,23 @@ cdef class DatabaseConnectionView:
                     ~enums.Capability.WRITE,
                     errors.DisabledCapabilityError,
                     msg,
+                )
+
+        if not self.in_tx() and query_capabilities & enums.Capability.WRITE:
+            isolation = self.config_lookup("default_transaction_isolation")
+            if isolation and isolation.to_str() != "Serializable":
+                raise query_capabilities.make_error(
+                    ~enums.Capability.WRITE,
+                    errors.TransactionError,
+                    f"default_transaction_isolation is set to "
+                    f"{isolation.to_str()}",
+                )
+            access_mode = self.config_lookup("default_transaction_access_mode")
+            if access_mode and access_mode.to_str() == "ReadOnly":
+                raise query_capabilities.make_error(
+                    ~enums.Capability.WRITE,
+                    errors.TransactionError,
+                    "default_transaction_access_mode is set to ReadOnly",
                 )
 
     async def reload_state_serializer(self):

edb/server/pgcon/__init__.py
@@ -33,6 +33,7 @@ from .connect import (
     pg_connect,
     SETUP_TEMP_TABLE_SCRIPT,
     SETUP_CONFIG_CACHE_SCRIPT,
+    SETUP_DML_DUMMY_TABLE_SCRIPT,
     RESET_STATIC_CFG_SCRIPT,
 )
 
@@ -45,5 +46,6 @@ __all__ = (
     'BackendCatalogNameError',
     'SETUP_TEMP_TABLE_SCRIPT',
     'SETUP_CONFIG_CACHE_SCRIPT',
+    'SETUP_DML_DUMMY_TABLE_SCRIPT',
     'RESET_STATIC_CFG_SCRIPT'
 )

edb/buildmeta.py
@@ -60,7 +60,7 @@ from edb.common import verutils
 # The merge conflict there is a nice reminder that you probably need
 # to write a patch in edb/pgsql/patches.py, and then you should preserve
 # the old value.
-EDGEDB_CATALOG_VERSION = 2024_02_03_00_00
+EDGEDB_CATALOG_VERSION = 2025_02_04_00_00
 EDGEDB_MAJOR_VERSION = 7
 
 

edb/ir/statypes.py
@@ -27,6 +27,7 @@ from typing import (
     Optional,
     Self,
     TypeVar,
+    TYPE_CHECKING,
 )
 
 import dataclasses
@@ -46,6 +47,9 @@ from edb.common import uuidgen
 
 from edb.schema import name as s_name
 from edb.schema import objects as s_obj
+
+if TYPE_CHECKING:
+    from edb.edgeql import qltypes
 
 MISSING: Any = object()
 
@@ -736,3 +740,74 @@ class EnabledDisabledType(
             EnabledDisabledEnum.Enabled: "true",
             EnabledDisabledEnum.Disabled: "false",
         }
+
+
+class TransactionAccessModeEnum(enum.StrEnum):
+    ReadOnly = "ReadOnly"
+    ReadWrite = "ReadWrite"
+
+
+class TransactionAccessMode(
+    EnumScalarType[TransactionAccessModeEnum],
+    edgeql_type="sys::TransactionAccessMode",
+):
+    @classmethod
+    def get_translation_map(cls) -> Mapping[TransactionAccessModeEnum, str]:
+        return {
+            TransactionAccessModeEnum.ReadOnly: "true",
+            TransactionAccessModeEnum.ReadWrite: "false",
+        }
+
+    def to_qltypes(self) -> qltypes.TransactionAccessMode:
+        from edb.edgeql import qltypes
+        match self._val:
+            case TransactionAccessModeEnum.ReadOnly:
+                return qltypes.TransactionAccessMode.READ_ONLY
+            case TransactionAccessModeEnum.ReadWrite:
+                return qltypes.TransactionAccessMode.READ_WRITE
+            case _:
+                raise AssertionError(f"unexpected value: {self._val!r}")
+
+
+class TransactionDeferrabilityEnum(enum.StrEnum):
+    Deferrable = "Deferrable"
+    NotDeferrable = "NotDeferrable"
+
+
+class TransactionDeferrability(
+    EnumScalarType[TransactionDeferrabilityEnum],
+    edgeql_type="sys::TransactionDeferrability",
+):
+    @classmethod
+    def get_translation_map(cls) -> Mapping[TransactionDeferrabilityEnum, str]:
+        return {
+            TransactionDeferrabilityEnum.Deferrable: "true",
+            TransactionDeferrabilityEnum.NotDeferrable: "false",
+        }
+
+
+class TransactionIsolationEnum(enum.StrEnum):
+    Serializable = "Serializable"
+    RepeatableRead = "RepeatableRead"
+
+
+class TransactionIsolation(
+    EnumScalarType[TransactionIsolationEnum],
+    edgeql_type="sys::TransactionIsolation",
+):
+    @classmethod
+    def get_translation_map(cls) -> Mapping[TransactionIsolationEnum, str]:
+        return {
+            TransactionIsolationEnum.Serializable: "serializable",
+            TransactionIsolationEnum.RepeatableRead: "repeatable read",
+        }
+
+    def to_qltypes(self) -> qltypes.TransactionIsolationLevel:
+        from edb.edgeql import qltypes
+        match self._val:
+            case TransactionIsolationEnum.Serializable:
+                return qltypes.TransactionIsolationLevel.SERIALIZABLE
+            case TransactionIsolationEnum.RepeatableRead:
+                return qltypes.TransactionIsolationLevel.REPEATABLE_READ
+            case _:
+                raise AssertionError(f"unexpected value: {self._val!r}")

edb/lib/cfg.edgeql
@@ -188,6 +188,47 @@ ALTER TYPE cfg::AbstractConfig {
         SET default := <std::duration>'60 seconds';
     };
 
+    CREATE REQUIRED PROPERTY default_transaction_isolation
+        -> sys::TransactionIsolation
+    {
+        CREATE ANNOTATION cfg::affects_compilation := 'true';
+        CREATE ANNOTATION cfg::backend_setting :=
+            '"default_transaction_isolation"';
+        CREATE ANNOTATION std::description :=
+            'Controls the default isolation level of each new transaction, \
+            including implicit transactions. Defaults to `Serializable`. \
+            Note that changing this to a lower isolation level implies \
+            that the transactions are also read-only by default regardless \
+            of the value of the `default_transaction_access_mode` setting.';
+        SET default := sys::TransactionIsolation.Serializable;
+    };
+
+    CREATE REQUIRED PROPERTY default_transaction_access_mode
+        -> sys::TransactionAccessMode
+    {
+        CREATE ANNOTATION cfg::affects_compilation := 'true';
+        CREATE ANNOTATION std::description :=
+            'Controls the default read-only status of each new transaction, \
+            including implicit transactions. Defaults to `ReadWrite`. \
+            Note that if `default_transaction_isolation` is set to any value \
+            other than Serializable this parameter is implied to be \
+            `ReadOnly` regardless of the actual value.';
+        SET default := sys::TransactionAccessMode.ReadWrite;
+    };
+
+    CREATE REQUIRED PROPERTY default_transaction_deferrable
+        -> sys::TransactionDeferrability
+    {
+        CREATE ANNOTATION cfg::backend_setting :=
+            '"default_transaction_deferrable"';
+        CREATE ANNOTATION std::description :=
+            'Controls the default deferrable status of each new transaction. \
+            It currently has no effect on read-write transactions or those \
+            operating at isolation levels lower than `Serializable`. \
+            The default is `NotDeferrable`.';
+        SET default := sys::TransactionDeferrability.NotDeferrable;
+    };
+
     CREATE REQUIRED PROPERTY session_idle_transaction_timeout -> std::duration {
         CREATE ANNOTATION cfg::backend_setting :=
             '"idle_in_transaction_session_timeout"';

edb/lib/sys.edgeql
@@ -24,6 +24,14 @@ CREATE SCALAR TYPE sys::TransactionIsolation
     EXTENDING enum<RepeatableRead, Serializable>;
 
 
+CREATE SCALAR TYPE sys::TransactionAccessMode
+    EXTENDING enum<ReadOnly, ReadWrite>;
+
+
+CREATE SCALAR TYPE sys::TransactionDeferrability
+    EXTENDING enum<Deferrable, NotDeferrable>;
+
+
 CREATE SCALAR TYPE sys::VersionStage
     EXTENDING enum<dev, alpha, beta, rc, final>;
 

edb/pgsql/compiler/clauses.py
@@ -489,7 +489,7 @@ def scan_check_ctes(
             name='flag', val=pgast.BooleanConstant(val=True)
         )],
         relation=pgast.RelRangeVar(relation=pgast.Relation(
-            schemaname='edgedb', name='_dml_dummy')),
+            name='_dml_dummy')),
         where_clause=pgast.Expr(
             name="=",
             lexpr=pgast.ColumnRef(name=["id"]),

edb/pgsql/metaschema.py
@@ -186,32 +186,6 @@ class InstDataTable(dbops.Table):
         )
 
 
-class DMLDummyTable(dbops.Table):
-    """A empty dummy table used when we need to emit no-op DML.
-
-    This is used by scan_check_ctes in the pgsql compiler to
-    force the evaluation of error checking.
-    """
-    def __init__(self) -> None:
-        super().__init__(name=('edgedb', '_dml_dummy'))
-
-        self.add_columns([
-            dbops.Column(name='id', type='int8'),
-            dbops.Column(name='flag', type='bool'),
-        ])
-
-        self.add_constraint(
-            dbops.UniqueConstraint(
-                table_name=('edgedb', '_dml_dummy'),
-                columns=['id'],
-            ),
-        )
-
-    SETUP_QUERY = '''
-        INSERT INTO edgedb._dml_dummy VALUES (0, false)
-    '''
-
-
 class QueryCacheTable(dbops.Table):
     def __init__(self) -> None:
         super().__init__(name=('edgedb', '_query_cache'))
@@ -5156,11 +5130,7 @@ def get_fixed_bootstrap_commands() -> dbops.CommandGroup:
             DBConfigTable(),
         ),
         # TODO: SHOULD THIS BE VERSIONED?
-        dbops.CreateTable(DMLDummyTable()),
-        # TODO: SHOULD THIS BE VERSIONED?
         dbops.CreateTable(QueryCacheTable()),
-
-        dbops.Query(DMLDummyTable.SETUP_QUERY),
 
         dbops.CreateDomain(BigintDomain()),
         dbops.CreateDomain(ConfigMemoryDomain()),

commit 0c081a201f639720e4d37f04603eabb2b5b2d4b7

bug fix: auto_rebuild_query_cache gate was broken (#8339)
Broken in #8276

edb/server/config/__init__.py
@@ -77,6 +77,7 @@ def lookup(
     spec: Spec,
     allow_unrecognized: bool = False,
 ) -> Any:
+    assert len(configs) > 0
 
     try:
         setting = spec[name]

edb/server/dbview/dbview.pyx
@@ -1440,7 +1440,7 @@ cdef class DatabaseConnectionView:
                 if unit.user_schema:
                     user_schema = unit.user_schema
                     user_schema_version = unit.user_schema_version
-            if user_schema and not self.server.config_lookup(
+            if user_schema and not self.config_lookup(
                 "auto_rebuild_query_cache",
             ):
                 user_schema = None

tests/test_server_ops.py
@@ -864,6 +864,27 @@ class TestServerOps(tb.TestCaseWithHttpClient, tb.CLITestCaseMixin):
                     #     con_c = con.with_config(apply_access_policies=False)
                     #     await con_c.query_sql(qry_sql)
 
+                    # At last, make sure recompilation switch works fine
+                    await con.execute(
+                        "configure current database "
+                        "set auto_rebuild_query_cache := false"
+                    )
+                    await con.query('''
+                        create type X
+                    ''')
+                    with self.assertChange(measure_compilations(sd), 1):
+                        await con.query(qry)
+                    with self.assertChange(measure_compilations(sd), 0):
+                        await con.query(qry)
+                    with self.assertChange(measure_sql_compilations(sd), 1):
+                        await con.query_sql(sql)
+                    with self.assertChange(measure_sql_compilations(sd), 0):
+                        await con.query_sql(sql)
+                    await con.execute(
+                        "configure current database "
+                        "reset auto_rebuild_query_cache"
+                    )
+
                 finally:
                     await con.aclose()
 